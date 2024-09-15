24858c2140d670b18ef74e7770f538bd5ac11397
mafuyusame <mafuyu@tutamail.com>
2024-09-15 23:46:55 +0900

    boj 1330

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? input = Console.ReadLine();
+
+        if (input == null)
+            return;
+        
+        string[] tokens = input.Split();
+        if (tokens.Length < 2)
+            return;
+
+        int token0 = int.Parse(tokens[0]);
+        int token1 = int.Parse(tokens[1]);
+
+        if (token0 > token1)
+        {
+            Console.Write('>');
+        }
+        else if (token0 < token1)
+        {
+            Console.Write('<');
+        }
+        else
+        {
+            Console.WriteLine("==");
+        }
+    }
+}
+
+//Console.ReadKey();
