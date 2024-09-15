20fb5330305e15b26a57b00a880a7ab81cb5945f
mafuyusame <mafuyu@tutamail.com>
2024-09-16 02:05:20 +0900

    boj 10871

--- Program.cs
+++ Program.cs
@@ -1,11 +1,38 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("\\    /\\");
-        Console.WriteLine(" )  ( ')");
-        Console.WriteLine("(  /  )");
-        Console.WriteLine(" \\(__)|");
+        string? input = Console.ReadLine();
+        if (input == null)
+            return;
+        
+        string[] tokens = input.Split();
+        if (tokens.Length < 2)
+            return;
+
+        int n = int.Parse(tokens[0]);
+        int x = int.Parse(tokens[1]);
+
+        input = Console.ReadLine();
+        if (input == null)
+            return;
+
+        tokens = input.Split();
+        if (tokens.Length < n)
+            return;
+
+        StringBuilder output = new();
+        for (int i = 0; i < n; ++i)
+        {
+            int m = int.Parse(tokens[i]);
+            if (x > m)
+            {
+                output.Append($"{m} ");
+            }
+        }
+        Console.Write(output);
     }
 }
 
