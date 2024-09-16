4b3e91fcc39cce9aff582962a7f6873e3f6625b6
mafuyusame <mafuyu@tutamail.com>
2024-09-16 23:29:19 +0900

    boj 4153

--- Program.cs
+++ Program.cs
@@ -4,18 +4,38 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string input = Console.ReadLine()!;
+        StringBuilder output = new();
         
-        string[] tokens = input.Split();
-        if (tokens == null || tokens.Length < 1)
-            return;
-        
-        int output_length = int.Parse(tokens[0]);
-        StringBuilder output = new(output_length);
-        output.Length = output_length;
-        
-        for (int i = 0; i < output_length; ++i)
-            output[i] = 'a';
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            if (input == null)
+                break;
+
+            int side_length_num = 3;
+
+            string[] tokens = input.Split();
+            if (tokens == null || tokens.Length < side_length_num)
+                break;
+
+            int left_side = 0;
+            int? max_side_length_sq = null;
+            for (int i = 0; i < side_length_num; ++i)
+            {
+                int side_length = int.Parse(tokens[i]);
+                int side_length_sq = side_length * side_length;
+
+                if (max_side_length_sq == null || side_length_sq > max_side_length_sq)
+                    max_side_length_sq = side_length_sq;
+
+                left_side += side_length_sq;
+            }
+
+            if (left_side == 0)
+                break;
+
+            output.AppendLine(((left_side - max_side_length_sq) == max_side_length_sq) ? "right" : "wrong");
+        }
         
         Console.Write(output);
     }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afad10decd1742b272761418b8abaf7ee1d15b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70c5a1818cf406b1314a7d106633ba90867d6f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
