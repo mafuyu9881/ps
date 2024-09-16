c70c5a1818cf406b1314a7d106633ba90867d6f3
mafuyusame <mafuyu@tutamail.com>
2024-09-16 23:08:12 +0900

    boj 20944

--- Program.cs
+++ Program.cs
@@ -5,38 +5,18 @@ internal class Program
     private static void Main(string[] args)
     {
         string input = Console.ReadLine()!;
-
+        
         string[] tokens = input.Split();
         if (tokens == null || tokens.Length < 1)
             return;
-
-        int t = int.Parse(tokens[0]);
-
-        StringBuilder output = new();
-
-        for (int i = 0; i < t; ++i)
-        {
-            string s = Console.ReadLine()!;
-
-            int sum = 0;
-            int streak_count = 1;
-            for (int j = 0; j < s.Length; ++j)
-            {
-                char c = s[j];
-                if (c == 'O')
-                {
-                    sum += streak_count;
-                    ++streak_count;
-                }
-                else
-                {
-                    streak_count = 1;
-                }
-            }
-
-            output.AppendLine(sum.ToString());
-        }
-
+        
+        int output_length = int.Parse(tokens[0]);
+        StringBuilder output = new(output_length);
+        output.Length = output_length;
+        
+        for (int i = 0; i < output_length; ++i)
+            output[i] = 'a';
+        
         Console.Write(output);
     }
 }
