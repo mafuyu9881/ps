88b9d125a6b48b71b4997e84c333d3424e9cbee4
mafuyusame <mafuyu@tutamail.com>
2024-09-16 13:25:05 +0900

    boj 10952

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        StringBuilder output = new();
+
         while (true)
         {
             string? input = Console.ReadLine();
@@ -20,10 +22,13 @@ internal class Program
             int a = int.Parse(tokens[0]);
             int b = int.Parse(tokens[1]);
 
-            // StringBuilder를 통해 while문 밖에서 한 번만 출력하고 마치고 싶지만,
-            // 입력의 끝이 불분명하기 때문에 입력을 종료할 수 없어서 이렇게 작성하였다.
-            Console.WriteLine((a + b).ToString());
+            if (a == 0 && b == 0)
+                break;
+
+            output.AppendLine((a + b).ToString());
         }
+
+        Console.Write(output);
     }
 }
 
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d6acdd224016e3f79f8ab54fbef448b571e81f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb54f847d959d7dd1ed1e38e3ce285c34e6cb5c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
