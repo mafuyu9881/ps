7294a78eff047ecdc3c6771e4b63c755eddc6f89
mafuyusame <mafuyu@tutamail.com>
2024-09-17 17:00:00 +0900

    boj 11022

--- Program.cs
+++ Program.cs
@@ -4,39 +4,30 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        string input = Console.ReadLine()!;
+
+        string[] tokens = input.Split();
+        if (tokens == null || tokens.Length < 1)
+            return;
+
+        int t = int.Parse(tokens[0]);
+
         StringBuilder output = new();
-        
-        while (true)
+
+        for (int i = 0; i < t; ++i)
         {
-            string? input = Console.ReadLine();
-            if (input == null)
-                break;
+            input = Console.ReadLine()!;
 
-            int side_length_num = 3;
+            tokens = input.Split();
+            if (tokens == null || tokens.Length < 2)
+                continue;
 
-            string[] tokens = input.Split();
-            if (tokens == null || tokens.Length < side_length_num)
-                break;
+            int a = int.Parse(tokens[0]);
+            int b = int.Parse(tokens[1]);
 
-            int left_side = 0;
-            int? max_side_length_sq = null;
-            for (int i = 0; i < side_length_num; ++i)
-            {
-                int side_length = int.Parse(tokens[i]);
-                int side_length_sq = side_length * side_length;
-
-                if (max_side_length_sq == null || side_length_sq > max_side_length_sq)
-                    max_side_length_sq = side_length_sq;
-
-                left_side += side_length_sq;
-            }
-
-            if (left_side == 0)
-                break;
-
-            output.AppendLine(((left_side - max_side_length_sq) == max_side_length_sq) ? "right" : "wrong");
+            output.AppendLine($"Case #{i + 1}: {a} + {b} = {a + b}");
         }
-        
+
         Console.Write(output);
     }
 }
