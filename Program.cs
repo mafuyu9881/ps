1db94def8f243ef49086783aa74801a4b0c0c76b
mafuyusame <mafuyu@tutamail.com>
2024-09-18 17:11:45 +0900

    boj 2869

--- Program.cs
+++ Program.cs
@@ -2,8 +2,43 @@
 {
     private static void Main(string[] args)
     {
-        int y = int.Parse(Console.ReadLine()!);
+        string[] tokens = Console.ReadLine()!.Split();
 
-        Console.Write(y - 543);
+        // 1 <= B < A <= V <= 10억이다.
+        // 'https://discord.com/channels/1285075305987309590/1285593827498790912'에 따라 일반적인 코테 환경에선 1초에 10억번 정도의 연산이 지원된다.
+        // 그러나 문제에서 제시한 시간 제한이 0.25초인 것으로 보아, 간단한 반복문으로는 풀게 해주지 않을 모양으로 생각된다.
+
+        int a = int.Parse(tokens[0]);
+        int b = int.Parse(tokens[1]);
+        int v = int.Parse(tokens[2]);
+
+        int v_b = v - b;
+        int a_b = a - b;
+
+        int days = (v - b) / (a - b);
+
+        if (v_b % a_b > 0)
+        {
+            ++days;
+        }
+
+        Console.Write(days);
+        
+        // 나이브하게 푼 버전의 코드
+        //int days = 0;
+
+        //while (true)
+        //{
+        //    ++days;
+
+        //    v -= a;
+
+        //    if (v < 1)
+        //        break;
+
+        //    v += b;
+        //}
+
+        //Console.Write(days);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7adbf3ebc35dcd14927ee6384a0c7d5b7e7b41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d6aa064c7ad06b60aca5027c43f8ec857d0f0d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
