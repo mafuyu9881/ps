3c2aadf7e3eb0cf5ca7052118b7ee5e4de67d74c
mafuyusame <mafuyu@tutamail.com>
2024-09-21 21:05:59 +0900

    boj 11050

--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        StringBuilder input = new(Console.ReadLine()!);
-        for (int i = 0; i < input.Length; ++i)
+        string[] tokens = Console.ReadLine()!.Split();
+        
+        int n = int.Parse(tokens[0]);
+        int k = int.Parse(tokens[1]);
+
+        int?[] backedup_factorials = new int?[((n > k) ? n : k) + 1];
+
+        Console.Write(factorial(n, backedup_factorials) / factorial(k, backedup_factorials) / factorial(n - k, backedup_factorials));
+    }
+
+    // backedup_factorials는 null이 아니고, n을 키 값으로 항상 사용할 수 있는 상태라고 전제합니다.
+    private static int factorial(int n, int?[] backedup_factorials)
+    {
+        if (n == 0)
         {
-            input[i] -= (char)32;
+            backedup_factorials[0] = 1;
+        }
+        else
+        {
+            if (backedup_factorials[n] == null)
+            {
+                backedup_factorials[n] = n * factorial(n - 1, backedup_factorials);
+            }
         }
 
-        Console.WriteLine(input);
+        return backedup_factorials[n]!.Value;
+    }
+
+    private static int factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+
+        return n * factorial(n - 1);
     }
 }
