320a4d3701efb0186b0d51e49ad1191613cd9fe9
mafuyusame <mafuyu@tutamail.com>
2024-09-23 14:22:58 +0900

    boj 28702

--- Program.cs
+++ Program.cs
@@ -1,35 +1,36 @@
-﻿// 동적 계획법을 통한 구현 (Memoization, top-down)
-internal class Program
+﻿internal class Program
 {
-    private static int?[,] binomial_coefficients = new int?[0, 0];
-
     private static void Main(string[] args)
     {
-        string[] tokens = Console.ReadLine()!.Split();
+        string[] tokens = { Console.ReadLine()!, Console.ReadLine()!, Console.ReadLine()! };
 
-        int n = int.Parse(tokens[0]);
-        int k = int.Parse(tokens[1]);
+        string fizz = "Fizz";
+        string buzz = "Buzz";
+        string fizzbuzz = "FizzBuzz";
 
-        binomial_coefficients = new int?[n + 1, n + 1];
+        for (int i = 0; i < tokens.Length; ++i)
+        {
+            string token = tokens[i];
+            
+            if (token.Contains(fizz) || token.Contains(buzz))
+                continue;
 
-        Console.Write(binomial_coefficient(n, k));
-    }
+            int output_n = int.Parse(token) + (3 - i);
 
-    private static int binomial_coefficient(int times, int left)
-    {
-        if (left < 0)
-            return 0;
+            string output_s;
+            bool divisible_by_3 = output_n % 3 == 0;
+            bool divisible_by_5 = output_n % 5 == 0;
+            if (divisible_by_3 && divisible_by_5)
+                output_s = fizzbuzz;
+            else if (divisible_by_3)
+                output_s = fizz;
+            else if (divisible_by_5)
+                output_s = buzz;
+            else
+                output_s = output_n.ToString();
 
-        if (times == 0)
-            return Convert.ToInt32(left == 0);
-
-        int? memoized_binomial_coefficient = binomial_coefficients[times, left];
-        if (memoized_binomial_coefficient != null)
-            return memoized_binomial_coefficient.Value;
-
-        int prev_times = times - 1;
-        int prev_left = left - 1;
-        binomial_coefficients[times, left] = binomial_coefficient(prev_times, left) + binomial_coefficient(prev_times, prev_left);
-        return binomial_coefficients[times, left]!.Value;
+            Console.Write(output_s);
+            break;
+        }
     }
 }
