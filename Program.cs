0fb14e74eea6719bd63b99b356907ca12c1e8d23
mafuyusame <mafuyu@tutamail.com>
2024-09-23 13:44:08 +0900

    boj 11050 구현 5 (DP, Memoization, top-down)

--- Program.cs
+++ Program.cs
@@ -1,34 +1,35 @@
-﻿// 동적 계획법을 통한 구현 (Memoization, bottom-up)
+﻿// 동적 계획법을 통한 구현 (Memoization, top-down)
 internal class Program
 {
     private static int?[,] binomial_coefficients = new int?[0, 0];
-    private static int n;
-    private static int k;
 
     private static void Main(string[] args)
     {
         string[] tokens = Console.ReadLine()!.Split();
 
-        n = int.Parse(tokens[0]);
-        k = int.Parse(tokens[1]);
+        int n = int.Parse(tokens[0]);
+        int k = int.Parse(tokens[1]);
 
         binomial_coefficients = new int?[n + 1, n + 1];
 
-        Console.Write(binomial_coefficient(0, 0));
+        Console.Write(binomial_coefficient(n, k));
     }
 
-    private static int binomial_coefficient(int times, int got)
+    private static int binomial_coefficient(int times, int left)
     {
-        if (times == n)
-            return Convert.ToInt32(got == k);
+        if (left < 0)
+            return 0;
 
-        int? memoized_binomial_coefficient = binomial_coefficients[times, got];
+        if (times == 0)
+            return Convert.ToInt32(left == 0);
+
+        int? memoized_binomial_coefficient = binomial_coefficients[times, left];
         if (memoized_binomial_coefficient != null)
             return memoized_binomial_coefficient.Value;
 
-        int next_times = times + 1;
-        int next_got = got + 1;
-        binomial_coefficients[times, got] = binomial_coefficient(next_times, got) + binomial_coefficient(next_times, next_got);
-        return binomial_coefficients[times, got]!.Value;
+        int prev_times = times - 1;
+        int prev_left = left - 1;
+        binomial_coefficients[times, left] = binomial_coefficient(prev_times, left) + binomial_coefficient(prev_times, prev_left);
+        return binomial_coefficients[times, left]!.Value;
     }
 }
