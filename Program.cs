74b3113609370e0de4a0497c382a21dc1028182a
mafuyusame <mafuyu@tutamail.com>
2024-09-23 18:00:40 +0900

    boj 1676

--- Program.cs
+++ Program.cs
@@ -1,30 +1,33 @@
-﻿/*
-N <= 10,000
-따라서, 브루트 포스로 탐색할 경우 최대 10,000,666의 순회가 요구된다. (실제로는 사이사이에 껴 있는 수들이 있을테니 10,000,666까지는 안 나올 것이다.)
-이 정도 순회면 브루트 포스로도 널널하게 풀릴 것 같다. 시도해보자.
-*/
+﻿// 0 ≤ N ≤ 500
+// N!의 최대는 1.220136825991110068701238785423e+1134로, 대충 11__자리 정도 되어보인다.
+// 2byte * 1200 = 2400byte = 2.4 kilobyte
+// 메모리 제한이 128MB이므로 문자열로 풀어도 문제없을 것으로 보인다.
+
+using System.Numerics;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        BigInteger n = BigInteger.Parse(Console.ReadLine()!);
         
-        int number = 666;
-
-        while (true)
+        BigInteger n_factorial = 1;
+        for (BigInteger i = 1; i <= n; ++i)
         {
-            // 간단하게 검증할 수 있지만 부하가 큰 방법이다.
-            if (number.ToString().Contains("666"))
-                --n;
-
-            if (n < 1)
-            {
-                Console.Write(number);
-                break;
-            }
-
-            ++number;
+            n_factorial *= i;
         }
+
+        string n_factorial_string = n_factorial.ToString();
+
+        int output = 0;
+        for (int i = n_factorial_string.Length - 1; i > -1; --i)
+        {
+            if (n_factorial_string[i] != '0')
+                break;
+            
+            ++output;
+        }
+
+        Console.Write(output);
     }
 }
