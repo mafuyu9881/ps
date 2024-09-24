12993bff7c7b92e62926de0d2d28aad2efac7a7d
mafuyusame <mafuyu@tutamail.com>
2024-09-24 12:41:30 +0900

    boj 10814 (계수 정렬)

--- Program.cs
+++ Program.cs
@@ -5,92 +5,44 @@
 // 12B * 100,000 = 1,200,000B = 1.2MB
 // 4B * (3 + 1 + 100) * 100,000 = 41,600,000B = 41.6MB
 
-using System.Text;
+// 계수 정렬을 이용한 풀이
 
-// age, order, name
-using Account = System.Tuple<int, int, string>;
+using System.Text;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine()!);
-
-        Account[] accounts = new Account[n];
+        
+        int max_age = 200;
+        int accounts_length = max_age + 1;
+        List<string>?[] accounts = new List<string>[accounts_length];
 
         for (int i = 0; i < n; ++i)
         {
             string[] tokens = Console.ReadLine()!.Split();
             
-            accounts[i] = new Account(int.Parse(tokens[0]), i, tokens[1]);
+            int age = int.Parse(tokens[0]);
+
+            if (accounts[age] == null)
+                accounts[age] = new();
+
+            accounts[age]!.Add(tokens[1]);
         }
 
-        merge_sort(accounts);
-        
         StringBuilder output = new();
-        for (int i = 0; i < accounts.Length; ++i)
+        for (int age = 1; age < accounts_length; ++age)
         {
-            output.AppendLine($"{accounts[i].Item1} {accounts[i].Item3}");
+            List<string>? same_age_accounts = accounts[age];
+            if (same_age_accounts == null)
+                continue;
+            
+            for (int i = 0; i < same_age_accounts.Count; ++i)
+            {
+                output.AppendLine($"{age} {same_age_accounts[i]}");
+            }
         }
         Console.Write(output);
-    }
-
-    private static void merge_sort(Span<Account> arr)
-    {
-        int arr_length = arr.Length;
-
-        if (arr_length < 2)
-            return;
-
-        int mid = arr_length / 2;
-        
-        merge_sort(arr.Slice(0, mid));
-        merge_sort(arr.Slice(mid));
-        merge(arr, mid);
-    }
-
-    private static void merge(Span<Account> arr, int mid)
-    {
-        Account[] backedup = arr.ToArray();
-
-        int arr_length = arr.Length;
-
-        int left_read_index = 0;
-        int right_read_index = mid;
-        int written_index = 0;
-
-        while (left_read_index < mid && right_read_index < arr_length)
-        {
-            Account left_read_account = backedup[left_read_index];
-            Account right_read_account = backedup[right_read_index];
-
-            if (left_read_account.Item1 < right_read_account.Item1 ||
-               (left_read_account.Item1 == right_read_account.Item1 &&
-                left_read_account.Item2 < right_read_account.Item2))
-            {
-                arr[written_index] = backedup[left_read_index];
-                ++left_read_index;
-            }
-            else
-            {
-                arr[written_index] = backedup[right_read_index];
-                ++right_read_index;
-            }
-            ++written_index;
-        }
-
-        while (left_read_index < mid)
-        {
-            arr[written_index] = backedup[left_read_index];
-            ++left_read_index;
-            ++written_index;
-        }
-
-        while (right_read_index < arr_length)
-        {
-            arr[written_index] = backedup[right_read_index];
-            ++right_read_index;
-            ++written_index;
-        }
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1042d4a83155766c138dc0f7e949e1e33553106e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+978937231b3a30425eb3358713e75258f194eed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
