978937231b3a30425eb3358713e75258f194eed6
mafuyusame <mafuyu@tutamail.com>
2024-09-24 12:22:54 +0900

    boj 10814

--- Program.cs
+++ Program.cs
@@ -1,121 +1,41 @@
-﻿using System.Text;
+﻿// 시간 제한: 3초
+// 메모리 제한: 256MB
+// 1 ≤ N ≤ 100,000
+// 100,000 ^ 2 = 10,000,000,000
+// 12B * 100,000 = 1,200,000B = 1.2MB
+// 4B * (3 + 1 + 100) * 100,000 = 41,600,000B = 41.6MB
+
+using System.Text;
+
+// age, order, name
+using Account = System.Tuple<int, int, string>;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        //int[] arr = [ 5 ];
-        //int[] arr = [ 5, 1 ];
-        int[] arr = [ 5, 3, 4, 1, 2 ];
+        int n = int.Parse(Console.ReadLine()!);
 
-        merge_sort(arr);
+        Account[] accounts = new Account[n];
 
-        StringBuilder output = new();
-        for (int i = 0; i < arr.Length; ++i)
+        for (int i = 0; i < n; ++i)
         {
-            output.AppendLine(arr[i].ToString());
+            string[] tokens = Console.ReadLine()!.Split();
+            
+            accounts[i] = new Account(int.Parse(tokens[0]), i, tokens[1]);
+        }
+
+        merge_sort(accounts);
+        
+        StringBuilder output = new();
+        for (int i = 0; i < accounts.Length; ++i)
+        {
+            output.AppendLine($"{accounts[i].Item1} {accounts[i].Item3}");
         }
         Console.Write(output);
     }
 
-    private static void bubble_sort(int[] arr)
-    {
-        int visit_limit = arr.Length - 1;
-
-        for (int i = 0; i < visit_limit; ++i)
-        {
-            for (int j = 0; j < visit_limit; ++j)
-            {
-                int j_element = arr[j];
-                int j_1_element = arr[j + 1];
-
-                if (j_element > j_1_element)
-                {
-                    arr[j] = j_1_element;
-                    arr[j + 1] = j_element;
-                }
-            }
-        }
-    }
-
-    private static void selection_sort(int[] arr)
-    {
-        int arr_length = arr.Length;
-
-        for (int i = 0; i < arr_length - 1; ++i)
-        {
-            int? exchange_index = null;
-            for (int j = i; j < arr_length; ++j)
-            {
-                if (exchange_index == null || arr[j] < arr[exchange_index.Value])
-                {
-                    exchange_index = j;
-                }
-            }
-
-            int temp = arr[i];
-            arr[i] = arr[exchange_index!.Value];
-            arr[exchange_index!.Value] = temp;
-        }
-    }
-
-    private static void insertion_sort_1(int[] arr)
-    {
-        int arr_length = arr.Length;
-
-        for (int i = 1; i < arr_length; ++i)
-        {
-            int exchange_index = i;
-            for (int j = i - 1; j > -1; --j)
-            {
-                int exchange_element = arr[exchange_index];
-                int j_element = arr[j];
-                
-                if (j_element > exchange_element)
-                {
-                    arr[j] = exchange_element;
-                    arr[exchange_index] = j_element;
-                    exchange_index = j;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-    }
-
-    private static void insertion_sort_2(int[] arr)
-    {
-        int arr_length = arr.Length;
-        
-        for (int i = 1; i < arr_length; ++i)
-        {
-            int exchange_index = i;
-            int exchange_element = arr[i];
-            for (int j = i - 1; j > -1; --j)
-            {
-                if (arr[j] > exchange_element)
-                {
-                    arr[j + 1] = arr[j];
-                    exchange_index = j;
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            arr[exchange_index] = exchange_element;
-        }
-    }
-
-    private static void merge_sort(int[] arr)
-    {
-        merge_sort(new Span<int>(arr));
-    }
-
-    private static void merge_sort(Span<int> arr)
+    private static void merge_sort(Span<Account> arr)
     {
         int arr_length = arr.Length;
 
@@ -123,16 +43,15 @@ internal class Program
             return;
 
         int mid = arr_length / 2;
-
+        
         merge_sort(arr.Slice(0, mid));
         merge_sort(arr.Slice(mid));
-
         merge(arr, mid);
     }
 
-    private static void merge(Span<int> arr, int mid)
+    private static void merge(Span<Account> arr, int mid)
     {
-        int[] backedup = arr.ToArray();
+        Account[] backedup = arr.ToArray();
 
         int arr_length = arr.Length;
 
@@ -142,7 +61,12 @@ internal class Program
 
         while (left_read_index < mid && right_read_index < arr_length)
         {
-            if (backedup[left_read_index] < backedup[right_read_index])
+            Account left_read_account = backedup[left_read_index];
+            Account right_read_account = backedup[right_read_index];
+
+            if (left_read_account.Item1 < right_read_account.Item1 ||
+               (left_read_account.Item1 == right_read_account.Item1 &&
+                left_read_account.Item2 < right_read_account.Item2))
             {
                 arr[written_index] = backedup[left_read_index];
                 ++left_read_index;
