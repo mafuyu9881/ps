a2b62687cd865edda987a60e636e04a5755804c9
mafuyusame <mafuyu@tutamail.com>
2024-09-25 21:42:45 +0900

    boj 10828

--- Program.cs
+++ Program.cs
@@ -1,32 +1,155 @@
-﻿// 시간 제한: 1초
-// 메모리 제한: 256MB
-// 1 ≤ 숫자 카드의 개수 ≤ 500,000
-// -10,000,000 ≤ 수의 크기 ≤ 10,000,000
-
-using System.Text;
+﻿using System.Text;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        int abs_number_limit = 10000000;
-
-        int[] lookup = new int[abs_number_limit * 2 + 1];
+        stack stack = new();
 
         int n = int.Parse(Console.ReadLine()!);
-        string[] tokens = Console.ReadLine()!.Split();
+
+        StringBuilder output = new();
         for (int i = 0; i < n; ++i)
         {
-            ++lookup[int.Parse(tokens[i]) + abs_number_limit];
-        }
+            string[] tokens = Console.ReadLine()!.Split();
 
-        int m = int.Parse(Console.ReadLine()!);
-        tokens = Console.ReadLine()!.Split();
-        StringBuilder output = new();
-        for (int i = 0; i < m; ++i)
-        {
-            output.Append($"{lookup[int.Parse(tokens[i]) + abs_number_limit]} ");
+            string command = tokens[0];
+
+            if (command == "push")
+            {
+                stack.push(int.Parse(tokens[1]));
+            }
+            else if (command == "pop")
+            {
+                output.AppendLine(stack.pop().ToString());
+            }
+            else if (command == "size")
+            {
+                output.AppendLine(stack.size().ToString());
+            }
+            else if (command == "empty")
+            {
+                output.AppendLine(stack.empty().ToString());
+            }
+            else if (command == "top")
+            {
+                output.AppendLine(stack.top().ToString());
+            }
         }
         Console.Write(output);
+    }
+}
+
+// 메모리 지역성보다 삽입 삭제 과정에서 발생하는 메모리 재할당의 오버헤드가 더 클 것 같아 구현체로 링크드리스트를 채택.
+public class stack
+{
+    private class node
+    {
+        private int data;
+        public int Data
+        {
+            get { return data; }
+        }
+        private node? prev = null;
+        public node? Prev
+        {
+            get { return prev!; }
+            set
+            {
+                if (value != null)
+                {
+                    value.next = this;
+                }
+                
+                prev = value;
+            }
+        }
+        private node? next = null;
+        public node? Next
+        {
+            get { return next!; }
+            set
+            {
+                if (value != null)
+                {
+                    value.prev = this;
+                }
+
+                next = value;
+            }
+        }
+
+        public node(int data)
+        {
+            this.data = data;
+        }
+    }
+    
+    private node? first = null;
+    private int count = 0;
+
+    public void push(int data)
+    {
+        node new_node = new(data);
+
+        if (first != null)
+        {
+            first.Prev = new_node;
+            first = new_node;
+        }
+        else
+        {
+            first = new_node;
+        }
+
+        ++count;
+    }
+
+    public int pop()
+    {
+        int output = top();
+
+        if (first != null)
+        {
+            node? new_first = first.Next;
+            if (new_first != null)
+            {
+                new_first.Prev = null;
+            }
+            first = new_first;
+
+            --count;
+        }
+
+        return output;
+    }
+
+    public int top()
+    {
+        if (first != null)
+        {
+            return first.Data;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public int empty()
+    {
+        if (first != null)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public int size()
+    {
+        return count;
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93fccb2321726d480f712b7b618338d9fb49cacb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d47a5677acda01819e6fa7473bc67b7f36812d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
