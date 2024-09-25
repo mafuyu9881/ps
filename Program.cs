93fccb2321726d480f712b7b618338d9fb49cacb
mafuyusame <mafuyu@tutamail.com>
2024-09-25 20:34:49 +0900

    boj 10773

--- Program.cs
+++ Program.cs
@@ -1,53 +1,29 @@
-﻿// 시간 제한: 1초
-// 메모리 제한: 128MB
-// 괄호 문자열의 길이는 2 이상 50 이하
-
-using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        SortedSet<char> begin_parenthesises = new();
-        begin_parenthesises.Add('(');
+        int k = int.Parse(Console.ReadLine()!);
 
-        SortedSet<char> end_parenthesises = new();
-        end_parenthesises.Add(')');
-
-        int n = int.Parse(Console.ReadLine()!);
-
-        StringBuilder output = new();
-        for (int i = 0; i < n; ++i)
+        Stack<int> stack = new();
+        for (int i = 0; i < k; ++i)
         {
-            string ps = Console.ReadLine()!;
-            
-            Stack<char> stack = new();
-            for (int j = 0; j < ps.Length; ++j)
-            {
-                char c = ps[j];
-                
-                if (begin_parenthesises.Contains(c))
-                {
-                    stack.Push(c);
-                }
+            int n = int.Parse(Console.ReadLine()!);
 
-                if (end_parenthesises.Contains(c))
-                {
-                    if (stack.Count < 1)
-                    {
-                        // 에러를 명시
-                        stack.Push(c);
-                        break;
-                    }
-                    else
-                    {
-                        stack.Pop();
-                    }
-                }
+            if (n != 0)
+            {
+                stack.Push(n);
             }
-            
-            output.AppendLine((stack.Count < 1) ? "YES" : "NO");
+            else
+            {
+                stack.Pop();
+            }
         }
-        Console.Write(output);
+
+        int sum = 0;
+        while (stack.Count > 0)
+        {
+            sum += stack.Pop();
+        }
+        Console.Write(sum);
     }
 }
