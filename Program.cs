58607559d8fe9255f23410455e6109c54dbf92a3
mafuyusame <mafuyu@tutamail.com>
2024-09-25 13:04:32 +0900

    boj 2164

--- Program.cs
+++ Program.cs
@@ -1,154 +1,24 @@
-﻿using System.Text;
+﻿// 1 ≤ N ≤ 500,000
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] arr = [5, 2, 3, 1, 4];
+        int n = int.Parse(Console.ReadLine()!);
 
-        merge_sort(arr);
-
-        for (int i = 0; i < arr.Length; ++i)
+        LinkedList<int> list = new();
+        for (int i = 0; i < n; ++i)
         {
-            Console.WriteLine(arr[i]);
-        }
-    }
-
-    private static void bubble_sort(int[] arr)
-    {
-        int visit_limit = arr.Length - 1;
-
-        for (int i = 0; i < visit_limit; ++i)
-        {
-            for (int j = 0; j < visit_limit; ++j)
-            {
-                int j_element = arr[j];
-                int j_1_element = arr[j + 1];
-                
-                if (j_element > j_1_element)
-                {
-                    arr[j] = j_1_element;
-                    arr[j + 1] = j_element;
-                }
-            }
-        }
-    }
-
-    private static void selection_sort(int[] arr)
-    {
-        int arr_length = arr.Length;
-
-        for (int i = 0; i < arr_length - 1; ++i)
-        {
-            int min_index = i;
-            for (int j = i + 1; j < arr_length; ++j)
-            {
-                if (arr[j] < arr[min_index])
-                {
-                    min_index = j;
-                }
-            }
-
-            int temp = arr[i];
-            arr[i] = arr[min_index];
-            arr[min_index] = temp;
-        }
-    }
-
-    private static void insertion_sort_1(int[] arr)
-    {
-        for (int i = 1; i < arr.Length; ++i)
-        {
-            int insertion_index = i;
-            for (int j = i - 1; j >= 0; --j)
-            {
-                if (arr[j] > arr[insertion_index])
-                {
-                    int temp = arr[j];
-                    arr[j] = arr[insertion_index];
-                    arr[insertion_index] = temp;
-                    insertion_index = j;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-    }
-
-    private static void insertion_sort_2(int[] arr)
-    {
-        for (int i = 1; i < arr.Length; ++i)
-        {
-            int insertion_element = arr[i];
-            int predecessor_index = i - 1;
-            for (; predecessor_index >= 0; --predecessor_index)
-            {
-                if (arr[predecessor_index] > insertion_element)
-                {
-                    arr[predecessor_index + 1] = arr[predecessor_index];
-                }
-                else
-                {
-                    break;
-                }
-            }
-            arr[predecessor_index + 1] = insertion_element;
-        }
-    }
-
-    private static void merge_sort(Span<int> arr)
-    {
-        int arr_length = arr.Length;
-
-        if (arr_length < 2)
-            return;
-
-        int mid_index = arr_length / 2;
-
-        merge_sort(arr.Slice(0, mid_index));
-        merge_sort(arr.Slice(mid_index));
-        merge(arr, mid_index);
-    }
-
-    private static void merge(Span<int> arr, int mid_index)
-    {
-        int[] backedup = arr.ToArray();
-
-        int arr_length = arr.Length;
-
-        int left_read_index = 0;
-        int right_read_index = mid_index;
-        int written_index = 0;
-
-        while (left_read_index < mid_index && right_read_index < arr_length)
-        {
-            if (backedup[left_read_index] < backedup[right_read_index])
-            {
-                arr[written_index] = backedup[left_read_index];
-                ++left_read_index;
-            }
-            else
-            {
-                arr[written_index] = backedup[right_read_index];
-                ++right_read_index;
-            }
-            ++written_index;
+            list.AddLast(i + 1);
         }
 
-        while (left_read_index < mid_index)
+        while (list.Count > 1)
         {
-            arr[written_index] = backedup[left_read_index];
-            ++left_read_index;
-            ++written_index;
+            list.RemoveFirst();
+            list.AddLast(list.First!.Value);
+            list.RemoveFirst();
         }
 
-        while (right_read_index < arr_length)
-        {
-            arr[written_index] = backedup[right_read_index];
-            ++right_read_index;
-            ++written_index;
-        }
+        Console.Write(list.First!.Value);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a783588a7f20d31b4c87e5ccc65471c23719be2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9174f4975fc61c9312829dbcf6dfcaf3757d8408")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
