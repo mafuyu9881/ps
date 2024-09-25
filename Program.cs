93d47a5677acda01819e6fa7473bc67b7f36812d
mafuyusame <mafuyu@tutamail.com>
2024-09-25 20:59:57 +0900

    boj 10816

--- Program.cs
+++ Program.cs
@@ -1,29 +1,32 @@
-﻿internal class Program
+﻿// 시간 제한: 1초
+// 메모리 제한: 256MB
+// 1 ≤ 숫자 카드의 개수 ≤ 500,000
+// -10,000,000 ≤ 수의 크기 ≤ 10,000,000
+
+using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int k = int.Parse(Console.ReadLine()!);
+        int abs_number_limit = 10000000;
 
-        Stack<int> stack = new();
-        for (int i = 0; i < k; ++i)
+        int[] lookup = new int[abs_number_limit * 2 + 1];
+
+        int n = int.Parse(Console.ReadLine()!);
+        string[] tokens = Console.ReadLine()!.Split();
+        for (int i = 0; i < n; ++i)
         {
-            int n = int.Parse(Console.ReadLine()!);
-
-            if (n != 0)
-            {
-                stack.Push(n);
-            }
-            else
-            {
-                stack.Pop();
-            }
+            ++lookup[int.Parse(tokens[i]) + abs_number_limit];
         }
 
-        int sum = 0;
-        while (stack.Count > 0)
+        int m = int.Parse(Console.ReadLine()!);
+        tokens = Console.ReadLine()!.Split();
+        StringBuilder output = new();
+        for (int i = 0; i < m; ++i)
         {
-            sum += stack.Pop();
+            output.Append($"{lookup[int.Parse(tokens[i]) + abs_number_limit]} ");
         }
-        Console.Write(sum);
+        Console.Write(output);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1736b1ffed98b86eb463de9d30090d6c2a78af34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93fccb2321726d480f712b7b618338d9fb49cacb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
