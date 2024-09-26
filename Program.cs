40c1d945a4dcfed56a36dde7265320bb3c83c68b
mafuyusame <mafuyu@tutamail.com>
2024-09-26 20:50:02 +0900

    boj 1874 (stack 구현)

--- Program.cs
+++ Program.cs
@@ -1,54 +1,152 @@
 ﻿// 시간 제한: 2초
 // 메모리 제한: 128MB
-// 1 ≦ K ≦ 10,000
-// 1 ≦ N ≦ 1,000,000
+
+using System.Text;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        string[] tokens = Console.ReadLine()!.Split();
+        int n = int.Parse(Console.ReadLine()!);
 
-        long k = long.Parse(tokens[0]);
-        long n = long.Parse(tokens[1]);
-
-        long[] lengths = new long[k];
-
-        long? max_length = null;
-        for (long i = 0; i < k; ++i)
+        int[] arr = new int[n];
+        for (int i = 0; i < n; ++i)
         {
-            long length = long.Parse(Console.ReadLine()!);
+            arr[i] = int.Parse(Console.ReadLine()!);
+        }
 
-            if (max_length == null || length > max_length)
+        stack stack = new();
+        int last_pushed = 0;
+        StringBuilder output = new();
+        Action set_output_as_no = () =>
+        {
+            if (output != null)
             {
-                max_length = length;
+                output = new("NO");
             }
-            
-            lengths[i] = length;
+        };
+        for (int i = 0; i < n; ++i)
+        {
+            int objective = arr[i];
+
+            if (stack.empty() ||
+                stack.top() < objective)
+            {
+                if (last_pushed > objective)
+                {
+                    set_output_as_no();
+                    break;
+                }
+
+                while (last_pushed < objective)
+                {
+                    ++last_pushed;
+                    stack.push(last_pushed);
+                    output.AppendLine("+");
+                }
+            }
+
+            if (stack.top() != objective)
+            {
+                set_output_as_no();
+                break;
+            }
+
+            stack.pop();
+            output.AppendLine("-");
+        }
+        Console.Write(output);
+    }
+}
+
+public class stack
+{
+    private class node
+    {
+        private int data;
+        public int Data
+        {
+            get { return data; }
+        }
+
+        private node? prev = null;
+        public node? Prev
+        {
+            get { return prev; }
+            set
+            {
+                if (value != null)
+                {
+                    value.next = this;
+                }
+                prev = value;
+            }
+        }
+        private node? next = null;
+        public node? Next
+        {
+            get { return next; }
+            set
+            {
+                if (value != null)
+                {
+                    value.prev = this;
+                }
+                next = value;
+            }
         }
         
-        long min_length = 1;
-        long answer_length = 0;
-        while (min_length <= max_length)
+        public node(int data)
         {
-            long mid_length = (min_length + max_length.Value) / 2;
-
-            long cable_count = 0;
-            for (long i = 0; i < k; ++i)
-            {
-                cable_count += lengths[i] / mid_length;
-            }
-
-            if (cable_count < n)
-            {
-                max_length = mid_length - 1;
-            }
-            else
-            {
-                answer_length = mid_length;
-                min_length = mid_length + 1;
-            }
+            this.data = data;
         }
-        Console.Write(answer_length);
+    }
+    
+    private node? head = null;
+    private int error = -1;
+    public int Error { get { return error; } }
+    private int count = 0;
+    public int Count { get { return count; } }
+
+    public void push(int data)
+    {
+        node new_node = new(data);
+        new_node.Next = head;
+        head = new_node;
+        ++count;
+    }
+
+    public int pop()
+    {
+        int output;
+        if (head != null)
+        {
+            output = head.Data;
+
+            var head_next = head.Next;
+            if (head_next != null)
+            {
+                head_next.Prev = null;
+            }
+
+            head.Next = null;
+            head = head_next;
+            --count;
+        }
+        else
+        {
+            output = Error;
+        }
+        return output;
+    }
+    
+    public int top()
+    {
+        return (head != null) ? head.Data : Error;
+    }
+
+    public bool empty()
+    {
+        return Count < 1;
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed8e511a7de2c3ccabe995ed3e1a9ecf816bc70a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7952e158393bc1a6784883e792a269903cb175e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
