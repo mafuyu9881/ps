e19cd6c68eb4c69b99ecce31fec6e0711a6eed6b
mafuyusame <mafuyu@tutamail.com>
2024-09-28 12:59:48 +0900

    boj 25372

--- Program.cs
+++ Program.cs
@@ -1,63 +1,29 @@
-﻿// 시간 제한: 1.5초
-// 메모리 제한: 4MB
-
-using System.Text;
+﻿using System.Text;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        int set_length = 20;
-        int set = 0;
+        int password_min_length = 6;
+        int password_max_length = 9;
 
-        int m = int.Parse(Console.ReadLine()!);
-
+        int n = int.Parse(Console.ReadLine()!);
+        
         StringBuilder output = new();
-        for (int i = 0; i < m; ++i)
+        for (int i = 0; i < n; ++i)
         {
-            string[] tokens = Console.ReadLine()!.Split();
-            
-            string command = tokens[0];
-
-            if (command == "add")
+            string password = Console.ReadLine()!;
+            int password_length = password.Length;
+            if (password_length >= password_min_length &&
+                password_length <= password_max_length)
             {
-                set |= 1 << data_to_index(int.Parse(tokens[1]));
+                output.AppendLine("yes");
             }
-            else if (command == "remove")
+            else
             {
-                set &= ~(1 << data_to_index(int.Parse(tokens[1])));
-            }
-            else if (command == "check")
-            {
-                int check = 1 << data_to_index(int.Parse(tokens[1]));
-                if ((set & check) != 0)
-                {
-                    output.AppendLine("1");
-                }
-                else
-                {
-                    output.AppendLine("0");
-                }
-            }
-            else if (command == "toggle")
-            {
-                int toggle = 1 << data_to_index(int.Parse(tokens[1]));
-                set ^= toggle;
-            }
-            else if (command == "all")
-            {
-                set = (1 << (set_length + 1)) - 1;
-            }
-            else if (command == "empty")
-            {
-                set = 0;
+                output.AppendLine("no");
             }
         }
         Console.Write(output);
-    }
-    
-    private static int data_to_index(int data)
-    {
-        return data - 1;
     }
 }
