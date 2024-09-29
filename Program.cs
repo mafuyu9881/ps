84cac43678882e7c91a8d487f7178c8d935b916c
mafuyusame <mafuyu@tutamail.com>
2024-09-29 18:42:47 +0900

    boj 1003

--- Program.cs
+++ Program.cs
@@ -1,40 +1,43 @@
-﻿internal class Program
+﻿// 시간 제한: 0.25초
+// 메모리 제한: 128MB
+
+using System.Text;
+using CallCount = System.Tuple<int, int>;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        string[] tokens = Console.ReadLine()!.Split();
-        
-        LinkedList<int> numbers = new();
-        for (int i = 0; i < tokens.Length; ++i)
-        {
-            int number = int.Parse(tokens[i]);
-            
-            if (numbers.Count > 0)
-            {
-                LinkedListNode<int>? objective_node;
-                for (objective_node = numbers.First; objective_node != null; objective_node = objective_node.Next)
-                {
-                    if (number < objective_node.Value)
-                    {
-                        break;
-                    }
-                }
-                
-                if (objective_node != null)
-                {
-                    numbers.AddBefore(objective_node, number);
-                }
-                else
-                {
-                    numbers.AddLast(number);
-                }
-            }
-            else
-            {
-                numbers.AddLast(number);
-            }
-        }
+        int max_n = 40;
+        int callcounts_length = max_n + 1;
 
-        Console.Write(numbers.First!.Next!.Value);
+        CallCount[] callcounts = new CallCount[callcounts_length];
+        callcounts[0] = new(1, 0);
+        callcounts[1] = new(0, 1);
+
+        int callcounts_written_index = 1;
+
+        int t = int.Parse(Console.ReadLine()!);
+
+        StringBuilder output = new();
+        for (int i = 0; i < t; ++i)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            
+            while (callcounts_written_index < n)
+            {
+                ++callcounts_written_index;
+
+                // p == previous
+                CallCount pp_callcount = callcounts[callcounts_written_index - 2];
+                CallCount p_callcount = callcounts[callcounts_written_index - 1];
+
+                callcounts[callcounts_written_index] = new(pp_callcount.Item1 + p_callcount.Item1, pp_callcount.Item2 + p_callcount.Item2);
+            }
+
+            CallCount n_callcount = callcounts[n];
+            output.AppendLine($"{n_callcount.Item1} {n_callcount.Item2}");
+        }
+        Console.Write(output);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b9a1466d419c028c82697b3a21e719660b080a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396d3ec8635f6c489c64be7c433c54a01a76ec67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
