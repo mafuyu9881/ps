6bdbe39a92fe580bdb7e98d89cc91f5796f1c492
mafuyusame <mafuyu@tutamail.com>
2024-09-30 14:38:29 +0900

    boj 9095 (dp)

--- Program.cs
+++ Program.cs
@@ -1,133 +1,37 @@
 ﻿// 시간 제한: 1초
-// 메모리 제한: 128MB
-// 1 <= (컴퓨터의 수) <= 100
+// 메모리 제한: 512MB
+// 0 < n < 11
+
+using System.Text;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        int tno_computers = int.Parse(Console.ReadLine()!);
+        int max_n = 10;
+        int n_variations_length = max_n + 1;
+        int[] n_variations = new int[n_variations_length];
+        n_variations[1] = 1;
+        n_variations[2] = 2;
+        n_variations[3] = 4;
+        int memoized_index = 3;
 
-        int connections = int.Parse(Console.ReadLine()!);
-
-        graph graph = new();
-        for (int i = 0; i < connections; ++i)
-        {
-            string[] tokens = Console.ReadLine()!.Split();
-
-            graph.connect(int.Parse(tokens[0]), int.Parse(tokens[1]));
-        }
-
-        int connected_nodes_count = graph.compute_connected_nodes_count_by_bfs(1);
-
-        Console.Write(connected_nodes_count);
-    }
-}
-
-public class graph
-{
-    class node
-    {
-        private int data;
-        public int Data { get { return data; } }
-        private LinkedList<node> adjacencies = new();
-
-        public node(int data)
-        {
-            this.data = data;
-        }
+        int t = int.Parse(Console.ReadLine()!);
         
-        public bool equals(node? other)
+        StringBuilder output = new();
+        for (int i = 0; i < t; ++i)
         {
-            if (other == null)
-                return false;
+            int n = int.Parse(Console.ReadLine()!);
 
-            return equals(other.Data);
-        }
-        public bool equals(int other)
-        {
-            return data == other;
-        }
-
-        public void add_unique_adjacency(node adjacency)
-        {
-            if (adjacency == null)
-                return;
-
-            for (var node = adjacencies.First; node != null; node = node.Next)
-                if (adjacency.equals(node.Value))
-                    return;
-
-            adjacencies.AddLast(adjacency);
-            adjacency.adjacencies.AddLast(this);
-        }
-
-        public void for_each_adjacencies(Action<node> predicate)
-        {
-            for (var node = adjacencies.First; node != null; node = node.Next)
-                predicate(node.Value);
-        }
-    }
-    
-    private LinkedList<node> nodes = new();
-    
-    private LinkedListNode<node>? find(int data)
-    {
-        for (var node = nodes.First; node != null; node = node.Next)
-            if (node.Value.equals(data))
-                return node;
-                
-        return null;
-    }
-
-    private LinkedListNode<node>? find_or_add(int data)
-    {
-        LinkedListNode<node>? output = find(data);
-        if (output == null)
-            output = nodes.AddLast(new node(data));
-
-        return output;
-    }
-
-    public void connect(int data0, int data1)
-    {
-        LinkedListNode<node>? node0 = find_or_add(data0);
-        LinkedListNode<node>? node1 = find_or_add(data1);
-
-        node0!.ValueRef.add_unique_adjacency(node1!.ValueRef);
-    }
-
-    public int compute_connected_nodes_count_by_bfs(int initial_data)
-    {
-        int output = 0;
-
-        var initial_node = find(initial_data);
-        if (initial_node != null)
-        {
-            HashSet<int> visited = new();
-            Queue<node> search_queue = new();
-            search_queue.Enqueue(initial_node.ValueRef);
-
-            while (search_queue.Count > 0)
+            while (memoized_index < n)
             {
-                node node = search_queue.Dequeue();
-                node.for_each_adjacencies((adjacent_node) =>
-                {
-                    int adjacent_node_data = adjacent_node.Data;
+                ++memoized_index;
 
-                    if (visited.Contains(adjacent_node_data))
-                        return;
-
-                    search_queue.Enqueue(adjacent_node);
-                    visited.Add(adjacent_node_data);
-                    ++output;
-                });
+                n_variations[memoized_index] = n_variations[memoized_index - 1] + n_variations[memoized_index - 2] + n_variations[memoized_index - 3];
             }
 
-            // initial_data에 해당하는 노드는 제외합니다.
-            output -= 1;
+            output.AppendLine($"{n_variations[n]}");
         }
-
-        return output;
+        Console.Write(output);
     }
 }
