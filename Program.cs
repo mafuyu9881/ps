13c7eb5b890a077eab91138419a472ca8f18b5f0
mafuyusame <mafuyu@tutamail.com>
2024-10-03 23:30:02 +0900

    boj 32142

--- Program.cs
+++ Program.cs
@@ -1,66 +1,17 @@
-﻿// 시간 제한: 2초
-// 메모리 제한: 128MB
-// 1,000,000 * 1B = 1,000KB = 1MB
+﻿// 시간 제한: 1초
+// 메모리 제한: 1024MB
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        string[] tokens = Console.ReadLine()!.Split();
-
-        int n = int.Parse(tokens[0]);
-        int k = int.Parse(tokens[1]);
-
-        int visitedIndicesLength = 100000 + 1;
-        bool[] visitedIndices = new bool[visitedIndicesLength];
-        visitedIndices[n] = true;
-
-        LinkedList<int> levelNumbers = new();
-        levelNumbers.AddLast(n);
-
-        int seconds = 0;
-        while (true)
+        int n = int.Parse(Console.ReadLine()!);
+        
+        for (int i = 0; i < n; ++i)
         {
-            LinkedList<int> nextLevelNumbers = new();
-
-            for (var node = levelNumbers.First; node != null; node = node.Next)
-            {
-                int levelNumber = node.Value;
-                if (levelNumber == k)
-                    goto Break;
-
-                int prevLevelNumber = levelNumber - 1;
-                int nextLevelNumber = levelNumber + 1;
-                int twiceLevelNumber = levelNumber * 2;
-
-                if (prevLevelNumber >= 0 &&
-                    visitedIndices[prevLevelNumber] == false)
-                {
-                    nextLevelNumbers.AddLast(prevLevelNumber);
-                    visitedIndices[prevLevelNumber] = true;
-                }
-
-                if (nextLevelNumber < visitedIndicesLength &&
-                    visitedIndices[nextLevelNumber] == false)
-                {
-                    nextLevelNumbers.AddLast(nextLevelNumber);
-                    visitedIndices[nextLevelNumber] = true;
-                }
-
-                if (twiceLevelNumber < visitedIndicesLength && 
-                    visitedIndices[twiceLevelNumber] == false)
-                {
-                    nextLevelNumbers.AddLast(twiceLevelNumber);
-                    visitedIndices[twiceLevelNumber] = true;
-                }
-            }
-
-            levelNumbers = nextLevelNumbers;
-            ++seconds;
+            Console.ReadLine();
         }
-Break:
-        Console.Write(seconds);
+
+        Console.Write(-1);
     }
 }
-
-// 진행 방향이 하나가 아니면 dp를 사용할 수 없다?
