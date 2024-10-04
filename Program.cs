e281c423ff5c5020038f700b1fd34432d0f4aa15
mafuyusame <mafuyu@tutamail.com>
2024-10-04 18:48:22 +0900

    boj 1074에 대한 접근 방식 1 (중도 포기)

--- Program.cs
+++ Program.cs
@@ -1,96 +1,51 @@
-﻿using System.Text;
-using Index2D = System.Tuple<int, int>;
+﻿// 시간 제한: 0.5초
+// 메모리 제한: 512MB
+// 1 ≤ N ≤ 15
+// 0 ≤ r, c < 2^N
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         string[] tokens = Console.ReadLine()!.Split();
-        
-        int height = int.Parse(tokens[0]);
-        int width = int.Parse(tokens[1]);
 
-        const int InvalidAttribute = -1;
-        int[, ] map = new int[height, width];
-        int[, ] distanceMap = new int[height, width];
-        Index2D? objectiveIndex2D = null;
+        int n = int.Parse(tokens[0]);
+        int r = int.Parse(tokens[1]);
+        int c = int.Parse(tokens[2]);
 
-        for (int row = 0; row < height; ++row)
+        int[] sqrtOfTwo = new int[n * 2 + 1];
+        sqrtOfTwo[0] = 1;
+        for (int i = 1; i < sqrtOfTwo.Length; ++i)
         {
-            tokens = Console.ReadLine()!.Split();
-            for (int col = 0; col < width; ++col)
-            {
-                int attribute = int.Parse(tokens[col]);
-
-                if (attribute == 2)
-                    objectiveIndex2D = new(row, col);
-
-                map[row, col] = attribute;
-                distanceMap[row, col] = InvalidAttribute;
-            }
+            sqrtOfTwo[i] = sqrtOfTwo[i - 1] * 2;
         }
 
-        Queue<Index2D> visitingQueue = new();
-        visitingQueue.Enqueue(objectiveIndex2D!);
-        distanceMap[objectiveIndex2D!.Item1, objectiveIndex2D!.Item2] = 0;
-        while (visitingQueue.Count > 0)
-        {
-            var originIndex2D = visitingQueue.Dequeue();
-            int originRow = originIndex2D.Item1;
-            int originCol = originIndex2D.Item2;
+        Index2D objectiveIndex2D = new(r, c);
+        int objectiveIndex1D = ConvertIndexTo1D(sqrtOfTwo[n], objectiveIndex2D);
 
-            Index2D[] adjacentIndices2D = new Index2D[]
-            {
-                new Index2D(originRow - 1, originCol),
-                new Index2D(originRow, originCol - 1),
-                new Index2D(originRow + 1, originCol),
-                new Index2D(originRow, originCol + 1)
-            };
+        // 2^2n / 4 == 2^(2n-2)
+        int temp = objectiveIndex1D / sqrtOfTwo[2 * n - 2];
+    }
 
-            for (int i = 0; i < adjacentIndices2D.Length; ++i)
-            {
-                var adjacentIndex2D = adjacentIndices2D[i];
-                int adjacentRow = adjacentIndex2D.Item1;
-                int adjacentCol = adjacentIndex2D.Item2;
+    private static int ConvertIndexTo1D(int width, Index2D index2D)
+    {
+        return index2D.row * width + index2D.col;
+    }
 
-                if (adjacentRow < 0 || adjacentRow > height - 1)
-                    continue;
-                
-                if (adjacentCol < 0 || adjacentCol > width - 1)
-                    continue;
+    private static Index2D ConvertIndexTo2D(int width, int index1D)
+    {
+        return new(index1D / width, index1D % width);
+    }
+}
 
-                if (objectiveIndex2D!.Item1 == adjacentRow &&
-                    objectiveIndex2D!.Item2 == adjacentCol)
-                    continue;
+struct Index2D
+{
+    public int row;
+    public int col;
 
-                if (map[adjacentRow, adjacentCol] == 0)
-                    continue;
-
-                if (distanceMap[adjacentRow, adjacentCol] != InvalidAttribute)
-                    continue;
-
-                visitingQueue.Enqueue(adjacentIndex2D);
-                distanceMap[adjacentRow, adjacentCol] = distanceMap[originRow, originCol] + 1;
-            }
-        }
-
-        StringBuilder output = new();
-        for (int row = 0; row < height; ++row)
-        {
-            for (int col = 0; col < width; ++col)
-            {
-                if (map[row, col] != 0)
-                {
-                    output.Append($"{distanceMap[row, col]} ");
-                }
-                else
-                {
-                    output.Append($"0 ");
-                }
-
-            }
-            output.AppendLine();
-        }
-        Console.Write(output);
+    public Index2D(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
     }
 }
