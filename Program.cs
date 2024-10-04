37966fffbff1439c0d66a783899382c92c23d693
mafuyusame <mafuyu@tutamail.com>
2024-10-04 16:37:55 +0900

    boj 14940 (너비 우선 탐색)

--- Program.cs
+++ Program.cs
@@ -1,102 +1,95 @@
-﻿// 시간 제한: 1초
-// 메모리 제한: 256MB
-// 1 ≤ N ≤ 100,000
-
-using System.Text;
+﻿using System.Text;
+using Index2D = System.Tuple<int, int>;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        string[] tokens = Console.ReadLine()!.Split();
+        
+        int height = int.Parse(tokens[0]);
+        int width = int.Parse(tokens[1]);
 
-        int[] maxHeap = new int[n];
-        int writtenIndex = -1;
-        const int InvalidIndex = -1;
+        const int InvalidAttribute = -1;
+        int[, ] map = new int[height, width];
+        int[, ] distanceMap = new int[height, width];
+        Index2D? objectiveIndex2D = null;
+
+        for (int row = 0; row < height; ++row)
+        {
+            tokens = Console.ReadLine()!.Split();
+            for (int col = 0; col < width; ++col)
+            {
+                int attribute = int.Parse(tokens[col]);
+
+                if (attribute == 2)
+                    objectiveIndex2D = new(row, col);
+
+                map[row, col] = attribute;
+                distanceMap[row, col] = InvalidAttribute;
+            }
+        }
+
+        Queue<Index2D> visitingQueue = new();
+        visitingQueue.Enqueue(objectiveIndex2D!);
+        distanceMap[objectiveIndex2D!.Item1, objectiveIndex2D!.Item2] = 0;
+        while (visitingQueue.Count > 0)
+        {
+            var originIndex2D = visitingQueue.Dequeue();
+            int originRow = originIndex2D.Item1;
+            int originCol = originIndex2D.Item2;
+
+            Index2D[] adjacentIndices2D = new Index2D[]
+            {
+                new Index2D(originRow - 1, originCol),
+                new Index2D(originRow, originCol - 1),
+                new Index2D(originRow + 1, originCol),
+                new Index2D(originRow, originCol + 1)
+            };
+
+            for (int i = 0; i < adjacentIndices2D.Length; ++i)
+            {
+                var adjacentIndex2D = adjacentIndices2D[i];
+                int adjacentRow = adjacentIndex2D.Item1;
+                int adjacentCol = adjacentIndex2D.Item2;
+
+                if (adjacentRow < 0 || adjacentRow > height - 1)
+                    continue;
+                
+                if (adjacentCol < 0 || adjacentCol > width - 1)
+                    continue;
+
+                if (objectiveIndex2D!.Item1 == adjacentRow &&
+                    objectiveIndex2D!.Item2 == adjacentCol)
+                    continue;
+
+                if (map[adjacentRow, adjacentCol] == 0)
+                    continue;
+
+                if (distanceMap[adjacentRow, adjacentCol] != InvalidAttribute)
+                    continue;
+
+                visitingQueue.Enqueue(adjacentIndex2D);
+                distanceMap[adjacentRow, adjacentCol] = distanceMap[originRow, originCol] + 1;
+            }
+        }
 
         StringBuilder output = new();
-        for (int i = 0; i < n; ++i)
+        for (int row = 0; row < height; ++row)
         {
-            int command = int.Parse(Console.ReadLine()!);
-
-            if (command != 0)
+            for (int col = 0; col < width; ++col)
             {
-                if (writtenIndex < 0)
+                if (map[row, col] != 0)
                 {
-                    maxHeap[0] = command;
-                    writtenIndex = 0;
+                    output.Append($"{distanceMap[row, col]} ");
                 }
                 else
                 {
-                    ++writtenIndex;
-                    maxHeap[writtenIndex] = command;
-                    
-                    int comparingIndex = writtenIndex;
-                    while (comparingIndex > 0)
-                    {
-                        int parentIndex = comparingIndex / 2;
-                        if (comparingIndex % 2 == 0)
-                            parentIndex -= 1;
-
-                        int comparingData = maxHeap[comparingIndex];
-                        int parentData = maxHeap[parentIndex];
-
-                        if (parentData >= comparingData)
-                            break;
-
-                        maxHeap[parentIndex] = comparingData;
-                        maxHeap[comparingIndex] = parentData;
-                        comparingIndex = parentIndex;
-                    }
+                    output.Append($"0 ");
                 }
+
             }
-            else
-            {
-                if (writtenIndex < 0)
-                {
-                    output.AppendLine("0");
-                }
-                else
-                {
-                    output.AppendLine($"{maxHeap[0]}");
-
-                    maxHeap[0] = maxHeap[writtenIndex];
-                    --writtenIndex;
-
-                    int comparingIndex = 0;
-                    while (comparingIndex < writtenIndex)
-                    {
-                        int leftChildIndex = comparingIndex * 2 + 1;
-                        int rightChildIndex = comparingIndex * 2 + 2;
-
-                        // leftChildIndex < rightChildIndex이므로 left에 대해서만 검사하면 됩니다.
-                        if (leftChildIndex > writtenIndex)
-                            break;
-
-                        int selectedIndex = comparingIndex;
-
-                        if (maxHeap[leftChildIndex] > maxHeap[selectedIndex])
-                        {
-                            selectedIndex = leftChildIndex;
-                        }
-
-                        if (rightChildIndex <= writtenIndex &&
-                            maxHeap[rightChildIndex] > maxHeap[selectedIndex])
-                        {
-                            selectedIndex = rightChildIndex;
-                        }
-
-                        if (selectedIndex == comparingIndex)
-                            break;
-                        
-                        int temp = maxHeap[comparingIndex];
-                        maxHeap[comparingIndex] = maxHeap[selectedIndex];
-                        maxHeap[selectedIndex] = temp;
-
-                        comparingIndex = selectedIndex;
-                    }
-                }
-            }
+            output.AppendLine();
         }
         Console.Write(output);
     }
