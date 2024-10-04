97418a402518844789bfc4f00a2ea21252344628
mafuyusame <mafuyu@tutamail.com>
2024-10-04 21:41:05 +0900

    boj 7576 (너비 우선 탐색)

--- Program.cs
+++ Program.cs
@@ -1,92 +1,119 @@
-﻿// 시간 제한: 0.5초
-// 메모리 제한: 512MB
-// 1 ≤ N ≤ 15
-// 0 ≤ r, c < 2^N
+﻿// 시간 제한: 1초
+// 메모리 제한: 256MB
+// 2 ≤ M,N ≤ 1,000
 
 internal class Program
 {
-    private static int[]? memoizedSqrtOfTwo = null;
-    private static int memoizedIndex = -1;
-
     private static void Main(string[] args)
     {
         string[] tokens = Console.ReadLine()!.Split();
 
-        int n = int.Parse(tokens[0]);
-        int r = int.Parse(tokens[1]);
-        int c = int.Parse(tokens[2]);
-        
-        memoizedSqrtOfTwo = new int[n + 1];
+        int width = int.Parse(tokens[0]);
+        int height = int.Parse(tokens[1]);
 
-        Index2D objectiveIndex2D = new(r, c);
+        const int ripeTomatoAttribute = 1;
+        const int unripeTomatoAttribute = 0;
+        const int blankAttribute = -1;
 
-        Console.Write(ComputeVisitingTurn(SqrtOfTwo(n), objectiveIndex2D));
-    }
+        const int InvalidDays = -1;
 
-    private static int ComputeVisitingTurn(int side, Index2D objectiveIndex2D)
-    {
-        int halfSide = side / 2;
-
-        int objectiveRow = objectiveIndex2D.row;
-        int objectiveCol = objectiveIndex2D.col;
-
-        int quadrant = 0;
-
-        Index2D adjustedObjectiveIndex2D = objectiveIndex2D;
-        
-        // 우상
-        if (objectiveRow < halfSide && objectiveCol >= halfSide)
+        int[, ] map = new int[height, width];
+        int[, ] daysTakenForRipeningMap = new int[height, width];
+        LinkedList<Index2D> unripeTomatoes = new();
+        Queue<Index2D> visitingQueue = new();
+        for (int row = 0; row < height; ++row)
         {
-            quadrant = 1;
-            adjustedObjectiveIndex2D = new(objectiveRow, objectiveCol - halfSide);
-        }
-        // 좌하
-        else if (objectiveRow >= halfSide && objectiveCol < halfSide)
-        {
-            quadrant = 2;
-            adjustedObjectiveIndex2D = new(objectiveRow - halfSide, objectiveCol);
-        }
-        // 우하
-        else if (objectiveRow >= halfSide && objectiveCol >= halfSide)
-        {
-            quadrant = 3;
-            adjustedObjectiveIndex2D = new(objectiveRow - halfSide, objectiveCol - halfSide);
-        }
-
-        int sum = (side > 2) ? ComputeVisitingTurn(halfSide, adjustedObjectiveIndex2D) : 0;
-
-        return sum + (quadrant * halfSide * halfSide);
-    }
-
-    // 배열의 범위를 벗어나는 값이 입력되지 않음이 전제됩니다.
-    private static int SqrtOfTwo(int exponent)
-    {
-        while (memoizedIndex < exponent)
-        {
-            ++memoizedIndex;
-
-            if (memoizedIndex < 1)
+            tokens = Console.ReadLine()!.Split();
+            for (int col = 0; col < width; ++col)
             {
-                memoizedSqrtOfTwo![memoizedIndex] = 1;
-            }
-            else
-            {
-                memoizedSqrtOfTwo![memoizedIndex] = memoizedSqrtOfTwo[memoizedIndex - 1] * 2;
+                int attribute = int.Parse(tokens[col]);
+
+                map[row, col] = attribute;
+
+                int days = InvalidDays;
+                Index2D index2D = new Index2D(row, col);
+                if (attribute == unripeTomatoAttribute)
+                {
+                    unripeTomatoes.AddLast(index2D);
+                }
+                else if (attribute == ripeTomatoAttribute)
+                {
+                    days = 0;
+                    visitingQueue.Enqueue(index2D);
+                }
+                daysTakenForRipeningMap[row, col] = days;
             }
         }
 
-        return memoizedSqrtOfTwo![exponent];
+        bool[, ] visited = new bool[height, width];
+        int entireDaysTakenForRipening = 0;
+        while (visitingQueue.Count > 0)
+        {
+            Index2D originIndex2D = visitingQueue.Dequeue();
+            int originRow = originIndex2D.row;
+            int originCol = originIndex2D.col;
+
+            Index2D[] adjacentIndices2D = new Index2D[]
+            {
+                new(originRow - 1, originCol),
+                new(originRow + 1, originCol),
+                new(originRow, originCol - 1),
+                new(originRow, originCol + 1),
+            };
+
+            for (int i = 0; i < adjacentIndices2D.Length; ++i)
+            {
+                Index2D adjacentIndex2D = adjacentIndices2D[i];
+                int adjacentRow = adjacentIndex2D.row;
+                int adjacentCol = adjacentIndex2D.col;
+
+                if (adjacentRow < 0 || adjacentRow > height - 1)
+                    continue;
+
+                if (adjacentCol < 0 || adjacentCol > width - 1)
+                    continue;
+
+                if (visited[adjacentRow, adjacentCol])
+                    continue;
+
+                if (map[adjacentRow, adjacentCol] == blankAttribute)
+                    continue;
+
+                if (map[adjacentRow, adjacentCol] == ripeTomatoAttribute)
+                    continue;
+
+                int daysTakenForRipening = daysTakenForRipeningMap[originRow, originCol] + 1;
+                if (daysTakenForRipening > entireDaysTakenForRipening)
+                {
+                    entireDaysTakenForRipening = daysTakenForRipening;
+                }
+                visited[adjacentRow, adjacentCol] = true;
+                daysTakenForRipeningMap[adjacentRow, adjacentCol] = daysTakenForRipening;
+                visitingQueue.Enqueue(adjacentIndex2D);
+            }
+        }
+
+        for (var node = unripeTomatoes.First; node != null; node = node.Next)
+        {
+            Index2D index2D = node.Value;
+            if (daysTakenForRipeningMap[index2D.row, index2D.col] == InvalidDays)
+            {
+                entireDaysTakenForRipening = -1;
+                break;
+            }
+        }
+
+        Console.Write(entireDaysTakenForRipening);
     }
-}
 
-struct Index2D
-{
-    public int row;
-    public int col;
-
-    public Index2D(int row, int col)
+    private struct Index2D
     {
-        this.row = row;
-        this.col = col;
+        public int row;
+        public int col;
+        public Index2D(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc2c3d9d1abf9e4deef92aed287234324e2c5305")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2dbee9bb23ea790113f44f202c167e4f7a9140")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
