2abe1942c3318432f3d9dbe5abd6dd1ee2121857
mafuyusame <mafuyu@tutamail.com>
2024-10-04 23:00:07 +0900

    boj 8370

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.Write("고려대학교");
+        string[] tokens = Console.ReadLine()!.Split();
+
+        int n1 = int.Parse(tokens[0]);
+        int k1 = int.Parse(tokens[1]);
+        int n2 = int.Parse(tokens[2]);
+        int k2 = int.Parse(tokens[3]);
+        
+        Console.Write(n1 * k1 + n2 * k2);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0416e141d2a230fb7a98add54113cd4456af3668")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c26108ad8b58b6675cd340232c64f3dc2ac013b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
