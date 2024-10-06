1d41018f9d51b858d787d427940926237a9d4462
mafuyusame <mafuyu@tutamail.com>
2024-10-06 12:04:46 +0900

    20241006 정렬 복습

--- Program.cs
+++ Program.cs
@@ -4,77 +4,94 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string[] tokens = Console.ReadLine()!.Split();
+        //int[] arr = [ 4, 1, 3, 5, 2 ];
+        int[] arr = [ 2, 3, 5, 8, 1, 4, 7, 6 ];
 
-        int n = int.Parse(tokens[0]);
-        int m = int.Parse(tokens[1]);
-
-        tokens = Console.ReadLine()!.Split();
-
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; ++i)
-        {
-            numbers[i] = int.Parse(tokens[i]);
-        }
-        MergeSort(numbers);
+        MergeSort(arr);
 
         StringBuilder output = new();
-        bool[] occupiedIndices = new bool[numbers.Length];
-        int[] sequence = new int[m];
-        PrintNonduplicatedSequences(ref output,
-                                    ref occupiedIndices,
-                                    numbers,
-                                    sequence,
-                                    sequence);
+        for (int i = 0; i < arr.Length; ++i)
+        {
+            output.Append($"{arr[i]} ");
+        }
         Console.Write(output);
     }
     
-    private static void PrintNonduplicatedSequences(ref StringBuilder output,
-                                                    ref bool[] occupiedIndices,
-                                                    Span<int> numbers,
-                                                    Span<int> sequence,
-                                                    Span<int> subsequence)
+    private static void BubbleSort(int[] arr)
     {
-        int numbersLength = numbers.Length;
-        int subsequenceLength = subsequence.Length;
-
-        for (int i = 0; i < numbersLength; ++i)
+        for (int i = 0; i < arr.Length - 1; ++i)
         {
-            if (occupiedIndices[i])
-                continue;
-
-            // 중복 제거
-            int iNumber = numbers[i];
-            if (subsequence[0] == iNumber)
-                continue;
-
-            subsequence.Clear();
-
-            occupiedIndices[i] = true;
-
-            subsequence[0] = iNumber;
-
-            if (subsequenceLength < 2)
+            for (int j = 0; j < arr.Length - 1 - i; ++j)
             {
-                for (int j = 0; j < sequence.Length; ++j)
+                if (arr[j] > arr[j + 1])
                 {
-                    output.Append($"{sequence[j]} ");
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-                output.AppendLine();
             }
-            else
-            {
-                PrintNonduplicatedSequences(ref output,
-                                            ref occupiedIndices,
-                                            numbers,
-                                            sequence,
-                                            subsequence.Slice(1));
-            }
-
-            occupiedIndices[i] = false;
         }
     }
-    
+
+    private static void SelectionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; ++i)
+        {
+            int selectedIndex = i;
+            for (int j = i; j < arr.Length; ++j)
+            {
+                if (arr[j] < arr[selectedIndex])
+                {
+                    selectedIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[selectedIndex];
+            arr[selectedIndex] = temp;
+        }
+    }
+
+    private static void InsertionSort1(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; ++i)
+        {
+            for (int j = i; j > 0; --j)
+            {
+                if (arr[j] < arr[j - 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void InsertionSort2(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; ++i)
+        {
+            int objectiveElement = arr[i];
+            int objectiveIndex = i;
+            for (int j = i - 1; j >= 0; --j)
+            {
+                if (arr[j] > objectiveElement)
+                {
+                    arr[j + 1] = arr[j];
+                    objectiveIndex = j;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            arr[objectiveIndex] = objectiveElement;
+        }
+    }
 
     private static void MergeSort(Span<int> arr)
     {
@@ -83,7 +100,7 @@ internal class Program
             return;
 
         int midIndex = arrLength / 2;
-
+        
         MergeSort(arr.Slice(0, midIndex));
         MergeSort(arr.Slice(midIndex));
         Merge(arr, midIndex);
@@ -98,20 +115,17 @@ internal class Program
         int leftReadIndex = 0;
         int rightReadIndex = midIndex;
         int writtenIndex = 0;
-        
+
         while (leftReadIndex < midIndex && rightReadIndex < arrLength)
         {
-            int leftReadElement = backedup[leftReadIndex];
-            int rightReadElement = backedup[rightReadIndex];
-
-            if (leftReadElement < rightReadElement)
+            if (backedup[leftReadIndex] < backedup[rightReadIndex])
             {
-                arr[writtenIndex] = leftReadElement;
+                arr[writtenIndex] = backedup[leftReadIndex];
                 ++leftReadIndex;
             }
             else
             {
-                arr[writtenIndex] = rightReadElement;
+                arr[writtenIndex] = backedup[rightReadIndex];
                 ++rightReadIndex;
             }
             ++writtenIndex;
