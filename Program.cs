5e33adfa92c5e17d48a8d24e7b09ede084ecebc3
mafuyusame <mafuyu@tutamail.com>
2024-10-07 00:11:48 +0900

    boj 1260 (dfs, bfs)

--- Program.cs
+++ Program.cs
@@ -5,46 +5,96 @@ internal class Program
     private static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
+        
         int n = tokens[0];
         int m = tokens[1];
+        int v = tokens[2];
 
-        StringBuilder output = new();
-        int[] sequence = new int[m];
-        PrintNonduplicatedSequences(ref output,
-                                    sequence,
-                                    sequence,
-                                    n,
-                                    1);
-        Console.Write(output);
+        Graph graph = new(n);
+        for (int i = 0; i < m; ++i)
+        {
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            graph.Connect(tokens[0], tokens[1]);
+        }
+
+        Console.WriteLine(graph.DFS(v));
+        Console.WriteLine(graph.BFS(v));
     }
 
-    private static void PrintNonduplicatedSequences(ref StringBuilder output,
-                                             Span<int> sequence,
-                                             Span<int> subsequence,
-                                             int maximumNaturalNumber,
-                                             int minimumNaturalNumber)
+    private class Graph
     {
-        for (int i = minimumNaturalNumber; i <= maximumNaturalNumber; ++i)
-        {
-            subsequence[0] = i;
+        private List<int>[] _adjacencyList;
 
-            if (subsequence.Length > 1)
+        public Graph(int vertexCount)
+        {
+            _adjacencyList = new List<int>[vertexCount + 1];
+            for (int i = 0; i < _adjacencyList.Length; ++i)
             {
-                PrintNonduplicatedSequences(ref output,
-                                            sequence,
-                                            subsequence.Slice(1),
-                                            maximumNaturalNumber,
-                                            i);
+                _adjacencyList[i] = new();
             }
-            else
+        }
+
+        public void Connect(int v0, int v1)
+        {
+            _adjacencyList[v0].Add(v1);
+            _adjacencyList[v1].Add(v0);
+            _adjacencyList[v0].Sort();
+            _adjacencyList[v1].Sort();
+        }
+
+        public StringBuilder DFS(int v, bool[]? visited = null)
+        {
+            if (visited == null)
+                visited = new bool[_adjacencyList.Length];
+
+            visited[v] = true;
+
+            StringBuilder output = new();
+            output.Append($"{v} ");
+            
+            var adjacencies = _adjacencyList[v];
+
+            for (int i = 0; i < adjacencies.Count; ++i)
             {
-                for (int j = 0; j < sequence.Length; ++j)
+                int adjacentV = adjacencies[i];
+
+                if (visited[adjacentV])
+                    continue;
+
+                output.Append(DFS(adjacentV, visited));
+            }
+            
+            return output;
+        }
+
+        public StringBuilder BFS(int firstV)
+        {
+            bool[] visited = new bool[_adjacencyList.Length];
+            Queue<int> visitingQueue = new();
+
+            visited[firstV] = true;
+            visitingQueue.Enqueue(firstV);
+
+            StringBuilder output = new();
+            while (visitingQueue.Count > 0)
+            {
+                int originV = visitingQueue.Dequeue();
+
+                output.Append($"{originV} ");
+
+                var adjacencies = _adjacencyList[originV];
+                for (int i = 0; i < adjacencies.Count; ++i)
                 {
-                    output.Append($"{sequence[j]} ");
+                    int adjacentV = adjacencies[i];
+
+                    if (visited[adjacentV])
+                        continue;
+
+                    visited[adjacentV] = true;
+                    visitingQueue.Enqueue(adjacentV);
                 }
-                output.AppendLine();
             }
+            return output;
         }
     }
 }
