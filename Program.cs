68dfe8d361d566d439e2b36d9bc0d27ece5cf48a
mafuyusame <mafuyu@tutamail.com>
2024-10-06 18:27:27 +0900

    boj 1629 (분할 정복을 이용한 거듭제곱)

--- Program.cs
+++ Program.cs
@@ -1,33 +1,35 @@
-﻿internal class Program
+﻿using System.Numerics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        string[] tokens = Console.ReadLine()!.Split();
 
-        PaintingCost[] accPaintingCosts = new PaintingCost[n + 1];
-        accPaintingCosts[0].r = 0;
-        accPaintingCosts[0].g = 0;
-        accPaintingCosts[0].b = 0;
-        for (int i = 1; i <= n; ++i)
+        BigInteger a = BigInteger.Parse(tokens[0]);
+        int b = int.Parse(tokens[1]);
+        int c = int.Parse(tokens[2]);
+
+        // exponentiation by squaring
+        BigInteger output = 1;
+        while (b > 0)
         {
-            string[] tokens = Console.ReadLine()!.Split();
+            // 짝수의 경우, 마지막 비트는 항상 0이다.
+            // 홀수의 경우, 마지막 비트는 항상 1이다.
+            if ((b & 1) == 1)
+            {
+                // 지수가 홀수일 때 남은 1을 처리하기 위한 것이다.
+                output *= a;
+                output %= c;
+            }
 
-            PaintingCost prevAccPaintingCost = accPaintingCosts[i - 1];
-            
-            PaintingCost currPaintingCost;
-            currPaintingCost.r = Math.Min(prevAccPaintingCost.g, prevAccPaintingCost.b) + int.Parse(tokens[0]);
-            currPaintingCost.g = Math.Min(prevAccPaintingCost.r, prevAccPaintingCost.b) + int.Parse(tokens[1]);
-            currPaintingCost.b = Math.Min(prevAccPaintingCost.r, prevAccPaintingCost.g) + int.Parse(tokens[2]);
+            // 밑을 제곱, 지수를 반으로 줄이기 위한 과정이다.
+            a *= a;
+            a %= c;
 
-            accPaintingCosts[i] = currPaintingCost;
+            // 위의 계산에 의거하여 지수를 반으로 줄인다.
+            b >>= 1;
         }
-        Console.Write(Math.Min(accPaintingCosts[n].r, Math.Min(accPaintingCosts[n].g, accPaintingCosts[n].b)));
-    }
-
-    private struct PaintingCost
-    {
-        public int r;
-        public int g;
-        public int b;
+        Console.Write(output);
     }
 }
