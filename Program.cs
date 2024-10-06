c3e3de633552b6df3c44100967168c3b6cb7e396
mafuyusame <mafuyu@tutamail.com>
2024-10-06 23:13:51 +0900

    boj 15652 (백트래킹)

--- Program.cs
+++ Program.cs
@@ -4,121 +4,47 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        const int InvalidData = -1;
-        
-        int n = int.Parse(Console.ReadLine()!);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        Graph graph = new();
-        for (int i = 0; i < n; ++i)
-        {
-            char[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), char.Parse);
-            
-            int parent = tokens[0] - 'A';
-            int leftChild = tokens[1] - 'A';
-            int rightChild = tokens[2] - 'A';
-
-            if (parent > InvalidData)
-            {
-                if (leftChild > InvalidData)
-                {
-                    graph.AddLeft(parent, leftChild);
-                }
-
-                if (rightChild > InvalidData)
-                {
-                    graph.AddRight(parent, rightChild);
-                }
-            }
-        }
+        int n = tokens[0];
+        int m = tokens[1];
 
         StringBuilder output = new();
-        output.AppendLine($"{graph.Order(Graph.TraversalMethod.Preorder)}");
-        output.AppendLine($"{graph.Order(Graph.TraversalMethod.Inorder)}");
-        output.AppendLine($"{graph.Order(Graph.TraversalMethod.Postorder)}");
+        int[] sequence = new int[m];
+        PrintNonduplicatedSequences(ref output,
+                                    sequence,
+                                    sequence,
+                                    n,
+                                    1);
         Console.Write(output);
     }
 
-    private class Graph
+    private static void PrintNonduplicatedSequences(ref StringBuilder output,
+                                             Span<int> sequence,
+                                             Span<int> subsequence,
+                                             int maximumNaturalNumber,
+                                             int minimumNaturalNumber)
     {
-        public enum TraversalMethod
+        for (int i = minimumNaturalNumber; i <= maximumNaturalNumber; ++i)
         {
-            Preorder,
-            Inorder,
-            Postorder,
-        }
+            subsequence[0] = i;
 
-        private struct Node
-        {
-            private int? _leftChild = null;
-            public int? LeftChild
+            if (subsequence.Length > 1)
             {
-                get { return _leftChild; }
-                set { _leftChild = value; }
+                PrintNonduplicatedSequences(ref output,
+                                            sequence,
+                                            subsequence.Slice(1),
+                                            maximumNaturalNumber,
+                                            i);
             }
-            private int? _rightChild = null;
-            public int? RightChild
+            else
             {
-                get { return _rightChild; }
-                set { _rightChild = value; }
+                for (int j = 0; j < sequence.Length; ++j)
+                {
+                    output.Append($"{sequence[j]} ");
+                }
+                output.AppendLine();
             }
-
-            public Node()
-            {
-
-            }
-        }
-
-        Node[] nodes;
-
-        public Graph()
-        {
-            nodes = new Node['Z' - 'A' + 1];
-        }
-
-        public void AddLeft(int parent, int child)
-        {
-            nodes[parent].LeftChild = child;
-        }
-
-        public void AddRight(int parent, int child)
-        {
-            nodes[parent].RightChild = child;
-        }
-
-        public StringBuilder Order(TraversalMethod traversalMethod)
-        {
-            StringBuilder output = new();
-            OrderImplementation(output, 0, traversalMethod);
-            return output;
-        }
-
-        private void OrderImplementation(StringBuilder output,
-                                         int parent,
-                                         TraversalMethod traversalMethod)
-        {
-            var parentNode = nodes[parent];
-            var leftChildNode = parentNode.LeftChild;
-            var rightChildNode = parentNode.RightChild;
-
-            Action print = () =>
-            {
-                output.Append((char)(parent + 'A'));
-            };
-
-            if (traversalMethod == TraversalMethod.Preorder)
-                print();
-
-            if (leftChildNode.HasValue)
-                OrderImplementation(output, leftChildNode.Value, traversalMethod);
-
-            if (traversalMethod == TraversalMethod.Inorder)
-                print();
-
-            if (rightChildNode.HasValue)
-                OrderImplementation(output, rightChildNode.Value, traversalMethod);
-
-            if (traversalMethod == TraversalMethod.Postorder)
-                print();
         }
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e52ad08411cc24690e5b8905a8028a5fdd25dbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519c8326405b77375b18258b5fc0e601cf6b313d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
