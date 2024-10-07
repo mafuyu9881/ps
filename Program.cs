910b0aecf9e8d7882b65a28f6d5da1acd4957537
mafuyusame <mafuyu@tutamail.com>
2024-10-07 09:16:13 +0900

    20241007 정렬 복습

--- Program.cs
+++ Program.cs
@@ -4,97 +4,144 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        
-        int n = tokens[0];
-        int m = tokens[1];
-        int v = tokens[2];
+        int[] arr = [ 2, 5, 1, 3, 4 ];
 
-        Graph graph = new(n);
-        for (int i = 0; i < m; ++i)
+        MergeSort(arr);
+
+        StringBuilder output = new();
+        for (int i = 0; i < arr.Length; ++i)
         {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            graph.Connect(tokens[0], tokens[1]);
+            output.Append($"{arr[i]} ");
         }
-
-        Console.WriteLine(graph.DFS(v));
-        Console.WriteLine(graph.BFS(v));
+        Console.Write(output);
     }
 
-    private class Graph
+    private static void BubbleSort(int[] arr)
     {
-        private List<int>[] _adjacencyList;
-
-        public Graph(int vertexCount)
+        for (int i = 0; i < arr.Length - 1; ++i)
         {
-            _adjacencyList = new List<int>[vertexCount + 1];
-            for (int i = 0; i < _adjacencyList.Length; ++i)
+            for (int j = 0; j < arr.Length - 1 - i; ++j)
             {
-                _adjacencyList[i] = new();
-            }
-        }
-
-        public void Connect(int v0, int v1)
-        {
-            _adjacencyList[v0].Add(v1);
-            _adjacencyList[v1].Add(v0);
-            _adjacencyList[v0].Sort();
-            _adjacencyList[v1].Sort();
-        }
-
-        public StringBuilder DFS(int v, bool[]? visited = null)
-        {
-            if (visited == null)
-                visited = new bool[_adjacencyList.Length];
-
-            visited[v] = true;
-
-            StringBuilder output = new();
-            output.Append($"{v} ");
-            
-            var adjacencies = _adjacencyList[v];
-
-            for (int i = 0; i < adjacencies.Count; ++i)
-            {
-                int adjacentV = adjacencies[i];
-
-                if (visited[adjacentV])
-                    continue;
-
-                output.Append(DFS(adjacentV, visited));
-            }
-            
-            return output;
-        }
-
-        public StringBuilder BFS(int firstV)
-        {
-            bool[] visited = new bool[_adjacencyList.Length];
-            Queue<int> visitingQueue = new();
-
-            visited[firstV] = true;
-            visitingQueue.Enqueue(firstV);
-
-            StringBuilder output = new();
-            while (visitingQueue.Count > 0)
-            {
-                int originV = visitingQueue.Dequeue();
-
-                output.Append($"{originV} ");
-
-                var adjacencies = _adjacencyList[originV];
-                for (int i = 0; i < adjacencies.Count; ++i)
+                if (arr[j] > arr[j + 1])
                 {
-                    int adjacentV = adjacencies[i];
-
-                    if (visited[adjacentV])
-                        continue;
-
-                    visited[adjacentV] = true;
-                    visitingQueue.Enqueue(adjacentV);
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            return output;
+        }
+    }
+
+    private static void SelectionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; ++i)
+        {
+            int selectedIndex = i;
+            for (int j = i + 1; j < arr.Length; ++j)
+            {
+                if (arr[j] < arr[selectedIndex])
+                {
+                    selectedIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[selectedIndex];
+            arr[selectedIndex] = temp;
+        }
+    }
+
+    private static void InsertionSort1(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; ++i)
+        {
+            for (int j = i; j > 0; --j)
+            {
+                if (arr[j] < arr[j - 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void InsertionSort2(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; ++i)
+        {
+            int objectiveIndex = i;
+            int objectiveElement = arr[i];
+            for (int j = i - 1; j > -1; --j)
+            {
+                if (objectiveElement < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    objectiveIndex = j;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            arr[objectiveIndex] = objectiveElement;
+        }
+    }
+    
+    private static void MergeSort(Span<int> arr)
+    {
+        int arrLength = arr.Length;
+        if (arrLength < 2)
+            return;
+
+        int midIndex = arrLength / 2;
+
+        MergeSort(arr.Slice(0, midIndex));
+        MergeSort(arr.Slice(midIndex));
+        Merge(arr, midIndex);
+    }
+    
+    private static void Merge(Span<int> arr, int midIndex)
+    {
+        int arrLength = arr.Length;
+
+        int[] backedup = arr.ToArray();
+
+        int leftReadIndex = 0;
+        int rightReadIndex = midIndex;
+        int writtenIndex = 0;
+
+        while (leftReadIndex < midIndex && rightReadIndex < arrLength)
+        {
+            if (backedup[leftReadIndex] < backedup[rightReadIndex])
+            {
+                arr[writtenIndex] = backedup[leftReadIndex];
+                ++leftReadIndex;
+            }
+            else
+            {
+                arr[writtenIndex] = backedup[rightReadIndex];
+                ++rightReadIndex;
+            }
+            ++writtenIndex;
+        }
+
+        while (leftReadIndex < midIndex)
+        {
+            arr[writtenIndex] = backedup[leftReadIndex];
+            ++leftReadIndex;
+            ++writtenIndex;
+        }
+        
+        while (rightReadIndex < arrLength)
+        {
+            arr[writtenIndex] = backedup[rightReadIndex];
+            ++rightReadIndex;
+            ++writtenIndex;
         }
     }
 }
