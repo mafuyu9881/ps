8f4f5e5b78abe6c7395a1116fc34c569c28a029b
mafuyusame <mafuyu@tutamail.com>
2024-10-09 15:05:47 +0900

    boj 1654 복습
    * 크기를 고려하여 자료형을 선택하자. 이 문제의 경우 int는 오버플로우가 발생한다.
    * top의 초기값을 길이들의 최솟값이 아니라 최댓값으로 설정해둬야 한다. 자르려는 길이보다 짧은 선들은 다 갖다 버리는 듯.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,50 @@
 {
     private static void Main(string[] args)
     {
-        string s = "강한친구 대한육군";
-        Console.Write($"{s}{Environment.NewLine}{s}");
+        const long InvalidLength = -1;
+
+        long[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
+
+        long k = tokens[0];
+        long n = tokens[1];
+
+        long maxLength = InvalidLength;
+        long[] lengths = new long[k];
+        for (long i = 0; i < lengths.Length; ++i)
+        {
+            long length = long.Parse(Console.ReadLine()!);
+
+            if (maxLength == InvalidLength || length > maxLength)
+            {
+                maxLength = length;
+            }
+
+            lengths[i] = length;
+        }
+
+        long bottom = 1;
+        long top = maxLength;
+        long answerLength = InvalidLength;
+        while (bottom <= top)
+        {
+            long mid = (bottom + top) / 2;
+
+            long counts = 0;
+            for (long i = 0; i < lengths.Length; ++i)
+            {
+                counts += lengths[i] / mid;
+            }
+            
+            if (counts >= n)
+            {
+                answerLength = mid;
+                bottom = mid + 1;
+            }
+            else
+            {
+                top = mid - 1;
+            }
+        }
+        Console.Write(answerLength);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6738e82edf2332fc0fe8232ecb33a3559f2e050b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7c4c9951fd3cb1d2ab942c71632d8c57287abe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
