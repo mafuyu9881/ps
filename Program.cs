a0d2196b51a03387f45f85b40ca9175d4390acfc
mafuyusame <mafuyu@tutamail.com>
2024-10-09 20:39:50 +0900

    boj 7562

--- Program.cs
+++ Program.cs
@@ -1,53 +1,91 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
+    private struct Index2D
+    {
+        public int row;
+        public int col;
+        public int moves;
+
+        public Index2D(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+            moves = 0;
+        }
+
+        public bool Equals(Index2D opponent)
+        {
+            return (row == opponent.row) && (col == opponent.col);
+        }
+    }
+
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        
-        int n = tokens[0];
-        int m = tokens[1];
+        int t = int.Parse(Console.ReadLine()!);
 
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        
-        int[] arr = new int[n];
-        for (int i = 0; i < arr.Length; ++i)
+        StringBuilder output = new();
+        for (int i = 0; i < t; ++i)
         {
-            arr[i] = tokens[i];
-        }
+            int width = int.Parse(Console.ReadLine()!);
 
-        int left = 0;
-        int right = arr.Max();
-        int answer = 0;
-        while (left <= right)
-        {
-            int mid = (left + right) >> 1;
+            int[] srcRowCol = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int[] dstRowCol = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-            int intervalCount = 1;
-            int min = arr[0];
-            int max = arr[0];
-            for (int i = 1; i < arr.Length; ++i)
+            Index2D srcIndex2D = new(srcRowCol[0], srcRowCol[1]);
+            Index2D dstIndex2D = new(dstRowCol[0], dstRowCol[1]);
+
+            bool[,] visited = new bool[width, width];
+            Queue<Index2D> visitingQueue = new();
+
+            visited[srcIndex2D.row, srcIndex2D.col] = true;
+            visitingQueue.Enqueue(srcIndex2D);
+http://boj.kr/b625c1c19e7b43d5b9cc20f20c0e0b47
+            while (visitingQueue.Count > 0)
             {
-                min = Math.Min(min, arr[i]);
-                max = Math.Max(max, arr[i]);
+                Index2D originIndex2D = visitingQueue.Dequeue();
+                int originRow = originIndex2D.row;
+                int originCol = originIndex2D.col;
 
-                if (max - min > mid)
+                if (originIndex2D.Equals(dstIndex2D))
                 {
-                    ++intervalCount;
-                    min = arr[i];
-                    max = arr[i];
+                    output.AppendLine($"{originIndex2D.moves}");
+                    break;
+                }
+
+                Index2D[] adjacentIndices2D =
+                {
+                    new(originRow - 2, originCol + 1),
+                    new(originRow - 1, originCol + 2),
+                    new(originRow + 1, originCol + 2),
+                    new(originRow + 2, originCol + 1),
+                    new(originRow + 2, originCol - 1),
+                    new(originRow + 1, originCol - 2),
+                    new(originRow - 1, originCol - 2),
+                    new(originRow - 2, originCol - 1),
+                };
+
+                for (int j = 0; j < adjacentIndices2D.Length; ++j)
+                {
+                    Index2D adjacentIndex2D = adjacentIndices2D[j];
+                    int adjacentRow = adjacentIndex2D.row;
+                    int adjacentCol = adjacentIndex2D.col;
+
+                    if (adjacentRow < 0 || adjacentRow > width - 1 ||
+                        adjacentCol < 0 || adjacentCol > width - 1)
+                        continue;
+
+                    if (visited[adjacentRow, adjacentCol])
+                        continue;
+
+                    visited[adjacentRow, adjacentCol] = true;
+
+                    adjacentIndex2D.moves = originIndex2D.moves + 1;
+                    visitingQueue.Enqueue(adjacentIndex2D);
                 }
             }
-
-            if (intervalCount > m)
-            {
-                left = mid + 1;
-            }
-            else
-            {
-                answer = mid;
-                right = mid - 1;
-            }
         }
-        Console.Write(answer);
+        Console.Write(output);
     }
 }
