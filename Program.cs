24ed0aff67f09520c180a8bb35081b70e5e8c951
mafuyusame <mafuyu@tutamail.com>
2024-10-11 16:13:10 +0900

    boj 3733

--- Program.cs
+++ Program.cs
@@ -2,89 +2,18 @@
 
 internal class Program
 {
-    private struct Index2D
-    {
-        public int row;
-        public int col;
-        public int moves;
-
-        public Index2D(int row, int col)
-        {
-            this.row = row;
-            this.col = col;
-            moves = 0;
-        }
-
-        public bool Equals(Index2D opponent)
-        {
-            return (row == opponent.row) && (col == opponent.col);
-        }
-    }
-
     private static void Main(string[] args)
     {
-        int t = int.Parse(Console.ReadLine()!);
-
         StringBuilder output = new();
-        for (int i = 0; i < t; ++i)
+        string input;
+        while ((input = Console.ReadLine()!) != null)
         {
-            int width = int.Parse(Console.ReadLine()!);
+            int[] tokens = Array.ConvertAll(input.Split(), int.Parse);
 
-            int[] srcRowCol = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int[] dstRowCol = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int n = tokens[0];
+            int s = tokens[1];
 
-            Index2D srcIndex2D = new(srcRowCol[0], srcRowCol[1]);
-            Index2D dstIndex2D = new(dstRowCol[0], dstRowCol[1]);
-
-            bool[,] visited = new bool[width, width];
-            Queue<Index2D> visitingQueue = new();
-
-            visited[srcIndex2D.row, srcIndex2D.col] = true;
-            visitingQueue.Enqueue(srcIndex2D);
-
-            while (visitingQueue.Count > 0)
-            {
-                Index2D originIndex2D = visitingQueue.Dequeue();
-                int originRow = originIndex2D.row;
-                int originCol = originIndex2D.col;
-
-                if (originIndex2D.Equals(dstIndex2D))
-                {
-                    output.AppendLine($"{originIndex2D.moves}");
-                    break;
-                }
-
-                Index2D[] adjacentIndices2D =
-                {
-                    new(originRow - 2, originCol + 1),
-                    new(originRow - 1, originCol + 2),
-                    new(originRow + 1, originCol + 2),
-                    new(originRow + 2, originCol + 1),
-                    new(originRow + 2, originCol - 1),
-                    new(originRow + 1, originCol - 2),
-                    new(originRow - 1, originCol - 2),
-                    new(originRow - 2, originCol - 1),
-                };
-
-                for (int j = 0; j < adjacentIndices2D.Length; ++j)
-                {
-                    Index2D adjacentIndex2D = adjacentIndices2D[j];
-                    int adjacentRow = adjacentIndex2D.row;
-                    int adjacentCol = adjacentIndex2D.col;
-
-                    if (adjacentRow < 0 || adjacentRow > width - 1 ||
-                        adjacentCol < 0 || adjacentCol > width - 1)
-                        continue;
-
-                    if (visited[adjacentRow, adjacentCol])
-                        continue;
-
-                    visited[adjacentRow, adjacentCol] = true;
-
-                    adjacentIndex2D.moves = originIndex2D.moves + 1;
-                    visitingQueue.Enqueue(adjacentIndex2D);
-                }
-            }
+            output.AppendLine($"{s / (n + 1)}");
         }
         Console.Write(output);
     }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab1bdf2fab6a335121f97f9ebcdc1f4915fbc9b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc517da28e17490ce19295343ab15de75e38c22")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
