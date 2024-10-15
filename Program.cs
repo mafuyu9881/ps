f13633c80ef13ec16d4d9ff31e433059e595d78a
mafuyusame <mafuyu@tutamail.com>
2024-10-15 12:45:56 +0900

    boj 12865 [배낭 DP]

--- Program.cs
+++ Program.cs
@@ -4,6 +4,35 @@
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        Console.Write(tokens[0] * tokens[1] - tokens[2] * tokens[3] * tokens[4]);
+        int n = tokens[0];
+        int k = tokens[1];
+
+        int[,] weightValues = new int[n, 2];
+        for (int i = 0; i < n; ++i)
+        {
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+            weightValues[i, 0] = tokens[0];
+            weightValues[i, 1] = tokens[1];
+        }
+
+        int[,] dp = new int[n + 1, k + 1];
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= k; ++j)
+            {
+                int selectedIndex = i - 1;
+
+                int weight = weightValues[selectedIndex, 0];
+                int value = weightValues[selectedIndex, 1];
+
+                if (j < weight)
+                    dp[i, j] = dp[i - 1, j];
+                else
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i - 1, j - weight] + value);
+            }
+        }
+
+        Console.Write(dp[n, k]);
     }
 }
