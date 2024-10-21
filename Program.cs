d1d80e721f7e3a14e89ae4515b2028e314bf2669
mafuyusame <mafuyu@tutamail.com>
2024-10-21 21:03:14 +0900

    disjointset를 '경로 압축' 까지만 구현한 모습

--- Program.cs
+++ Program.cs
@@ -2,18 +2,49 @@
 
 internal class Program
 {
+    private class DisjointSet
+    {
+        private int[] parents;
+
+        public DisjointSet(int n)
+        {
+            // MakeSet
+            parents = new int[n + 1];
+            for (int i = 0; i < parents.Length; ++i)
+            {
+                parents[i] = i;
+            }
+        }
+
+        public int Find(int x)
+        {
+            int parent = parents[x];
+
+            if (x != parent)
+            {
+                // 경로 압축
+                return parents[x] = Find(parent);
+            }
+            else
+            {
+                return x;
+            }
+        }
+
+        public void Union(int a, int b)
+        {
+            parents[Find(a)] = Find(b);
+        }
+    }
+
     private static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
         int n = tokens[0];
         int m = tokens[1];
-
-        int[] sets = new int[n + 1];
-        for (int i = 0; i < sets.Length; ++i)
-        {
-            sets[i] = i;
-        }
+        
+        DisjointSet disjointSet = new(n);
 
         StringBuilder output = new();
         for (int i = 0; i < m; ++i)
@@ -26,17 +57,11 @@ internal class Program
 
             if (command == 0)
             {
-                int aID = sets[a];
-                int bID = sets[b];
-
-                int unionID = Math.Min(aID, bID);
-
-                sets[a] = unionID;
-                sets[b] = unionID;
+                disjointSet.Union(a, b);
             }
-            else // (command == 1)
+            else
             {
-                output.AppendLine((sets[a] == sets[b]) ? "YES" : "NO");
+                output.AppendLine((disjointSet.Find(a) == disjointSet.Find(b)) ? "YES" : "NO");
             }
         }
         Console.Write(output);
