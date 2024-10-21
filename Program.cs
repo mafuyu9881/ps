270762492405565a6d46979a2834899d956ec2c4
mafuyusame <mafuyu@tutamail.com>
2024-10-21 09:10:05 +0900

    BOJ 1717을 독자적인 방법으로 풀어보려다 중단
    반례:
    
    sets가 0 1 2 3 3 3 일 때 1번과 3번에 합집합 연산을 수행하면
    
    0 1 2 1 3 3이 되어 집합이 분리된다.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,44 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        Console.Write(4000 * n);
+        int n = tokens[0];
+        int m = tokens[1];
+
+        int[] sets = new int[n + 1];
+        for (int i = 0; i < sets.Length; ++i)
+        {
+            sets[i] = i;
+        }
+
+        StringBuilder output = new();
+        for (int i = 0; i < m; ++i)
+        {
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+            int command = tokens[0];
+            int a = tokens[1];
+            int b = tokens[2];
+
+            if (command == 0)
+            {
+                int aID = sets[a];
+                int bID = sets[b];
+
+                int unionID = Math.Min(aID, bID);
+
+                sets[a] = unionID;
+                sets[b] = unionID;
+            }
+            else // (command == 1)
+            {
+                output.AppendLine((sets[a] == sets[b]) ? "YES" : "NO");
+            }
+        }
+        Console.Write(output);
     }
 }
