59ce773c4a63d8664711e1567834654225dbfbea
mafuyusame <mafuyu@tutamail.com>
2024-10-24 12:12:34 +0900

    boj 12865 (dp)의 코드를 조금 더 간결하게 작성

--- Program.cs
+++ Program.cs
@@ -2,66 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        const int Width = 3;
+        int n = tokens[0];
+        int k = tokens[1];
 
-        int dpWidth = 1 + Width + 1;
-        int dpHeight = n + 1;
-
-        const int Infinity = 10;
-
-        int[,] maxDP = new int[dpHeight, dpWidth];
-        int[,] minDP = new int[dpHeight, dpWidth];
-        for (int row = 1; row < dpHeight; ++row)
+        int[,] dp = new int[n + 1, k + 1];
+        for (int i = 1; i <= n; ++i)
         {
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-            int prevRow = row - 1;
-
-            int left = tokens[0];
-            int center = tokens[1];
-            int right = tokens[2];
-
-            maxDP[row, 1] = left + Math.Max(maxDP[prevRow, 1], maxDP[prevRow, 2]);
-            maxDP[row, 2] = center + Math.Max(maxDP[prevRow, 1], Math.Max(maxDP[prevRow, 2], maxDP[prevRow, 3]));
-            maxDP[row, 3] = right + Math.Max(maxDP[prevRow, 2], maxDP[prevRow, 3]);
-
-            minDP[row, 1] = left + Math.Min(minDP[prevRow, 1], minDP[prevRow, 2]);
-            minDP[row, 2] = center + Math.Min(minDP[prevRow, 1], Math.Min(minDP[prevRow, 2], minDP[prevRow, 3]));
-            minDP[row, 3] = right + Math.Min(minDP[prevRow, 2], minDP[prevRow, 3]);
-            minDP[row, 0] = Infinity;
-            minDP[row, dpWidth - 1] = Infinity;
-
-            // NOTE: minDP의 각 원소는 Infinity보다 큰 값이 될 수 있습니다.
-            // 때문에 아래의 코드는 통과할 수 없습니다.
-            //for (int col = 1; col <= Width; ++col)
-            //{
-            //    int token = tokens[col - 1];
-            //
-            //    int leftUpMaxDP = maxDP[prevRow, col - 1];
-            //    int centerUpMaxDP = maxDP[prevRow, col];
-            //    int rightUpMaxDP = maxDP[prevRow, col + 1];
-            //
-            //    maxDP[row, col] = token + Math.Max(leftUpMaxDP, Math.Max(centerUpMaxDP, rightUpMaxDP));
-            //
-            //    int leftUpMinDP = minDP[prevRow, col - 1];
-            //    int centerUpMinDP = minDP[prevRow, col];
-            //    int rightUpMinDP = minDP[prevRow, col + 1];
-            //
-            //    minDP[row, col] = token + Math.Min(leftUpMinDP, Math.Min(centerUpMinDP, rightUpMinDP));
-            //    minDP[row, 0] = Infinity;
-            //    minDP[row, dpWidth - 1] = Infinity;
-            //}
+            int w = tokens[0];
+            int v = tokens[1];
+            
+            for (int j = 1; j <= k; ++j)
+            {
+                if (j < w)
+                {
+                    dp[i, j] = dp[i - 1, j];
+                }
+                else
+                {
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i - 1, j - w] + v);
+                }
+            }
         }
-
-        int max = maxDP[n, 1];
-        int min = minDP[n, 1];
-        for (int i = 2; i <= Width; ++i)
-        {
-            max = Math.Max(max, maxDP[n, i]);
-            min = Math.Min(min, minDP[n, i]);
-        }
-        Console.Write($"{max} {min}");
+        Console.Write(dp[n, k]);
     }
 }
