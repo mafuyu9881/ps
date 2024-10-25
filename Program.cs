7f5a1b2d8e5cb28313184d66e5d803464d69904b
mafuyusame <mafuyu@tutamail.com>
2024-10-25 20:34:20 +0900

    boj 1240 (bfs)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿// 1 <= (최댓값의 최솟값) <= 30000
-// 0 <= log (최댓값의 최솟값) <= 14.xxx (단, log의 밑은 2)
-
-using System.Text;
+﻿using System.Text;
 
 internal class Program
 {
@@ -9,136 +6,103 @@ internal class Program
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
         
-        int n = tokens[0];
-        int m = tokens[1];
-
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-        int[] beadValues = new int[n];
-        for (int i = 0; i < n; ++i)
+        int nodeCount = tokens[0];
+        int distanceComputingCount = tokens[1];
+        
+        Graph graph = new(nodeCount + 1);
+        for (int i = 0; i < nodeCount - 1; ++i)
         {
-            beadValues[i] = tokens[i];
-        }
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        int minOfMaxGroupBeadValue = 0;
-        int left = 1;
-        int right = 30000;
-        LinkedList<int>? groupedBeadCountList = null;
-        LinkedListNode<int>? maxGroupedBeadCountNode = null;
-        while (left <= right)
-        {
-            int mid = (left + right) / 2;
-
-            LinkedList<int>? tempGroupedBeadCountList = null;
-            LinkedListNode<int>? tempMaxGroupedBeadCountNode = null;
-            if (Validation(out tempGroupedBeadCountList,
-                           out tempMaxGroupedBeadCountNode,
-                           beadValues,
-                           mid,
-                           m))
-            {
-                right = mid - 1;
-                minOfMaxGroupBeadValue = mid;
-                groupedBeadCountList = tempGroupedBeadCountList;
-                maxGroupedBeadCountNode = tempMaxGroupedBeadCountNode;
-            }
-            else
-            {
-                left = mid + 1;
-            }
+            graph.Connect(tokens[0], tokens[1], tokens[2]);
         }
 
         StringBuilder output = new();
-        
-        output.AppendLine($"{minOfMaxGroupBeadValue}");
-
-        while (groupedBeadCountList!.Count != m)
+        for (int i = 0; i < distanceComputingCount; ++i)
         {
-            LinkedListNode<int>? separatingCountNode = null;
-            while (separatingCountNode == null)
-            {
-                for (var node = groupedBeadCountList!.First; node != null; node = node.Next)
-                {
-                    if (node == maxGroupedBeadCountNode)
-                        continue;
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-                    if (node.Value < 2)
-                        continue;
-
-                    separatingCountNode = node;
-                    break;
-                }
-            }
-
-            int separatedCount = separatingCountNode!.Value / 2;
-            separatingCountNode.ValueRef -= separatedCount;
-            groupedBeadCountList.AddAfter(separatingCountNode, separatedCount);
+            output.AppendLine($"{graph.ComputeDistance(tokens[0], tokens[1])}");
         }
-
-        for (var node = groupedBeadCountList!.First; node != null; node = node.Next)
-        {
-            output.Append($"{node.Value} ");
-        }
-
         Console.Write(output);
     }
-
-    private static bool Validation(out LinkedList<int>? outGroupedBeadCountList,
-                            out LinkedListNode<int>? outMaxGroupedBeadCountNode,
-                            int[] beadValues,
-                            int mid,
-                            int m)
+    
+    private struct DistanceAccumulator
     {
-        outGroupedBeadCountList = new();
-        outMaxGroupedBeadCountNode = null;
+        public int dstNode;
+        public int accumuldatedDistance;
 
-        LinkedListNode<int>? groupedBeadCountNode = null;
-        int groupedBeadValue = 0;
-        for (int i = 0; i < beadValues.Length; ++i)
+        public DistanceAccumulator(int dstNode, int accumuldatedDistance)
         {
-            int beadValue = beadValues[i];
+            this.dstNode = dstNode;
+            this.accumuldatedDistance = accumuldatedDistance;
+        }
+    }
+    
+    private class Graph
+    {
+        private LinkedList<int>[]? _adjList = null;
+        private int[,]? _costMap = null;
 
-            // 1. (i == 0)
-            // 2. (groupedBeadCountNode == null)
-            // 3. (outGroupedBeadCountList.Count == 0)
-            // 1, 2, 3 모두 첫 순회임을 나타내는 조건입니다.
-            // 컴파일 경고를 피하기 위해 2번의 조건문으로 선택했습니다.
-            if (groupedBeadCountNode == null ||
-                groupedBeadValue + beadValue > mid)
+        public Graph(int nodeCount)
+        {
+            _adjList = new LinkedList<int>[nodeCount];
+            for (int i = 0; i < _adjList.Length; ++i)
             {
-                groupedBeadCountNode = outGroupedBeadCountList.AddLast(0);
-                groupedBeadValue = 0;
+                _adjList[i] = new();
             }
 
-            ++groupedBeadCountNode.ValueRef;
-            groupedBeadValue += beadValue;
-
-            // 구슬 묶음의 총합이 요구된 매개변수 값보다 크다면,
-            // 이 매개변수는 실현될 수 없는 값입니다.
-            if (groupedBeadValue > mid)
-                return false;
-
-            // 구슬 묶음의 총합이 요구된 매개변수와 일치하는 묶음을 기억해둡니다.
-            // 이후의 정답 출력 단계에서 각 묶음을 구성하는 구슬의 개수를 출력할 때
-            // 전체 묶음의 수가 부족할 경우
-            // 일부 묶음을 분할하여 요구된 묶음의 개수 m을 채워야하는데,
-            // 이 때 이 묶음을 분할해선 안되기 때문입니다.
-
-            // 물론 outGroupedBeadCountList에 이 조건을 만족하는 묶음이 항상 있을 수는 없습니다.
-            // 하지만 매개변수 탐색의 과정 상, 반복 과정에서 필연적으로 그런 mid 값이 나타나기 때문에,
-            // 이렇게 작성할 수 있습니다.
-
-            if (groupedBeadValue == mid)
-            {
-                if ((outMaxGroupedBeadCountNode == null) ||
-                    (outMaxGroupedBeadCountNode != null &&
-                    groupedBeadCountNode.Value < outMaxGroupedBeadCountNode.Value))
-                {
-                    outMaxGroupedBeadCountNode = groupedBeadCountNode;
-                }
-            }
+            _costMap = new int[nodeCount, nodeCount];
         }
 
-        return outGroupedBeadCountList.Count <= m;
+        public void Connect(int node0, int node1, int cost)
+        {
+            if (_adjList == null || _costMap == null)
+                return;
+
+            _adjList[node0].AddLast(node1);
+            _adjList[node1].AddLast(node0);
+
+            _costMap[node0, node1] = cost;
+            _costMap[node1, node0] = cost;
+        }
+        
+        public int ComputeDistance(int node0, int node1)
+        {
+            if (_adjList == null || _costMap == null)
+                return -1;
+
+            int adjListLength = _adjList.Length;
+
+            bool[] visitedTable = new bool[adjListLength];
+
+            Queue<DistanceAccumulator> visitingQueue = new();
+            visitedTable[node0] = true;
+            visitingQueue.Enqueue(new(node0, 0));
+            while (visitingQueue.Count > 0)
+            {
+                DistanceAccumulator dstDA = visitingQueue.Dequeue();
+                int dstNode = dstDA.dstNode;
+                int dstAccumulatedDistance = dstDA.accumuldatedDistance;
+
+                var adjacencies = _adjList[dstNode];
+                for (var listNode = adjacencies.First; listNode != null; listNode = listNode.Next)
+                {
+                    int dstAdjNode = listNode.Value;
+
+                    if (visitedTable[dstAdjNode])
+                        continue;
+
+                    int newAccumulatedDistance = dstAccumulatedDistance + _costMap[dstNode, dstAdjNode];
+                    if (dstAdjNode == node1)
+                        return newAccumulatedDistance;
+
+                    visitedTable[dstAdjNode] = true;
+                    visitingQueue.Enqueue(new(dstAdjNode, newAccumulatedDistance));
+                }
+            }
+
+            return -1;
+        }
     }
 }
