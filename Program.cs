987e0cf236196e5c9c4ee0b8e80cd6f5f2cca442
mafuyusame <mafuyu@tutamail.com>
2024-10-28 12:56:06 +0900

    boj 9086 (문자열)

--- Program.cs
+++ Program.cs
@@ -2,128 +2,17 @@
 
 internal class Program
 {
-    const int InvalidVertex = -1;
-
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
-
-        Graph graph = new(n + 1);
-        for (int i = 0; i < n; ++i)
-        {
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            
-            graph.Connect(tokens[0], tokens[1]);
-        }
-        
-        graph.ComputeDistances(out int[] distances);
+        int t = int.Parse(Console.ReadLine()!);
 
         StringBuilder output = new();
-        for (int i = 1; i < distances.Length; ++i)
+        for (int i = 0; i < t; ++i)
         {
-            output.Append($"{distances[i]} ");
+            string s = Console.ReadLine()!;
+
+            output.AppendLine($"{s[0]}{s[s.Length - 1]}");
         }
-        Console.Write($"{output}");
-    }
-
-    private class Graph
-    {
-        private int _vertexCount;
-        private LinkedList<int>[]? _adjList;
-        
-        public Graph(int n)
-        {
-            _vertexCount = n;
-
-            _adjList = new LinkedList<int>[_vertexCount];
-            for (int i = 0; i < _vertexCount; ++i)
-            {
-                _adjList[i] = new();
-            }
-        }
-
-        public void Connect(int a, int b)
-        {
-            if (_adjList == null)
-                return;
-
-            _adjList[a].AddLast(b);
-            _adjList[b].AddLast(a);
-        }
-        
-        public void ComputeDistances(out int[] distances)
-        {
-            distances = new int[_vertexCount];
-
-            if (_adjList == null)
-                return;
-
-            const int Infinity = -1;
-
-            bool[] visited = new bool[_vertexCount];
-            bool[] cyclic = new bool[_vertexCount];
-            DetectCyclicVertices(visited, cyclic, 1, InvalidVertex);
-
-            Queue<int> visitingQueue = new();
-
-            for (int i = 0; i < distances.Length; ++i)
-            {
-                int distance = Infinity;
-
-                if (cyclic[i])
-                {
-                    distance = 0;
-                    visitingQueue.Enqueue(i);
-                }
-
-                distances[i] = distance;
-            }
-
-            while (visitingQueue.Count > 0)
-            {
-                int v = visitingQueue.Dequeue();
-
-                LinkedList<int> adjacencies = _adjList[v];
-                for (LinkedListNode<int>? node = adjacencies.First; node != null; node = node.Next)
-                {
-                    int adjV = node.Value;
-
-                    if (distances[adjV] != Infinity)
-                        continue;
-
-                    distances[adjV] = distances[v] + 1;
-                    visitingQueue.Enqueue(adjV);
-                }
-            }
-        }
-        
-        private int DetectCyclicVertices(bool[] visited, bool[] cyclic, int currV, int prevV)
-        {
-            if (visited[currV])
-                return currV;
-
-            visited[currV] = true;
-            
-            LinkedList<int> adjacencies = _adjList![currV];
-            for (LinkedListNode<int>? node = adjacencies.First; node != null; node = node.Next)
-            {
-                int nextV = node.Value;
-
-                // 현재 컨텍스트를 발생시킨 직전의 정점인지 검사하는 것이 조건의 내용입니다.
-                // 때문에 visited[nextV]로 검사해선 안됩니다.
-                // 만약 위 조건으로 검사할 경우, 사이클을 감지할 수 없습니다.
-                if (nextV == prevV)
-                    continue;
-
-                int futureV = DetectCyclicVertices(visited, cyclic, nextV, currV);
-                if (futureV == InvalidVertex)
-                    continue;
-
-                cyclic[currV] = true;
-                return (futureV == currV) ? InvalidVertex : futureV;
-            }
-
-            return InvalidVertex;
-        }
+        Console.Write(output);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f1f504b004f56df95bc89a42ac51a90e165ea06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+967614aef89e8a9a63942ea5510740df4eca03ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
