baed2f3eb5f8fcedbb804ba640e97b4d3570be3d
mafuyusame <mafuyu@tutamail.com>
2024-11-01 22:37:06 +0900

    boj 25379 풀이 도중 커밋

--- Program.cs
+++ Program.cs
@@ -2,9 +2,35 @@
 {
     private static void Main(string[] args)
     {
-        int a = int.Parse(Console.ReadLine()!);
-        int b = int.Parse(Console.ReadLine()!);
-        
-        Console.Write(a * b);
+        int n = int.Parse(Console.ReadLine()!);
+        int halfN = n / 2;
+
+        int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+        int backIndex = arr.Length - 1;
+
+        int output = 0;
+        for (int frontIndex = 0; (frontIndex < halfN) && (backIndex > halfN); ++frontIndex)
+        {
+            int frontElement = arr[frontIndex];
+
+            if (frontElement % 2 == 0)
+                continue;
+
+            while (backIndex > halfN)
+            {
+                int backElement = arr[backIndex];
+
+                if (backElement % 2 != 0)
+                    continue;
+
+                arr[frontIndex] = backElement;
+                arr[backIndex] = frontElement;
+                ++output;
+
+                --backIndex;
+            }
+        }
+        Console.Write(output);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bedbfa64b5aa7bb3d6454afcf5c6ae0b9ec3e254")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef6f978ff92e5e78e2664a250e0b43a15935660")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
