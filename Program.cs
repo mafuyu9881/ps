55c7a6ec0d64ccad70b2d20cbbe8df3ecfb58f2e
mafuyusame <mafuyu@tutamail.com>
2024-11-03 20:38:50 +0900

    boj 24568

--- Program.cs
+++ Program.cs
@@ -1,17 +1,10 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-        int t = tokens[0];
-        int s = tokens[1];
-
-        StringBuilder output = new();
-        if (s == 1 || t < 12 || t > 16) output.Append("280");
-        else output.Append("320");
-        Console.Write(output);
+        int cupcakeCount = 0;
+        cupcakeCount += int.Parse(Console.ReadLine()!) * 8;
+        cupcakeCount += int.Parse(Console.ReadLine()!) * 3;
+        Console.Write(cupcakeCount - 28);
     }
 }
