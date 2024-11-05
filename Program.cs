e8565c735baa72b35e1b5ccd391a476fefc97944
mafuyusame <mafuyu@tutamail.com>
2024-11-05 21:52:50 +0900

    boj 16099

--- Program.cs
+++ Program.cs
@@ -1,39 +1,33 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine()!);
 
-        int[] sequence = new int[n];
-        int[] lisLengths = new int[n];
-        int[] ldsLengths = new int[n];
+        StringBuilder output = new();
         for (int i = 0; i < n; ++i)
         {
-            sequence[i] = int.Parse(Console.ReadLine()!);
-            lisLengths[i] = 1;
-            ldsLengths[i] = 1;
-        }
+            long[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
 
-        int output = 0;
-        for (int i = 1; i < n; ++i)
-        {
-            for (int j = 0; j < i; ++j)
+            long tArea = tokens[0] * tokens[1];
+            long eArea = tokens[2] * tokens[3];
+
+            string winner;
+            if (tArea > eArea)
             {
-                int iElement = sequence[i];
-                int jElement = sequence[j];
-                
-                if (jElement < iElement)
-                {
-                    lisLengths[i] = Math.Max(lisLengths[j] + 1, lisLengths[i]);
-                }
-
-                if (jElement > iElement)
-                {
-                    ldsLengths[i] = Math.Max(ldsLengths[j] + 1, ldsLengths[j]);
-                }
-
-                output = Math.Max(output, lisLengths[i] + ldsLengths[i] - 1);
+                winner = "TelecomParisTech";
             }
+            else if (tArea < eArea)
+            {
+                winner = "Eurecom";
+            }
+            else
+            {
+                winner = "Tie";
+            }
+            output.AppendLine(winner);
         }
         Console.Write(output);
     }
