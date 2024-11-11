8b9447f277cdcfd19ab22450943a307c0529fcb4
mafuyusame <mafuyu@tutamail.com>
2024-11-11 22:57:21 +0900

    boj 11973 (매개 변수 탐색)

--- Program.cs
+++ Program.cs
@@ -2,47 +2,43 @@
 {
     private static void Main(string[] args)
     {
+        const int InvalidLandingPosition = -1;
+        const int RangeStart = 0;
+        const int RangeEnd = 1000000000;
+
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
         int n = tokens[0];
-        int m = tokens[1];
+        int k = tokens[1];
 
-        int[] requirements = new int[n];
+        int[] hayBalePositions = new int[n];
         for (int i = 0; i < n; ++i)
         {
-            requirements[i] = int.Parse(Console.ReadLine()!);
+            hayBalePositions[i] = int.Parse(Console.ReadLine()!);
         }
+        Array.Sort(hayBalePositions);
 
         int low = 1 - 1;
-        int high = requirements.Sum() + 1;
-        while (low + 1 < high)
+        int high = RangeEnd / 2 + 1;
+        while (low < high - 1)
         {
-            int mid = (low + high) / 2; // withdrawal amount;
+            int mid = (low + high) / 2;
 
-            int holdingMoney = 0;
-            int withdrawalCount = 0;
-            for (int i = 0; i < requirements.Length; ++i)
+            int shoots = 0;
+            int lastLandingPosition = InvalidLandingPosition;
+            for (int i = 0; i < hayBalePositions.Length; ++i)
             {
-                int requirement = requirements[i];
+                int hayBalePosition = hayBalePositions[i];
 
-                if (holdingMoney < requirement)
+                if ((lastLandingPosition == InvalidLandingPosition) ||
+                    (hayBalePosition > lastLandingPosition + mid))
                 {
-                    if (mid < requirement)
-                    {
-                        withdrawalCount = m + 1; // make an error intentionally
-                        break;
-                    }
-
-                    holdingMoney = mid - requirement;
-                    ++withdrawalCount;
-                }
-                else
-                {
-                    holdingMoney -= requirement;
+                    lastLandingPosition = Math.Clamp(hayBalePosition + mid, RangeStart + mid, RangeEnd - mid);
+                    ++shoots;
                 }
             }
 
-            if (withdrawalCount > m)
+            if (shoots > k)
             {
                 low = mid;
             }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f6a09567f000ff09a3399f766a1f4ba52e8c4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788c16926796880dd15a15c968077789c8b08e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
