8ab9a57079f54be347a215c32a923ef0d818d9d2
mafuyusame <mafuyu@tutamail.com>
2024-11-12 19:42:57 +0900

    boj 21300

--- Program.cs
+++ Program.cs
@@ -2,38 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int[] emptyContainerCounts = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        int s = tokens[0];
-        int c = tokens[1];
-
-        long[] springOnions = new long[s];
-        for (int i = 0; i < s; ++i)
-        {
-            springOnions[i] = int.Parse(Console.ReadLine()!);
-        }
-
-        long low = 1 - 1;
-        long high = springOnions.Max() + 1;
-        while (low < high - 1)
-        {
-            long mid = (low + high) / 2;
-
-            long distributed = 0;
-            for (int i = 0; i < s; ++i)
-            {
-                distributed += springOnions[i] / mid;
-            }
-
-            if (distributed < c)
-            {
-                high = mid;
-            }
-            else
-            {
-                low = mid;
-            }
-        }
-        Console.Write(springOnions.Sum() - (low * c));
+        Console.Write(emptyContainerCounts.Sum() * 5);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95fb13f8430bd36690c09cd5b0650ffaca5be28f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82ef91d1f78ef01236e9bdb2814b1e40ce63db2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
