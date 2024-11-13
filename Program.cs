c53fbc4b62e5584175e6522d1e2020c27188e53d
mafuyusame <mafuyu@tutamail.com>
2024-11-13 16:14:29 +0900

    boj 15810

--- Program.cs
+++ Program.cs
@@ -2,8 +2,34 @@
 {
     private static void Main(string[] args)
     {
-        int[] emptyContainerCounts = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        Console.Write(emptyContainerCounts.Sum() * 5);
+        int n = tokens[0];
+        int m = tokens[1];
+        
+        int[] timeTakens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+        long low = 1 - 1;
+        long high = (long)timeTakens.Max() * m + 1;
+        while (low < high - 1)
+        {
+            long mid = (low + high) / 2;
+
+            long remainM = m;
+            for (int i = 0; i < n; ++i)
+            {
+                remainM -= (int)(mid / timeTakens[i]);
+            }
+
+            if (remainM > 0)
+            {
+                low = mid;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+        Console.Write(high);
     }
 }
