c47698869b202d179a341849efc487102dc41424
mafuyusame <mafuyu@tutamail.com>
2024-11-13 18:24:37 +0900

    boj 16401 (매개변수 탐색)

--- Program.cs
+++ Program.cs
@@ -4,32 +4,32 @@
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        int n = tokens[0];
-        int m = tokens[1];
-        
-        int[] timeTakens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int m = tokens[0];
+        int n = tokens[1];
 
-        long low = 1 - 1;
-        long high = (long)timeTakens.Max() * m + 1;
+        int[] snackLengths = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+        int low = 1 - 1;
+        int high = snackLengths.Max() + 1;
         while (low < high - 1)
         {
-            long mid = (low + high) / 2;
+            int mid = (low + high) / 2;
 
-            long remainM = m;
+            int snackCount = 0;
             for (int i = 0; i < n; ++i)
             {
-                remainM -= (int)(mid / timeTakens[i]);
+                snackCount += snackLengths[i] / mid;
             }
 
-            if (remainM > 0)
-            {
-                low = mid;
-            }
-            else
+            if (snackCount < m)
             {
                 high = mid;
             }
+            else
+            {
+                low = mid;
+            }
         }
-        Console.Write(high);
+        Console.Write(low);
     }
 }
