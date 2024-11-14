5e6152fb0d2c96f61b5847444a104f297a806ea6
mafuyusame <mafuyu@tutamail.com>
2024-11-14 20:19:52 +0900

    boj 14575 (매개변수 탐색)

--- Program.cs
+++ Program.cs
@@ -2,34 +2,80 @@
 {
     private static void Main(string[] args)
     {
-        long[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0];
+        int t = tokens[1];
 
-        long n = tokens[0];
-        long k = tokens[1];
+        LR[] lrs = new LR[n];
+        for (int i = 0; i < n; ++i)
+        {
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);;
+            lrs[i] = new(tokens[0], tokens[1]);
+        }
 
-        long[] scores = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
-
-        long low = 0 - 1;
-        long high = scores.Max() + 1;
+        const int InvalidMinimumS = int.MaxValue;
+        int minimumS = InvalidMinimumS;
+        int low = 1 - 1;
+        int high = t + 1;
         while (low < high - 1)
         {
-            long mid = (low + high) / 2;
+            int mid = (low + high) / 2;
 
-            long remainK = k;
-            for (int i = 0; i < n; ++i)
-            {
-                remainK -= Math.Max(0, scores[i] - mid);
-            }
-
-            if (remainK < 0)
-            {
-                low = mid;
-            }
-            else
+            if (Check(ref minimumS, lrs, t, mid) == LowerUpper.Lower)
             {
                 high = mid;
             }
+            else
+            {
+                low = mid;
+            }
         }
-        Console.Write(high);
+        Console.Write((minimumS != InvalidMinimumS) ? minimumS : -1);
+    }
+
+    private enum LowerUpper
+    {
+        Lower,
+        Upper,
+    }
+    private static LowerUpper Check(ref int minimumS, LR[] lrs, int t, int s)
+    {
+        int remainT = t;
+        int clearance = 0;
+        for (int i = 0; i < lrs.Length; ++i)
+        {
+            int l = lrs[i].L;
+            int r = lrs[i].R;
+
+            if (s < l)
+                return LowerUpper.Upper;
+            
+            clearance += Math.Min(s, r) - l;
+
+            remainT -= l;
+        }
+
+        if (remainT < 0)
+            return LowerUpper.Lower;
+
+        if (remainT > clearance)
+            return LowerUpper.Upper;
+
+        minimumS = Math.Min(minimumS, s);
+
+        return LowerUpper.Lower;
+    }
+
+    private struct LR
+    {
+        private int _l;
+        private int _r;
+        public int L => _l;
+        public int R => _r;
+        public LR(int l, int r)
+        {
+            _l = l;
+            _r = r;
+        }
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35faa83cc7f2113133982dbed5eceb79ac0ceb63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304f312b9dfe563baac31285c731009a7be5840f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
