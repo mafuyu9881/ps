224175f358812138ca2bb97d18ea0952dc78fb82
mafuyusame <mafuyu@tutamail.com>
2024-11-15 12:19:42 +0900

    boj 14452 (매개변수 탐색)

--- Program.cs
+++ Program.cs
@@ -4,34 +4,82 @@
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
         int n = tokens[0];
-        int k = tokens[1];
-        int[] levels = new int[n];
+        int t = tokens[1];
+        int[] durations = new int[n];
         for (int i = 0; i < n; ++i)
         {
-            levels[i] = int.Parse(Console.ReadLine()!);
+            durations[i] = int.Parse(Console.ReadLine()!);
         }
 
-        int low = 1 - 1;
-        int high = 2000000000 + 1;
+        const int FailedRemainT = -1;
+
+        int low = 0 - 1;
+        int high = n + 1;
         while (low < high - 1)
         {
-            int mid = (int)((low + (long)high) / 2);
+            int mid = (low + high) / 2;
 
-            int remainK = k;
-            for (int i = 0; i < n; ++i)
+            int remainT = t;
+            if (mid > 0)
             {
-                remainK = Math.Max(-1, remainK - Math.Max(0, mid - levels[i]));
-            }
+                LinkedList<int> durationsInProgress = new(durations);
+                while (durationsInProgress.Count > 0)
+                {
+                    LinkedListNode<int>?[] nodes = new LinkedListNode<int>[mid];
+                    nodes[0] = durationsInProgress.First;
+                    int minDuration = nodes[0]!.Value;
+                    for (int i = 1; i < mid; ++i)
+                    {
+                        var prevNode = nodes[i - 1];
+                        if (prevNode == null)
+                            break;
 
-            if (remainK < 0)
-            {
-                high = mid;
+                        var currNode = prevNode.Next;
+                        if (currNode == null) // It can't be, but to prevent a compile warning.
+                            break;
+
+                        int duration = currNode.Value;
+                        if (duration < minDuration)
+                        {
+                            minDuration = duration;
+                        }
+
+                        nodes[i] = currNode;
+                    }
+
+                    remainT = Math.Max(FailedRemainT, remainT - minDuration);
+                    if (remainT == FailedRemainT)
+                        break;
+
+                    for (int i = 0; i < mid; ++i)
+                    {
+                        var node = nodes[i];
+                        if (node == null) // It can be when the remains are less than mid.
+                            break;
+
+                        node.ValueRef -= minDuration;
+
+                        if (node.Value > 0)
+                            continue;
+
+                        durationsInProgress.Remove(node);
+                    }
+                }
             }
             else
             {
+                remainT = FailedRemainT;
+            }
+
+            if (remainT == FailedRemainT)
+            {
                 low = mid;
             }
+            else
+            {
+                high = mid;
+            }
         }
-        Console.Write(low);
+        Console.Write(high);
     }
 }
