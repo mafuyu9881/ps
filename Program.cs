de4c9bb8205319f4e5fff7c2a32b8f484853ab60
mafuyusame <mafuyu@tutamail.com>
2024-11-16 11:13:42 +0900

    boj 17179 2n% 실패 코드
    부분 문제의 합이 전체 문제의 해결로 이어지지 않는 문제인듯

--- Program.cs
+++ Program.cs
@@ -1,43 +1,90 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
         int n = tokens[0];
         int m = tokens[1];
-        int c = tokens[2];
-        int[] arrivalTimes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        Array.Sort(arrivalTimes);
-        
-        int low = 0 - 1;
-        int high = 1000000000 + 1;
-        while (low < high - 1)
+        int l = tokens[2];
+
+        int[] cutPoints = new int[m + 1];
+        for (int i = 0; i < m; ++i)
         {
-            int mid = (low + high) / 2;
+            cutPoints[i] = int.Parse(Console.ReadLine()!);
+        }
+        cutPoints[m] = l;
 
-            int remainM = m - 1;
-            int waitingCowIndex = 0; // cows corresponding to indices prior to this value have already boarded the bus
-            for (int i = 0; i < n; ++i)
+        StringBuilder output = new();
+        for (int i = 0; i < n; ++i)
+        {
+            int cutCount = int.Parse(Console.ReadLine()!);
+            int pieceCount = cutCount + 1;
+
+            // 1 < L ≤ 4,000,000
+            int low = 1 - 1;
+            int high = 4000000 + 1;
+            while (low < high - 1)
             {
-                int waitingCowCount = i - waitingCowIndex + 1;
-                int waitingTime = arrivalTimes[i] - arrivalTimes[waitingCowIndex];
+                int mid = (low + high) / 2;
 
-                if (waitingCowCount > c || waitingTime > mid)
+                LinkedList<int> slicedLengthLL = new();
+                PriorityQueue<LinkedListNode<int>, int> slicedLengthPQ = new();
+                int prevCutPoint = 0;
+                for (int j = 0; j < cutPoints.Length; ++j)
                 {
-                    waitingCowIndex = i;
-                    --remainM;
+                    int currCutPoint = cutPoints[j];
+
+                    int slicedLength = currCutPoint - prevCutPoint;
+
+                    var node = slicedLengthLL.AddLast(slicedLength);
+                    slicedLengthPQ.Enqueue(node, slicedLength);
+
+                    prevCutPoint = currCutPoint;
+                }
+
+                while (slicedLengthLL.Count > pieceCount)
+                {
+                    var node = slicedLengthPQ.Dequeue();
+                    if (node.List == null)
+                        continue;
+
+                    var prevNode = node.Previous;
+                    var nextNode = node.Next;
+                    LinkedListNode<int>? mergeTargetNode = prevNode;
+                    if ((mergeTargetNode == null) ||
+                        (nextNode != null && nextNode.Value < mergeTargetNode.Value))
+                    {
+                        mergeTargetNode = nextNode;
+                    }
+
+                    // there is no chance both Next and Previous are null
+                    int mergedLength = node.Value + mergeTargetNode!.Value;
+                    var mergedNode = slicedLengthLL.AddBefore(node, mergedLength);
+                    slicedLengthPQ.Enqueue(mergedNode, mergedLength);
+
+                    slicedLengthLL.Remove(node);
+                    slicedLengthLL.Remove(mergeTargetNode);
+                }
+
+                // get rid of invalid nodes
+                while (slicedLengthPQ.Peek().List == null)
+                {
+                    slicedLengthPQ.Dequeue();
+                }
+
+                if (mid < slicedLengthPQ.Peek().Value)
+                {
+                    low = mid;
+                }
+                else
+                {
+                    high = mid;
                 }
             }
-
-            if (remainM < 0)
-            {
-                low = mid;
-            }
-            else
-            {
-                high = mid;
-            }
+            output.AppendLine($"{high}");
         }
-        Console.Write(high);
+        Console.Write(output);
     }
 }
