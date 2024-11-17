51ae7cba130bca47ac9239e450a0cf494ed70748
mafuyusame <mafuyu@tutamail.com>
2024-11-17 11:07:31 +0900

    boj 25418 (BFS)

--- Program.cs
+++ Program.cs
@@ -1,59 +1,33 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0];
-        int m = tokens[1];
+        int a = tokens[0];
+        int k = tokens[1];
 
-        LinkedList<int>[] adjList = new LinkedList<int>[n];
-        for (int i = 0; i < n; ++i) // max tc = n = 250 (1 <= n <= 250)
-        {
-            adjList[i] = new();
-        }
-        for (int i = 0; i < m; ++i) // max tc = 250 * (250 - 1) / 2 = 31,125 (1 <= m <= n*(n-1)/2)
-        {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-            int c0 = tokens[0] - 1;
-            int c1 = tokens[1] - 1;
-
-            adjList[c0].AddLast(c1);
-            adjList[c1].AddLast(c0);
-        }
-
-        bool[] visitedTable = new bool[n];
+        int[] depths = new int[k + 1];
         Queue<int> visitingQueue = new();
-        int v0 = 0;
-        visitingQueue.Enqueue(v0);
-        visitedTable[v0] = true;
+        visitingQueue.Enqueue(a);
         while (visitingQueue.Count > 0)
         {
-            int v = visitingQueue.Dequeue();
+            int ai = visitingQueue.Dequeue(); // which means the ith element in integer A's sequence
 
-            var adjs = adjList[v];
-            for (var node = adjs.First; node != null; node = node.Next) // max tc = n - 1 (exclude self)
+            int aiVisitedTurns = depths[ai];
+
+            int next = ai + 1;
+            if (next <= k && depths[next] < 1)
             {
-                int adjV = node.Value;
-
-                if (visitedTable[adjV])
-                    continue;
-
-                visitingQueue.Enqueue(adjV);
-                visitedTable[adjV] = true;
+                visitingQueue.Enqueue(next);
+                depths[next] = aiVisitedTurns + 1;
+            }
+            int twice = ai * 2;
+            if (twice <= k && depths[twice] < 1)
+            {
+                visitingQueue.Enqueue(twice);
+                depths[twice] = aiVisitedTurns + 1;
             }
         }
-        
-        StringBuilder output = new();
-        for (int v = 0; v < n; ++v) // max tc = n = 250 (1 <= n <= 250)
-        {
-            if (visitedTable[v])
-                continue;
-            
-            output.AppendLine($"{v + 1}");
-        }
-        Console.Write((output.Length < 1) ? "0" : output);
+        Console.Write(depths[k]);
     }
 }
