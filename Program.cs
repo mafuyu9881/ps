aea0c6abe0bc0deb69636c2fb647244e628541e2
mafuyusame <mafuyu@tutamail.com>
2024-11-22 12:27:40 +0900

    boj 1271

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿internal class Program
+﻿using System.Numerics;
+using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int x = int.Parse(Console.ReadLine()!);
-        int y = int.Parse(Console.ReadLine()!);
-
-        int output;
-        if (x > 0 && y > 0) output = 1;
-        else if (x < 0 && y > 0) output = 2;
-        else if (x < 0 && y < 0) output = 3;
-        else output = 4;
+        BigInteger[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), BigInteger.Parse);
+        BigInteger n = tokens[0];
+        BigInteger m = tokens[1];
         
+        StringBuilder output = new();
+        output.AppendLine($"{n / m}");
+        output.AppendLine($"{n % m}");
         Console.Write(output);
     }
 }
