d48e48688d2d21062673d79641dc784528197732
mafuyusame <mafuyu@tutamail.com>
2024-11-26 21:30:52 +0900

    boj 1967 시간 초과

--- Program.cs
+++ Program.cs
@@ -1,47 +1,17 @@
 ﻿internal class Program
 {
-    private struct ConnectionData // sc = 8B
-    {
-        private int _dst;
-        public int Dst => _dst;
-        private int _cost;
-        public int Cost => _cost;
-
-        public ConnectionData(int dst, int cost)
-        {
-            _dst = dst;
-            _cost = cost;
-        }
-    }
-
-    private struct AccData // sc = 8B
-    {
-        private int _v;
-        public int V => _v;
-        private int _oldCost;
-        public int OldCost => _oldCost;
-
-        public AccData(int v, int oldCost)
-        {
-            _v = v;
-            _oldCost = oldCost;
-        }
-    }
-
     private static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine()!);
         int extendedN = n + 1;
 
-        // sc = (2n - 2) * 8B
-        // max sc = 19998 * 8B = 159984B = approx 160KB
-        List<ConnectionData>[] adjList = new List<ConnectionData>[extendedN];
+        LinkedList<(int v, int weight)>[] adjList = new LinkedList<(int, int)>[extendedN];
         for (int i = 0; i < (n - 1); ++i)
         {
             int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
             int parent = tokens[0];
             int child = tokens[1];
-            int cost = tokens[2];
+            int weight = tokens[2];
 
             var parentAdjs = adjList[parent];
             if (parentAdjs == null)
@@ -49,7 +19,7 @@
                 parentAdjs = new();
                 adjList[parent] = parentAdjs;
             }
-            parentAdjs.Add(new ConnectionData(child, cost));
+            parentAdjs.AddLast((child, weight));
 
             var childAdjs = adjList[child];
             if (childAdjs == null)
@@ -57,35 +27,37 @@
                 childAdjs = new();
                 adjList[child] = childAdjs;
             }
-            childAdjs.Add(new ConnectionData(parent, cost));
+            childAdjs.AddLast((parent, weight));
         }
 
         int maxDiameter = 0;
-        for (int i = 1; i < extendedN; ++i) // tc = n, max tc = 10000
+        for (int i = 1; i < extendedN; ++i)
         {
-            bool[] visited = new bool[extendedN]; // sc = 1B * n, max sc = 10000B = 10KB
-            Stack<AccData> stack = new(); // sc = 8B * n, max sc = 80000B = 80KB
+            GC.Collect();
+            
+            bool[] visited = new bool[extendedN];
+            Stack<(int v, int weightSum)> stack = new();
             stack.Push(new(i, 0));
             visited[i] = true;
-            while (stack.Count > 0) // tc = n, max tc = 10000
+            while (stack.Count > 0)
             {
-                AccData accData = stack.Pop();
-                int v = accData.V;
-                int oldCost = accData.OldCost;
+                var stackData = stack.Pop();
+                int v = stackData.v;
+                int oldWeightSum = stackData.weightSum;
 
                 var adjs = adjList[v];
                 if (adjs == null)
                     continue;
 
-                for (int j = 0; j < adjs.Count; ++j) // max tc = 3
+                for (var node = adjs.First; node != null; node = node.Next)
                 {
-                    int adjV = adjs[j].Dst;
+                    int adjV = node.Value.v;
                     if (visited[adjV])
                         continue;
 
-                    int newCost = oldCost + adjs[j].Cost;
-                    maxDiameter = Math.Max(maxDiameter, newCost);
-                    stack.Push(new(adjV, newCost));
+                    int newWeightSum = oldWeightSum + node.Value.weight;
+                    maxDiameter = Math.Max(maxDiameter, newWeightSum);
+                    stack.Push(new(adjV, newWeightSum));
                     visited[adjV] = true;
                 }
             }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db1e5c1e084e45a8499657f88ea41f268051854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d048aea0b0592d2a6cfbb92f1811a67d66e6c02")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
