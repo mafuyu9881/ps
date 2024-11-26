d6ba5614c5a770c0b2f68782c5cb1cc411f1e887
mafuyusame <mafuyu@tutamail.com>
2024-11-26 12:44:13 +0900

    boj 1967 풀이 도중 중단

--- Program.cs
+++ Program.cs
@@ -1,44 +1,88 @@
 ﻿internal class Program
 {
+    private struct ChildConnectionData
+    {
+        private int _child;
+        public int Child => _child;
+        private int _cost;
+        public int Cost => _cost;
+
+        public ChildConnectionData(int child, int cost)
+        {
+            _child = child;
+            _cost = cost;
+        }
+    }
+
     private static void Main(string[] args)
     {
-        const int Infinity = -1;
+        int n = int.Parse(Console.ReadLine()!); // 1 ≤ n ≤ 10,000
+        int extendedN = n + 1;
 
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int a = tokens[0];
-        int b = tokens[1];
-        
-        int[] distances = new int[b + 1];
-        for (int i = 0; i < distances.Length; ++i)
+        LinkedList<ChildConnectionData>[] childrenList = new LinkedList<ChildConnectionData>[extendedN];
+        for (int i = 1; i < extendedN; ++i) // tc = n
         {
-            distances[i] = (i != a) ? Infinity : 0;
+            childrenList[i] = new();
         }
-        Queue<long> queue = new();
-        queue.Enqueue(a);
-        while (queue.Count > 0)
+        LinkedList<int> visitableParents = new();
+        for (int i = 0; i < n; ++i) // tc = n
         {
-            long num = queue.Dequeue();
-            int newDistance = distances[num] + 1;
-            long[] nextNums = new long[] { num * 2, num * 10 + 1 };
-            for (int i = 0; i < nextNums.Length; ++i)
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int parent = tokens[0];
+            int child = tokens[1];
+            int cost = tokens[2];
+
+            visitableParents.AddLast(parent);
+            childrenList[parent].AddLast(new ChildConnectionData(child, cost));
+        }
+
+        //int[] diameters = new int[extendedN];
+        int maxDiameter = 0;
+        while (true)
+        {
+            if (visitableParents.Count < 1)
+                break;
+
+            var node = visitableParents.First!;
+            int parent = node.Value;
+            visitableParents.Remove(node);
+
+            int diameter = 0;
+
+            Queue<ChildConnectionData> ccdQueue = new();
+            var adjs = childrenList[parent];
+            for (var iteratingNode = adjs.First; iteratingNode != null; iteratingNode = iteratingNode.Next)
             {
-                long nextNum = nextNums[i];
-                if (nextNum > b)
-                    continue;
-                
-                int oldDistance = distances[nextNum];
-                if (oldDistance != Infinity && oldDistance < newDistance)
+                ccdQueue.Enqueue(iteratingNode.Value);
+            }
+            while (ccdQueue.Count > 0)
+            {
+                ChildConnectionData ccd = ccdQueue.Dequeue();
+
+                diameter += ccd.Cost;
+
+                int child = ccd.Child;
+
+                var leftGrandchildNode = childrenList[child].First;
+                var rightGrandchildNode = childrenList[child].Last;
+                if (leftGrandchildNode == null) // same semantics with 'if (leftGrandchildNode == null && rightGrandchildNode == null)'
                     continue;
 
-                distances[nextNum] = newDistance;
-                queue.Enqueue(nextNum);
+                if (leftGrandchildNode != rightGrandchildNode)
+                {
+                    int leftCost = leftGrandchildNode.Value.Cost;
+                    int rightCost = rightGrandchildNode.Value.Cost;
+                }
+                else // child has only one node
+                {
+                    ccdQueue.Enqueue(leftGrandchildNode.Value);
+                }
+            }
+
+            if (diameter > maxDiameter)
+            {
+                maxDiameter = diameter;
             }
         }
-        int distance = distances[b];
-        if (distance != -1)
-        {
-            ++distance;
-        }
-        Console.Write(distance);
     }
 }
