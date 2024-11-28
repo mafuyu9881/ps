66a01d006d8097bd5509f9a44ac259bf08ed0acd
mafuyusame <mafuyu@tutamail.com>
2024-11-28 20:49:27 +0900

    boj 9625 (dp)

--- Program.cs
+++ Program.cs
@@ -1,31 +1,21 @@
-﻿using System.Numerics;
-using System.Runtime.Intrinsics;
-using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
-        Vector3[] points = new Vector3[n];
-        for (int i = 0; i < n; ++i)
+        int k = int.Parse(Console.ReadLine()!);
+
+        int currACount = 1;
+        int currBCount = 0;
+
+        for (int i = 0; i < k; ++i)
         {
-            float[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), float.Parse);
-            points[i] = new(tokens[0], tokens[1], 0.0f);
+            int prevACount = currACount;
+            int prevBCount = currBCount;
+
+            currACount = prevBCount;
+            currBCount = prevACount + prevBCount;
         }
 
-        float area = 0;
-        Vector3 p0 = points[0];
-        for (int i = 1; i < n - 1; ++i)
-        {
-            Vector3 p1 = points[i];
-            Vector3 p2 = points[i + 1];
-
-            Vector3 p0p1 = p1 - p0;
-            Vector3 p0p2 = p2 - p0;
-
-            area += Vector3.Cross(p0p1, p0p2).Length() * 0.5f;
-        }
-        Console.WriteLine(area.ToString("0.0"));
+        Console.Write($"{currACount} {currBCount}");
     }
 }
