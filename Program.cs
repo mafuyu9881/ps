9142e836e135a289399be0eeb85dbef95c67d093
mafuyusame <mafuyu@tutamail.com>
2024-11-30 23:38:56 +0900

    boj 11404 (다익스트라)
    플로이드 와샬이라는 알고리즘으로도 풀 수 있는건지, 문제의 이름이 '플로이드'다.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,77 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine()!);
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        if (dp.Length > 2) dp[2] = 2; // 1 ≤ N ≤ 1000
-        for (int i = 3; i < dp.Length; ++i)
+        int exN = n + 1;
+
+        int m = int.Parse(Console.ReadLine()!);
+
+        LinkedList<(int v, int cost)>[] adjList = new LinkedList<(int, int)>[exN];
+        for (int i = 0; i < m; ++i)
         {
-            // Min or Max, no need to care about choosing which one
-            // But set 0 index's element as 0 and start the loop from 3 is
-            // quite important point for automation
-            // If we build dp from 1, the loop should be started from 4,
-            // then we should set 3 index's element as 1 when we use Min,
-            // or 3 when we use Max
-            dp[i] = Math.Min(dp[i - 1] + 1, dp[i - 3] + 1);
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int a = tokens[0];
+            int b = tokens[1];
+            int c = tokens[2];
+            
+            var adjs = adjList[a];
+            if (adjs == null)
+            {
+                adjs = new();
+                adjList[a] = adjs;
+            }
+            adjs.AddLast((b, c));
         }
-        Console.Write((dp[n] % 2 != 0) ? "CY" : "SK");
+
+        const int Infinity = -1;
+
+        StringBuilder output = new();
+        for (int s = 1; s < exN; ++s) // max tc = 100 (2 ≤ n ≤ 100)
+        {
+            int[] minCosts = new int[exN];
+            for (int i = 0; i < minCosts.Length; ++i) // max tc = 101
+            {
+                minCosts[i] = (s != i) ? Infinity : 0;
+            }
+            
+            PriorityQueue<(int v, int cost), int> pq = new();
+            int selfCost = 0;
+            pq.Enqueue((s, selfCost), selfCost);
+            while (pq.Count > 0) // max tc = 100 * log2(100)
+            {
+                var element = pq.Dequeue();
+                int v = element.v;
+                int cost = element.cost;
+
+                var adjs = adjList[v];
+                if (adjs == null)
+                    continue;
+
+                for (var node = adjs.First; node != null; node = node.Next)
+                {
+                    var adjElement = node.Value;
+                    int adjV = adjElement.v;
+                    int adjCost = adjElement.cost;
+                    
+                    int oldCost = minCosts[adjV];
+                    int newCost = cost + adjCost;
+                    if (oldCost == Infinity || newCost < oldCost)
+                    {
+                        minCosts[adjV] = newCost;
+                        pq.Enqueue((adjV, newCost), newCost);
+                    }
+                }
+            }
+            for (int i = 1; i < minCosts.Length; ++i)
+            {
+                int minCost = minCosts[i];
+                output.Append($"{((minCost != Infinity) ? minCost : 0)} ");
+            }
+            output.AppendLine();
+        }
+        Console.Write(output);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+740c7cde298a6262955c0ad79939d0985b0f2393")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+986ad8767d8edde8dbadffd7a7baae103946d7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
