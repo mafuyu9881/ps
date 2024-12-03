130032e3832a6082a5f97a8269b1bb5250c079a4
mafuyusame <mafuyu@tutamail.com>
2024-12-03 18:04:40 +0900

    boj 14916 (dp)

--- Program.cs
+++ Program.cs
@@ -3,20 +3,36 @@
     private static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine()!); // 1 ≤ n ≤ 100,000
-        int output = 0;
-        while (n > 0)
+
+        int[] coins = new int[] { 2, 5 };
+
+        const int Invalid = -1;
+        int[] dp = new int[n + 1];
+        for (int i = 1; i < dp.Length; ++i)
         {
-            if (n % 5 != 0)
+            dp[i] = Invalid;
+
+            for (int j = 0; j < coins.Length; ++j)
             {
-                ++output;
-                n -= 2;
-            }
-            else
-            {
-                output += n / 5;
-                n = 0;
+                int coin = coins[j];
+
+                if (i - coin < 0)
+                    continue;
+
+                if (dp[i - coin] != Invalid)
+                {
+                    if (dp[i] != Invalid)
+                    {
+                        dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
+                    }
+                    else
+                    {
+                        dp[i] = dp[i - coin] + 1;
+                    }
+                }
             }
         }
-        Console.WriteLine((n != 0) ? -1 : output);
+
+        Console.Write(dp[n]);
     }
 }
