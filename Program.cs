f590d9a2fecff21d4e436c2a0d82766ebbc31099
mafuyusame <mafuyu@tutamail.com>
2024-12-05 19:08:53 +0900

    boj 1789 (greedy)

--- Program.cs
+++ Program.cs
@@ -2,26 +2,12 @@
 {
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0];
-        int exN = n + 1;
-        int k = tokens[1];
-        int exK = k + 1;
-        int[,] dp = new int[exN, exK];
-        for (int row = 1; row < exN; ++row)
+        long s = long.Parse(Console.ReadLine()!);
+        uint output = 1;
+        while (s >= 0)
         {
-            for (int col = 1; col < exK; ++col)
-            {
-                if (col == 1 || row == col)
-                {
-                    dp[row, col] = 1;
-                }
-                else
-                {
-                    dp[row, col] = dp[row - 1, col - 1] + dp[row - 1, col];
-                }
-            }
+            s -= output++;
         }
-        Console.Write(dp[n, k]);
+        Console.Write(output - 2);
     }
 }
