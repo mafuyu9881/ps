1a6c9733743fb82ab6061f9e19be22133a878fa5
mafuyusame <mafuyu@tutamail.com>
2024-12-06 13:40:55 +0900

    boj 19947 (dp)
    정밀도 문제 때문에 float 말고 double을 썼어야 했다.

--- Program.cs
+++ Program.cs
@@ -1,46 +1,25 @@
 ﻿internal class Program
 {
-    private struct AnnualInterestRate
-    {
-        private int _years;
-        public int Years => _years;
-        private float _interestRate;
-        public float InterestRate => _interestRate;
-
-        public AnnualInterestRate(int years, float interestRate)
-        {
-            _years = years;
-            _interestRate = interestRate;
-        }
-    }
-
     private static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
         int h = tokens[0]; // 10,000 ≤ H ≤ 100,000
         int y = tokens[1]; // 0 ≤ Y ≤ 10
 
-        AnnualInterestRate[] airs = new AnnualInterestRate[]
-        {
-            new(1, 1.05f),
-            new(3, 1.2f),
-            new(5, 1.35f),
-        };
-
         int[] dp = new int[y + 1];
         dp[0] = h;
         for (int currIndex = 1; currIndex < dp.Length; ++currIndex)
         {
-            for (int j = 0; j < airs.Length; ++j)
+            dp[currIndex] = (int)(dp[currIndex - 1] * 1.05);
+
+            if (currIndex >= 3)
             {
-                ref var air = ref airs[j];
-                int years = air.Years;
+                dp[currIndex] = Math.Max(dp[currIndex], (int)(dp[currIndex - 3] * 1.2));
+            }
 
-                int prevIndex = currIndex - years;
-                if (prevIndex < 0)
-                    continue;
-
-                dp[currIndex] = Math.Max(dp[currIndex], (int)(dp[prevIndex] * air.InterestRate));
+            if (currIndex >= 5)
+            {
+                dp[currIndex] = Math.Max(dp[currIndex], (int)(dp[currIndex - 5] * 1.35));
             }
         }
         Console.Write(dp[y]);
