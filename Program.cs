b316637491f77561215d7239c3724923908f19fc
mafuyusame <mafuyu@tutamail.com>
2024-12-11 22:05:47 +0900

    boj 6550 (greedy)
    짚고 갈 점들
    1. c# 입력 코드에서 eof 처리
    2. 부분 문자열 풀이에서 큐 또는 스택의 활용

--- Program.cs
+++ Program.cs
@@ -1,47 +1,38 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        // 1 ≤ a, b < 1000, a ≠ b
-        int a = tokens[0];
-        int b = tokens[1];
-
-        // 1 ≤ n ≤ 5
-        int n = int.Parse(Console.ReadLine()!);
-        int[] frequencies = new int[n];
-        for (int i = 0; i < frequencies.Length; ++i)
+        StringBuilder output = new();
+        while (true)
         {
-            frequencies[i] = int.Parse(Console.ReadLine()!);
-        }
+            string? input = Console.ReadLine();
+            if (input == null)
+                break;
 
-        int output = 0;
-        while (a != b)
-        {
-            int newA;
+            string[] tokens = input.Split();
 
-            int leftA = a - 1;
-            int rightA = a + 1;
-            if (Math.Abs(b - leftA) < Math.Abs(b - rightA))
+            string substring = tokens[0];
+            Queue<char> substringQueue = new();
+            for (int i = 0; i < substring.Length; ++i)
             {
-                newA = leftA;
-            }
-            else
-            {
-                newA = rightA;
+                substringQueue.Enqueue(substring[i]);
             }
 
-            for (int i = 0; i < frequencies.Length; ++i)
+            string mainstring = tokens[1];
+            for (int i = 0; i < mainstring.Length; ++i)
             {
-                int frequency = frequencies[i];
-                if (Math.Abs(b - frequency) < Math.Abs(b - newA))
-                {
-                    newA = frequency;
-                }
-            }
+                if (substringQueue.Peek() != mainstring[i])
+                    continue;
 
-            a = newA;
-            ++output;
+                substringQueue.Dequeue();
+
+                if (substringQueue.Count < 1)
+                    break;
+            }
+            
+            output.AppendLine(substringQueue.Count < 1 ? "Yes" : "No");
         }
         Console.Write(output);
     }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f178da8ad5c5efe3a6d1fc88c281fb2db7ba17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09982dfee0282df252c337f232a0beb53cb48ac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
