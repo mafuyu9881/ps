f4e82e34f507e4df442f07c4ddc833f3fa467afc
mafuyusame <mafuyu@tutamail.com>
2024-12-15 11:47:28 +0900

    boj 9461 (dp)

--- Program.cs
+++ Program.cs
@@ -1,25 +1,30 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        int t = int.Parse(Console.ReadLine()!);
 
-        PriorityQueue<int, int> cardBundles = new();
-        for (int i = 0; i < n; ++i)
+        StringBuilder output = new();
+        for (int i = 0; i < t; ++i)
         {
-            int cardBundle = int.Parse(Console.ReadLine()!);
+            int[] baseDP = new int[] { 1, 1, 1, 2, 2 };
+            
+            int n = int.Parse(Console.ReadLine()!); // 1 ≤ n ≤ 100
 
-            cardBundles.Enqueue(cardBundle, cardBundle);
-        }
-        
-        int output = 0;
-        while (cardBundles.Count > 1)
-        {
-            int firstCardBundle = cardBundles.Dequeue();
-            int secondCardBundle = cardBundles.Dequeue();
-            int newCardBundle = firstCardBundle + secondCardBundle;
-            cardBundles.Enqueue(newCardBundle, newCardBundle);
-            output += newCardBundle;
+            long[] dp = new long[Math.Max(baseDP.Length, n)];
+            for (int j = 0; j < baseDP.Length; ++j)
+            {
+                dp[j] = baseDP[j];
+            }
+
+            for (int j = baseDP.Length; j < dp.Length; ++j)
+            {
+                dp[j] = dp[j - 1] + dp[j - 5];
+            }
+
+            output.AppendLine($"{dp[n - 1]}");
         }
         Console.Write(output);
     }
