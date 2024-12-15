4e59e1ae1f38d8661dfcb3bf1fb330a9ede0f5c7
mafuyusame <mafuyu@tutamail.com>
2024-12-15 14:25:09 +0900

    boj 11727 풀이 도중 커밋

--- Program.cs
+++ Program.cs
@@ -4,44 +4,20 @@
     {
         int n = int.Parse(Console.ReadLine()!);
 
-        int containerLength = Math.Max(3, n);
+        int[] cylicNumbers = new int[] { 1, 2, 3, 4, 0, 0, 0 };
 
-        // 1 2 3 4 1 2 3 4로 진행해야하나?
-        int[] prefixSums = new int[containerLength];
-        prefixSums[0] = 1;
-        prefixSums[1] = 4;
-        prefixSums[2] = 9;
-        for (int i = 3; i < prefixSums.Length; ++i)
-        {
-            int prevPrefixSum = prefixSums[i - 1];
-            prefixSums[i] = prevPrefixSum + (prevPrefixSum + ComputeCyclicNumber(i - 2, 3));
-        }
-
-        int[] dp = new int[containerLength];
+        int[] dp = new int[Math.Max(3, n)];
         dp[0] = 1;
         dp[1] = 3;
         dp[2] = 5;
         for (int i = 3; i < dp.Length; ++i)
         {
-            //dp[i] = prefixSums[i - 1] + ComputeCyclicNumber(i - 2, 3);
             for (int j = 0; j < i; ++j)
             {
                 dp[i] += dp[j];
             }
-            dp[i] += ComputeCyclicNumber(i - 2, 4);
+            dp[i] += cylicNumbers[(i - 2 - 1) % cylicNumbers.Length];
         }
         Console.Write(dp[n - 1]);
-    }
-    
-    private static int ComputeCyclicNumber(int input, int period)
-    {
-        int output = input % period;
-
-        if (output == 0)
-        {
-            output = period;
-        }
-
-        return output;
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e82e34f507e4df442f07c4ddc833f3fa467afc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42219d0deb75d3424717f962da1ccb6759b5d83e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
