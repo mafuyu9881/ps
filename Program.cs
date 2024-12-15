42219d0deb75d3424717f962da1ccb6759b5d83e
mafuyusame <mafuyu@tutamail.com>
2024-12-15 14:12:24 +0900

    boj 11727 풀이 도중 저장

--- Program.cs
+++ Program.cs
@@ -1,31 +1,47 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        int t = int.Parse(Console.ReadLine()!);
+        int n = int.Parse(Console.ReadLine()!);
 
-        StringBuilder output = new();
-        for (int i = 0; i < t; ++i)
+        int containerLength = Math.Max(3, n);
+
+        // 1 2 3 4 1 2 3 4로 진행해야하나?
+        int[] prefixSums = new int[containerLength];
+        prefixSums[0] = 1;
+        prefixSums[1] = 4;
+        prefixSums[2] = 9;
+        for (int i = 3; i < prefixSums.Length; ++i)
         {
-            int[] baseDP = new int[] { 1, 1, 1, 2, 2 };
-            
-            int n = int.Parse(Console.ReadLine()!); // 1 ≤ n ≤ 100
-
-            long[] dp = new long[Math.Max(baseDP.Length, n)];
-            for (int j = 0; j < baseDP.Length; ++j)
-            {
-                dp[j] = baseDP[j];
-            }
-
-            for (int j = baseDP.Length; j < dp.Length; ++j)
-            {
-                dp[j] = dp[j - 1] + dp[j - 5];
-            }
-
-            output.AppendLine($"{dp[n - 1]}");
+            int prevPrefixSum = prefixSums[i - 1];
+            prefixSums[i] = prevPrefixSum + (prevPrefixSum + ComputeCyclicNumber(i - 2, 3));
         }
-        Console.Write(output);
+
+        int[] dp = new int[containerLength];
+        dp[0] = 1;
+        dp[1] = 3;
+        dp[2] = 5;
+        for (int i = 3; i < dp.Length; ++i)
+        {
+            //dp[i] = prefixSums[i - 1] + ComputeCyclicNumber(i - 2, 3);
+            for (int j = 0; j < i; ++j)
+            {
+                dp[i] += dp[j];
+            }
+            dp[i] += ComputeCyclicNumber(i - 2, 4);
+        }
+        Console.Write(dp[n - 1]);
+    }
+    
+    private static int ComputeCyclicNumber(int input, int period)
+    {
+        int output = input % period;
+
+        if (output == 0)
+        {
+            output = period;
+        }
+
+        return output;
     }
 }
