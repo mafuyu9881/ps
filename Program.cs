000c4ea749553a331034f2b5b321ec98538e3b9f
mafuyusame <mafuyu@tutamail.com>
2024-12-15 15:45:09 +0900

    boj 11727 정해 (dp)

--- Program.cs
+++ Program.cs
@@ -4,22 +4,12 @@
     {
         int n = int.Parse(Console.ReadLine()!);
 
-        int[] cylicNumbers = new int[] { 2, 1 };
-
-        int[] dp = new int[Math.Max(3, n)];
+        int[] dp = new int[Math.Max(2, n)];
         dp[0] = 1;
         dp[1] = 3;
-        dp[2] = 5;
-        for (int i = 3; i < dp.Length; ++i)
+        for (int i = 2; i < dp.Length; ++i)
         {
-            for (int j = 0; j < i; ++j)
-            {
-                dp[i] += dp[j];
-            }
-            // '-2' means we will count the number from 1
-            // '-1' means array's index
-            dp[i] += cylicNumbers[(i - 2 - 1) % cylicNumbers.Length];
-            dp[i] %= 10007;
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
         }
         Console.Write(dp[n - 1]);
     }
