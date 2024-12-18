a5a90096fa3e1c344dc7e9402768c9456c6da707
mafuyusame <mafuyu@tutamail.com>
2024-12-18 20:24:59 +0900

    boj 21736 (bfs/dfs)

--- Program.cs
+++ Program.cs
@@ -1,90 +1,107 @@
 ﻿internal class Program
 {
+    private struct Index2D
+    {
+        private int _row;
+        public int Row => _row;
+        private int _col;
+        public int Col => _col;
+
+        public Index2D(int row, int col)
+        {
+            _row = row;
+            _col = col;
+        }
+    }
+
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // 1 ≤ m, n ≤ 500
-        int m = tokens[1];
-        int b = tokens[2]; // 0 ≤ b ≤ 6.4 × 10^7
+        int[] integerTokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int height = integerTokens[0]; // 1 <= n <= 600
+        int width = integerTokens[1]; // 1 <= m <= 600
 
-        // 일일이 특정 셀에 블록이 몇 개인지 기록하고 찾아볼 필요 없음
-        // 딱 처음에
-        // 처음부터 보유 중인 블록의 개수
-        // 디깅 할 블록의 개수
-        // 라이징 할 블록의 개수
-        // 를 기록해두고 거기서 더하고 빼면 될 것으로 보임
-        const int InvalidHeight = -1; // 0 ≤ height ≤ 256
-        int minimumHeight = InvalidHeight;
-        int maximumHeight = InvalidHeight;
-        int[] map = new int[n * m];
-        for (int row = 0; row < n; ++row)
+        const char Wall = 'X';
+        const char Start = 'I';
+        const char Objective = 'P';
+
+        char[] map = new char[height * width];
+        int startIndex1D = 0;
+        for (int row = 0; row < height; ++row)
         {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            for (int col = 0; col < m; ++ col)
+            string stringToken = Console.ReadLine()!;
+            for (int col = 0; col < width; ++col)
             {
-                int height = tokens[col];
+                int index1D = ConvertIndex2DTo1D(width, new(row, col));
+                char c = stringToken[col];
 
-                if (minimumHeight != InvalidHeight)
+                if (c == Start)
                 {
-                    minimumHeight = Math.Min(minimumHeight, height);
+                    startIndex1D = index1D;
                 }
-                else
-                {
-                    minimumHeight = height;
-                }
-
-                if (maximumHeight != InvalidHeight)
-                {
-                    maximumHeight = Math.Max(maximumHeight, height);
-                }
-                else
-                {
-                    maximumHeight = height;
-                }
-
-                map[row * m + col] = height;
+                
+                map[index1D] = c;
             }
         }
-
-        const int InvalidCost = -1;
-        int minimumCost = InvalidCost;
-        PriorityQueue<int, int> minimumCostHeights = new();
-        for (int desiredHeight = minimumHeight; desiredHeight <= maximumHeight; ++desiredHeight)
+        
+        bool[] visited = new bool[height * width];
+        Queue<int> queue = new();
+        visited[startIndex1D] = true;
+        queue.Enqueue(startIndex1D);
+        int achievedObjectives = 0;
+        while (queue.Count > 0)
         {
-            int holdingBlocks = b;
-            int cost = 0;
+            int index1D = queue.Dequeue();
+            Index2D index2D = ConvertIndex1DTo2D(width, index1D);
+            int row = index2D.Row;
+            int col = index2D.Col;
 
-            for (int j = 0; j < map.Length; ++j)
+            Index2D[] adjIndices2D = new Index2D[]
             {
-                int height = map[j];
-
-                if (height > desiredHeight)
-                {
-                    int diggedHeight = height - desiredHeight;
-                    holdingBlocks += diggedHeight;
-                    cost += diggedHeight * 2;
-                }
-                else if (height < desiredHeight)
-                {
-                    int risedHeight = desiredHeight - height;
-                    holdingBlocks -= risedHeight;
-                    cost += risedHeight;
-                }
-            }
-
-            if (holdingBlocks < 0)
-                break;
-
-            if (minimumCost != InvalidCost && cost > minimumCost)
-                continue;
-
-            if (minimumCost == InvalidCost || cost < minimumCost)
+                new (row - 1, col),
+                new (row + 1, col),
+                new (row, col - 1),
+                new (row, col + 1),
+            };
+            for (int i = 0; i < adjIndices2D.Length; ++i)
             {
-                minimumCost = cost;
-                minimumCostHeights.Clear();
+                Index2D adjIndex2D = adjIndices2D[i];
+
+                int adjRow = adjIndex2D.Row;
+                if (adjRow < 0 || adjRow > height - 1)
+                    continue;
+
+                int adjCol = adjIndex2D.Col;
+                if (adjCol < 0 || adjCol > width - 1)
+                    continue;
+
+                int adjIndex1D = ConvertIndex2DTo1D(width, adjIndex2D);
+                if (visited[adjIndex1D])
+                    continue;
+
+                char adjC = map[adjIndex1D];
+                if (adjC == Objective)
+                {
+                    ++achievedObjectives;
+                }
+
+                if (adjC != Wall)
+                {
+                    queue.Enqueue(adjIndex1D);
+                }
+
+                visited[adjIndex1D] = true;
             }
-            minimumCostHeights.Enqueue(desiredHeight, -desiredHeight);
         }
-        Console.Write($"{minimumCost} {minimumCostHeights.Dequeue()}");
+        Console.Write((achievedObjectives > 0) ? achievedObjectives : "TT");
+    }
+
+    private static int ConvertIndex2DTo1D(int width, Index2D index2D)
+    {
+        return index2D.Row * width + index2D.Col;
+    }
+
+    private static Index2D ConvertIndex1DTo2D(int width, int index1D)
+    {
+        return new(index1D / width, index1D % width);
     }
 }
