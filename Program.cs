72c30bdf00a19eb9e9bb6b9cb43c4f87f3fa1f5e
mafuyusame <mafuyu@tutamail.com>
2024-12-18 17:24:35 +0900

    boj 18111 (implementation, bruteforcing)

--- Program.cs
+++ Program.cs
@@ -1,31 +1,90 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        StringBuilder input = new(Console.ReadLine()!);
-        input.Append('\0');
-        int output = 0;
-        bool minusFlag = false;
-        StringBuilder sb = new();
-        for (int i = 0; i < input.Length; ++i)
-        {
-            char c = input[i];
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // 1 ≤ m, n ≤ 500
+        int m = tokens[1];
+        int b = tokens[2]; // 0 ≤ b ≤ 6.4 × 10^7
 
-            if (c < '0')
+        // 일일이 특정 셀에 블록이 몇 개인지 기록하고 찾아볼 필요 없음
+        // 딱 처음에
+        // 처음부터 보유 중인 블록의 개수
+        // 디깅 할 블록의 개수
+        // 라이징 할 블록의 개수
+        // 를 기록해두고 거기서 더하고 빼면 될 것으로 보임
+        const int InvalidHeight = -1; // 0 ≤ height ≤ 256
+        int minimumHeight = InvalidHeight;
+        int maximumHeight = InvalidHeight;
+        int[] map = new int[n * m];
+        for (int row = 0; row < n; ++row)
+        {
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            for (int col = 0; col < m; ++ col)
             {
-                if (sb.Length > 0)
+                int height = tokens[col];
+
+                if (minimumHeight != InvalidHeight)
                 {
-                    output += Math.Abs(int.Parse(sb.ToString())) * (minusFlag ? -1 : 1);
-                    sb.Clear();
+                    minimumHeight = Math.Min(minimumHeight, height);
+                }
+                else
+                {
+                    minimumHeight = height;
                 }
 
-                minusFlag |= c == '-';
+                if (maximumHeight != InvalidHeight)
+                {
+                    maximumHeight = Math.Max(maximumHeight, height);
+                }
+                else
+                {
+                    maximumHeight = height;
+                }
+
+                map[row * m + col] = height;
+            }
+        }
+
+        const int InvalidCost = -1;
+        int minimumCost = InvalidCost;
+        PriorityQueue<int, int> minimumCostHeights = new();
+        for (int desiredHeight = minimumHeight; desiredHeight <= maximumHeight; ++desiredHeight)
+        {
+            int holdingBlocks = b;
+            int cost = 0;
+
+            for (int j = 0; j < map.Length; ++j)
+            {
+                int height = map[j];
+
+                if (height > desiredHeight)
+                {
+                    int diggedHeight = height - desiredHeight;
+                    holdingBlocks += diggedHeight;
+                    cost += diggedHeight * 2;
+                }
+                else if (height < desiredHeight)
+                {
+                    int risedHeight = desiredHeight - height;
+                    holdingBlocks -= risedHeight;
+                    cost += risedHeight;
+                }
             }
 
-            sb.Append(c);
+            if (holdingBlocks < 0)
+                break;
+
+            if (minimumCost != InvalidCost && cost > minimumCost)
+                continue;
+
+            if (minimumCost == InvalidCost || cost < minimumCost)
+            {
+                minimumCost = cost;
+                minimumCostHeights.Clear();
+            }
+            minimumCostHeights.Enqueue(desiredHeight, -desiredHeight);
         }
-        Console.Write(output);
+        Console.Write($"{minimumCost} {minimumCostHeights.Dequeue()}");
     }
 }
