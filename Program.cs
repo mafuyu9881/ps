5fb22acbadfa4858f381eb05adb341a9220adc4a
mafuyusame <mafuyu@tutamail.com>
2024-12-19 12:05:13 +0900

    boj 30804 풀이 도중 커밋

--- Program.cs
+++ Program.cs
@@ -1,107 +1,60 @@
 ﻿internal class Program
 {
-    private struct Index2D
-    {
-        private int _row;
-        public int Row => _row;
-        private int _col;
-        public int Col => _col;
-
-        public Index2D(int row, int col)
-        {
-            _row = row;
-            _col = col;
-        }
-    }
-
     private static void Main(string[] args)
     {
-        int[] integerTokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int height = integerTokens[0]; // 1 <= n <= 600
-        int width = integerTokens[1]; // 1 <= m <= 600
+        int n = int.Parse(Console.ReadLine()!); // 1 ≤ n ≤ 200,000
 
-        const char Wall = 'X';
-        const char Start = 'I';
-        const char Objective = 'P';
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // 1 ≤ Si ≤ 9
 
-        char[] map = new char[height * width];
-        int startIndex1D = 0;
-        for (int row = 0; row < height; ++row)
+        int begin = 0;
+        int end = 0;
+
+        bool[] included = new bool[10];
+        LinkedList<int> includedNumbers = new();
+        included[begin] = true;
+        includedNumbers.AddLast(begin);
+
+        int minimumLength = 1;
+        int length = 1;
+        while (true)
         {
-            string stringToken = Console.ReadLine()!;
-            for (int col = 0; col < width; ++col)
-            {
-                int index1D = ConvertIndex2DTo1D(width, new(row, col));
-                char c = stringToken[col];
+            //if (begin < end)
+            //{
+            //}
+            //else
+            //{
+            //    ++end;
+            //    if (end > tokens.Length - 1)
+            //    {
+            //        break;
+            //    }
+            //}
 
-                if (c == Start)
+            ++end;
+
+            int endNumber = tokens[end];
+
+            if (included[endNumber])
+            {
+                ++length;
+            }
+            else
+            {
+                if (includedNumbers.Count < 2)
                 {
-                    startIndex1D = index1D;
+                    included[endNumber] = true;
+                    includedNumbers.AddLast(endNumber);
                 }
-                
-                map[index1D] = c;
+                else
+                {
+                    //var firstIncludedNumberNode = includedNumbers.First!;
+                    //int firstIncludedNumber = firstIncludedNumberNode.Value;
+                    //includedNumbers.Remove(firstIncludedNumberNode);
+                    //included[firstIncludedNumber] = false;
+                    //length = 
+                    
+                }
             }
         }
-        
-        bool[] visited = new bool[height * width];
-        Queue<int> queue = new();
-        visited[startIndex1D] = true;
-        queue.Enqueue(startIndex1D);
-        int achievedObjectives = 0;
-        while (queue.Count > 0)
-        {
-            int index1D = queue.Dequeue();
-            Index2D index2D = ConvertIndex1DTo2D(width, index1D);
-            int row = index2D.Row;
-            int col = index2D.Col;
-
-            Index2D[] adjIndices2D = new Index2D[]
-            {
-                new (row - 1, col),
-                new (row + 1, col),
-                new (row, col - 1),
-                new (row, col + 1),
-            };
-            for (int i = 0; i < adjIndices2D.Length; ++i)
-            {
-                Index2D adjIndex2D = adjIndices2D[i];
-
-                int adjRow = adjIndex2D.Row;
-                if (adjRow < 0 || adjRow > height - 1)
-                    continue;
-
-                int adjCol = adjIndex2D.Col;
-                if (adjCol < 0 || adjCol > width - 1)
-                    continue;
-
-                int adjIndex1D = ConvertIndex2DTo1D(width, adjIndex2D);
-                if (visited[adjIndex1D])
-                    continue;
-
-                char adjC = map[adjIndex1D];
-                if (adjC == Objective)
-                {
-                    ++achievedObjectives;
-                }
-
-                if (adjC != Wall)
-                {
-                    queue.Enqueue(adjIndex1D);
-                }
-
-                visited[adjIndex1D] = true;
-            }
-        }
-        Console.Write((achievedObjectives > 0) ? achievedObjectives : "TT");
-    }
-
-    private static int ConvertIndex2DTo1D(int width, Index2D index2D)
-    {
-        return index2D.Row * width + index2D.Col;
-    }
-
-    private static Index2D ConvertIndex1DTo2D(int width, int index1D)
-    {
-        return new(index1D / width, index1D % width);
     }
 }
