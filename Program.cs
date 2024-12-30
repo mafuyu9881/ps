7306ba320a9d5d4ef0769017af9041250f64663a
mafuyusame <mafuyu@tutamail.com>
2024-12-30 20:30:00 +0900

    boj 7662 엣지 케이스에서 무한 루프 발생하는 코드

--- Program.cs
+++ Program.cs
@@ -1,114 +1,155 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        const int Unriped = 0;
-        const int Riped = 1;
-        
-        int offsetCount = 6;
-        int[] colOffsets = new int[] { -1, 1, 0, 0, 0, 0 };
-        int[] rowOffsets = new int[] { 0, 0, -1, 1, 0, 0 };
-        int[] layerOffsets = new int[] { 0, 0, 0, 0, -1, 1 };
+        LinkedList<int> buffer = new();
 
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int width = tokens[0]; // [2..100]
-        int height = tokens[1]; // [2..100]
-        int layers = tokens[2]; // [1..100]
-
-        int[] map = new int[width * height * layers]; // [4..1000000]
-        LinkedList<int> ripeds = new();
-        int unripedCount = 0;
-        for (int layer = 0; layer < layers; ++layer)
+        StringBuilder output = new();
+        int t = int.Parse(Console.ReadLine()!);
+        for (int i = 0; i < t; ++i)
         {
-            for (int row = 0; row < height; ++row)
+            Heap maxHeap = new Heap((int a, int b) => a > b);
+            Heap minHeap = new Heap((int a, int b) => a < b);
+
+            int k = int.Parse(Console.ReadLine()!);
+            for (int j = 0; j < k; ++j)
             {
-                tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-                for (int col = 0; col < width; ++col)
+                string[] tokens = Console.ReadLine()!.Split();
+
+                string command = tokens[0];
+                int n = int.Parse(tokens[1]);
+                if (command == "I")
                 {
-                    int index = width * height * layer + width * row + col;
-                    int element = tokens[col];
-                    if (element == Unriped)
-                    {
-                        ++unripedCount;
-                    }
-                    else if (element == Riped)
-                    {
-                        ripeds.AddLast(index);
-                    }
-                    map[index] = element;
+                    maxHeap.Enqueue(n);
+                    minHeap.Enqueue(n);
                 }
-            }
-        }
-
-        string output;
-        if (unripedCount > 0)
-        {
-            int[] ripedDays = new int[map.Length];
-            Queue<int> visitingQueue = new();
-            while (true)
-            {
-                if (visitingQueue.Count < 1)
+                else if (command == "D")
                 {
-                    if (ripeds.Count > 0)
+                    Heap mainHeap;
+                    Heap subHeap;
+                    if (n == 1)
                     {
-                        visitingQueue.Enqueue(ripeds.First!.Value);
-                        ripeds.RemoveFirst();
+                        mainHeap = maxHeap;
+                        subHeap = minHeap;
                     }
-                    else
+                    else // if (n == -1)
                     {
-                        break;
+                        mainHeap = minHeap;
+                        subHeap = maxHeap;
                     }
-                }
 
-                int index = visitingQueue.Dequeue();
-                int layer = index / (width * height);
-                int row = (index % (width * height)) / width;
-                int col = (index % (width * height)) % width;
+                    if (mainHeap.Dequeue(out int dequeuedMainData))
+                    {
+                        while (true) // max tc = log 1,000,000 = 19.xxxx
+                        {
+                            subHeap.Dequeue(out int data);
 
-                for (int i = 0; i < offsetCount; ++i)
-                {
-                    int adjLayer = layer + layerOffsets[i];
-                    if (adjLayer < 0 || adjLayer > layers - 1)
-                        continue;
+                            if (data == dequeuedMainData)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                buffer.AddLast(data);
+                            }
+                        }
 
-                    int adjRow = row + rowOffsets[i];
-                    if (adjRow < 0 || adjRow > height - 1)
-                        continue;
-
-                    int adjCol = col + colOffsets[i];
-                    if (adjCol < 0 || adjCol > width - 1)
-                        continue;
-
-                    int adjIndex = adjLayer * width * height + adjRow * width + adjCol;
-                    if (map[adjIndex] != Unriped)
-                        continue;
-
-                    int adjRipedDay = ripedDays[adjIndex];
-                    int newRipedDay = ripedDays[index] + 1;
-                    if (adjRipedDay > 0 && adjRipedDay <= newRipedDay)
-                        continue;
-
-                    if (adjRipedDay == 0)
-                        --unripedCount;
-
-                    ripedDays[adjIndex] = newRipedDay;
-                    visitingQueue.Enqueue(adjIndex);
+                        while (buffer.Count > 0) // max tc = 19.xxxx
+                        {
+                            var node = buffer.First!;
+                            subHeap.Enqueue(node.Value);
+                            buffer.RemoveFirst();
+                        }
+                    }
                 }
             }
 
-            if (unripedCount > 0)
+            if (maxHeap.Dequeue(out int maxData))
             {
-                output = "-1";
+                minHeap.Dequeue(out int minData);
+                output.AppendLine($"{maxData} {minData}");
             }
             else
             {
-                output = $"{ripedDays.Max()}";
+                output.AppendLine("EMPTY");
             }
         }
-        else
-        {
-            output = "0";
-        }
         Console.Write(output);
+    }
+
+    private class Heap
+    {
+        private Func<int, int, bool> _priority;
+        private int[] _arr;
+        private int _count;
+
+        public Heap(Func<int, int, bool> priority)
+        {
+            _priority = priority;
+            _arr = new int[1000000]; // sc = 4B * 1,000,000 = 4MB
+            _count = 0;
+        }
+
+        public void Enqueue(int data)
+        {
+            _arr[_count] = data;
+
+            int writtenIndex = _count;
+            while (writtenIndex > 0)
+            {
+                int parentIndex = (writtenIndex - 1) / 2;
+                
+                int parentData = _arr[parentIndex];
+                int writtenData = _arr[writtenIndex];
+                if (_priority(parentData, writtenData))
+                    break;
+
+                _arr[parentIndex] = writtenData;
+                _arr[writtenIndex] = parentData;
+                writtenIndex = parentIndex;
+            }
+
+            ++_count;
+        }
+
+        public bool Dequeue(out int output)
+        {
+            output = _arr[0];
+
+            if (_count < 1)
+                return false;
+
+            _arr[0] = _arr[_count - 1];
+            --_count;
+            int parentIndex = 0;
+            while (true)
+            {
+                int leftChildIndex = parentIndex * 2 + 1;
+                int rightChildIndex = parentIndex * 2 + 2;
+
+                int nextParentIndex = parentIndex;
+
+                if (leftChildIndex < _count &&
+                    _priority(_arr[leftChildIndex], _arr[nextParentIndex]))
+                    nextParentIndex = leftChildIndex;
+
+                if (rightChildIndex < _count &&
+                    _priority(_arr[rightChildIndex], _arr[nextParentIndex]))
+                    nextParentIndex = rightChildIndex;
+
+                if (nextParentIndex == parentIndex)
+                    break;
+
+                int temp = _arr[parentIndex];
+                _arr[parentIndex] = _arr[nextParentIndex];
+                _arr[nextParentIndex] = temp;
+
+                parentIndex = nextParentIndex;
+            }
+
+            return true;
+        }
     }
 }
