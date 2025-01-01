e507b3ff48a36a1f0b8ec98de061f191676c64b9
mafuyusame <mafuyu@tutamail.com>
2025-01-01 14:17:21 +0900

    boj 9019 메모리 초과

--- Program.cs
+++ Program.cs
@@ -4,181 +4,99 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        LinkedList<int> buffer = new();
-
-        Heap maxHeap = new Heap((int a, int b) => a > b);
-        Heap minHeap = new Heap((int a, int b) => a < b);
-        SortedDictionary<int, int> availables = new();
+        LinkedList<char>[] commands = new LinkedList<char>[10000];
+        for (int i = 0; i < commands.Length; ++i) // tc = 10000
+        {
+            commands[i] = new();
+        }
 
         StringBuilder output = new();
-        int t = int.Parse(Console.ReadLine()!); // max tc = unkown
-        for (int i = 0; i < t; ++i)
+        int t = int.Parse(Console.ReadLine()!);
+        for (int i = 0; i < t; ++i) // max tc = unknown
         {
-            int k = int.Parse(Console.ReadLine()!); // max tc = 1,000,000
-            for (int j = 0; j < k; ++j)
-            {
-                string[] tokens = Console.ReadLine()!.Split();
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int a = tokens[0]; // [0, 10000)
+            int b = tokens[1];
 
-                string command = tokens[0];
-                int n = int.Parse(tokens[1]);
-                if (command == "I")
+            for (int j = 0; j < commands.Length; ++j) // tc = 10000
+            {
+                commands[j].Clear();
+            }
+            Queue<int> visitingQueue = new();
+
+            visitingQueue.Enqueue(a);
+            while (visitingQueue.Count > 0) // max tc = 10000
+            {
+                int v = visitingQueue.Dequeue();
+                int newCost = commands[v].Count + 1;
+
+                char[] adjCommands = new char[4]
                 {
-                    maxHeap.Enqueue(n); // max tc = log2(1,000,000) = 19.xxxx
-                    minHeap.Enqueue(n);
+                    'D',
+                    'S',
+                    'L',
+                    'R',
+                };
+                int[] adjVs = new int[4]
+                {
+                    CommandD(v),
+                    CommandS(v),
+                    CommandL(v),
+                    CommandR(v),
+                };
+
+                for (int j = 0; j < adjVs.Length; ++j) // tc = 4
+                {
+                    int adjV = adjVs[j];
+
+                    if (adjV == a)
+                        continue;
+
+                    int oldCost = commands[adjV].Count;
+                    if (oldCost > 0)
+                        continue;
                     
-                    if (availables.ContainsKey(n))
+                    for (var node = commands[v].First; node != null; node = node.Next)
                     {
-                        ++availables[n];
+                        commands[adjV].AddLast(node.Value);
                     }
-                    else
-                    {
-                        availables.Add(n, 1);
-                    }
-                }
-                else if (command == "D")
-                {
-                    Heap targetHeap = (n == 1) ? maxHeap : minHeap;
-                    targetHeap.DequeueWithValidation(out int dequeuedData, availables);
+                    commands[adjV].AddLast(adjCommands[j]);
+                    visitingQueue.Enqueue(adjV);
                 }
             }
 
-            if (maxHeap.PeekWithValidation(out int maxData, availables) &&
-                minHeap.PeekWithValidation(out int minData, availables))
+            for (var node =  commands[b].First; node != null; node = node.Next)
             {
-                output.AppendLine($"{maxData} {minData}");
+                output.Append(node.Value);
             }
-            else
-            {
-                output.AppendLine("EMPTY");
-            }
-
-            maxHeap.Clear();
-            minHeap.Clear();
-            availables.Clear();
+            output.AppendLine();
         }
         Console.Write(output);
     }
 
-    private class Heap
+    private static int CommandD(int input)
     {
-        private Func<int, int, bool> _priority;
-        private int[] _arr;
-        private int _count;
-        public int Count => _count;
+        return (input * 2) % 10000;
+    }
 
-        public Heap(Func<int, int, bool> priority)
+    private static int CommandS(int input)
+    {
+        if (--input < 0)
         {
-            _priority = priority;
-            _arr = new int[1000000]; // sc = 4B * 1,000,000 = 4MB
-            _count = 0;
+            input = 9999;
         }
+        return input;
+    }
 
-        public void Enqueue(int data)
-        {
-            _arr[_count] = data;
-
-            int writtenIndex = _count;
-            while (writtenIndex > 0)
-            {
-                int parentIndex = (writtenIndex - 1) / 2;
-
-                int parentData = _arr[parentIndex];
-                int writtenData = _arr[writtenIndex];
-                if (_priority(parentData, writtenData))
-                    break;
-
-                _arr[parentIndex] = writtenData;
-                _arr[writtenIndex] = parentData;
-                writtenIndex = parentIndex;
-            }
-
-            ++_count;
-        }
-
-        public bool PeekWithValidation(out int output, SortedDictionary<int, int> availables)
-        {
-            output = _arr[0]; // peeked data
-
-            while (_count > 0)
-            {
-                output = _arr[0];
-
-                if (availables.ContainsKey(output))
-                {
-                    return true;
-                }
-                else
-                {
-                    Dequeue(out int dequeuedData);
-                }
-            }
-
-            return false;
-        }
-
-        public bool DequeueWithValidation(out int output, SortedDictionary<int, int> availables)
-        {
-            output = 0;
-
-            while (_count > 0)
-            {
-                if (Dequeue(out output) == false)
-                    return false;
-
-                if (availables.ContainsKey(output))
-                {
-                    if (--availables[output] < 1)
-                    {
-                        availables.Remove(output);
-                    }
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public bool Dequeue(out int output)
-        {
-            output = _arr[0];
-
-            if (_count < 1)
-                return false;
-
-            _arr[0] = _arr[_count - 1];
-            --_count;
-            int parentIndex = 0;
-            while (true)
-            {
-                int leftChildIndex = parentIndex * 2 + 1;
-                int rightChildIndex = parentIndex * 2 + 2;
-
-                int nextParentIndex = parentIndex;
-
-                if (leftChildIndex < _count &&
-                    _priority(_arr[leftChildIndex], _arr[nextParentIndex]))
-                    nextParentIndex = leftChildIndex;
-
-                if (rightChildIndex < _count &&
-                    _priority(_arr[rightChildIndex], _arr[nextParentIndex]))
-                    nextParentIndex = rightChildIndex;
-
-                if (nextParentIndex == parentIndex)
-                    break;
-
-                int temp = _arr[parentIndex];
-                _arr[parentIndex] = _arr[nextParentIndex];
-                _arr[nextParentIndex] = temp;
-
-                parentIndex = nextParentIndex;
-            }
-
-            return true;
-        }
-
-        public void Clear()
-        {
-            _count = 0;
-        }
+    private static int CommandL(int input)
+    {
+        int d1 = input / 1000;
+        return ((input * 10) % 10000) + d1;
+    }
+    
+    private static int CommandR(int input)
+    {
+        int d4 = input % 10;
+        return (input / 10) + (d4 * 1000);
     }
 }
