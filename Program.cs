a66bd05e8bfd4c96198c3c3a64ac8e1ee4ce8d9c
mafuyusame <mafuyu@tutamail.com>
2025-01-03 08:24:25 +0900

    boj 15666 1% 오답

--- Program.cs
+++ Program.cs
@@ -1,136 +1,88 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        LinkedList<(int[] rowOffsets, int[] colOffsets)> offsets = new();
-        offsets.AddLast((
-            new int[] { 0, 1, 2, 3 }, // I tetromino (vertical)
-            new int[] { 0, 0, 0, 0 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 0, 0, 0 }, // I tetromino (horizontal)
-            new int[] { 0, 1, 2, 3 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 0, 1, 1 }, // O tetromino
-            new int[] { 0, 1, 0, 1 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 1, 2, 2 }, // L tetromino
-            new int[] { 0, 0, 0, 1 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 0, 0, 1 }, // L tetromino -90deg
-            new int[] { 0, 1, 2, 0 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 0, 1, 2 }, // L tetromino -180deg
-            new int[] { 0, 1, 1, 1 }
-        ));
-        offsets.AddLast((
-            new int[] { 1, 1, 1, 0 }, // L tetromino -270deg
-            new int[] { 0, 1, 2, 2 }
-        ));
-        offsets.AddLast((
-            new int[] { 2, 2, 1, 0 }, // J tetromino 
-            new int[] { 0, 1, 1, 1 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 1, 1, 1 }, // J tetromino -90deg
-            new int[] { 0, 0, 1, 2 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 0, 1, 2 }, // J tetromino -180deg
-            new int[] { 0, 1, 0, 0 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 0, 0, 1 }, // J tetromino -270deg
-            new int[] { 0, 1, 2, 2 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 1, 1, 2 }, // S tetromino
-            new int[] { 0, 0, 1, 1 }
-        ));
-        offsets.AddLast((
-            new int[] { 1, 1, 0, 0 }, // S tetromino -90deg
-            new int[] { 0, 1, 1, 2 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 1, 1, 2 }, // Z tetromino
-            new int[] { 1, 1, 0, 0 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 0, 1, 1 }, // Z tetromino -90deg
-            new int[] { 0, 1, 1, 2 }
-        ));
-        offsets.AddLast((
-            new int[] { 1, 1, 1, 0 }, // T tetromino
-            new int[] { 0, 1, 2, 1 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 1, 1, 2 }, // T tetromino -90deg
-            new int[] { 0, 0, 1, 0 }
-        ));
-        offsets.AddLast((
-            new int[] { 0, 0, 0, 1 }, // T tetromino -180deg
-            new int[] { 0, 1, 2, 1 }
-        ));
-        offsets.AddLast((
-            new int[] { 1, 0, 1, 2 }, // T tetromino -270deg
-            new int[] { 0, 1, 1, 1 }
-        ));
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // tc = 2
+        int n = tokens[0];
+        int m = tokens[1];
 
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int height = tokens[0];
-        int width = tokens[1];
-
-        int[] map = new int[width * height];
-        for (int row = 0; row < height; ++row)
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // max tc = 8
+        SortedSet<int> sorter = new();
+        for (int i = 0; i < tokens.Length; ++i) // max tc = 8
         {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            for (int col = 0; col < width; ++col)
-            {
-                map[row * width + col] = tokens[col]; // [1, 1000]
-            }
-        }
-        
-        const int InvalidSum = -1;
-
-        int maxSum = InvalidSum;
-        for (int index1D = 0; index1D < map.Length; ++index1D)
-        {
-            int row = index1D / width;
-            int col = index1D % width;
-
-            for (var node = offsets.First; node != null; node = node.Next)
-            {
-                int sum = 0;
-                int[] rowOffsets = node.Value.rowOffsets;
-                int[] colOffsets = node.Value.colOffsets;
-                for (int j = 0; j < rowOffsets.Length; ++j)
-                {
-                    int adjRow = row + rowOffsets[j];
-                    int adjCol = col + colOffsets[j];
-                    if (adjRow < 0 || adjRow > height - 1 ||
-                        adjCol < 0 || adjCol > width - 1)
-                    {
-                        sum = InvalidSum;
-                        break;
-                    }
-
-                    sum += map[adjRow * width + adjCol];
-                }
-
-                if (sum == InvalidSum)
-                    continue;
-
-                if (maxSum != InvalidSum && sum < maxSum)
-                    continue;
-
-                maxSum = sum;
-            }
+            sorter.Add(tokens[i]);
         }
 
-        Console.Write(maxSum);
+        int[] selectables = new int[sorter.Count];
+        for (int i = 0; i < selectables.Length; ++i) // max tc = 8
+        {
+            int min = sorter.Min; // max tc = log2(8)
+            selectables[i] = min;
+            sorter.Remove(min);
+        }
+
+        LinkedList<int> sequences = new();
+        Solver(sequences, selectables, m, 0, 0, 0);
+
+        StringBuilder output = new();
+        for (var node = sequences.First; node != null; node = node.Next)
+        {
+            for (int i = m; i > 0; --i) // max tc = 8
+            {
+                int place = ExponentiationBySquaringIteratively(10, i);
+                output.Append($"{(node.Value % (place * 10)) / place} ");
+            }
+            output.AppendLine();
+        }
+        Console.Write(output);
+    }
+
+    private static void Solver(LinkedList<int> sequences,
+                               int[] selectables,
+                               int sequenceLength,
+                               int leastSelectableIndex,
+                               int recursionDepth,
+                               int sequence)
+    {
+        int newRecursionDepth = recursionDepth + 1;
+
+        int placeExponent = sequenceLength - recursionDepth;
+        int place = ExponentiationBySquaringIteratively(10, placeExponent);
+
+        for (int i = leastSelectableIndex; i < selectables.Length; ++i)
+        {
+            int newSequence = sequence + selectables[i] * place;
+
+            if (newRecursionDepth < sequenceLength)
+            {
+                Solver(sequences,
+                       selectables,
+                       sequenceLength,
+                       i,
+                       newRecursionDepth,
+                       newSequence);
+            }
+            else
+            {
+                sequences.AddLast(newSequence);
+            }
+        }
+    }
+
+    private static int ExponentiationBySquaringIteratively(int basis, int exponent)
+    {
+        int output = 1;
+        while (exponent > 0)
+        {
+            if ((exponent & 1) == 1)
+            {
+                output *= basis;
+            }
+            basis *= basis;
+            exponent >>= 1;
+        }
+        return output;
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20d3c57661ae457659dcaec2eb0083647aa2803d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99efe302df62725644b5673de06d541cdb35821")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
