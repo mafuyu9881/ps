c1fa1f306e329ac1e035f95d5e3e6bae7d992703
mafuyusame <mafuyu@tutamail.com>
2025-01-08 16:48:14 +0900

    boj 1043 disjoint set로 풀다가 다른 간단한 풀이가 있을것 같아서 중단

--- Program.cs
+++ Program.cs
@@ -2,97 +2,92 @@
 {
     private static void Main(string[] args)
     {
-        int gridSize = int.Parse(Console.ReadLine()!);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int peopleCount = tokens[0];
+        int partyCount = tokens[1];
 
-        int[] map = new int[gridSize * gridSize];
-        for (int row = 0; row < gridSize; ++row)
+        DisjointSet ds = new(peopleCount);
+
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int witnessCount = tokens[0];
+        LinkedList<int> witnesses = new();
+        for (int i = 1; i < tokens.Length; ++i)
         {
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            for (int col = 0; col < gridSize; ++col)
+            int witness = tokens[i];
+
+            ds.Union(0, witness);
+            witnesses.AddLast(witness);
+        }
+
+        for (int i = 0; i < partyCount; ++i)
+        {
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int memberCount = tokens[0];
+            for (int j = 1; j < tokens.Length; ++j)
             {
-                map[row * gridSize + col] = tokens[col];
+
+            }
+        }
+    }
+
+    private class DisjointSet
+    {
+        private int[] parents;
+        private int[] ranks;
+        
+        public DisjointSet(int n)
+        {
+            int count = n + 1;
+
+            parents = new int[count];
+            for (int i = 0; i < parents.Length; ++i)
+            {
+                parents[i] = i;
+            }
+
+            ranks = new int[count];
+        }
+
+        // find the representative of the set including x
+        public int Find(int x)
+        {
+            int parent = parents[x];
+
+            if (x != parent)
+            {
+                // return Find(parent); // find without compressing path
+                return parents[x] = Find(parent); // find with compressing path
+            }
+            else
+            {
+                return x;
             }
         }
 
-        int srcTailIndex1D = 0; // 0 * gridSize + 0
-        int srcHeadIndex1D = 1; // 0 * gridSize + 1
-
-        int[] visitedCount = new int[map.Length];
-        // state 0 = horizontal, 1 = diagonal, 2 = vertical
-        Queue<(int state, int headIndex1D)> visitingQueue = new();
-
-        visitedCount[srcTailIndex1D] = 1;
-        visitedCount[srcHeadIndex1D] = 1;
-        visitingQueue.Enqueue(new(0, srcHeadIndex1D));
-
-        int[] rowMovementOffsets = new int[] { 0, 1, 1 };
-        int[] colMovementOffsets = new int[] { 1, 1, 0 };
-
-        // the index of the first dimention means state
-        int[][] rowTestOffsetsBundle = new int[3][];
-        int[][] colTestOffsetsBundle = new int[3][];
-
-        rowTestOffsetsBundle[0] = new int[] { 0 };
-        colTestOffsetsBundle[0] = new int[] { 1 };
-
-        rowTestOffsetsBundle[1] = new int[] { 0, 1, 1 };
-        colTestOffsetsBundle[1] = new int[] { 1, 1, 0 };
-
-        rowTestOffsetsBundle[2] = new int[] { 1 };
-        colTestOffsetsBundle[2] = new int[] { 0 };
-
-        while (visitingQueue.Count > 0)
+        public void Union(int a, int b)
         {
-            var dequeuedElement = visitingQueue.Dequeue();
-            int state = dequeuedElement.state;
-            int headIndex1D = dequeuedElement.headIndex1D;
-            int headRow = headIndex1D / gridSize;
-            int headCol = headIndex1D % gridSize;
+            int aSetRepresentative = Find(a);
+            int bSetRepresentative = Find(b);
+            if (aSetRepresentative == bSetRepresentative)
+                return;
 
-            int minNewState = Math.Max(0, state - 1);
-            int maxNewState = Math.Min(state + 1, 2);
-
-            for (int newState = minNewState; newState <= maxNewState; ++newState)
+            int aSetRank = ranks[aSetRepresentative];
+            int bSetRank = ranks[bSetRepresentative];
+            if (aSetRank > bSetRank)
             {
-                int[] rowTestOffsets = rowTestOffsetsBundle[newState];
-                int[] colTestOffsets = colTestOffsetsBundle[newState];
-                bool testSucceeded = true;
-                for (int i = 0; i < rowTestOffsets.Length; ++i)
-                {
-                    int testingRow = headRow + rowTestOffsets[i];
-                    if (testingRow < 0 || testingRow > gridSize - 1)
-                    {
-                        testSucceeded = false;
-                        break;
-                    }
-
-                    int testingCol = headCol + colTestOffsets[i];
-                    if (testingCol < 0 || testingCol > gridSize - 1)
-                    {
-                        testSucceeded = false;
-                        break;
-                    }
-
-                    int testingIndex1D = testingRow * gridSize + testingCol;
-                    if (map[testingIndex1D] == 1)
-                    {
-                        testSucceeded = false;
-                        break;
-                    }
-                }
-
-                if (testSucceeded == false)
-                    continue;
-
-                int newRow = headRow + rowMovementOffsets[newState];
-                int newCol = headCol + colMovementOffsets[newState];
-                int newHeadIndex1D = newRow * gridSize + newCol;
-
-                ++visitedCount[newHeadIndex1D];
-                visitingQueue.Enqueue(new(newState, newHeadIndex1D));
+                parents[bSetRepresentative] = aSetRepresentative;
             }
-        }
+            else if (aSetRank < bSetRank)
+            {
+                parents[aSetRepresentative] = bSetRepresentative;
+            }
+            else
+            {
+                parents[bSetRepresentative] = aSetRepresentative;
+                ++ranks[aSetRepresentative];
+            }
 
-        Console.Write(visitedCount[(gridSize - 1) * gridSize + (gridSize - 1)]);
+            parents[Find(a)] = Find(b); // compress path
+        }
     }
 }
