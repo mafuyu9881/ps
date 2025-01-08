2d8e7386885cd2ace51282d1253f0696b5456de5
mafuyusame <mafuyu@tutamail.com>
2025-01-08 18:20:56 +0900

    [disjoint set] boj 1043

--- Program.cs
+++ Program.cs
@@ -3,31 +3,61 @@
     private static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int peopleCount = tokens[0];
-        int partyCount = tokens[1];
+        int peopleCount = tokens[0]; // [1, 50]
+        int partyCount = tokens[1]; // [1, 50]
 
         DisjointSet ds = new(peopleCount);
 
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int witnessCount = tokens[0];
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // [1, 50]
         LinkedList<int> witnesses = new();
         for (int i = 1; i < tokens.Length; ++i)
         {
             int witness = tokens[i];
-
-            ds.Union(0, witness);
+            ds.Union(tokens[1], witness);
             witnesses.AddLast(witness);
         }
 
+        LinkedList<LinkedList<int>> parties = new();
         for (int i = 0; i < partyCount; ++i)
         {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int memberCount = tokens[0];
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // [2, 50]
+            LinkedList<int> party = new();
             for (int j = 1; j < tokens.Length; ++j)
             {
+                int member = tokens[j];
+                ds.Union(tokens[1], member);
+                party.AddLast(member);
+            }
+            parties.AddLast(party);
+        }
 
+        int output = 0;
+        for (var partyNode = parties.First; partyNode != null; partyNode = partyNode.Next)
+        {
+            if (CanParticipate(ds, witnesses, partyNode.Value))
+            {
+                ++output;
             }
         }
+        Console.Write(output);
+    }
+
+    private static bool CanParticipate(DisjointSet ds, LinkedList<int> witnesses, LinkedList<int> party)
+    {
+        if (witnesses.Count < 1)
+            return true;
+
+        int witness = witnesses.First!.Value;
+
+        for (var node = party.First; node != null; node = node.Next)
+        {
+            if (ds.Find(witness) == ds.Find(node.Value))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private class DisjointSet
