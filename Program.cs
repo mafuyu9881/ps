8e26a1e3a6cbb5f41eab78b6b5cafadc8cb87d21
mafuyusame <mafuyu@tutamail.com>
2025-01-09 23:57:36 +0900

    [coordinate compression] boj 12867

--- Program.cs
+++ Program.cs
@@ -1,65 +1,77 @@
 ﻿internal class Program
 {
-    private static int _boardSize;
-    private static int _requiredQueens;
-    private static bool[]? _lockedRows = null;
-    private static bool[]? _lockedCols = null;
-    private static bool[]? _lockedSlashDiagonal = null;
-    private static bool[]? _lockedBackslashDiagonal = null;
-    private static int _cases = 0;
-
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
-        _boardSize = n;
-        _requiredQueens = n;
+        int dimension = int.Parse(Console.ReadLine()!);
 
-        _lockedRows = new bool[_boardSize];
-        _lockedCols = new bool[_boardSize];
-        _lockedSlashDiagonal = new bool[_boardSize * 2 - 1];
-        _lockedBackslashDiagonal = new bool[_boardSize * 2 - 1];
-
-        Solve(0);
-        Console.Write(_cases);
-    }
-
-    private static void Solve(int currRow)
-    {
-        for (int col = 0; col < _boardSize; ++col)
+        int plans = int.Parse(Console.ReadLine()!);
+        // max tc = 50
+        long[] movedComponents = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
+        
+        SortedSet<long> uniqueComponents = new();
+        for (int i = 0; i < plans; ++i) // max tc = 50
         {
-            if (_lockedRows![currRow])
-                continue;
-
-            if (_lockedCols![col])
-                continue;
-
-            int slashDiagonalIndex = col + currRow;
-            if (_lockedSlashDiagonal![slashDiagonalIndex])
-                continue;
-
-            int backslashDiagonalIndex = col - currRow + (_boardSize - 1);
-            if (_lockedBackslashDiagonal![backslashDiagonalIndex])
-                continue;
-
-            _lockedRows[currRow] = true;
-            _lockedCols[col] = true;
-            _lockedSlashDiagonal[slashDiagonalIndex] = true;
-            _lockedBackslashDiagonal[backslashDiagonalIndex] = true;
-
-            int nextRow = currRow + 1;
-            if (nextRow < _requiredQueens)
-            {
-                Solve(nextRow);
-            }
-            else
-            {
-                ++_cases;
-            }
-
-            _lockedRows[currRow] = false;
-            _lockedCols[col] = false;
-            _lockedSlashDiagonal[slashDiagonalIndex] = false;
-            _lockedBackslashDiagonal[backslashDiagonalIndex] = false;
+            uniqueComponents.Add(movedComponents[i]);
         }
+        
+        SortedDictionary<long, int> compressedComponentMap = new();
+        while (uniqueComponents.Count > 0) // max tc = 50
+        {
+            long component = uniqueComponents.Min;
+            uniqueComponents.Remove(component);
+
+            compressedComponentMap.Add(component, compressedComponentMap.Count);
+        }
+
+        int[][] coordinateHistory = new int[plans + 1][];
+        for (int i = 0; i < coordinateHistory.Length; ++i)
+        {
+            coordinateHistory[i] = new int[compressedComponentMap.Count];
+        }
+
+        string operators = Console.ReadLine()!;
+
+        int output = 1;
+        for (int i = 1; i <= plans; ++i) // max tc = 50
+        {
+            for (int k = 0; k < compressedComponentMap.Count; ++k) // max tc = 50
+            {
+                coordinateHistory[i][k] = coordinateHistory[i - 1][k];
+            }
+
+            long component = movedComponents[i - 1];
+            int compressedComponent = compressedComponentMap[component]; // max tc = log2(50) = 5.xxx
+            
+            if (operators[i - 1] == '+')
+            {
+                ++coordinateHistory[i][compressedComponent];
+            }
+            else // if (operators[i] == '-')
+            {
+                --coordinateHistory[i][compressedComponent];
+            }
+
+            for (int j = 0; j < i; ++j) // max tc = 50
+            {
+                bool duplicated = true;
+                for (int k = 0; k < compressedComponentMap.Count; ++k) // max tc = 50
+                {
+                    if (coordinateHistory[i][k] == coordinateHistory[j][k])
+                        continue;
+
+                    duplicated = false;
+                }
+
+                if (duplicated == false)
+                    continue;
+
+                output = 0;
+                break;
+            }
+
+            if (output == 0)
+                break;
+        }
+        Console.Write(output);
     }
 }
