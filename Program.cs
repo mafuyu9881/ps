ac9e938f186c6001870c9a7b9ab07cde835099d2
mafuyusame <mafuyu@tutamail.com>
2025-01-11 14:17:54 +0900

    boj 1790 풀이 도중 커밋

--- Program.cs
+++ Program.cs
@@ -2,55 +2,29 @@
 {
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // max tc = 1000000
-        int stations = tokens[0]; // [2, 100000]
-        int soldiers = tokens[1]; // [2, 100000]
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // [1, 100`000`000]
+        int k = tokens[1]; // [1, 1`000`000`000]
 
-        // element in [1, 1000]
-        int[] intervalTimes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // max tc = 1000000
-
-        // SI = station interval
-        int[] accIntervalTimes = new int[intervalTimes.Length + 1];
-        // leave 0 index as zero
-        for (int i = 1; i < accIntervalTimes.Length; ++i)
+        int[] arr = new int[]; // max sc = 9 + 1 = 10
+        for (int i = 1; i < arr.Length; ++i) // max tc = 10
         {
-            accIntervalTimes[i] = accIntervalTimes[i - 1] + intervalTimes[i - 1];
+            arr[i] = arr[i - 1] + i * 9 * ExponentiationBySquaringIteratively(10, (i - 1));
         }
+    }
 
-        int cycleTime = accIntervalTimes[accIntervalTimes.Length - 1];
-
-        int travelTime = 0;
-        for (int i = 0; i < soldiers; ++i) // max tc = 100000
+    private static int ExponentiationBySquaringIteratively(int basis, int exponent)
+    {
+        int output = 1;
+        while (exponent > 0)
         {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            // points are 1-based
-            int bp = tokens[0] - 1; // boarding point [0, 100000 - 1]
-            int ap = tokens[1] - 1; // alighting point [0, 100000 - 1]
-            int bt = tokens[2]; // boardable time [0, 1000000000]
-
-            int cyclicClampedBT = bt % cycleTime;
-
-            // ceil or floor boardable time to accumulated interval time
-            int boardingTime = bt + (accIntervalTimes[bp] - cyclicClampedBT);
-            // if it floored, we need just one more cycle
-            // no need more over than one because we used cyclicly clamped boardable time (cyclicClampedBT)
-            if (boardingTime < bt)
+            if ((exponent & 1) == 1)
             {
-                boardingTime += cycleTime;
+                output *= basis;
             }
-
-            // It's guaranteed that boarding point and alighting point are not equeal
-            int fromBPToAPTime = accIntervalTimes[ap] - accIntervalTimes[bp];
-            // if fromBPToAPTime is lower than 0, it means the boarding point is on before the alighting point
-            if (fromBPToAPTime < 0)
-            {
-                fromBPToAPTime += cycleTime;
-            }
-
-            int alightingTime = boardingTime + fromBPToAPTime;
-
-            travelTime = Math.Max(travelTime, alightingTime);
+            basis *= basis;
+            exponent >>= 1;
         }
-        Console.Write(travelTime);
+        return output;
     }
 }
