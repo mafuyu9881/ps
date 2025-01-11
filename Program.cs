5c8f42004b563fd089621ed5ea9856281654c0a6
mafuyusame <mafuyu@tutamail.com>
2025-01-11 23:24:38 +0900

    boj 1790 배열 인덱스 접근 위반 코드

--- Program.cs
+++ Program.cs
@@ -1,67 +1,52 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int boardSize = tokens[0];
-        int coins = tokens[1];
+        int n = tokens[0]; // [1, 100`000`000]
+        int k = tokens[1]; // [1, 1`000`000`000]
 
+        int nDigitCount = (int)Math.Log10(n) + 1; // [1, 9]
+        int kNumberDigitCount = 1; // digit count of the number which includes kth digit [1, 9]
 
-    }
-
-    private static int[]? Solve(int baseIndex, int boardSize, int coins)
-    {
-        int[]? output = null;
-
-        int cells = boardSize * boardSize;
-
-        // board size is even
-        if (boardSize % 2 == 0)
+        int arrLength = nDigitCount + 1;
+        // start number among the same digit numbers
+        int[] starts = new int[arrLength];
+        // counts of the same digit numbers
+        long[] counts = new long[arrLength];
+        long[] accCounts = new long[arrLength]; // max sc = 8B * (9 + 1) = 80B
+        for (int i = 1; i < arrLength; ++i) // max tc = 10
         {
-            int blacks = cells / 2;
-            int whites = blacks;
+            starts[i] = ExponentiationBySquaringIteratively(10, i - 1);
+            counts[i] = i * 9 * starts[i];
+            accCounts[i] = accCounts[i - 1] + counts[i];
 
-            if (coins < blacks)
-                return output;
-
-            coins -= blacks;
-            if (coins % 2 == 1)
-                return output;
-
-            output = new int[2];
-            output[0] = blacks + coins / 2;
-            output[1] = coins / 2;
-            return output;
-        }
-        else
-        {
-            int blacks = cells / 2 + 1;
-            int whites = cells / 2 + 0;
-
-            int[] squaresArr = new int[] { blacks, whites };
-
-            for (int color0 = 0; color0 < squaresArr.Length; ++color0)
+            if (k > accCounts[i])
             {
-                int color1 = color0 == 0 ? 1 : 0;
-
-                int color0Squares = squaresArr[color0];
-
-                if (coins < color0Squares)
-                    continue;
-
-                int remainCoins = coins - color0Squares;
-                if ((remainCoins % cells) == 1)
-                    continue;
-
-                // base coins + 
-                output[color0] += color0Squares; // base coins
-                // piled up coins
-                // remain coins
-                 + remainCoins - (remainCoins % (cells * 2));
-                break;
+                ++kNumberDigitCount;
             }
         }
 
-        // board size is odd
+        long clampedK = k - accCounts[kNumberDigitCount - 1]; // [1, ?)
+        long placedNumberCount = (clampedK - 1) / kNumberDigitCount;
+        long kNumber = starts[kNumberDigitCount] + placedNumberCount;
+        Console.Write($"{kNumber}"[(int)((clampedK - 1) % kNumberDigitCount)]);
+    }
+
+    private static int ExponentiationBySquaringIteratively(int basis, int exponent)
+    {
+        int output = 1;
+        while (exponent > 0)
+        {
+            if ((exponent & 1) == 1)
+            {
+                output *= basis;
+            }
+            basis *= basis;
+            exponent >>= 1;
+        }
+        return output;
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c436db6ddddd6240926020ad612aa525490a2ef4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fd72631ca355484d80bdb712f128afa9cf3f8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
