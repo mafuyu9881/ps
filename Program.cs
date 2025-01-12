99a8afe1b74c7e5e56fbe5fb3fc41a533ded6eca
mafuyusame <mafuyu@tutamail.com>
2025-01-12 19:33:56 +0900

    [math] boj 27433

--- Program.cs
+++ Program.cs
@@ -1,59 +1,14 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // [1, 100`000`000]
-        int k = tokens[1]; // [1, 1`000`000`000]
+        int n = int.Parse(Console.ReadLine()!); // [0, 20]
 
-        int nDigitCount = (int)Math.Log10(n) + 1; // [1, 9]
-        int kNumberDigitCount = 1; // digit count of the number which includes kth digit [1, 9]
-
-        int arrLength = 9 + 1;
-        // start number among the same digit numbers
-        long[] starts = new long[arrLength];
-        // counts of the same digit numbers
-        long[] counts = new long[arrLength];
-        long[] accCounts = new long[arrLength]; // max sc = 8B * (9 + 1) = 80B
-        for (int i = 1; i < arrLength; ++i) // max tc = 10
+        long output = 1;
+        for (int i = n; i > 0; --i)
         {
-            starts[i] = ExponentiationBySquaringIteratively(10, i - 1);
-            counts[i] = i * 9 * starts[i];
-            accCounts[i] = accCounts[i - 1] + counts[i];
-
-            if (k > accCounts[i])
-            {
-                ++kNumberDigitCount;
-            }
+            output *= i;
         }
-
-        long clampedK = k - accCounts[kNumberDigitCount - 1]; // [1, ?)
-        long placedNumberCount = (clampedK - 1) / kNumberDigitCount;
-        long kNumber = starts[kNumberDigitCount] + placedNumberCount;
-        if (n < kNumber)
-        {
-            Console.Write(-1);
-        }
-        else
-        {
-            Console.Write($"{kNumber}"[(int)((clampedK - 1) % kNumberDigitCount)]);
-        }
-    }
-
-    private static int ExponentiationBySquaringIteratively(int basis, int exponent)
-    {
-        int output = 1;
-        while (exponent > 0)
-        {
-            if ((exponent & 1) == 1)
-            {
-                output *= basis;
-            }
-            basis *= basis;
-            exponent >>= 1;
-        }
-        return output;
+        Console.Write(output);
     }
 }
