ac1a599f4cc3961ff77f9bc80ab725096dd3208d
mafuyusame <mafuyu@tutamail.com>
2025-01-13 19:01:13 +0900

    [math] boj 30030

--- Program.cs
+++ Program.cs
@@ -2,13 +2,6 @@
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [0, 20]
-
-        long output = 1;
-        for (int i = n; i > 0; --i)
-        {
-            output *= i;
-        }
-        Console.Write(output);
+        Console.Write(int.Parse(Console.ReadLine()!) / 11 * 10);
     }
 }
