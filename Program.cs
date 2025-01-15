44ff9aabd22a087c6901789c052dd84e9b9440b6
mafuyusame <mafuyu@tutamail.com>
2025-01-15 18:16:52 +0900

    boj 10476 12% 오답 코드

--- Program.cs
+++ Program.cs
@@ -1,27 +1,85 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
+    const int InvalidValue = -1;
+
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // [3, 200]
+        int k = tokens[1]; // [0, n]
 
-        StringBuilder output = new();
+        int[,] museum = new int[n, 2];
         for (int i = 0; i < n; ++i)
         {
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int brains = tokens[0];
-            int requiredBrains = tokens[1];
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            museum[i, 0] = tokens[0]; // [0, 100]
+            museum[i, 1] = tokens[1]; // [0, 100]
+        }
 
-            if (brains < requiredBrains)
+        // the element on third dimension means state
+        // state consists of these following things
+        // 0: left gallery is only available
+        // 1: right gallery is only available
+        // 2: both galleries in the same row are available
+        int firstDimensionLength = n;
+        int secondDimensionLength = Math.Max(k + 1, 2);
+        int thirdDimensionLength = 3;
+        int[,,] dp = new int[firstDimensionLength, secondDimensionLength, thirdDimensionLength]; // max sc = 200 * (200 + 1) * 3
+        for (int i = 0; i < firstDimensionLength; ++i)
+        {
+            for (int j = 0; j < secondDimensionLength; ++j)
             {
-                output.AppendLine("NO BRAINS");
-            }
-            else
-            {
-                output.AppendLine("MMM BRAINS");
+                for (int l = 0; l < thirdDimensionLength; ++l)
+                {
+                    dp[i, j, l] = InvalidValue;
+                }
             }
         }
-        Console.Write(output);
+        
+        dp[0, 1, 0] = museum[0, 0];
+        dp[0, 1, 1] = museum[0, 1];
+        dp[0, 0, 2] = museum[0, 0] + museum[0, 1];
+
+        for (int row = 1; row < n; ++row)
+        {
+            for (int closed = 0; closed <= k; ++closed)
+            {
+                int leftGalleryValue = museum[row, 0];
+                int rightGalleryValue = museum[row, 1];
+
+                if (closed > 0)
+                {
+                    dp[row, closed, 0] = leftGalleryValue + Max(new int[] { dp[row - 1, closed - 1, 0], dp[row - 1, closed - 1, 2] });
+                    dp[row, closed, 1] = rightGalleryValue + Max(new int[] { dp[row - 1, closed - 1, 1], dp[row - 1, closed - 1, 2] });
+                }
+
+                if (k < n)
+                {
+                    dp[row, closed, 2] = leftGalleryValue + rightGalleryValue + Max(new int[] { dp[row - 1, closed, 0], dp[row - 1, closed, 1], dp[row - 1, closed, 2] });
+                }
+            }
+        }
+
+        Console.Write(Max(new int[] { dp[n - 1, k, 0], dp[n - 1, k, 1], dp[n - 1, k, 2] }));
+    }
+    
+    private static int Max(ReadOnlySpan<int> span)
+    {
+        int output = InvalidValue;
+
+        for (int i = 0; i < span.Length; ++i)
+        {
+            int element = span[i];
+
+            if (element == InvalidValue)
+                continue;
+
+            if (output != InvalidValue && element < output)
+                continue;
+
+            output = element;
+        }
+
+        return output;
     }
 }
