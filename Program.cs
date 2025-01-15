7cc040c30944cb8c51554e12dabc6c9701206dbb
mafuyusame <mafuyu@tutamail.com>
2025-01-16 06:12:53 +0900

    [string] boj 28064

--- Program.cs
+++ Program.cs
@@ -1,87 +1,53 @@
 ﻿internal class Program
 {
-    const int InvalidValue = -1;
-
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // [3, 200]
-        int k = tokens[1]; // [0, n]
+        int n = int.Parse(Console.ReadLine()!); // [1, 100]
 
-        int[,] museum = new int[n, 2];
+        string[] strings = new string[n];
         for (int i = 0; i < n; ++i)
         {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            museum[i, 0] = tokens[0]; // [0, 100]
-            museum[i, 1] = tokens[1]; // [0, 100]
+            strings[i] = Console.ReadLine()!;
         }
-        
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        // the element on third dimension means state
-        // state consists of these following things
-        // 0: left gallery is only available
-        // 1: right gallery is only available
-        // 2: both galleries in the same row are available
-        int firstDimensionLength = n;
-        int secondDimensionLength = Math.Max(k + 1, 2);
-        int thirdDimensionLength = 3;
-        int[,,] dp = new int[firstDimensionLength, secondDimensionLength, thirdDimensionLength]; // max sc = 200 * (200 + 1) * 3
-        for (int i = 0; i < firstDimensionLength; ++i)
+        bool[,] duplicated = new bool[n, n]; // max sc = 10'000
+
+        int output = 0;
+        for (int i = 0; i < n; ++i) // max tc = 100
         {
-            for (int j = 0; j < secondDimensionLength; ++j)
+            for (int j = 0; j < n; ++j) // max tc = 100
             {
-                for (int l = 0; l < thirdDimensionLength; ++l)
+                if (i == j)
+                    continue;
+
+                if (duplicated[i, j])
+                    continue;
+
+                string s0 = strings[i];
+                string s1 = strings[j];
+                for (int k = 0; k < Math.Min(s0.Length, s1.Length); ++k) // max tc = 20
                 {
-                    dp[i, j, l] = InvalidValue;
+                    // compare the rear of s0 and the head of s1
+                    bool localDuplicated = true;
+                    for (int l = 0; l <= k; ++l) // max tc = 20
+                    {
+                        if (s0[s0.Length - 1 - k + l] == s1[l])
+                            continue;
+
+                        localDuplicated = false;
+                        break;
+                    }
+
+                    if (localDuplicated == false)
+                        continue;
+
+                    duplicated[i, j] = true;
+                    duplicated[j, i] = true;
+                    ++output;
+                    break;
                 }
             }
         }
-        
-        dp[0, 1, 0] = museum[0, 0];
-        dp[0, 1, 1] = museum[0, 1];
-        dp[0, 0, 2] = museum[0, 0] + museum[0, 1];
-
-        for (int row = 1; row < n; ++row)
-        {
-            for (int closed = 0; closed <= k; ++closed)
-            {
-                int leftGalleryValue = museum[row, 0];
-                int rightGalleryValue = museum[row, 1];
-
-                if (closed > 0 && closed <= row + 1)
-                {
-                    dp[row, closed, 0] = leftGalleryValue + Max(new int[] { dp[row - 1, closed - 1, 0], dp[row - 1, closed - 1, 2] });
-                    dp[row, closed, 1] = rightGalleryValue + Max(new int[] { dp[row - 1, closed - 1, 1], dp[row - 1, closed - 1, 2] });
-                }
-
-                if (closed < row + 1)
-                {
-                    dp[row, closed, 2] = leftGalleryValue + rightGalleryValue + Max(new int[] { dp[row - 1, closed, 0], dp[row - 1, closed, 1], dp[row - 1, closed, 2] });
-                }
-            }
-        }
-
-        Console.Write(Max(new int[] { dp[n - 1, k, 0], dp[n - 1, k, 1], dp[n - 1, k, 2] }));
-    }
-    
-    private static int Max(ReadOnlySpan<int> span)
-    {
-        int output = InvalidValue;
-
-        for (int i = 0; i < span.Length; ++i)
-        {
-            int element = span[i];
-
-            if (element == InvalidValue)
-                continue;
-
-            if (output != InvalidValue && element < output)
-                continue;
-
-            output = element;
-        }
-
-        return output;
+        Console.Write(output);
     }
 }
