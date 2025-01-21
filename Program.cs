ca0b99e6e447522604d6128c8dba4b942252ffac
mafuyusame <mafuyu@tutamail.com>
2025-01-21 22:57:44 +0900

    boj 18430 프루닝 하기 전 코드

--- Program.cs
+++ Program.cs
@@ -1,69 +1,124 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
+    private const int Vacated = 0;
+    private const int Occupied = 1;
+
+    private static int _height;
+    private static int _width;
+
+    private static int[,] _map = null!;
+
+    private static int[,] _solidity = null!;
+    
+    // type a = ┓, b = ┛, c = ┗, d = ┏
+    private static (int row, int col)[] _aOffsets = new (int, int)[] { (0, 0), (0, 1), (1, 1) };
+    private static (int row, int col)[] _bOffsets = new (int, int)[] { (1, 0), (1, 1), (0, 1) };
+    private static (int row, int col)[] _cOffsets = new (int, int)[] { (0, 0), (1, 0), (1, 1) };
+    private static (int row, int col)[] _dOffsets = new (int, int)[] { (1, 0), (0, 0), (0, 1) };
+
     private static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int height = tokens[0]; // [1, 1'000]
-        int width = tokens[1]; // [1, 1'000]
+        _height = tokens[0];
+        _width = tokens[1];
 
-        int subjects = int.Parse(Console.ReadLine()!);
+        _map = new int[_height, _width];
 
-        int[,] jungles = new int[height, width];
-        int[,] oceans = new int[height, width];
-        int[,] ices = new int[height, width];
-
-        for (int row = 0; row < height; ++row) // max tc = 1'000
+        _solidity = new int[_height, _width];
+        for (int row = 0; row < _height; ++row)
         {
-            string line = Console.ReadLine()!;
-            for (int col = 0; col < width; ++col) // max tc = 1'000
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            for (int col = 0; col < _width; ++col)
             {
-                char biome = line[col];
-                Accumulate(jungles, row, col, biome == 'J');
-                Accumulate(oceans, row, col, biome == 'O');
-                Accumulate(ices, row, col, biome == 'I');
+                _solidity[row, col] = tokens[col];
             }
         }
 
-        StringBuilder output = new();
-        for (int i = 0; i < subjects; ++i)
+        int maxSolidities = 0;
+        int solidities = 0;
+        Solve(ref maxSolidities, ref solidities);
+        Console.Write(maxSolidities);
+    }
+
+    private static void Solve(ref int maxSolidities, ref int solidities)
+    {
+        for (int row = 0; row < _height - 1; ++row)
         {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int x0 = tokens[0] - 1;
-            int y0 = tokens[1] - 1;
-            int x1 = tokens[2] - 1;
-            int y1 = tokens[3] - 1;
+            for (int col = 0; col < _width - 1; ++col)
+            {
+                if (Placeable(_aOffsets, row, col))
+                {
+                    solidities += Write(_aOffsets, row, col, Occupied);
+                    maxSolidities = Math.Max(maxSolidities, solidities);
+                    Solve(ref maxSolidities, ref solidities);
+                    solidities -= Write(_aOffsets, row, col, Vacated);
+                }
 
-            Research(output, jungles, x0, y0, x1, y1);
-            Research(output, oceans, x0, y0, x1, y1);
-            Research(output, ices, x0, y0, x1, y1);
-            output.AppendLine();
+                if (Placeable(_bOffsets, row, col))
+                {
+                    solidities += Write(_bOffsets, row, col, Occupied);
+                    maxSolidities = Math.Max(maxSolidities, solidities);
+                    Solve(ref maxSolidities, ref solidities);
+                    solidities -= Write(_bOffsets, row, col, Vacated);
+                }
+
+                if (Placeable(_cOffsets, row, col))
+                {
+                    solidities += Write(_cOffsets, row, col, Occupied);
+                    maxSolidities = Math.Max(maxSolidities, solidities);
+                    Solve(ref maxSolidities, ref solidities);
+                    solidities -= Write(_cOffsets, row, col, Vacated);
+                }
+
+                if (Placeable(_dOffsets, row, col))
+                {
+                    solidities += Write(_dOffsets, row, col, Occupied);
+                    maxSolidities = Math.Max(maxSolidities, solidities);
+                    Solve(ref maxSolidities, ref solidities);
+                    solidities -= Write(_dOffsets, row, col, Vacated);
+                }
+            }
         }
-        Console.Write(output);
+    }
+    
+    private static bool Placeable((int row, int col)[] offsets, int basisRow, int basisCol)
+    {
+        for (int i = 0; i < offsets.Length; ++i)
+        {
+            var offset = offsets[i];
+
+            int row = basisRow + offset.row;
+            if (row > _height - 1)
+                return false;
+
+            int col = basisCol + offset.col;
+            if (col > _width - 1)
+                return false;
+
+            if (_map[row, col] == Occupied)
+                return false;
+        }
+
+        return true;
     }
 
-    private static void Accumulate(int[,] arr, int row, int col, bool detected)
+    private static int Write((int row, int col)[] offsets, int basisRow, int basisCol, int state)
     {
-        bool leftUnavailable = (col - 1) < 0;
-        bool upUnavailable = (row - 1) < 0;
+        int solidities = 0;
 
-        int fromLeft = leftUnavailable ? 0 : arr[row, col - 1];
-        int fromUp = upUnavailable ? 0 : arr[row - 1, col];
-        int duplicated = (leftUnavailable || upUnavailable) ? 0 : arr[row - 1, col - 1];
-        
-        arr[row, col] = fromLeft + fromUp + (detected ? 1 : 0) - duplicated;
-    }
+        for (int i = 0; i < offsets.Length; ++i)
+        {
+            var offset = offsets[i];
 
-    private static void Research(StringBuilder output, int[,] arr, int x0, int y0, int x1, int y1)
-    {
-        bool leftUnavailable = (x0 - 1) < 0;
-        bool upUnavailable = (y0 - 1) < 0;
+            // it is guaranteed that row and col are valid here
+            int row = basisRow + offset.row;
+            int col = basisCol + offset.col;
 
-        int fromLeft = leftUnavailable ? 0 : arr[x0 - 1, y1];
-        int fromUp = upUnavailable ? 0 : arr[x1, y0 - 1];
-        int duplicated = (leftUnavailable || upUnavailable) ? 0 : arr[x0 - 1, y0 - 1];
+            _map[row, col] = state;
 
-        output.Append($"{arr[x1, y1] - fromUp - fromLeft + duplicated} ");
+            solidities += _solidity[row, col] * ((i != 1) ? 1 : 2);
+        }
+
+        return solidities;
     }
 }
