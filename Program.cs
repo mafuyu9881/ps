c32c6f7341883d4a453fab21bf805b15ad28ce5d
mafuyusame <mafuyu@tutamail.com>
2025-01-25 21:46:33 +0900

    connected component with stack 버전 1

--- Program.cs
+++ Program.cs
@@ -4,32 +4,94 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
-        int[] holes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int h = tokens[0]; // [1, 500]
+        int w = tokens[1]; // [1, 500]
 
-        int[] outHoles = new int[100000 + 1];
-        int writingIndex = 1;
-        for (int i = 0; i < n; ++i) // max tc = 100'000
+        int[] map = new int[w * h];
+        LinkedList<int> objectives = new();
+        for (int r = 0; r < h; ++r)
         {
-            // the ith hole's actual size is
-            // (its original size) + (its order), (i starts from 0)
-            // this is because the acorns shrink with each roll
-            int actualHole = Math.Min(holes[i] + i, 100000); // and there is no acorn larger than 10000 (to prevent out-of-range index errors)
-
-            while (writingIndex <= actualHole) // tc (total loops count, excluding duplicates) = 100'000
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            for (int c = 0; c < w; ++c)
             {
-                outHoles[writingIndex] = i + 1;
-                ++writingIndex;
+                int idx = r * w + c;
+                map[idx] = tokens[c];
+                objectives.AddLast(idx);
             }
         }
 
-        int q = int.Parse(Console.ReadLine()!);
-        int[] acorns = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        StringBuilder output = new();
-        for (int i = 0; i < acorns.Length; ++i)
+        Stack<int> s = new();
+        bool[] visited = new bool[map.Length];
+        LinkedList<LinkedList<int>> pictures = new();
+        LinkedList<int> maxPicture = null!;
+        LinkedList<int> picture = new();
+
+        while (true) // max tc = 250'000
         {
-            output.AppendLine($"{outHoles[acorns[i]]}");
+            while (objectives.Count > 0)
+            {
+                var node = objectives.First!;
+                s.Push(node.Value);
+                objectives.Remove(node);
+                break;
+            }
+            
+            if (s.Count < 1)
+                break;
+
+            int idx = s.Pop();
+            if (visited[idx])
+                continue;
+
+            visited[idx] = true;
+            if (map[idx] == 0)
+                continue;
+
+            picture.AddLast(idx);
+            
+            int r = idx / w;
+            int c = idx % w;
+            
+            bool isolated = true;
+            (int r, int c)[] offsets = new (int, int)[] { (-1, 0), (1, 0), (0, -1), (0, 1) };
+            for (int i = 0; i < offsets.Length; ++i)
+            {
+                var offset = offsets[i];
+
+                int adjR = r + offset.r;
+                if (adjR < 0 || adjR > h - 1)
+                    continue;
+
+                int adjC = c + offset.c;
+                if (adjC < 0 || adjC > w - 1)
+                    continue;
+
+                int adjIdx = adjR * w + adjC;
+                if (visited[adjIdx])
+                    continue;
+
+                if (map[adjIdx] == 0)
+                    continue;
+
+                s.Push(adjIdx);
+                isolated = false;
+            }
+
+            if (isolated)
+            {
+                pictures.AddLast(picture);
+                if (maxPicture == null || picture.Count > maxPicture.Count)
+                {
+                    maxPicture = picture;
+                }
+                picture = new();
+            }
         }
+
+        StringBuilder output = new();
+        output.AppendLine($"{pictures.Count}");
+        output.AppendLine($"{maxPicture.Count}");
         Console.Write(output);
     }
 }
