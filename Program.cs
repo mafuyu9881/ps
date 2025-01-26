826e6b3b98a68e85a3d7644190d60cc2891cfa3e
mafuyusame <mafuyu@tutamail.com>
2025-01-26 19:22:12 +0900

    [implementation] boj 30678

--- Program.cs
+++ Program.cs
@@ -2,96 +2,74 @@
 
 internal class Program
 {
+    private static int[,] _shape = new int[,]
+    {
+        { 0, 0, 1, 0, 0 },
+        { 0, 0, 1, 0, 0 },
+        { 1, 1, 1, 1, 1 },
+        { 0, 1, 1, 1, 0 },
+        { 0, 1, 0, 1, 0 },
+    };
+    
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int h = tokens[0]; // [1, 500]
-        int w = tokens[1]; // [1, 500]
+        int n = int.Parse(Console.ReadLine()!); // [0, 5]
 
-        int[] map = new int[w * h];
-        LinkedList<int> candidates = new();
-        for (int r = 0; r < h; ++r) // max tc = 500
-        {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            for (int c = 0; c < w; ++c) // max tc = 500
-            {
-                int idx = r * w + c;
-                map[idx] = tokens[c];
-                candidates.AddLast(idx);
-            }
-        }
-
-        Queue<int> q = new();
-        bool[] visited = new bool[map.Length];
-        LinkedList<LinkedList<int>> pictures = new();
-        LinkedList<int> maxPicture = new();
-        LinkedList<int> picture = new();
-
-        while (true) // max tc = 250'000
-        {
-            while (q.Count < 1 && candidates.Count > 0)
-            {
-                var node = candidates.First!;
-                int candidateIdx = node.Value;
-                candidates.Remove(node);
-
-                if (visited[candidateIdx])
-                    continue;
-
-                if (map[candidateIdx] == 0)
-                    continue;
-
-                picture = new();
-                pictures.AddLast(picture);
-                visited[candidateIdx] = true;
-                q.Enqueue(node.Value);
-                break;
-            }
-            
-            if (q.Count < 1)
-                break;
-
-            int idx = q.Dequeue();
-            int r = idx / w;
-            int c = idx % w;
-
-            picture.AddLast(idx);
-            if (picture.Count > maxPicture.Count)
-            {
-                maxPicture = picture;
-            }
-            
-            (int r, int c)[] offsets = new (int, int)[] { (-1, 0), (1, 0), (0, -1), (0, 1) };
-            for (int i = 0; i < offsets.Length; ++i)
-            {
-                var offset = offsets[i];
-
-                int adjR = r + offset.r;
-                if (adjR < 0 || adjR > h - 1)
-                    continue;
-
-                int adjC = c + offset.c;
-                if (adjC < 0 || adjC > w - 1)
-                    continue;
-
-                int adjIdx = adjR * w + adjC;
-                if (visited[adjIdx])
-                    continue;
-
-                if (map[adjIdx] == 0)
-                    continue;
-
-                visited[adjIdx] = true;
-                q.Enqueue(adjIdx);
-            }
-        }
-
-        // in line 46 and 84, we have to code them in both places rather than line 57
-        // if we code it on line 57, we will face duplicated elements in q
-
+        int w = ExponentiationBySquaringIteratively(5, n);
+        
         StringBuilder output = new();
-        output.AppendLine($"{pictures.Count}");
-        output.AppendLine($"{maxPicture.Count}");
+        if (n > 0)
+        {
+            for (int r = 0; r < w; ++r) // max tc = 5 ^ 5 = 3'125
+            {
+                for (int c = 0; c < w; ++c) // max tc = 3'125
+                {
+                    output.Append(Evaluate(r, c, n - 1) ? "*" : " ");
+                }
+                output.AppendLine();
+            }
+        }
+        else
+        {
+            output.Append("*");
+        }
         Console.Write(output);
+    }
+
+    private static int ExponentiationBySquaringIteratively(int basis, int exponent)
+    {
+        int output = 1;
+        while (exponent > 0)
+        {
+            if (exponent % 2 == 1)
+            {
+                output *= basis;
+            }
+            basis *= basis;
+            exponent >>= 1;
+        }
+        return output;
+    }
+
+    private static bool Evaluate(int r, int c, int n)
+    {
+        int powN = ExponentiationBySquaringIteratively(5, n);
+
+        int rowSec = r / powN;
+        int colSec = c / powN;
+
+        if (_shape[rowSec, colSec] == 0)
+            return false;
+
+        if (n > 0)
+        {
+            int nextR = r - rowSec * powN;
+            int nextC = c - colSec * powN;
+            return Evaluate(nextR, nextC, n - 1);
+        }
+        else
+        {
+            return true;
+        }
     }
 }
