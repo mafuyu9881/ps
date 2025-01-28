e00fb56132a850a28a999c2ea9ea44542b62e674
mafuyusame <mafuyu@tutamail.com>
2025-01-28 18:37:48 +0900

    boj 1600 풀이 도중 커밋

--- Program.cs
+++ Program.cs
@@ -2,25 +2,114 @@
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [2, 100]
+        const int AdjOffsets = 4;
+        int[] adjRowOffsets = new int[AdjOffsets] { -1, 1, 0, 0 };
+        int[] adjColOffsets = new int[AdjOffsets] { 0, 0, -1, 1 };
+        const int HorseOffsets = 8;
+        int[] horseRowOffsets = new int[HorseOffsets] { -1, -2, -2, -1, 1, 2, 2, 1 };
+        int[] horseColOffsets = new int[HorseOffsets] { -2, -1, 1, 2, -2, -1, 1, 2 };
 
-        // dp[r, d] = all possible ways to place them in row 'r', at distance 'd'
-        int[,] dp = new int[n + 1, n + 1];
-        dp[2, 1] = 2;
-        for (int r = 3; r <= n; ++r)
+        int k = int.Parse(Console.ReadLine()!); // [0, 30]
+
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int width = tokens[0]; // [1, 200]
+        int height = tokens[1]; // [1, 200]
+
+        int[] map = new int[width * height];
+        for (int row = 0; row < height; ++row)
         {
-            for (int d = 1; d < r; ++d)
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            for (int col = 0; col < width; ++col)
             {
-                // rhs = down/down + diagonal/diagonal + down/diagonal + diagonal/down
-                dp[r, d] = (dp[r - 1, d] + dp[r - 1, d] + dp[r - 1, d - 1] + dp[r - 1, d + 1]) % 10007;
+                map[row * width + col] = tokens[col];
             }
         }
 
-        int output = 0;
-        for (int d = 1; d < n; ++d)
+        const int InvalidMoves = -1;
+        
+        Queue<int> waitings = new();
+        (int moves, int usedK)[] cost = new (int, int)[width * height];
+        for (int i = 0; i < cost.Length; ++i) // max tc = 40'000
         {
-            output = (output + dp[n, d]) % 10007;
+            cost[i] = (InvalidMoves, 0);
         }
-        Console.Write(output);
+
+        int srcIndex = 0;
+        waitings.Enqueue(srcIndex);
+        cost[srcIndex] = (0, 0);
+
+        // it's is better to get more k count when both have same distance
+        while (waitings.Count > 0)
+        {
+            int index = waitings.Dequeue();
+            int row = index / width;
+            int col = index % width;
+
+            int newMoves = cost[index].moves + 1;
+            int newUsedK = cost[index].usedK;
+
+            for (int i = 0; i < AdjOffsets; ++i)
+            {
+                int adjRow = row + adjRowOffsets[i];
+                if (adjRow < 0 || adjRow > height - 1)
+                    continue;
+
+                int adjCol = col + adjColOffsets[i];
+                if (adjCol < 0 || adjCol > width - 1)
+                    continue;
+
+                int adjIndex = adjRow * width + adjCol;
+                if (adjIndex == srcIndex)
+                    continue;
+
+                if (map[adjIndex] == 1)
+                    continue;
+
+                int oldMoves = cost[adjIndex].moves;
+                int oldUsedK = cost[adjIndex].usedK;
+                if ((oldMoves != InvalidMoves) &&
+                    ((oldMoves < newMoves) || 
+                     (oldMoves == newMoves && oldUsedK <= newUsedK)))
+                    continue;
+
+                cost[adjIndex] = (newMoves, newUsedK);
+                waitings.Enqueue(adjIndex);
+            }
+
+            if (newUsedK < k)
+            {
+                ++newUsedK;
+
+                for (int i = 0; i < HorseOffsets; ++i)
+                {
+                    int horseRow = row + horseRowOffsets[i];
+                    if (horseRow < 0 || horseRow > height - 1)
+                        continue;
+
+                    int horseCol = col + horseColOffsets[i];
+                    if (horseCol < 0 || horseCol > width - 1)
+                        continue;
+
+                    int horseIndex = horseRow * width + horseCol;
+                    if (horseIndex == 0)
+                        continue;
+
+                    if (map[horseIndex] == 1)
+                        continue;
+
+                    int oldMoves = cost[horseIndex].moves;
+                    int oldUsedK = cost[horseIndex].usedK;
+                    if ((oldMoves != InvalidMoves) &&
+                        ((oldMoves < newMoves) ||
+                         (oldMoves == newMoves && oldUsedK <= newUsedK)))
+                        continue;
+
+                    cost[horseIndex] = (newMoves, newUsedK);
+                    waitings.Enqueue(horseIndex);
+                }
+            }
+        }
+
+        Console.Write(cost[cost.Length - 1].moves);
     }
 }
