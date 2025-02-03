46db6ca01cc085a2b19aee94bdde127db117abbb
mafuyusame <mafuyu@tutamail.com>
2025-02-03 23:00:40 +0900

    [bruteforcing] boj 1503

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿internal class Program
 {
-    private static int MAX = 1001;
+    private const int MAX = 1001;
+    private const int InvalidDiff = -1;
     private static bool[] _s = new bool[MAX + 1];
 
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
         int n = tokens[0]; // [1, 1'000]
         int m = tokens[1]; // [0, 50]
 
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // element = [1, 1'000]
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), int.Parse); // element = [1, 1'000]
         for (int i = 0; i < tokens.Length; ++i) // max tc = 50
         {
             _s[tokens[i]] = true;
         }
 
-        const int InvalidDiff = -1;
         int diff = InvalidDiff;
         for (int x = 1; x <= MAX; ++x)
         {
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dc5c9f4ed05b51eb039a1c39aae9226e2e646eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+942897061b96ac7bfa3c62807b9e28d33021825d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
