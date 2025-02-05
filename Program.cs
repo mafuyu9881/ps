10423979750b948f8721d9cda3ed2deb8be274c1
mafuyusame <mafuyu@tutamail.com>
2025-02-05 22:56:02 +0900

    [bfs] boj 2660

--- Program.cs
+++ Program.cs
@@ -4,33 +4,92 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        const int Threshold = 1000000009;
+        int n = int.Parse(Console.ReadLine()!); // [?, 50]
 
-        int t = int.Parse(Console.ReadLine()!); // [?, ?]
-
-        int[,] dp = new int[1001, 1001];
-        dp[1, 1] = 1;
-        dp[2, 1] = 1;
-        dp[2, 2] = 1;
-        dp[3, 1] = 1;
-        dp[3, 2] = 2;
-        dp[3, 3] = 1;
-        for (int j = 4; j < 1001; ++j) // tc = 997
+        LinkedList<int>[] adjList = new LinkedList<int>[n + 1];
+        while (true)
         {
-            for (int k = 2; k <= j; ++k) // max tc = 999
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int a = tokens[0]; // [1, 50]
+            int b = tokens[1]; // [1, 50]
+
+            if (a == -1 && b == a)
+                break;
+
+            if (adjList[a] == null)
             {
-                dp[j, k] += (dp[j - 1, k - 1] + dp[j - 2, k - 1]) % Threshold;
-                dp[j, k] = (dp[j, k] + dp[j - 3, k - 1]) % Threshold;
+                adjList[a] = new();
+            }
+            if (adjList[b] == null)
+            {
+                adjList[b] = new();
+            }
+
+            adjList[a].AddLast(b);
+            adjList[b].AddLast(a);
+        }
+
+        const int InvalidDistance = -1;
+
+        int mainScore = InvalidDistance;
+        PriorityQueue<int, int> candidates = new();
+        for (int beginV = 1; beginV <= n; ++beginV) // max tc = 50
+        {
+            int[] distances = new int[n + 1];
+            for (int i = 0; i < distances.Length; ++i) // max tc = 51
+            {
+                distances[i] = InvalidDistance;
+            }
+            Queue<int> q = new();
+            
+            distances[beginV] = 0;
+            q.Enqueue(beginV);
+            
+            int subScore = InvalidDistance;
+            while (q.Count > 0) // max tc = 50 * (50 - 1) / 2 = 1'225
+            {
+                int v = q.Dequeue();
+
+                int newDistance = distances[v] + 1;
+
+                var adjs = adjList[v];
+                for (var node = adjs.First; node != null; node = node.Next) // max tc = 49
+                {
+                    int adjV = node.Value;
+
+                    int oldDistance = distances[adjV];
+                    if (oldDistance != InvalidDistance &&
+                        oldDistance <= newDistance)
+                        continue;
+
+                    distances[adjV] = newDistance;
+                    q.Enqueue(adjV);
+
+                    if (subScore != InvalidDistance &&
+                        subScore >= newDistance)
+                        continue;
+
+                    subScore = newDistance;
+                }
+            }
+
+            if (mainScore == InvalidDistance || mainScore > subScore)
+            {
+                mainScore = subScore;
+                candidates.Clear();
+            }
+
+            if (mainScore == subScore)
+            {
+                candidates.Enqueue(beginV, beginV); // max tc = log2(50) = 5.xxx
             }
         }
 
         StringBuilder sb = new();
-        for (int i = 0; i < t; ++i)
+        sb.AppendLine($"{mainScore} {candidates.Count}");
+        while (candidates.Count > 0) // max tc = 50
         {
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int n = tokens[0]; // [1, 1'000]
-            int m = tokens[1]; // [1, n]
-            sb.AppendLine($"{dp[n, m]}");
+            sb.Append($"{candidates.Dequeue()} ");
         }
         Console.Write(sb);
     }
