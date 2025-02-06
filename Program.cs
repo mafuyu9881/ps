a103c2175bac15259f6ba61983c6c1a6e5237e8e
mafuyusame <mafuyu@tutamail.com>
2025-02-06 13:26:13 +0900

    [priority queue] boj 11256
    실제 문제 태그는 greedy, sort

--- Program.cs
+++ Program.cs
@@ -7,13 +7,14 @@ internal class Program
         int t = int.Parse(Console.ReadLine()!);
 
         StringBuilder sb = new();
-        for (int i = 0; i < t; ++i)
+        for (int i = 0; i < t; ++i) // max tc = 1'000
         {
             int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int candies = tokens[0]; // [1, 1'000]
+            int j = tokens[0]; // [1, 1'000]
+            int n = tokens[1]; // [1, 1'000]
 
             PriorityQueue<int, int> boxes = new();
-            for (int j = 0; j < tokens[1]; ++j) // max tc = 1'000
+            for (int k = 0; k < n; ++k) // max tc = 1'000
             {
                 tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
                 int v = tokens[0]; // [1, 10'000]
@@ -23,9 +24,9 @@ internal class Program
             }
             
             int usage = 0;
-            while (candies > 0)
+            while (j > 0)
             {
-                candies -= boxes.Dequeue();
+                j -= boxes.Dequeue();
                 ++usage;
             }
             sb.AppendLine($"{usage}");
