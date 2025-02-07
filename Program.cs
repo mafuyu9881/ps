77c40eed74aac5d6fc9f702918aafd66ee08a30b
mafuyusame <mafuyu@tutamail.com>
2025-02-07 21:29:48 +0900

    [string] boj 2744

--- Program.cs
+++ Program.cs
@@ -1,64 +1,29 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
-    private static (int row, int col)[] _zero = new (int, int)[]
-    {
-        (1, 2), (1, 3), (2, 1), (2, 4), (3, 1), (3, 4), (4, 1), (4, 4), (5, 2), (5, 3)
-    };
-    
-    private static (int row, int col)[] _one = new (int, int)[]
-    {
-        (1, 3), (2, 2), (2, 3), (3, 3), (4, 3), (5, 3)
-    };
-
-    private static (int row, int col)[] _two = new (int, int)[]
-    {
-    };
-
-    private static (int row, int col)[] _three = new (int, int)[]
-    {
-    };
-
-    private static (int row, int col)[] _four = new (int, int)[]
-    {
-    };
-    
-    private static (int row, int col)[] _five = new (int, int)[]
-    {
-    };
-    
-    private static (int row, int col)[] _six = new (int, int)[]
-    {
-    };
-
-    private static (int row, int col)[] _seven = new (int, int)[]
-    {
-    };
-
-    private static (int row, int col)[] _eight = new (int, int)[]
-    {
-    };
-
-    private static (int row, int col)[] _nine = new (int, int)[]
-    {
-    };
-
     private static void Main(string[] args)
     {
-        int boardHeight = 7;
-        int boardWidth;
-        
-        int[][] map = new int[boardHeight][];
-        for (int row = 0; row < map.Length; ++row) // tc = 7
-        {
-            string s = Console.ReadLine()!;
-            map[row] = new int[s.Length];
-            boardWidth = s.Length;
-            for (int col = 0; col < s.Length; ++col)
-            {
-                map[row][col] = s[col] - '0';
-            }
-        }
+        string s = Console.ReadLine()!;
 
-        
+        char converter = (char)('a' - 'A');
+
+        StringBuilder sb = new();
+        for (int i = 0; i < s.Length; ++i)
+        {
+            char c = s[i];
+
+            if (c < 'a')
+            {
+                c += converter;
+            }
+            else
+            {
+                c -= converter;
+            }
+
+            sb.Append(c);
+        }
+        Console.Write(sb);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba05074ca2977109a8ade0f39374fc326bc68c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8444c08d6d210f7ce288a3be733b803a760b9753")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
