ff379fee31429a64d36998894cc658145ec90145
mafuyusame <mafuyu@tutamail.com>
2025-02-10 20:22:44 +0900

    [implementation] boj 4101

--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c40eed74aac5d6fc9f702918aafd66ee08a30b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60070f1013e9beeb358948c4953e354be4e7d021")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
