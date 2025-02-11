4f076641bf5ed2a453189b3c3c53d0a2ed6ce897
mafuyusame <mafuyu@tutamail.com>
2025-02-11 19:51:44 +0900

    boj 18428 컴파일 에러
    boj에선 using 지시문에 값 튜플을 지정할 수 없습니다.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,133 @@
-﻿using System.Text;
+﻿using STLine = (int st0, int st1);
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        StringBuilder sb = new();
+        int n = int.Parse(Console.ReadLine()!); // [3, 6]
+
+        char[] map = new char[n * n];
+        for (int row = 0; row < n; ++row) // max tc = 6
+        {
+            char[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), char.Parse); // max tc = 6
+            for (int col = 0; col < n; ++col) // max tc = 6
+            {
+                map[row * n + col] = tokens[col];
+            }
+        }
+
+        const char X = 'X';
+        const int InvalidIndex = -1;
+
+        LinkedList<STLine> horizontals = new();
+        for (int row = 0; row < n; ++row) // max tc = 6
+        {
+            int lastSTIndex = InvalidIndex;
+
+            for (int col = 0; col < n; ++col) // max tc = 6
+            {
+                int currIndex = row * n + col;
+
+                char currCharacter = map[currIndex];
+                if (currCharacter == X)
+                    continue;
+
+                if (lastSTIndex != InvalidIndex && currCharacter != map[lastSTIndex])
+                    horizontals.AddLast((lastSTIndex, currIndex));
+
+                lastSTIndex = currIndex;
+            }
+        }
+
+        LinkedList<STLine> verticals = new();
+        for (int col = 0; col < n; ++col) // max tc = 6
+        {
+            int lastSTIndex = InvalidIndex;
+
+            for (int row = 0; row < n; ++row) // max tc = 6
+            {
+                int currIndex = row * n + col;
+
+                char currCharacter = map[currIndex];
+                if (currCharacter == X)
+                    continue;
+
+                if (lastSTIndex != InvalidIndex && currCharacter != map[lastSTIndex])
+                    verticals.AddLast((lastSTIndex, currIndex));
+
+                lastSTIndex = currIndex;
+            }
+        }
+        
+        int obstacles = 0;
         while (true)
         {
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int a = tokens[0]; // [1, 1'000'000]
-            int b = tokens[1]; // [1, 1'000'000]
-            if (a == 0 && b == 0)
+            LinkedListNode<STLine>? selfNode = null;
+            LinkedList<STLine>? opponents = null;
+            if (horizontals.Count > 0)
+            {
+                selfNode = horizontals.First;
+                opponents = verticals;
+            }
+            else if (verticals.Count > 0)
+            {
+                selfNode = verticals.First;
+                opponents = horizontals;
+            }
+            
+            if (selfNode == null || opponents == null) // check opponents too to avoid compiler's warning
                 break;
 
-            sb.AppendLine((a > b) ? "Yes" : "No");
+            int selfST0Index = selfNode.Value.st0;
+            int selfST0Row = selfST0Index / n;
+            int selfST0Col = selfST0Index % n;
+            int selfST1Index = selfNode.Value.st1;
+            int selfST1Row = selfST1Index / n;
+            int selfST1Col = selfST1Index % n;
+
+            for (var opponentNode = opponents.First; opponentNode != null; opponentNode = opponentNode.Next)
+            {
+                int oppoST0Index = opponentNode.Value.st0;
+                int oppoST1Index = opponentNode.Value.st1;
+
+                if (opponents == verticals)
+                {
+                    int oppoCol = oppoST0Index % n;
+                    if (oppoCol <= selfST0Col || oppoCol >= selfST1Col)
+                        continue;
+
+                    int selfRow = selfST0Index / n;
+                    int oppoST0Row = oppoST0Index / n;
+                    int oppoST1Row = oppoST1Index / n;
+                    if (selfRow <= oppoST0Row || selfRow >= oppoST1Row)
+                        continue;
+
+                    opponents.Remove(opponentNode);
+                    break;
+                }
+                else
+                {
+                    int oppoRow = oppoST0Index / n;
+                    if (oppoRow <= selfST0Row || oppoRow >= selfST1Row)
+                        continue;
+
+                    int selfCol = selfST0Index % n;
+                    int oppoST0Col = oppoST0Index % n;
+                    int oppoST1Col = oppoST1Index % n;
+                    if (selfCol <= oppoST0Col || selfCol >= oppoST1Col)
+                        continue;
+
+                    opponents.Remove(opponentNode);
+                    break;
+                }
+            }
+
+            var selves = selfNode.List!;
+            selves.Remove(selfNode);
+
+            ++obstacles;
         }
-        Console.Write(sb);
+
+        Console.Write((obstacles > 3) ? "NO" : "YES");
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60070f1013e9beeb358948c4953e354be4e7d021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff379fee31429a64d36998894cc658145ec90145")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
