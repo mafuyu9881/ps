396981056407e4732ec752818001389ec14394ac
mafuyusame <mafuyu@tutamail.com>
2025-02-19 13:39:14 +0900

    [math, implementation] boj 15552

--- Program.cs
+++ Program.cs
@@ -4,27 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [1, 1'000]
-        
-        // length = [1, 1'000]
-        // element = [1, 1'000'000]
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-        int[] prefixSum = new int[tokens.Length];
-        for (int i = 0; i < prefixSum.Length; ++i) // max tc = 1'000
-        {
-            prefixSum[i] = tokens[i];
-
-            if (i > 0)
-            {
-                prefixSum[i] += prefixSum[i - 1];
-            }
-        }
+        int t = int.Parse(Console.ReadLine()!); // [1, 1'000'000]
 
         StringBuilder sb = new();
-        for (int i = 1; i <= n; ++i) // max tc = 1'000
+        for (int i = 0; i < t; ++i) // max tc = 1'000'000
         {
-            
+            // element = [1, 1'000]
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            sb.AppendLine($"{tokens[0] + tokens[1]}");
         }
         Console.Write(sb);
     }
