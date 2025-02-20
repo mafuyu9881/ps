3347b80a3077a759b0a5af11222a463b6a1a42c3
mafuyusame <mafuyu@tutamail.com>
2025-02-20 22:38:48 +0900

    boj 2758 풀이 도중 커밋

--- Program.cs
+++ Program.cs
@@ -4,89 +4,38 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [1, 1'000]
+        int t = int.Parse(Console.ReadLine()!); // [1, 10]
 
-        int m = int.Parse(Console.ReadLine()!); // [1, 100'000]
-
-        int[] tokens = null!;
-        LinkedList<(int dstV, int cost)>[] adjList = new LinkedList<(int, int)>[n + 1];
-        for (int i = 0; i < m; ++i) // max tc = 100'000
-        {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-            int v0 = tokens[0]; // [1, 1'000]
-            int v1 = tokens[1]; // [1, 1'000]
-            int cost = tokens[2]; // [0, 100'000]
-
-            if (adjList[v0] == null)
-                adjList[v0] = new();
-
-            adjList[v0].AddLast((v1, cost));
-        }
-
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // tc = 2
-        int departureV = tokens[0];
-        int arrivalV = tokens[1];
-
-        PriorityQueue<int, int> frontier = new();
-        
-        const int InvalidCost = -1;
-        int[] minCosts = new int[n + 1];
-        for (int i = 1; i < minCosts.Length; ++i) // max tc = 1'000
-        {
-            minCosts[i] = InvalidCost;
-        }
-
-        frontier.Enqueue(departureV, 0);
-        minCosts[departureV] = 0;
-        
-        int[] path = new int[n + 1];
-        while (frontier.Count > 0) // max tc = 1'000 * (1'000 - 1) / 2 = 499'500
-        {
-            int v = frontier.Dequeue();
-            if (v == arrivalV)
-                break;
-
-            var adjs = adjList[v];
-            if (adjs == null)
-                continue;
-
-            for (var lln = adjs.First; lln != null; lln = lln.Next)
-            {
-                var adj = lln.Value;
-                int adjV = adj.dstV;
-                int adjCost = adj.cost;
-
-                int oldCost = minCosts[adjV];
-                int newCost = minCosts[v] + adjCost;
-                if (oldCost != InvalidCost && oldCost <= newCost)
-                    continue;
-
-                minCosts[adjV] = newCost;
-                frontier.Enqueue(adjV, newCost);
-                path[adjV] = v;
-            }
-        }
-
-        int backtrackingV = arrivalV;
-        LinkedList<int> arrivalPath = new();
-        while (true) // max tc = 1'000
-        {
-            arrivalPath.AddFirst(backtrackingV);
-
-            if (backtrackingV == departureV)
-                break;
-
-            backtrackingV = path[backtrackingV];
-        }
-        
         StringBuilder sb = new();
-        sb.AppendLine($"{minCosts[arrivalV]}");
-        sb.AppendLine($"{arrivalPath.Count}");
-        for (var lln = arrivalPath.First; lln != null; lln = lln.Next) // max tc = 1'000
+        for (int i = 0; i < t; ++i) // max tc = 10
         {
-            sb.Append($"{lln.Value} ");
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int n = tokens[0]; // [1, 10]
+            int m = tokens[1]; // [1, 2'000]
+
+            int output = 0;
+            Solve(ref output, n, m, 1, 1);
+            sb.AppendLine($"{output}");
         }
         Console.Write(sb);
+    }
+
+    private static void Solve(ref int output, int n, int m, int currBeginNumber, int depth)
+    {
+        if (depth < n)
+        {
+            for (int i = currBeginNumber; i <= m; ++i)
+            {
+                int nextBeginNumber = i * 2;
+                if (nextBeginNumber > m)
+                    break;
+
+                Solve(ref output, n, m, nextBeginNumber, depth + 1);
+            }
+        }
+        else
+        {
+            output += m - currBeginNumber + 1;
+        }
     }
 }
