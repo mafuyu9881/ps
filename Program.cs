afeb37d6dbc376cd77fa0093c210d2aa24515ee9
mafuyusame <mafuyu@tutamail.com>
2025-02-23 18:26:44 +0900

    [two pointers] boj 22862

--- Program.cs
+++ Program.cs
@@ -2,97 +2,41 @@
 {
     private static void Main(string[] args)
     {
-        // element = [1, 100]
-        int[] integerTokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int width = integerTokens[0];
-        int height = integerTokens[1];
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // [1, 1'000'000]
+        int k = tokens[1]; // [1, 100'000]
 
-        LinkedList<int> waitings = new();
-        char[] map = new char[width * height];
-        for (int row = 0; row < height; ++row) // max tc = 100
+        // element = [1, 1'000'000]
+        int[] sequence = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+        int s = 0;
+        int e = 0;
+
+        int longestLength = 0;
+        LinkedList<int> oddIndices = new();
+        while (true) // max tc = 1'000'000
         {
-            string token = Console.ReadLine()!;
-            for (int col = 0; col < width; ++col) // max tc = 100
+            int newLength = e - s + 1 - oddIndices.Count;
+            if (sequence[s] % 2 == 1)
+                --newLength;
+
+            longestLength = Math.Max(longestLength, newLength);
+            
+            ++e;
+
+            if (e > sequence.Length - 1)
+                break;
+
+            if (sequence[e] % 2 == 1)
+                oddIndices.AddLast(e);
+
+            if (oddIndices.Count > k)
             {
-                int index = row * width + col;
-
-                map[index] = token[col];
-
-                waitings.AddLast(index);
+                var lln = oddIndices.First!;
+                s = lln.Value;
+                oddIndices.Remove(lln);
             }
         }
-
-        const int AdjOffsets = 4;
-        int[] AdjRowOffsets = new int[AdjOffsets] { -1, 1, 0, 0 };
-        int[] AdjColOffsets = new int[AdjOffsets] { 0, 0, -1, 1 };
-
-        bool[] visited = new bool[map.Length];
-        Queue<int> frontier = new();
-
-        LinkedList<int> cc = null!;
-        LinkedList<LinkedList<int>> wccs = new();
-        LinkedList<LinkedList<int>> bccs = new();
-        while (waitings.Count > 0) // max tc = 10'000
-        {
-            if (frontier.Count < 1)
-            {
-                var lln = waitings.First!;
-                int llnValue = lln.Value;
-                waitings.Remove(lln);
-
-                if (visited[llnValue])
-                    continue;
-                
-                visited[llnValue] = true;
-
-                frontier.Enqueue(llnValue);
-                
-                cc = new();
-                if (map[llnValue] == 'W')
-                    wccs.AddLast(cc);
-                else
-                    bccs.AddLast(cc);
-            }
-
-            int index = frontier.Dequeue();
-            int row = index / width;
-            int col = index % width;
-
-            cc.AddLast(index);
-
-            for (int i = 0; i < AdjOffsets; ++i)
-            {
-                int adjRow = row + AdjRowOffsets[i];
-                if (adjRow < 0 || adjRow > height - 1)
-                    continue;
-
-                int adjCol = col + AdjColOffsets[i];
-                if (adjCol < 0 || adjCol > width - 1)
-                    continue;
-
-                int adjIndex = adjRow * width + adjCol;
-                if (visited[adjIndex])
-                    continue;
-
-                if (map[index] != map[adjIndex])
-                    continue;
-
-                visited[adjIndex] = true;
-                
-                frontier.Enqueue(adjIndex);
-            }
-        }
-
-        int wforce = 0;
-        for (var lln = wccs.First; lln != null; lln = lln.Next)
-        {
-            wforce += lln.Value.Count * lln.Value.Count;
-        }
-        int bforce = 0;
-        for (var lln = bccs.First; lln != null; lln = lln.Next)
-        {
-            bforce += lln.Value.Count * lln.Value.Count;
-        }
-        Console.Write($"{wforce} {bforce}");
+        Console.Write(longestLength);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23020b8a69785a8e4d021186e7800698c2dd6a40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28f65daa40342a57d82025e42eed4a8cc1b12a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
