df74771f56dd14c63b4a6ee255550edfe1c76423
mafuyusame <mafuyu@tutamail.com>
2025-02-23 14:59:45 +0900

    boj 22857 98% 오답 코드

--- Program.cs
+++ Program.cs
@@ -2,29 +2,44 @@
 {
     private static void Main(string[] args)
     {
-        string token = Console.ReadLine()!;
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // [1, 50'000]
+        int k = tokens[1]; // [1, 100]
 
-        int invalidBrackets = 0;
-        int leftBrackets = 0;
-        for (int i = 0; i < token.Length; ++i)
+        int[] sequence = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        int longestLength = 0;
+        int[][] dp = new int[n][];
+        dp[0] = new int[k + 1];
+        for (int j = 0; j <= k; ++j) // max tc = 101
         {
-            char c = token[i];
-            if (c == '(')
+            dp[0][j] = sequence[0] % 2 == 0 ? 1 : 0;
+        }
+        for (int i = 1; i < dp.Length; ++i) // max tc = 49'999
+        {
+            dp[i] = new int[dp[0].Length];
+
+            for (int j = 0; j <= k; ++j) // max tc = 101
             {
-                ++leftBrackets;
-            }
-            else // if (c == ')')
-            {
-                if (leftBrackets > 0)
+                if (sequence[i] % 2 == 0)
                 {
-                    --leftBrackets;
+                    dp[i][j] = dp[i - 1][j] + 1;
                 }
                 else
                 {
-                    ++invalidBrackets;
+                    if (j > 0)
+                    {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                    else
+                    {
+                        dp[i][j] = 0;
+                    }
                 }
+
+                longestLength = Math.Max(longestLength, dp[i][j]);
             }
         }
-        Console.Write(invalidBrackets + leftBrackets);
+        Console.Write(longestLength);
     }
 }
