d8bec251ca25780e636adb38cf0d39fd9b5be322
mafuyusame <mafuyu@tutamail.com>
2025-02-28 21:31:54 +0900

    boj 2022 30% 오답 코드

--- Program.cs
+++ Program.cs
@@ -1,135 +1,36 @@
 ﻿internal class Program
 {
-    private class DisjointSet
-    {
-        private int[] _parent = null!;
-        private int[] _rank = null!;
-
-        public DisjointSet(int n)
-        {
-            _parent = new int[n];
-            for (int i = 0; i < _parent.Length; ++i)
-            {
-                _parent[i] = i;
-            }
-
-            _rank = new int[n];
-        }
-        
-        public int Find(int x)
-        {
-            int parent = _parent[x];
-
-            if (parent != x)
-            {
-                return _parent[x] = Find(parent); // find with path compressing
-            }
-            else
-            {
-                return x; // it's a representative
-            }
-        }
-
-        public void Union(int a, int b)
-        {
-            int aSetRepresentative = Find(a);
-            int bSetRepresentative = Find(b);
-
-            if (aSetRepresentative == bSetRepresentative)
-                return;
-
-            int aSetRank = _rank[aSetRepresentative];
-            int bSetRank = _rank[bSetRepresentative];
-
-            if (aSetRank > bSetRank)
-            {
-                _parent[bSetRepresentative] = aSetRepresentative;
-            }
-            else if (aSetRank < bSetRank)
-            {
-                _parent[aSetRepresentative] = bSetRepresentative;
-            }
-            else
-            {
-                _parent[bSetRepresentative] = aSetRepresentative;
-                ++_rank[aSetRepresentative];
-            }
-
-            // path compressing
-            Find(a);
-            Find(b);
-        }
-
-        public bool United()
-        {
-            if (_parent.Length < 1)
-                return true; // undefined behaviour
-
-            for (int i = 1; i < _parent.Length; ++i) // max tc = 50
-            {
-                if (_parent[Find(i)] != _parent[Find(0)])
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-    }
-
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [1, 50]
+        // element = (0, 3'000'000'000]
+        double[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), double.Parse);
+        double x = tokens[0];
+        double y = tokens[1];
+        double c = tokens[2];
 
-        PriorityQueue<(int s, int e, int cost), int> frontier = new();
-
-        int donatable = 0;
-
-        for (int s = 0; s < n; ++s)
+        double lo = 0.0;
+        double hi = Math.Min(x, y);
+        while (hi - lo > 1e-6) // max tc = log2(3'000'000'000) = 31.xxx
         {
-            string stringToken = Console.ReadLine()!;
-            for (int e = 0; e < n; ++e)
+            double mid = (lo + hi) / 2;
+
+            double c1 = Math.Sqrt(x * x - mid * mid);
+            double c2 = Math.Sqrt(y * y - mid * mid);
+
+            double computedC = c1 * c2 / (c1 + c2);
+            if (Math.Abs(computedC - c) < 1e-6)
             {
-                char charToken = stringToken[e];
-
-                if (charToken != '0')
-                {
-                    int cost;
-                    if (charToken >= 'a' && charToken <= 'z')
-                    {
-                        cost = charToken - 'a' + 1;
-                    }
-                    else
-                    {
-                        cost = charToken - 'A' + 27;
-                    }
-
-                    frontier.Enqueue((s, e, cost), cost);
-
-                    donatable += cost;
-                }
+                Console.Write(mid.ToString("F3"));
+                break;
+            }
+            else if (computedC > c)
+            {
+                lo = mid;
+            }
+            else // if (computedC < c)
+            {
+                hi = mid;
             }
         }
-
-        DisjointSet ds = new(n);
-
-        while (frontier.Count > 0)
-        {
-            var secost = frontier.Dequeue();
-            int s = secost.s;
-            int e = secost.e;
-
-            if (ds.Find(s) == ds.Find(e))
-                continue;
-
-            ds.Union(s, e);
-
-            donatable -= secost.cost;
-        }
-
-        if (ds.United() == false)
-            donatable = -1;
-            
-        Console.Write(donatable);
     }
 }
