e47ebd992aa6a8b74a66c1006e2c15b064af5a28
mafuyusame <mafuyu@tutamail.com>
2025-03-09 13:13:09 +0900

    [simulation, greedy] boj 31964

--- Program.cs
+++ Program.cs
@@ -15,19 +15,18 @@
             int t = ts[i];
             xts[i] = (x, t);
         }
-        
         Array.Sort(xts, (a, b) => a.x.CompareTo(b.x));
 
         bool[] picked = new bool[n];
 
+        int truckX = 0;
         long elapsedT = 0;
 
         for (int i = 0; i < n; ++i) // max tc = 3'000
         {
-            int prevX = (i <= 0) ? 0 : xts[i - 1].x;
             int currX = xts[i].x;
 
-            int deltaT = currX - prevX;
+            int deltaT = currX - truckX;
             int currT = xts[i].t;
 
             elapsedT += deltaT;
@@ -36,6 +35,8 @@
             {
                 picked[i] = true;
             }
+            
+            truckX = currX;
         }
 
         for (int i = n - 1; i >= 0; --i) // max tc = 3'000
@@ -43,14 +44,9 @@
             if (picked[i])
                 continue;
 
-            int prevX = (i >= n - 1) ? xts[n - 1].x : xts[i + 1].x;
-            if (i >= n - 1)
-                prevX = xts[n - 1].x;
-
-
             int currX = xts[i].x;
 
-            int deltaT = prevX - currX;
+            int deltaT = truckX - currX;
             int currT = xts[i].t;
 
             elapsedT += deltaT;
@@ -61,9 +57,11 @@
             }
 
             picked[i] = true;
+            truckX = currX;
         }
         
-        elapsedT += xts[0].x;
+        elapsedT += truckX;
+        truckX = 0;
 
         Console.Write(elapsedT);
     }
