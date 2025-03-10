ab751b50ce7a81fddbe9afa7f40e828229146edf
mafuyusame <mafuyu@tutamail.com>
2025-03-10 11:29:12 +0900

    [geometry] boj 14842

--- Program.cs
+++ Program.cs
@@ -1,105 +1,21 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
-    const int Size = 9;
-    
-    private static bool[] _contained = new bool[Size + 1];
-
     private static void Main(string[] args)
     {
-        int t = int.Parse(Console.ReadLine()!); // [1, 100]
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int width = tokens[0]; // [1, 10'000]
+        int height = tokens[1]; // [1, 10'000]
+        int n = tokens[2]; // [2, 10'000'000]
 
-        int[,] board = new int[Size, Size];
+        double segmentedWidth = height / (double)n;
 
-        StringBuilder sb = new();
-        for (int i = 0; i < t; ++i) // max tc = 100
+        double required = 0.0;
+        for (int i = ((n & 1) == 1) ? 1 : 2; i < n; i += 2)
         {
-            if (i > 0)
-            {
-                Console.ReadLine();
-            }
-            
-            for (int row = 0; row < Size; ++row) // tc = 9
-            {
-                int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-                for (int col = 0; col < Size; ++col) // tc = 9
-                {
-                    board[row, col] = tokens[col];
-                }
-            }
-
-            bool correct = true;
-
-            for (int row = 0; row < Size; ++row) // tc = 9
-            {
-                ClearContained();
-                for (int col = 0; col < Size; ++col) // tc = 9
-                {
-                    int n = board[row, col];
-
-                    if (_contained[n])
-                    {
-                        correct = false;
-                        goto Evaluate;
-                    }
-
-                    _contained[n] = true;
-                }
-            }
-
-            for (int col = 0; col < Size; ++col) // tc = 9
-            {
-                ClearContained();
-                for (int row = 0; row < Size; ++row) // tc = 9
-                {
-                    int n = board[row, col];
-
-                    if (_contained[n])
-                    {
-                        correct = false;
-                        goto Evaluate;
-                    }
-
-                    _contained[n] = true;
-                }
-            }
-
-            for (int rowBasis = 0; rowBasis < Size; rowBasis += 3) // tc = 3
-            {
-                for (int colBasis = 0; colBasis < Size; colBasis += 3) // tc = 3
-                {
-                    ClearContained();
-                    for (int rowOffset = 0; rowOffset < 3; ++rowOffset) // tc = 3
-                    {
-                        for (int colOffset = 0; colOffset < 3; ++colOffset) // tc = 3
-                        {
-                            int n = board[rowBasis + rowOffset, colBasis + colOffset];
-
-                            if (_contained[n])
-                            {
-                                correct = false;
-                                goto Evaluate;
-                            }
-
-                            _contained[n] = true;
-                        }
-                    }
-                }
-            }
-            
-Evaluate:
-            sb.Append($"Case {i + 1}: ");
-            sb.AppendLine(correct ? "CORRECT" : "INCORRECT");
+            required += segmentedWidth * i;
         }
-        Console.Write(sb);
-    }
+        required *= 2;
 
-    private static void ClearContained()
-    {
-        for (int i = 1; i < _contained.Length; ++i) // tc = 9
-        {
-            _contained[i] = false;
-        }
+        Console.Write(required.ToString("F6"));
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ecc8494f9e1a430780af9675fcfac8b18f7debc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f735138211713f5a5bb17345622c63f32d2c45")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
