6e5a3c22a114c9af92fecde2ae4aa9fc32058a99
mafuyusame <mafuyu@tutamail.com>
2025-03-12 16:05:43 +0900

    [bruteforcing, graphs] boj 14502

--- Program.cs
+++ Program.cs
@@ -1,74 +1,130 @@
 ﻿internal class Program
 {
+    const int Blank = 0;
+    const int Wall = 1;
+    const int Virus = 2;
+    const int Infested = 3;
+
+    private static int _height = 0;
+    private static int _width = 0;
+    private static bool[] _occupied = null!;
+    private static int[] _map = null!;
+    private static LinkedList<int> _viruses = new();
+    
+    private const int Offsets = 4;
+    private static int[] _rowOffsets = new int[Offsets] { -1, 1, 0, 0 };
+    private static int[] _colOffsets = new int[Offsets] { 0, 0, -1, 1 };
+
+    private static int _maxSafety = 0;
+
     private static void Main(string[] args)
     {
-        int m = int.Parse(Console.ReadLine()!); // [1, 10'000]
+        // length = 2
+        // element = [3, 8]
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        _height = tokens[0];
+        _width = tokens[1];
 
-        (int n, int s)[] nss = new (int, int)[m];
+        _occupied = new bool[_height * _width];
 
-        for (int i = 0; i < nss.Length; ++i) // max tc = 10'000
+        _map = new int[_height * _width];
+
+        for (int row = 0; row < _height; ++row) // max tc = 8
         {
-            // length = 2
-            // element = [1, 1'000'000'000]
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-            int n = tokens[0];
-            int s = tokens[1];
-
-            nss[i] = (n, s);
-        }
-
-        const long X = 1_000_000_007;
-
-        long qSumModX = 0;
-        for (int i = 0; i < nss.Length; ++i)
-        {
-            long a = nss[i].s;
-            long b = nss[i].n;
-
-            long gcd = GCD(a, b);
-            a /= gcd;
-            b /= gcd;
-
-            long bInverseModX = EBS(b, X - 2, X);
-
-            qSumModX = (qSumModX + (((a % X) * bInverseModX) % X)) % X;
-        }
-        Console.Write(qSumModX);
-    }
-
-    private static long GCD(long a, long b)
-    {
-        long bigger = Math.Max(a, b);
-        long smaller = Math.Min(a, b);
-
-        while (smaller > 0)
-        {
-            bigger %= smaller;
-
-            long temp = bigger;
-            bigger = smaller;
-            smaller = temp;
-        }
-
-        return bigger;
-    }
-
-    private static long EBS(long basis, long exponent, long modulus)
-    {
-        long output = 1;
-
-        while (exponent > 0)
-        {
-            if ((exponent & 1) == 1)
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            for (int col = 0; col < _width; ++col) // max tc = 8
             {
-                output = (output * basis) % modulus;
+                int index = row * _width + col;
+
+                int attr = tokens[col];
+                if (attr == Virus)
+                {
+                    _viruses.AddLast(index);
+                }
+
+                _map[index] = tokens[col];
+            }
+        }
+
+        Simulate(1, 0);
+
+        Console.Write(_maxSafety);
+    }
+    
+    // depth has 1-based value
+    private static void Simulate(int depth, int beginIndex)
+    {
+        for (int i = beginIndex; i < _map.Length; ++i) // max tc = 64
+        {
+            if (_map[i] == Wall ||
+                _map[i] == Virus)
+                continue;
+
+            _occupied[i] = true;
+
+            if (depth < 3)
+            {
+                Simulate(depth + 1, i + 1);
+            }
+            else
+            {
+                for (int j = 0; j < _map.Length; ++j) // max tc = 64
+                {
+                    if (_map[j] == Infested)
+                    {
+                        _map[j] = Blank;
+                    }
+                }
+
+                for (var lln = _viruses.First; lln != null; lln = lln.Next) // max tc = 64
+                {
+                    Queue<int> frontier = new();
+                    frontier.Enqueue(lln.Value);
+
+                    while (frontier.Count > 0) // max tc = 64
+                    {
+                        int index = frontier.Dequeue();
+                        int row = index / _width;
+                        int col = index % _width;
+                        
+                        for (int j = 0; j < Offsets; ++j)
+                        {
+                            int adjRow = row + _rowOffsets[j];
+                            if (adjRow < 0 || adjRow > _height - 1)
+                                continue;
+
+                            int adjCol = col + _colOffsets[j];
+                            if (adjCol < 0 || adjCol > _width - 1)
+                                continue;
+
+                            int adjIndex = adjRow * _width + adjCol;
+                            if (_occupied[adjIndex] ||
+                                _map[adjIndex] == Wall ||
+                                _map[adjIndex] == Virus ||
+                                _map[adjIndex] == Infested)
+                                continue;
+
+                            _map[adjIndex] = Infested;
+                            frontier.Enqueue(adjIndex);
+                        }
+                    }
+                }
+
+                int safety = 0;
+                for (int j = 0; j < _map.Length; ++j)
+                {
+                    if (_occupied[j])
+                        continue;
+
+                    if (_map[j] != Blank)
+                        continue;
+
+                    ++safety;
+                }
+                _maxSafety = Math.Max(_maxSafety, safety);
             }
 
-            basis = (basis * basis) % modulus;
-            exponent >>= 1;
+            _occupied[i] = false;
         }
-
-        return output;
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d8490ac6e2aeadcbafb7953474239847ff0835d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65cfad0e08a99610ab3feb25eaae3dd4d7e075cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
