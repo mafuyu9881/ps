1ca68927d9756aa4a4378ba038851651323209a7
mafuyusame <mafuyu@tutamail.com>
2025-03-14 17:52:11 +0900

    boj 31230 75% 시간 초과

--- Program.cs
+++ Program.cs
@@ -1,76 +1,93 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
+    const long InvalidCost = -1;
+
+    private static int _n; // [2, 200'000]
+    private static LinkedList<(int v, int cost)>[] _adjList = null!;
+
     private static void Main(string[] args)
     {
-        const int InvalidDistance = -1;
-
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // [1, 100]
-        int m = tokens[1]; // [1, 15]
-        int r = tokens[2]; // [1, 100]
+        _n = tokens[0];
+        int m = tokens[1]; // [1, 300'000]
+        int a = tokens[2] - 1; // [1, 200'000]
+        int b = tokens[3] - 1; // [1, 200'000]
 
-        // element = [1, 30]
-        int[] items = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-        int[,] adjMatrix = new int[n, n];
-        for (int row = 0; row < n; ++row) // max tc = 100
-        {
-            for (int col = 0; col < n; ++col) // max tc = 100
-            {
-                adjMatrix[row, col] = (row == col) ? 0 : InvalidDistance;
-            }
-        }
-
-        for (int i = 0; i < r; ++i) // max tc = 100
+        _adjList = new LinkedList<(int v, int cost)>[_n];
+        for (int i = 0; i < m; ++i) // max tc = 300'000
         {
             tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int v0 = tokens[0] - 1; // [1, 200'000]
+            int v1 = tokens[1] - 1; // [1, 200'000]
+            int cost = tokens[2]; // [1, 1'000'000'000]
 
-            int a = tokens[0] - 1;
-            int b = tokens[1] - 1;
-            int l = tokens[2]; // [1, 15]
+            if (_adjList[v0] == null)
+                _adjList[v0] = new();
+            
+            if (_adjList[v1] == null)
+                _adjList[v1] = new();
 
-            adjMatrix[a, b] = l;
-            adjMatrix[b, a] = l;
+            _adjList[v0].AddLast((v1, cost));
+            _adjList[v1].AddLast((v0, cost));
         }
 
-        for (int transit = 0; transit < n; ++transit) // max tc = 100
-        {
-            for (int src = 0; src < n; ++src) // max tc = 100
-            {
-                for (int dst = 0; dst < n; ++dst) // max tc = 100
-                {
-                    int transitDistance0 = adjMatrix[src, transit];
-                    int transitDistance1 = adjMatrix[transit, dst];
-                    if (transitDistance0 == InvalidDistance || transitDistance1 == InvalidDistance)
-                        continue;
+        long[] aMinCost = ComputeMinCost(a);
+        long[] bMinCost = ComputeMinCost(b);
 
-                    int oldDistance = adjMatrix[src, dst];
-                    int newDistance = transitDistance0 + transitDistance1;
-                    if (oldDistance != InvalidDistance && oldDistance <= newDistance)
-                        continue;
-                    
-                    adjMatrix[src, dst] = newDistance;
-                }
+        LinkedList<int> designatables = new();
+        for (int i = 0; i < _n; ++i) // max tc = 200'000
+        {
+            if (aMinCost[i] + bMinCost[i] == aMinCost[b])
+            {
+                designatables.AddLast(i);
             }
         }
 
-        int maxCollectableItems = 0;
-        for (int src = 0; src < n; ++src) // max tc = 100
+        StringBuilder sb = new();
+        sb.AppendLine($"{designatables.Count}");
+        for (var lln = designatables.First; lln != null; lln = lln.Next) // max tc = 200'000
         {
-            int collectableItems = 0;
-            for (int dst = 0; dst < n; ++dst) // max tc = 100
-            {
-                int distance = adjMatrix[src, dst];
-                if (distance == InvalidDistance)
-                    continue;
-
-                if (distance > m)
-                    continue;
-
-                collectableItems += items[dst];
-            }
-            maxCollectableItems = Math.Max(maxCollectableItems, collectableItems);
+            sb.Append($"{lln.Value + 1} ");
         }
-        Console.Write(maxCollectableItems);
+        Console.Write(sb);
+    }
+
+    private static long[] ComputeMinCost(int s)
+    {
+        long[] minCost = new long[_n];
+        for (int i = 0; i < _n; ++i) // max tc = 200'000
+        {
+            minCost[i] = InvalidCost;
+        }
+        PriorityQueue<(int dst, long cost), long> pq = new();
+
+        minCost[s] = 0;
+        pq.Enqueue((s, 0), 0); // tc = 1
+
+        while (pq.Count > 0) // max tc = 200'000 * log2(300'000) = 200'000 * 18.xxx
+        {
+            var e = pq.Dequeue();
+            int dst = e.dst;
+            long srcDstCost = e.cost;
+
+            var adjs = _adjList[dst];
+            for (var lln = adjs.First; lln != null; lln = lln.Next)
+            {
+                int adj = lln.Value.v;
+                long dstAdjCost = lln.Value.cost;
+
+                long oldSrcDstAdjCost = minCost[adj];
+                long newSrcDstAdjCost = srcDstCost + dstAdjCost;
+                if (oldSrcDstAdjCost != InvalidCost && oldSrcDstAdjCost <= newSrcDstAdjCost)
+                    continue;
+
+                minCost[adj] = newSrcDstAdjCost;
+                pq.Enqueue((adj, newSrcDstAdjCost), newSrcDstAdjCost);
+            }
+        }
+
+        return minCost;
     }
 }
