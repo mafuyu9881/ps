acf387482371a0d6c9c7d63f567f01fef195413b
mafuyusame <mafuyu@tutamail.com>
2025-03-16 10:21:09 +0900

    boj 7795 풀이 도중 다른 문제를 마저 풀기 위해 중단

--- Program.cs
+++ Program.cs
@@ -2,55 +2,23 @@
 {
     private static void Main(string[] args)
     {
-        long[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
-        long n = tokens[0]; // [1, 10^2]
-        long t = tokens[1]; // [1, 10^18]
-
-        int[] pointings = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        for (int i = 0; i < pointings.Length; ++i) // max tc = 100
+        int t = int.Parse(Console.ReadLine()!); // [?, ?]
+        for (int i = 0; i < t; ++i)
         {
-            --pointings[i]; // adjust to 0-based
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int n = tokens[0]; // [1, 20'000]
+            int m = tokens[1]; // [1, 20'000]
+
+            // length = [1, 20'000]
+            // element = [1, ?]
+            int[] aSizes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int[] bSizes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+            // max tc = 20'000 * log2(20'000) = 20'000 * 14.xxx
+            Array.Sort(aSizes);
+            Array.Sort(bSizes);
+
+            
         }
-
-        const int InvalidPointedOrder = -1;
-
-        int[] pointedOrder = new int[n];
-        for (int i = 0; i < pointedOrder.Length; ++i)
-        {
-            pointedOrder[i] = InvalidPointedOrder;
-        }
-
-        LinkedList<int> orderLookupLL = new();
-        int x = 0;
-        int cycleBeginOrder = InvalidPointedOrder;
-        while (true) // max tc = 100
-        {
-            x = pointings[x];
-
-            if (pointedOrder[x] != InvalidPointedOrder)
-            {
-                cycleBeginOrder = pointedOrder[x];
-                break;
-            }
-
-            orderLookupLL.AddLast(x);
-            pointedOrder[x] = orderLookupLL.Count() - 1;
-        }
-
-        int[] orderLookupArr = orderLookupLL.ToArray();
-        
-        long adjustedT = t - 1;
-        long defeatedPlayerOrder;
-        if (adjustedT > orderLookupArr.Length - 1)
-        {
-            // the cycle inevitably happens
-            defeatedPlayerOrder = cycleBeginOrder + (adjustedT - cycleBeginOrder) % (orderLookupArr.Length - cycleBeginOrder);
-        }
-        else
-        {
-            defeatedPlayerOrder = adjustedT;
-        }
-
-        Console.Write(orderLookupArr[defeatedPlayerOrder] + 1);
     }
 }
