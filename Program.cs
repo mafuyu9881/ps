cee3fd0721692bb09c0efcf30260aae199dd3d52
mafuyusame <mafuyu@tutamail.com>
2025-03-17 14:37:23 +0900

    [binary search] boj 7795

--- Program.cs
+++ Program.cs
@@ -1,31 +1,54 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [1, 100'000]
+        int t = int.Parse(Console.ReadLine()!); // [?, ?]
 
-        // length = [1, 100'000]
-        // element = [1, 1'000'000'000]
-        int[] resistances = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-        // 0: O X
-        // 1: X O
-        // 2: O O
-        // 'X X' can't happen
-        long[,] dp = new long[n, 3];
-        dp[0, 0] = 0;
-        dp[0, 1] = resistances[0];
-        dp[0, 2] = resistances[0];
-        for (int i = 1; i < n; ++i) // max tc = 100'000
+        StringBuilder sb = new();
+        for (int i = 0; i < t; ++i) // max tc = ?
         {
-            int resistance = resistances[i];
-            dp[i, 0] = Math.Min(dp[i - 1, 1], dp[i - 1, 2]);
-            dp[i, 1] = dp[i - 1, 0] + resistance;
-            dp[i, 2] = Math.Min(dp[i - 1, 1] + resistance, dp[i - 1, 2] + resistance);
-        }
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int n = tokens[0]; // [1, 20'000]
+            int m = tokens[1]; // [1, 20'000]
 
-        long energy = Math.Min(dp[n - 1, 0], dp[n - 1, 1]);
-        energy = Math.Min(energy, dp[n - 1, 2]);
-        Console.Write(energy);
+            // length = [1, 20'000]
+            // element = [1, ?]
+            int[] aSizes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int[] bSizes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+            // max tc = 20'000 * log2(20'000) = 20'000 * 14.xxx
+            Array.Sort(aSizes);
+            Array.Sort(bSizes);
+
+            int pairs = 0;
+            for (int j = 0; j < aSizes.Length; ++j) // max tc = 20'000
+            {
+                int aSize = aSizes[j];
+                
+                int lo = -1;
+                int hi = (bSizes.Length - 1) + 1;
+                while (lo < hi - 1) // max tc = log2(20'000) = 13.xxx
+                {
+                    int mid = (hi + lo) / 2;
+                    
+                    int bSize = bSizes[mid];
+
+                    if (aSize > bSize)
+                    {
+                        lo = mid;
+                    }
+                    else
+                    {
+                        hi = mid;
+                    }
+                }
+
+                pairs += lo + 1;
+            }
+            sb.AppendLine($"{pairs}");
+        }
+        Console.Write(sb);
     }
 }
