d6dac3e74285ca80904ec80cb41c8352e244e8c9
mafuyusame <mafuyu@tutamail.com>
2025-03-19 10:22:21 +0900

    boj 32963 1% 오답 코드

--- Program.cs
+++ Program.cs
@@ -1,30 +1,111 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         // length = 2
-        // element = [1, 300'000]
+        // element = [1, 200'000]
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
         int n = tokens[0];
-        int k = tokens[1];
+        int q = tokens[1];
 
-        // length = [1, 300'000]
-        // element = [-10^8, 10^8]
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        // length = [1, 200'000]
+        // element = [1, 1'000'000'000]
+        int[] tastes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int[] sizes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        long[] prefixSum = new long[n];
-        prefixSum[0] = tokens[0];
-        for (int i = 1; i < prefixSum.Length; ++i) // max tc = 300'000 - 1
+        (int taste, int size)[] apples = new (int, int)[n];
+        for (int i = 0; i < apples.Length; ++i) // max tc = 200'000
         {
-            prefixSum[i] = prefixSum[i - 1] + tokens[i];
+            apples[i] = (tastes[i], sizes[i]);
         }
-        Array.Sort(prefixSum, (x, y) => y.CompareTo(x));
-        
-        long sum = 0;
-        for (int i = 0; i < k; ++i)
+        Array.Sort(apples, (x, y) => y.taste.CompareTo(x.taste)); // max tc = 200'000 * log2(200'000) = 200'000 * 17.xxx
+
+        const int Invalid = -1;
+        int computingMaxSize = Invalid;
+        int computingCount = Invalid;
+
+        LinkedList<(int taste, int count)> answerLL = new();
+        (int taste, int count)[] answerArr = null!;
+        for (int i = 0; i < apples.Length; ++i) // max tc = 200'000
         {
-            sum += prefixSum[i];
+            var apple = apples[i];
+
+            int computingTaste = apple.taste;
+            if (i == 0)
+            {
+                computingMaxSize = apple.size;
+                computingCount = 1;
+            }
+            else
+            {
+                if (computingMaxSize == apple.size)
+                {
+                    ++computingCount;
+                }
+                else if (computingMaxSize < apple.size)
+                {
+                    computingMaxSize = apple.size;
+                    computingCount = 1;
+                }
+            }
+
+            int j = i + 1;
+            while (j < apples.Length && apple.taste == apples[j].taste) // it doesn't take additional tc
+            {
+                int sameTasteAppleSize = apples[j].size;
+                
+                if (computingMaxSize == sameTasteAppleSize)
+                {
+                    ++computingCount;
+                }
+                else if (computingMaxSize < sameTasteAppleSize)
+                {
+                    computingMaxSize = sameTasteAppleSize;
+                    computingCount = 1;
+                }
+                
+                ++j;
+            }
+            i = j - 1;
+
+            answerLL.AddLast((computingTaste, computingCount));
         }
-        Console.Write(sum);
+        answerArr = answerLL.ToArray(); // max tc = 200'000
+
+        StringBuilder sb = new();
+        for (int i = 0; i < q; ++i) // max tc = 200'000
+        {
+            int p = int.Parse(Console.ReadLine()!);
+
+            int lo = 0 - 1;
+            int hi = (answerArr.Length - 1) + 1;
+            while (lo < hi - 1) // max tc = 17.xxx
+            {
+                int mid = (lo + hi) / 2;
+
+                if (answerArr[mid].taste > p)
+                {
+                    lo = mid;
+                }
+                else
+                {
+                    hi = mid;
+                }
+            }
+
+            int answer;
+            if (hi < -1 || hi > answerArr.Length - 1)
+            {
+                answer = 0;
+            }
+            else
+            {
+                answer = answerArr[hi].count;
+            }
+            sb.AppendLine($"{answer}");
+        }
+        Console.Write(sb);
     }
 }
