e8d4c4206a3cee0ae0cf06712fd36e62b8325d30
mafuyusame <mafuyu@tutamail.com>
2025-03-20 10:40:17 +0900

    boj 17144 풀이 도중 중단

--- Program.cs
+++ Program.cs
@@ -1,111 +1,28 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        // length = 2
-        // element = [1, 200'000]
+        // length = 3
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0];
-        int q = tokens[1];
+        int height = tokens[0]; // [6, 50]
+        int width = tokens[1]; // [6, 50]
+        int t = tokens[2]; // [1, 1'000]
 
-        // length = [1, 200'000]
-        // element = [1, 1'000'000'000]
-        int[] tastes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int[] sizes = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int[,] buffer0 = new int[height, width];
+        int[,] buffer1 = new int[height, width];
 
-        (int taste, int size)[] apples = new (int, int)[n];
-        for (int i = 0; i < apples.Length; ++i) // max tc = 200'000
+        int[,] mainBuffer = buffer0;
+        int[,] backBuffer = buffer1;
+
+        for (int row = 0; row < height; ++row)
         {
-            apples[i] = (tastes[i], sizes[i]);
-        }
-        Array.Sort(apples, (x, y) => y.taste.CompareTo(x.taste)); // max tc = 200'000 * log2(200'000) = 200'000 * 17.xxx
-
-        const int Invalid = -1;
-        int computingMaxSize = Invalid;
-        int computingCount = Invalid;
-
-        LinkedList<(int taste, int count)> answerLL = new();
-        (int taste, int count)[] answerArr = null!;
-        for (int i = 0; i < apples.Length; ++i) // max tc = 200'000
-        {
-            var apple = apples[i];
-
-            int computingTaste = apple.taste;
-            if (i == 0)
+            // length = width = [6, 50]
+            // element = [-1, 1'000]
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            for (int col = 0; col < width; ++col)
             {
-                computingMaxSize = apple.size;
-                computingCount = 1;
-            }
-            else
-            {
-                if (computingMaxSize == apple.size)
-                {
-                    ++computingCount;
-                }
-                else if (computingMaxSize < apple.size)
-                {
-                    computingMaxSize = apple.size;
-                    computingCount = 1;
-                }
-            }
-
-            int j = i + 1;
-            while (j < apples.Length && apple.taste == apples[j].taste) // it doesn't take additional tc
-            {
-                int sameTasteAppleSize = apples[j].size;
                 
-                if (computingMaxSize == sameTasteAppleSize)
-                {
-                    ++computingCount;
-                }
-                else if (computingMaxSize < sameTasteAppleSize)
-                {
-                    computingMaxSize = sameTasteAppleSize;
-                    computingCount = 1;
-                }
-                
-                ++j;
             }
-            i = j - 1;
-
-            answerLL.AddLast((computingTaste, computingCount));
         }
-        answerArr = answerLL.ToArray(); // max tc = 200'000
-
-        StringBuilder sb = new();
-        for (int i = 0; i < q; ++i) // max tc = 200'000
-        {
-            int p = int.Parse(Console.ReadLine()!);
-
-            int lo = 0 - 1;
-            int hi = (answerArr.Length - 1) + 1;
-            while (lo < hi - 1) // max tc = 17.xxx
-            {
-                int mid = (lo + hi) / 2;
-
-                if (answerArr[mid].taste >= p)
-                {
-                    lo = mid;
-                }
-                else
-                {
-                    hi = mid;
-                }
-            }
-
-            int answer;
-            if (lo < 0 || lo > answerArr.Length - 1)
-            {
-                answer = 0;
-            }
-            else
-            {
-                answer = answerArr[lo].count;
-            }
-            sb.AppendLine($"{answer}");
-        }
-        Console.Write(sb);
     }
 }
