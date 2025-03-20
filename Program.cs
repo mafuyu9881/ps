29bf26dc77289048bd578580b4016d4995fc5c9d
mafuyusame <mafuyu@tutamail.com>
2025-03-20 19:34:06 +0900

    [ad hoc, math] boj 28292

--- Program.cs
+++ Program.cs
@@ -1,51 +1,22 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        // [1, 100]
-        int n = int.Parse(Console.ReadLine()!);
+        int n = int.Parse(Console.ReadLine()!); // [1, 100]
 
-        StringBuilder[] sequences = new StringBuilder[n];
-        for (int i = 0; i < sequences.Length; ++i) // max tc = 100
+        string s;
+        if (n < 3)
         {
-            sequences[i] = new();
+            s = "1";
         }
-
-        sequences[0].Append(1);
-
-        for (int i = 1; i < sequences.Length; ++i) // max tc = 100
+        else if (n < 6)
         {
-            StringBuilder prevSequence = sequences[i - 1];
-            StringBuilder currSequence = sequences[i];
-
-            int currIndex = 0;
-            int number = prevSequence[currIndex] - '0';
-            int count = 1;
-
-            while (currIndex < prevSequence.Length)
-            {
-                int nextIndex = currIndex + 1;
-
-                if (nextIndex < prevSequence.Length && (prevSequence[nextIndex] - '0') == number)
-                {
-                    ++count;
-                }
-                else
-                {
-                    currSequence.Append(number);
-                    currSequence.Append(count);
-
-                    if (nextIndex < prevSequence.Length)
-                    {
-                        number = prevSequence[nextIndex] - '0';
-                        count = 1;
-                    }
-                }
-
-                currIndex = nextIndex;
-            }
+            s = "2";
         }
+        else
+        {
+            s = "3";
+        }
+        Console.Write(s);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c2b95492afdf9c57c8b7165262c9618f6b8ae4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8837c546a026ac023f460c6de995a046ba711162")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
