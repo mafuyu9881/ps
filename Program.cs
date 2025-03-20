52c2b95492afdf9c57c8b7165262c9618f6b8ae4
mafuyusame <mafuyu@tutamail.com>
2025-03-20 15:18:56 +0900

    boj 28292 풀이 도중 LinkedList<int>를 StringBuilder로 바꾸기 위해 커밋

--- Program.cs
+++ Program.cs
@@ -2,26 +2,46 @@
 {
     private static void Main(string[] args)
     {
-        // length = 3
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int height = tokens[0]; // [6, 50]
-        int width = tokens[1]; // [6, 50]
-        int t = tokens[2]; // [1, 1'000]
+        // [1, 100]
+        int n = int.Parse(Console.ReadLine()!);
 
-        int[,] buffer0 = new int[height, width];
-        int[,] buffer1 = new int[height, width];
-
-        int[,] mainBuffer = buffer0;
-        int[,] backBuffer = buffer1;
-
-        for (int row = 0; row < height; ++row)
+        LinkedList<int>[] sequences = new LinkedList<int>[n];
+        for (int i = 0; i < sequences.Length; ++i) // max tc = 100
         {
-            // length = width = [6, 50]
-            // element = [-1, 1'000]
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            for (int col = 0; col < width; ++col)
+            sequences[i] = new();
+        }
+
+        sequences[0].AddLast(1);
+
+        for (int i = 1; i < sequences.Length; ++i) // max tc = 100
+        {
+            LinkedList<int> prevSequence = sequences[i - 1];
+            LinkedList<int> currSequence = sequences[i];
+
+            LinkedListNode<int>? currLLN = prevSequence.First!;
+            int number = currLLN.Value;
+            int count = 1;
+            while (currLLN != null)
             {
+                LinkedListNode<int>? nextLLN = currLLN.Next;
+
+                if (nextLLN != null && nextLLN.Value == number)
+                {
+                    ++count;
+                }
+                else
+                {
+                    currSequence.AddLast(number);
+                    currSequence.AddLast(count);
+
+                    if (nextLLN != null)
+                    {
+                        number = nextLLN.Value;
+                        count = 1;
+                    }
+                }
                 
+                currLLN = nextLLN;
             }
         }
     }
