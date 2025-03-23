361bb6e8429a084bc6824c1a85ee24906f2e958a
mafuyusame <mafuyu@tutamail.com>
2025-03-23 17:50:03 +0900

    boj 25511 98% 오답 코드

--- Program.cs
+++ Program.cs
@@ -1,36 +1,56 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        const int MaxDays = 641;
+        // length = 2
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // [2, 100'000]
+        int k = tokens[1]; // [0, n - 1] = [0, 99'999]
 
-        int n = int.Parse(Console.ReadLine()!); // [1, 5'000]
-
-        string paper = Console.ReadLine()!;
-
-        int currIndex = 0;
-        int soldiers = 0;
-        while (currIndex < paper.Length) // max tc = 5'000
+        LinkedList<int>[] adjList = new LinkedList<int>[n];
+        for (int i = 0; i < adjList.Length; ++i) // max tc = 100'000
         {
-            StringBuilder daysSB = new();
-            for (int subIndex = currIndex; subIndex < paper.Length && subIndex < currIndex + 3; ++subIndex)
-            {
-                daysSB.Append(paper[subIndex]);
-            }
-
-            int nextIndex = currIndex + daysSB.Length;
-            while ((int.Parse($"{daysSB}") > MaxDays) || (nextIndex < paper.Length && paper[nextIndex] == '0'))
-            {
-                daysSB = daysSB.Remove(daysSB.Length - 1, 1);
-                nextIndex = currIndex + daysSB.Length;
-            }
-
-            ++soldiers;
-
-            currIndex = nextIndex;
+            adjList[i] = new();
         }
-        Console.Write(soldiers);
+
+        for (int i = 0; i < n - 1; ++i) // max tc = 99'999
+        {
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            
+            int p = tokens[0];
+            int c = tokens[1];
+
+            adjList[p].AddLast(c);
+        }
+
+        int[] value = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+        Queue<(int v, int depth)> frontier = new();
+
+        frontier.Enqueue((0, 0));
+
+        while (frontier.Count > 0)
+        {
+            var element = frontier.Dequeue();
+
+            int p = element.v;
+            int pDepth = element.depth;
+            int cDepth = pDepth + 1;
+
+            var adjs = adjList[p];
+            for (var lln = adjs.First; lln != null; lln = lln.Next) // max tc = 2
+            {
+                int c = lln.Value;
+                if (value[c] == k)
+                {
+                    Console.Write(cDepth);
+                    return;
+                }
+                else
+                {
+                    frontier.Enqueue((c, cDepth));
+                }
+            }
+        }
     }
 }
