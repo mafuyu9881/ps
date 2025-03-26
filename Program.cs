0ef3af9f6d5cc9968e08ee6b98b59d5c6908fbf4
mafuyusame <mafuyu@tutamail.com>
2025-03-26 21:22:47 +0900

    boj 26607 bfs 시도 도중 커밋

--- Program.cs
+++ Program.cs
@@ -2,107 +2,23 @@
 {
     private static void Main(string[] args)
     {
-        const int Wall = 1;
+        // length = 3
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // [1, 80]
+        int k = tokens[1]; // [1, n] = [1, 80]
+        int x = tokens[2]; // [1, 200]
 
-        const int InvalidDistance = -1;
-
-        const int Offsets = 4;
-        int[] RowOffsets = new int[Offsets] { -1, 1, 0, 0 };
-        int[] ColOffsets = new int[Offsets] { 0, 0, -1, 1 };
-
-        // length = 2
-        // element = [1, 1'000]
-        int[] integerTokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int height = integerTokens[0];
-        int width = integerTokens[1];
-
-        int[] map = new int[height * width];
-        for (int row = 0; row < height; ++row)
+        (int a, int b)[] stats = new (int, int)[n];
+        for (int i = 0; i < n; ++i) // max tc = 80
         {
-            string stringToken = Console.ReadLine()!;
-            for (int col = 0; col < width; ++col)
-            {
-                map[row * width + col] = stringToken[col] - '0';
-            }
+            // length = 2
+            // element = [0, x] = [0, 200]
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int a = tokens[0];
+            int b = tokens[1];
+            stats[i] = (a, b);
         }
 
-        int historyHeight = height * width;
-        int historyWidth = 2;
-
-        int[,] history = new int[historyHeight, historyWidth];
-        for (int i = 0; i < historyHeight; ++i)
-        {
-            for (int j = 0; j < historyWidth; ++j)
-            {
-                history[i, j] = InvalidDistance;
-            }
-        }
-
-        Queue<(int index, int smashes, int distance)> frontier = new();
-
-        int s = 0;
-        history[s, 0] = 1;
-        frontier.Enqueue((s, 0, history[s, 0]));
-
-        while (frontier.Count > 0)
-        {
-            var element = frontier.Dequeue();
-
-            int index = element.index;
-            int row = index / width;
-            int col = index % width;
-            int smashes = element.smashes;
-            int newSmashes = smashes + 1;
-            int newDistance = element.distance + 1;
-
-            for (int i = 0; i < Offsets; ++i)
-            {
-                int adjRow = row + RowOffsets[i];
-                if (adjRow < 0 || adjRow > height - 1)
-                    continue;
-
-                int adjCol = col + ColOffsets[i];
-                if (adjCol < 0 || adjCol > width - 1)
-                    continue;
-
-                int adjIndex = adjRow * width + adjCol;
-                if (map[adjIndex] == Wall)
-                {
-                    if (smashes < 1)
-                    {
-                        int oldDistance = history[adjIndex, newSmashes];
-                        if (oldDistance == InvalidDistance || newDistance < oldDistance)
-                        {
-                            history[adjIndex, newSmashes] = newDistance;
-                            frontier.Enqueue((adjIndex, newSmashes, newDistance));
-                        }
-                    }
-                }
-                else
-                {
-                    int oldDistance = history[adjIndex, smashes];
-                    if (oldDistance == InvalidDistance || newDistance < oldDistance)
-                    {
-                        history[adjIndex, smashes] = newDistance;
-                        frontier.Enqueue((adjIndex, smashes, newDistance));
-                    }
-                }
-            }
-        }
-
-        int minDistance = InvalidDistance;
-        for (int i = 0; i < historyWidth; ++i)
-        {
-            int distance = history[historyHeight - 1, i];
-
-            if (distance == InvalidDistance)
-                continue;
-
-            if (minDistance != InvalidDistance && minDistance <= distance)
-                continue;
-                
-            minDistance = distance;
-        }
-        Console.Write($"{minDistance}");
+        bool[] visited = new bool[64000000 + 1];
     }
 }
