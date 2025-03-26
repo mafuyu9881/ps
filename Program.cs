5d1f37051c9cd283ad4f498ee6df27b332d62588
mafuyusame <mafuyu@tutamail.com>
2025-03-26 12:10:14 +0900

    boj 26607 1% 오답 코드

--- Program.cs
+++ Program.cs
@@ -1,59 +1,44 @@
 ﻿internal class Program
 {
-    const int InvalidElement = -1;
-
     private static void Main(string[] args)
     {
-        // length = 2
-        // element = [1, 1'000'000]
+        // length = 3
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int a = tokens[0];
-        int b = tokens[1];
+        int n = tokens[0]; // [1, 80]
+        int k = tokens[1]; // [1, n] = [1, 80]
+        int x = tokens[2]; // [1, 200]
 
-        int[,] dp = new int[b + 1, 2];
-        for (int i = 0; i <= b; ++i)
+        (int a, int b)[] people = new (int, int)[n];
+        for (int i = 0; i < n; ++i) // max tc = 80
         {
-            dp[i, 0] = InvalidElement;
-            dp[i, 1] = InvalidElement;
+            // length = 2
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int a = tokens[0]; // [0, 200]
+            int b = tokens[1]; // [0, 200]
+            people[i] = (a, b);
         }
+        
+        Array.Sort(people, (x, y) => Math.Abs(y.a - y.b).CompareTo(Math.Abs(x.a - x.b)));
 
-        dp[a, 0] = 0;
-        dp[a, 1] = 0;
-        for (int i = a; i <= b; ++i)
+        int dpHeight = people.Length + 1;
+        int dpWidth = k + 1;
+
+        (int sa, int sb)[,] dp = new (int, int)[dpHeight, dpWidth];
+        for (int i = 1; i < dpHeight; ++i)
         {
-            int j = i + 1;
-            if (j <= b)
+            for (int j = 1; j < dpWidth; ++j)
             {
-                Update(ref dp[j, 0], dp[i, 0] + 1);
-                Update(ref dp[j, 1], dp[i, 1] + 1);
-            }
+                // prevent to leave the element as (0, 0)
+                dp[i, j].sa = dp[i - 1, j - 1].sa + people[i - 1].a;
+                dp[i, j].sb = dp[i - 1, j - 1].sb + people[i - 1].b;
 
-            int k = i * 2;
-            if (k <= b)
-            {
-                Update(ref dp[k, 0], dp[i, 0] + 1);
-                Update(ref dp[k, 1], dp[i, 1] + 1);
-            }
-
-            int l = i * 10;
-            if (l <= b)
-            {
-                Update(ref dp[l, 1], dp[i, 0] + 1);
+                if ((dp[i - 1, j].sa * dp[i - 1, j].sb) > (dp[i, j].sa * dp[i, j].sb))
+                {
+                    dp[i, j].sa = dp[i - 1, j].sa;
+                    dp[i, j].sb = dp[i - 1, j].sb;
+                }
             }
         }
-
-        Console.Write(Math.Min(dp[b, 0], dp[b, 1]));
-    }
-
-    private static void Update(ref int element, int value)
-    {
-        if (element == InvalidElement)
-        {
-            element = value;
-        }
-        else
-        {
-            element = Math.Min(element, value);
-        }
+        Console.Write(dp[n, k].sa * dp[n, k].sb);
     }
 }
