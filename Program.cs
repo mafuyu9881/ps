25e56fb3be36de2aa417a392d9b7369482da5a95
mafuyusame <mafuyu@tutamail.com>
2025-03-26 15:20:03 +0900

    boj 26607 1% 오답 코드

--- Program.cs
+++ Program.cs
@@ -8,37 +8,42 @@
         int k = tokens[1]; // [1, n] = [1, 80]
         int x = tokens[2]; // [1, 200]
 
-        (int a, int b)[] people = new (int, int)[n];
+        (int a, int b)[] stats = new (int, int)[n];
         for (int i = 0; i < n; ++i) // max tc = 80
         {
             // length = 2
             tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
             int a = tokens[0]; // [0, 200]
             int b = tokens[1]; // [0, 200]
-            people[i] = (a, b);
+            stats[i] = (a, b);
         }
         
-        Array.Sort(people, (x, y) => Math.Abs(y.a - y.b).CompareTo(Math.Abs(x.a - x.b)));
+        Array.Sort(stats, (x, y) => Math.Abs(y.a - y.b).CompareTo(Math.Abs(x.a - x.b)));
 
-        int dpHeight = people.Length + 1;
+        int dpHeight = stats.Length + 1;
         int dpWidth = k + 1;
+        int dpLayers = x * k + 1;
+        int[,,] dp = new int[dpHeight, dpWidth, dpLayers];
 
-        (int sa, int sb)[,] dp = new (int, int)[dpHeight, dpWidth];
-        for (int i = 1; i < dpHeight; ++i)
+        int maxStatsSum = 0;
+        for (int i = 1; i < dpHeight; ++i) // max tc = 80
         {
-            for (int j = 1; j < dpWidth; ++j)
+            for (int j = 1; j < dpWidth; ++j) // max tc = 80
             {
-                // prevent to leave the element as (0, 0)
-                dp[i, j].sa = dp[i - 1, j - 1].sa + people[i - 1].a;
-                dp[i, j].sb = dp[i - 1, j - 1].sb + people[i - 1].b;
-
-                if ((dp[i - 1, j].sa * dp[i - 1, j].sb) > (dp[i, j].sa * dp[i, j].sb))
+                for (int l = 1; l < dpLayers; ++l) // max tc = 80 * 200 = 16'000
                 {
-                    dp[i, j].sa = dp[i - 1, j].sa;
-                    dp[i, j].sb = dp[i - 1, j].sb;
+                    dp[i, j, l] = dp[i - 1, j, l];
+                    
+                    int a = stats[i - 1].a;
+                    if (l - a >= 0)
+                    {
+                        dp[i, j, l] = Math.Max(dp[i, j, l], dp[i - 1, j - 1, l - a] + a);
+                    }
+
+                    maxStatsSum = Math.Max(maxStatsSum, l * (x * k - l));
                 }
             }
         }
-        Console.Write(dp[n, k].sa * dp[n, k].sb);
+        Console.Write(maxStatsSum);
     }
 }
