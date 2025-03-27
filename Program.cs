418b537aa675cd0d2bf296a681a828705784624f
mafuyusame <mafuyu@tutamail.com>
2025-03-27 12:40:03 +0900

    [math, greedy] boj 16208

--- Program.cs
+++ Program.cs
@@ -2,23 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        // length = 3
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // [1, 80]
-        int k = tokens[1]; // [1, n] = [1, 80]
-        int x = tokens[2]; // [1, 200]
+        int n = int.Parse(Console.ReadLine()!); // [1, 500'000]
 
-        (int a, int b)[] stats = new (int, int)[n];
-        for (int i = 0; i < n; ++i) // max tc = 80
+        int[] requireds = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // [1, 101]
+
+        int stick = requireds.Sum(); // [1, 50'500'000]
+
+        long cost = 0;
+        Array.Sort(requireds);
+        for (int i = 0; i < requireds.Length; ++i)
         {
-            // length = 2
-            // element = [0, x] = [0, 200]
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int a = tokens[0];
-            int b = tokens[1];
-            stats[i] = (a, b);
-        }
+            int required = requireds[i];
 
-        bool[] visited = new bool[64000000 + 1];
+            stick -= required;
+
+            cost += stick * (long)required;
+        }
+        Console.Write(cost);
     }
 }
