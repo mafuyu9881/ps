8496c2bfc8e3a15d06259c4f0535fec52ee95a0e
mafuyusame <mafuyu@tutamail.com>
2025-03-30 21:24:37 +0900

    [math, implementation] boj 6487

--- Program.cs
+++ Program.cs
@@ -2,36 +2,107 @@
 
 internal class Program
 {
+    private enum Relationship
+    {
+        BothVertical,
+        EitherVertical,
+    }
+
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [1, 5 * 10^5]
-
-        // length = [1, n] = [1, 5 * 10^5]
-        // element = [1, 10^18]
-        long[] inks = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
-        long[] viscosities = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
+        int t = int.Parse(Console.ReadLine()!); // [1, 10]
 
         StringBuilder sb = new();
-        for (int i = 0; i < inks.Length; ++i)
+        for (int i = 0; i < t; ++i) // max tc = 10
         {
-            int lo = (i + 1) - 1;
-            int hi = (viscosities.Length - 1) + 1;
-            while (lo < hi - 1)
-            {
-                int mid = (lo + hi) / 2;
+            // length = 8
+            // element = [-1'000, 1'000]
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int x1 = tokens[0];
+            int y1 = tokens[1];
+            int x2 = tokens[2];
+            int y2 = tokens[3];
+            int x3 = tokens[4];
+            int y3 = tokens[5];
+            int x4 = tokens[6];
+            int y4 = tokens[7];
 
-                if (inks[i] < viscosities[mid])
+            var a1 = ComputeSlope(x1, y1, x2, y2);
+            var a2 = ComputeSlope(x3, y3, x4, y4);
+
+            if (a1.vertical && a2.vertical)
+            {
+                if (x1 == x3)
                 {
-                    hi = mid;
+                    sb.AppendLine("LINE");
                 }
                 else
                 {
-                    lo = mid;
+                    sb.AppendLine("NONE");
                 }
             }
+            else if (a1.vertical == false && a2.vertical == false && Math.Abs(a1.slope - a2.slope) < 0.01f)
+            {
+                float b1 = ComputeYIntercept(x1, y1, a1.slope);
+                float b2 = ComputeYIntercept(x3, y3, a2.slope);
 
-            sb.Append($"{lo - (i + 1) + 1} ");
+                if (Math.Abs(b1 - b2) < 0.01f)
+                {
+                    sb.AppendLine("LINE");
+                }
+                else
+                {
+                    sb.AppendLine("NONE");
+                }
+            }
+            else
+            {
+                float x;
+                float y;
+
+                if (a1.vertical)
+                {
+                    float b2 = ComputeYIntercept(x3, y3, a2.slope);
+
+                    x = x1;
+                    y = a2.slope * x + b2;
+                }
+                else if (a2.vertical)
+                {
+                    float b1 = ComputeYIntercept(x1, y1, a1.slope);
+
+                    x = x2;
+                    y = a1.slope * x + b1;
+                }
+                else
+                {
+                    float b1 = ComputeYIntercept(x1, y1, a1.slope);
+                    float b2 = ComputeYIntercept(x3, y3, a2.slope);
+
+                    x = (b2 - b1) / (float)(a1.slope - a2.slope);
+                    y = a1.slope * x + b1;
+                }
+                sb.AppendLine($"POINT {x.ToString("F2")} {y.ToString("F2")}");
+            }
         }
         Console.Write(sb);
+    }
+
+    private static (bool vertical, float slope) ComputeSlope(int x1, int y1, int x2, int y2)
+    {
+        bool vertical = (x1 == x2);
+        float slope = 0.0f;
+
+        if (vertical == false)
+        {
+            slope = (y2 - y1) / (float)(x2 - x1);
+        }
+
+        return (vertical, slope);
+    }
+
+    private static float ComputeYIntercept(int x, int y, float slope)
+    {
+        return y - slope * x;
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397dbb173cf6a2a166471d816d497198e601aee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be93f3597457a1ddae5c1624d51c97beb938cf98")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
