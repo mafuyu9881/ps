634a6e3ecfcebb69c71ab7c7326e10fd929aa376
mafuyusame <mafuyu@tutamail.com>
2025-03-31 15:50:29 +0900

    [dp, sweeping] boj 25427

--- Program.cs
+++ Program.cs
@@ -1,108 +1,46 @@
-﻿using System.Text;
-
-internal class Program
+﻿internal class Program
 {
-    private enum Relationship
-    {
-        BothVertical,
-        EitherVertical,
-    }
-
     private static void Main(string[] args)
     {
-        int t = int.Parse(Console.ReadLine()!); // [1, 10]
+        int n = int.Parse(Console.ReadLine()!); // [1, 100'000]
 
-        StringBuilder sb = new();
-        for (int i = 0; i < t; ++i) // max tc = 10
+        const char D = 'D';
+        const char K = 'K';
+        const char S = 'S';
+        const char H = 'H';
+        
+        SortedDictionary<char, char> previous = new()
         {
-            // length = 8
-            // element = [-1'000, 1'000]
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int x1 = tokens[0];
-            int y1 = tokens[1];
-            int x2 = tokens[2];
-            int y2 = tokens[3];
-            int x3 = tokens[4];
-            int y3 = tokens[5];
-            int x4 = tokens[6];
-            int y4 = tokens[7];
+            { K, D },
+            { S, K },
+            { H, S },
+        };
 
-            var a1 = ComputeSlope(x1, y1, x2, y2);
-            var a2 = ComputeSlope(x3, y3, x4, y4);
+        SortedDictionary<char, long> combination = new()
+        {
+            { D, 0 },
+            { K, 0 },
+            { S, 0 },
+            { H, 0 },
+        };
 
-            if (a1.vertical && a2.vertical)
+        string token = Console.ReadLine()!;
+        for (int i = 0; i < token.Length; ++i) // max tc = 100'000
+        {
+            char c = token[i];
+
+            if (combination.ContainsKey(c)) // max tc = log2(4) = 2
             {
-                if (x1 == x3)
+                if (c == D)
                 {
-                    sb.AppendLine("LINE");
+                    ++combination[c]; // max tc = log2(4) = 2;
                 }
                 else
                 {
-                    sb.AppendLine("NONE");
+                    combination[c] += combination[previous[c]]; // max tc = log2(4) + log2(4) + log2(3) = 5.xxx
                 }
-            }
-            else if (a1.vertical == false && a2.vertical == false && Math.Abs(a1.slope - a2.slope) < 0.01f)
-            {
-                float b1 = ComputeYIntercept(x1, y1, a1.slope);
-                float b2 = ComputeYIntercept(x3, y3, a2.slope);
-
-                if (Math.Abs(b1 - b2) < 0.01f)
-                {
-                    sb.AppendLine("LINE");
-                }
-                else
-                {
-                    sb.AppendLine("NONE");
-                }
-            }
-            else
-            {
-                float x;
-                float y;
-
-                if (a1.vertical)
-                {
-                    float b2 = ComputeYIntercept(x3, y3, a2.slope);
-
-                    x = x1;
-                    y = a2.slope * x + b2;
-                }
-                else if (a2.vertical)
-                {
-                    float b1 = ComputeYIntercept(x1, y1, a1.slope);
-
-                    x = x2;
-                    y = a1.slope * x + b1;
-                }
-                else
-                {
-                    float b1 = ComputeYIntercept(x1, y1, a1.slope);
-                    float b2 = ComputeYIntercept(x3, y3, a2.slope);
-
-                    x = (b2 - b1) / (float)(a1.slope - a2.slope);
-                    y = a1.slope * x + b1;
-                }
-                sb.AppendLine($"POINT {x.ToString("F2")} {y.ToString("F2")}");
             }
         }
-        Console.Write(sb);
-    }
-
-    private static (bool vertical, float slope) ComputeSlope(int x1, int y1, int x2, int y2)
-    {
-        bool vertical = (x1 == x2);
-        float slope = 0.0f;
-
-        if (vertical == false)
-        {
-            slope = (y2 - y1) / (float)(x2 - x1);
-        }
-
-        return (vertical, slope);
-    }
-
-    private static float ComputeYIntercept(int x, int y, float slope)
-    {
-        return y - slope * x;
+        Console.Write(combination[H]);
     }
 }
