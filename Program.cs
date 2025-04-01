e9c85fbf3caa260b40454d651cd5ea2fcb31f963
mafuyusame <mafuyu@tutamail.com>
2025-04-02 08:25:25 +0900

    boj 13905 오답 코드

--- Program.cs
+++ Program.cs
@@ -1,91 +1,65 @@
 ﻿internal class Program
 {
-    const int InvalidV = -1;
-    const int InvalidCost = -1;
-
-    private static LinkedList<(int dst, int cost)>[] _adjList = null!;
-
     private static void Main(string[] args)
     {
         int[] tokens = null!;
 
         tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // length = 2
-        int n = tokens[0]; // [1, 100'000]
-        int m = tokens[1]; // [1, 200'000]
+        int n = tokens[0]; // [2, 100'000]
+        int m = tokens[1]; // [1, 300'000]
 
-        _adjList = new LinkedList<(int, int)>[n + 1];
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // length = 2
+        int s = tokens[0]; // [1, n] = [1, 100'000]
+        int e = tokens[1]; // [1, n] = [1, 100'000]
 
-        for (int i = 1; i < _adjList.Length; ++i) // max tc = n = 100'000
+        LinkedList<(int v, int weightLimit)>[] adjList = new LinkedList<(int, int)>[n + 1];
+        for (int i = 0; i < adjList.Length; ++i) // max tc = n = 100'000
         {
-            _adjList[i] = new();
-        }
-        
-        for (int i = 0; i < m; ++i) // max tc = m = 200'000
-        {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // length = 3
-            int u = tokens[0]; // [1, n] = [1, 100'000]
-            int v = tokens[1]; // [1, n] = [1, 100'000]
-            int w = tokens[2]; // [1, 10'000]
-            _adjList[u].AddLast((v, w));
+            adjList[i] = new();
         }
 
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // length = 3
-        int x = tokens[0];
-        int y = tokens[1];
-        int z = tokens[2];
-
-        int[] xMinCost = ComputeMinCost(x, InvalidV);
-        int[] yMinCost = ComputeMinCost(y, InvalidV);
-        int[] xMinCostWithoutY = ComputeMinCost(x, y);
-
-        int case0 = (xMinCost[y] == InvalidCost || yMinCost[z] == InvalidCost) ? InvalidCost : xMinCost[y] + yMinCost[z];
-        int case1 = xMinCostWithoutY[z];
-
-        Console.Write($"{case0} {case1}");
-    }
-
-    // max tc = 100'000 * log2(200'000) = 100'000 * 17.xxx
-    private static int[] ComputeMinCost(int s, int bannedV)
-    {
-        int[] minCost = new int[_adjList.Length];
-        for (int i = 0; i < minCost.Length; ++i)
+        for (int i = 0; i < m; ++i) // max tc = m = 300'000
         {
-            minCost[i] = InvalidCost;
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // length = 2
+            int h1 = tokens[0]; // [1, n] = [1, 100'000]
+            int h2 = tokens[1]; // [1, n] = [1, 100'000]
+            int k = tokens[2]; // [1, 1'000'000]
+            adjList[h1].AddLast((h2, k));
+            adjList[h2].AddLast((h1, k));
         }
 
-        PriorityQueue<(int v, int cost), int> frontier = new();
-        
-        minCost[s] = 0;
-        frontier.Enqueue((s, minCost[s]), minCost[s]);
+        const int Infinity = -2;
+
+        int[] maxCapacity = new int[adjList.Length];
+
+        Queue<(int v, int capacity)> frontier = new();
+
+        maxCapacity[s] = Infinity;
+        frontier.Enqueue((s, maxCapacity[s]));
         while (frontier.Count > 0)
         {
             var element = frontier.Dequeue();
-            
+
             int v = element.v;
-            int cost = element.cost;
+            int capacity = element.capacity;
 
-            if (minCost[v] < cost) // minCost[v] can't be set as InvalidCost in this stream
-                continue;
-
-            var adjs = _adjList[v];
+            var adjs = adjList[v];
             for (var lln = adjs.First; lln != null; lln = lln.Next)
             {
-                int adjV = lln.Value.dst;
-                if (adjV == bannedV)
+                int adjV = lln.Value.v;
+
+                int oldAdjVCapacity = maxCapacity[adjV];
+                if (oldAdjVCapacity == Infinity)
                     continue;
 
-                int adjCost = lln.Value.cost;
-
-                int oldCost = minCost[adjV];
-                int newCost = cost + adjCost;
-                if (oldCost != InvalidCost && oldCost <= newCost)
+                int newAdjVCapacity = lln.Value.weightLimit;
+                if (oldAdjVCapacity >= newAdjVCapacity)
                     continue;
 
-                minCost[adjV] = newCost;
-                frontier.Enqueue((adjV, minCost[adjV]), minCost[adjV]);
+                maxCapacity[adjV] = newAdjVCapacity;
+                frontier.Enqueue((adjV, maxCapacity[adjV]));
             }
         }
-
-        return minCost;
+        Console.Write(maxCapacity[e]);
     }
 }
