f07f09653281d54343541c992877f9314501d653
mafuyusame <mafuyu@tutamail.com>
2025-04-02 09:34:03 +0900

    boj 13905 2% 시간 초과 코드

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
 ﻿internal class Program
 {
+    const int InvalidHouse = -1;
+    const int Infinity = -1;
+
+    private static int _s = InvalidHouse;
+    private static int _e = InvalidHouse;
+
+    private static LinkedList<(int v, int weightLimit)>[] _adjList = null!;
+
+    private static int _maxCapacity = 0;
+
+    private static bool[] _visited = null!;
+
     private static void Main(string[] args)
     {
         int[] tokens = null!;
@@ -9,13 +21,13 @@
         int m = tokens[1]; // [1, 300'000]
 
         tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // length = 2
-        int s = tokens[0]; // [1, n] = [1, 100'000]
-        int e = tokens[1]; // [1, n] = [1, 100'000]
+        _s = tokens[0]; // [1, n] = [1, 100'000]
+        _e = tokens[1]; // [1, n] = [1, 100'000]
 
-        LinkedList<(int v, int weightLimit)>[] adjList = new LinkedList<(int, int)>[n + 1];
-        for (int i = 0; i < adjList.Length; ++i) // max tc = n = 100'000
+        _adjList = new LinkedList<(int, int)>[n + 1];
+        for (int i = 0; i < _adjList.Length; ++i) // max tc = n = 100'000
         {
-            adjList[i] = new();
+            _adjList[i] = new();
         }
 
         for (int i = 0; i < m; ++i) // max tc = m = 300'000
@@ -24,42 +36,47 @@
             int h1 = tokens[0]; // [1, n] = [1, 100'000]
             int h2 = tokens[1]; // [1, n] = [1, 100'000]
             int k = tokens[2]; // [1, 1'000'000]
-            adjList[h1].AddLast((h2, k));
-            adjList[h2].AddLast((h1, k));
+            _adjList[h1].AddLast((h2, k));
+            _adjList[h2].AddLast((h1, k));
         }
+        
+        _visited = new bool[_adjList.Length];
+        _visited[_s] = true;
+        DFS(_s, Infinity);
+        _visited[_s] = false;
 
-        const int Infinity = -2;
+        Console.Write(_maxCapacity);
+    }
 
-        int[] maxCapacity = new int[adjList.Length];
-
-        Queue<(int v, int capacity)> frontier = new();
-
-        maxCapacity[s] = Infinity;
-        frontier.Enqueue((s, maxCapacity[s]));
-        while (frontier.Count > 0)
+    private static void DFS(int v, int weightLimit)
+    {
+        var adjs = _adjList[v];
+        for (var lln = adjs.First; lln != null; lln = lln.Next)
         {
-            var element = frontier.Dequeue();
+            int adjV = lln.Value.v;
+            if (_visited[adjV])
+                continue;
 
-            int v = element.v;
-            int capacity = element.capacity;
-
-            var adjs = adjList[v];
-            for (var lln = adjs.First; lln != null; lln = lln.Next)
+            int adjWeightLimit;
+            if (weightLimit == Infinity)
             {
-                int adjV = lln.Value.v;
-
-                int oldAdjVCapacity = maxCapacity[adjV];
-                if (oldAdjVCapacity == Infinity)
-                    continue;
-
-                int newAdjVCapacity = lln.Value.weightLimit;
-                if (oldAdjVCapacity >= newAdjVCapacity)
-                    continue;
-
-                maxCapacity[adjV] = newAdjVCapacity;
-                frontier.Enqueue((adjV, maxCapacity[adjV]));
+                adjWeightLimit = lln.Value.weightLimit;
+            }
+            else
+            {
+                adjWeightLimit = Math.Min(weightLimit, lln.Value.weightLimit);
+            }
+            
+            if (adjV == _e)
+            {
+                _maxCapacity = Math.Max(_maxCapacity, adjWeightLimit);
+            }
+            else
+            {
+                _visited[adjV] = true;
+                DFS(adjV, adjWeightLimit);
+                _visited[adjV] = false;
             }
         }
-        Console.Write(maxCapacity[e]);
     }
 }
