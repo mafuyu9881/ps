55444bf082c27a6904413f22f959de10de2db046
mafuyusame <mafuyu@tutamail.com>
2025-04-03 16:57:41 +0900

    [math, binary search tree] boj 2358

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@
     {
         int n = int.Parse(Console.ReadLine()!); // [1, 100'000]
 
-        SortedDictionary<int, int> xs = new();
-        SortedDictionary<int, int> ys = new();
+        SortedSet<int> xPoints = new();
+        SortedSet<int> yPoints = new();
+        
+        SortedSet<int> xLines = new();
+        SortedSet<int> yLines = new();
 
-        LinkedList<(int x, int y)> points = new();
         for (int i = 0; i < n; ++i) // max tc = 100'000
         {
             // length = 2
@@ -16,46 +18,16 @@
             int x = tokens[0];
             int y = tokens[1];
             
-            CollectPoint(xs, x);
-            CollectPoint(ys, y);
+            if (xPoints.Contains(x))
+                xLines.Add(x);
 
-            points.AddLast((x, y));
+            if (yPoints.Contains(y))
+                yLines.Add(y);
+
+            xPoints.Add(x);
+            yPoints.Add(y);
         }
 
-        int lines = 0;
-        for (var lln = points.First; lln != null; lln = lln.Next) // max tc = 100'000
-        {
-            lines += ComputeLines(xs, lln.Value.x);
-            lines += ComputeLines(ys, lln.Value.y);
-        }
-        Console.Write(lines);
-    }
-
-    private static void CollectPoint(SortedDictionary<int, int> ps, int p)
-    {
-        if (ps.ContainsKey(p)) // max tc = log2(100'000) = 16.xxx
-        {
-            ++ps[p]; // max tc = 16.xxx
-        }
-        else
-        {
-            ps.Add(p, 1); // max tc = 16.xxx
-        }
-    }
-
-    private static int ComputeLines(SortedDictionary<int, int> ps, int p)
-    {
-        int lines = 0;
-
-        if (ps.ContainsKey(p) == false)
-            return lines;
-
-        lines = ps[p] - 1;
-        
-        --ps[p];
-        if (ps[p] < 1)
-            ps.Remove(p);
-
-        return lines;
+        Console.Write(xLines.Count + yLines.Count);
     }
 }
