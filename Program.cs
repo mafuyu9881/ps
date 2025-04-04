21def50a30ea3e4b1aa4db7950423e07c7153a65
mafuyusame <mafuyu@tutamail.com>
2025-04-04 10:34:25 +0900

    boj 28683 시간 초과 코드 (미제출)

--- Program.cs
+++ Program.cs
@@ -1,79 +1,101 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
+    const int MaxSqrtN = 1000000;
+
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [2, 500]
+        long n = long.Parse(Console.ReadLine()!); // [1, 10^12]
 
-        int m = int.Parse(Console.ReadLine()!); // [1, 10'000]
-
-        LinkedList<int>[] adjList = new LinkedList<int>[n + 1];
-        for (int i = 1; i < adjList.Length; ++i) // max tc = 500
+        int satisfieds;
+        if (PerfectSquare(n)) // max tc = 19.xxx
         {
-            adjList[i] = new();
+            // If sqrt(n) is an integer, it is intuitively clear
+            // that setting it as the base or height of a right triangle
+            // allows for the creation of infinitely many right triangles
+            // in which at least two sides have integer lengths.
+            satisfieds = -1;
         }
-
-        for (int i = 0; i < m; ++i) // max tc = 10'000
+        else
         {
-            // length = 2
-            // element = [1, n] = [1, 500]
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int a = tokens[0];
-            int b = tokens[1];
-            adjList[a].AddLast(b);
-            adjList[b].AddLast(a);
-        }
+            satisfieds = 0;
 
-        const int InvalidCost = -1;
+            SortedSet<long> founds = new();
 
-        int[] minCosts = new int[adjList.Length];
-        for (int i = 0; i < minCosts.Length; ++i)
-        {
-            minCosts[i] = InvalidCost;
-        }
-        
-        PriorityQueue<(int v, int cost), int> frontier = new();
-
-        int s = 1;
-        int sCost = 0;
-        minCosts[s] = sCost;
-        frontier.Enqueue((s, sCost), sCost);
-        while (frontier.Count > 0)
-        {
-            var element = frontier.Dequeue();
-            int v = element.v;
-            int vCost = element.cost;
-
-            if (vCost > minCosts[v])
-                continue;
-
-            var adjs = adjList[v];
-            for (var lln = adjs.First; lln != null; lln = lln.Next)
+            for (long a = 1; a * a < n; ++a) // max tc = about 10^6
             {
-                int adjV = lln.Value;
+                long sqrA = a * a;
+                long sqrB = n - sqrA;
 
-                int oldAdjVMinCost = minCosts[adjV];
-                int newAdjVMinCost = vCost + 1;
-                if (oldAdjVMinCost != InvalidCost && oldAdjVMinCost <= newAdjVMinCost)
+                if (PerfectSquare(sqrB) == false) // max tc = 19.xxx
                     continue;
 
-                minCosts[adjV] = newAdjVMinCost;
-                frontier.Enqueue((adjV, newAdjVMinCost), newAdjVMinCost);
+                if (founds.Contains(sqrA) || founds.Contains(sqrB)) // max tc = 19.xxx * 2
+                    continue;
+
+                founds.Add(sqrA);
+                founds.Add(sqrB);
+
+                ++satisfieds;
+            }
+
+            {
+                long a = 1;
+                while ((a + 1) * (a + 1) - a * a <= n)
+                {
+                    long sqrA = a * a;
+                    long sqrB = sqrA + n;
+                    
+                    // it doesn't matter if the pair of integers `a`, `b` that satisfy
+                    // the condition in this loop has already been found during the
+                    // iteration on line 26
+
+                    // although the pair on line 26 consists of two values that both form the
+                    // non-hypotenuse side, in this case, on of the values forms the hypotenuse,
+                    // meaning they represent different triangles
+
+                    // additionally, since `a` continues to increase and the expression adds n
+                    // to it, the structure of the formula prevents any duplicate pairs
+                    // from being found
+
+                    if (PerfectSquare(sqrB))
+                        ++satisfieds;
+
+                    ++a;
+                }
+            }
+        }
+        Console.Write(satisfieds);
+    }
+
+    private static bool PerfectSquare(long x)
+    {
+        bool output = false;
+
+        int lo = 0 - 1;
+        int hi = MaxSqrtN + 1;
+        while (lo < hi - 1) // max tc = log2(1'000'000) = 19.xxx
+        {
+            int mid = (lo + hi) / 2;
+            
+            long sqrMid = mid * (long)mid;
+
+            if (sqrMid > x)
+            {
+                hi = mid;
+            }
+            else if (sqrMid < x)
+            {
+                lo = mid;
+            }
+            else
+            {
+                output = true;
+                break;
             }
         }
 
-        int invitables = 0;
-        for (int i = 2; i < minCosts.Length; ++i)
-        {
-            int minCost = minCosts[i];
-
-            if (minCost == InvalidCost)
-                continue;
-
-            if (minCost > 2)
-                continue;
-
-            ++invitables;
-        }
-        Console.Write(invitables);
+        return output;
     }
 }
