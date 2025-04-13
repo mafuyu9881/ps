ddd6a9c836b4a9297a50e2ac616a890a9062b3cb
mafuyusame <mafuyu@tutamail.com>
2025-04-13 10:40:37 +0900

    [math, bruteforcing, euclidean algorithm] boj 16970

--- Program.cs
+++ Program.cs
@@ -2,106 +2,80 @@
 {
     private static void Main(string[] args)
     {
-        const int Village = 1;
-        const int InvalidCost = -1;
-        const int Offsets = 4;
-        int[] RowOffsets = new int[Offsets] { -1, 1, 0, 0 };
-        int[] ColOffsets = new int[Offsets] { 0, 0, -1, 1 };
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // length = 3
+        int width = tokens[0] + 1; // [1 + 1, 50 + 1]
+        int height = tokens[1] + 1; // [1 + 1, 50 + 1]
+        int objective = tokens[2]; // [2, 50]
 
-        // length = 2
-        // element = [2, 20]
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int height = tokens[0];
-        int width = tokens[1];
-        
-        Func<int, int, int> ConvertIndex2DTo1D = (row, col) =>
-        {
-            return row * width + col;
-        };
+        int end = width * height;
 
-        int[] map = new int[height * width];
-        for (int row = 0; row < height; ++row) // max tc = 20
+        int segments = 0;
+        for (int i0 = 0; i0 < end; ++i0) // max tc = 51 * 51 = 2'601
         {
-            string line = Console.ReadLine()!;
-            for (int col = 0; col < width; ++col) // max tc = 20
+            for (int i1 = i0 + 1; i1 < end; ++i1) // max tc = 2'601
             {
-                map[ConvertIndex2DTo1D(row, col)] = line[col] - '0';
+                int x0 = i0 % width;
+                int y0 = i0 / width;
+
+                int x1 = i1 % width;
+                int y1 = i1 / width;
+
+                int xStride = x1 - x0;
+                int yStride = y1 - y0;
+
+                int strideGCD = GCD(Math.Abs(xStride), Math.Abs(yStride));
+
+                xStride /= strideGCD;
+                yStride /= strideGCD;
+
+                int i = 0;
+                while (true) // max tc = 50
+                {
+                    if (i > objective)
+                        break;
+
+                    int x = x0 + xStride * i;
+                    if (x < 0 || x > width - 1)
+                        break;
+
+                    if (xStride < 0 && x < x1)
+                        break;
+
+                    if (xStride > 0 && x > x1)
+                        break;
+
+                    int y = y0 + yStride * i;
+                    if (y < 0 || y > height - 1)
+                        break;
+
+                    if (yStride < 0 && y < y1)
+                        break;
+
+                    if (yStride > 0 && y > y1)
+                        break;
+
+                    ++i;
+                }
+
+                if (i == objective)
+                    ++segments;
             }
         }
+        Console.Write(segments);
+    }
 
-        Action<int[], int> BFS = (minCost, s) =>
+    private static int GCD(int a, int b)
+    {
+        int max = Math.Max(a, b);
+        int min = Math.Min(a, b);
+
+        while (min > 0)
         {
-            Queue<int> frontier = new();
-
-            minCost[s] = 0;
-            frontier.Enqueue(s);
-            while (frontier.Count > 0)
-            {
-                int index = frontier.Dequeue();
-                int row = index / width;
-                int col = index % width;
-
-                int adjNewMinCost = minCost[index] + 1;
-
-                for (int i = 0; i < Offsets; ++i)
-                {
-                    int adjRow = row + RowOffsets[i];
-                    if (adjRow < 0 || adjRow > height - 1)
-                        continue;
-
-                    int adjCol = col + ColOffsets[i];
-                    if (adjCol < 0 || adjCol > width - 1)
-                        continue;
-
-                    int adjIndex = ConvertIndex2DTo1D(adjRow, adjCol);
-                    int adjOldMinCost = minCost[adjIndex];
-                    if (adjOldMinCost != InvalidCost && adjOldMinCost <= adjNewMinCost)
-                        continue;
-
-                    minCost[adjIndex] = adjNewMinCost;
-                    frontier.Enqueue(adjIndex);
-                }
-            }
-        };
-
-        int minPoisoningCost = InvalidCost;
-        for (int i0 = 0; i0 < map.Length; ++i0) // max tc = 400
-        {
-            if (map[i0] == Village)
-                continue;
-
-            for (int i1 = i0 + 1; i1 < map.Length; ++i1) // max tc = 400
-            {
-                if (map[i1] == Village)
-                    continue;
-
-                int[] minCost = new int[map.Length];
-                for (int i = 0; i < minCost.Length; ++i) // max tc = 400
-                {
-                    minCost[i] = InvalidCost;
-                }
-
-                BFS(minCost, i0);
-                BFS(minCost, i1);
-
-                int poisoningCost = InvalidCost;
-                for (int i = 0; i < minCost.Length; ++i) // max tc = 400
-                {
-                    if (map[i] != Village)
-                        continue;
-
-                    if (poisoningCost != InvalidCost && poisoningCost >= minCost[i])
-                        continue;
-
-                    poisoningCost = minCost[i];
-                }
-
-                if (minPoisoningCost == InvalidCost || poisoningCost < minPoisoningCost)
-                {
-                    minPoisoningCost = poisoningCost;
-                }
-            }
+            int temp = max % min;
+            max = min;
+            min = temp;
         }
-        Console.Write(minPoisoningCost);
+
+        return max;
     }
 }
