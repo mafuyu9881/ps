4dc6568f00280b4c22c6af1d0dbd07d1378855e7
mafuyusame <mafuyu@tutamail.com>
2025-04-13 15:23:52 +0900

    boj 32347 12% 오답 코드

--- Program.cs
+++ Program.cs
@@ -2,80 +2,59 @@
 {
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // length = 3
-        int width = tokens[0] + 1; // [1 + 1, 50 + 1]
-        int height = tokens[1] + 1; // [1 + 1, 50 + 1]
-        int objective = tokens[2]; // [2, 50]
+        int[] tokens = null!;
 
-        int end = width * height;
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // [2, 200'000]
+        int k = tokens[1]; // [2, n) = [2, 200'000)
 
-        int segments = 0;
-        for (int i0 = 0; i0 < end; ++i0) // max tc = 51 * 51 = 2'601
+        Func<int, bool> Condition = (t) =>
         {
-            for (int i1 = i0 + 1; i1 < end; ++i1) // max tc = 2'601
+            int day = n;
+
+            bool[] visited = new bool[n + 1];
+            visited[n] = true;
+
+            while (true)
             {
-                int x0 = i0 % width;
-                int y0 = i0 / width;
-
-                int x1 = i1 % width;
-                int y1 = i1 / width;
-
-                int xStride = x1 - x0;
-                int yStride = y1 - y0;
-
-                int strideGCD = GCD(Math.Abs(xStride), Math.Abs(yStride));
-
-                xStride /= strideGCD;
-                yStride /= strideGCD;
-
-                int i = 0;
-                while (true) // max tc = 50
+                if (tokens[day - 1] == 1)
                 {
-                    if (i > objective)
-                        break;
-
-                    int x = x0 + xStride * i;
-                    if (x < 0 || x > width - 1)
-                        break;
-
-                    if (xStride < 0 && x < x1)
-                        break;
-
-                    if (xStride > 0 && x > x1)
-                        break;
-
-                    int y = y0 + yStride * i;
-                    if (y < 0 || y > height - 1)
-                        break;
-
-                    if (yStride < 0 && y < y1)
-                        break;
-
-                    if (yStride > 0 && y > y1)
-                        break;
-
-                    ++i;
+                    day = Math.Max(1, day - t);
+                }
+                else
+                {
+                    ++day;
                 }
 
-                if (i == objective)
-                    ++segments;
+                if (visited[day])
+                    return false;
+
+                visited[day] = true;
+
+                if (day == 1)
+                    return true;
+            }
+        };
+
+        // length = [2, 200'000]
+        // element = [0, 1]
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        int lo = 0 - 1;
+        int hi = n + 1;
+        while (lo < hi - 1)
+        {
+            int mid = (lo + hi) / 2;
+
+            if (Condition(mid))
+            {
+                hi = mid;
+            }
+            else
+            {
+                lo = mid;
             }
         }
-        Console.Write(segments);
-    }
-
-    private static int GCD(int a, int b)
-    {
-        int max = Math.Max(a, b);
-        int min = Math.Min(a, b);
-
-        while (min > 0)
-        {
-            int temp = max % min;
-            max = min;
-            min = temp;
-        }
-
-        return max;
+        Console.Write(hi);
     }
 }
