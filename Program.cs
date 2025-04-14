8756bd19977800ab1f4d0800b928f3ccab3d5dad
mafuyusame <mafuyu@tutamail.com>
2025-04-14 22:09:15 +0900

    boj 1865 2% 오답 코드

--- Program.cs
+++ Program.cs
@@ -2,92 +2,69 @@
 
 internal class Program
 {
-    private class DisjointSet
-    {
-        private int[] _parents = null!;
-
-        public DisjointSet(int n)
-        {
-            _parents = new int[n];
-            for (int i = 0; i < _parents.Length; ++i)
-            {
-                _parents[i] = i;
-            }
-        }
-        
-        public int Find(int x)
-        {
-            int parent = _parents[x];
-            if (parent != x)
-            {
-                _parents[x] = Find(parent);
-            }
-            return _parents[x];
-        }
-
-        public void Union(int a, int b)
-        {
-            int aSetRepresentative = Find(a);
-            int bSetRepresentative = Find(b);
-
-            if (aSetRepresentative < bSetRepresentative)
-            {
-                _parents[aSetRepresentative] = bSetRepresentative;
-            }
-            else if (aSetRepresentative > bSetRepresentative)
-            {
-                _parents[bSetRepresentative] = aSetRepresentative;
-            }
-        }
-    }
-
     private static void Main(string[] args)
     {
-        const int Blank = 0;
+        const int InvalidCost = -1;
+
+        int t = int.Parse(Console.ReadLine()!); // [1, 5]
 
         int[] tokens = null!;
 
-        // length = 2
-        // element = [1, 10^5]
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0];
-        int q = tokens[1];
-
-        int[] canvas = new int[n + 1];
-
-        SortedSet<int> set = new();
-        for (int i = 1; i < canvas.Length; ++i) // max tc = 10^5
-        {
-            set.Add(i);
-        }
-
-        DisjointSet ds = new(n + 1);
-        
-        for (int i = 0; i < q; ++i) // max tc = 10^5
+        StringBuilder sb = new();
+        for (int i = 0; i < t; ++i) // max t = 5
         {
             tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int a = tokens[0]; // [1, n] = [1, 10^5]
-            int b = tokens[1]; // [1, n] = [1, 10^5]
-            int x = tokens[2]; // [1, 10^9]
+            int n = tokens[0]; // [1, 500]
+            int m = tokens[1]; // [1, 2'500]
+            int w = tokens[2]; // [1, 200]
 
-            for (int j = a; j <= b; ++j) // average tc = α(10^5)
+            LinkedList<(int s, int e, int cost)> edges = new();
+
+            for (int j = 0; j < m; ++j) // max tc = 2'500
             {
-                if (canvas[j] != Blank)
-                {
-                    j = ds.Find(j);
-                }
-                else
-                {
-                    canvas[j] = x;
-                }
-                ds.Union(j, b);
+                tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+                int s = tokens[0];
+                int e = tokens[1];
+                int cost = tokens[2];
+                edges.AddLast((s, e, cost));
+                edges.AddLast((e, s, cost));
             }
-        }
 
-        StringBuilder sb = new();
-        for (int i = 1; i < canvas.Length; ++i)
-        {
-            sb.Append($"{canvas[i]} ");
+            for (int j = 0; j < w; ++j) // max tc = 200
+            {
+                tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+                int s = tokens[0];
+                int e = tokens[1];
+                int cost = tokens[2];
+                edges.AddLast((s, e, -cost));
+            }
+
+            int[] minCost = new int[n + 1];
+            for (int j = 1; j < minCost.Length; ++j) // max tc = 200
+            {
+                minCost[j] = InvalidCost;
+            }
+            minCost[1] = 0;
+            
+            for (int j = 0; j < n; ++j) // max tc = 500
+            {
+                for (var lln = edges.First; lln != null; lln = lln.Next) // max tc = 2'500
+                {
+                    int s = lln.Value.s;
+                    if (minCost[s] == InvalidCost)
+                        continue;
+
+                    int e = lln.Value.e;
+                    int eOldMinCost = minCost[e];
+                    int eNewMinCost = minCost[s] + lln.Value.cost;
+                    if (eOldMinCost != InvalidCost && eOldMinCost <= eNewMinCost)
+                        continue;
+
+                    minCost[e] = eNewMinCost;
+                }
+            }
+
+            sb.AppendLine(minCost[1] < 0 ? "YES" : "NO");
         }
         Console.Write(sb);
     }
