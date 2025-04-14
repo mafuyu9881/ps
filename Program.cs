42f981ccd4630102f93374be547e151f7647ac5c
mafuyusame <mafuyu@tutamail.com>
2025-04-14 12:21:16 +0900

    [disjoint set] boj 29618

--- Program.cs
+++ Program.cs
@@ -2,8 +2,49 @@
 
 internal class Program
 {
+    private class DisjointSet
+    {
+        private int[] _parents = null!;
+
+        public DisjointSet(int n)
+        {
+            _parents = new int[n];
+            for (int i = 0; i < _parents.Length; ++i)
+            {
+                _parents[i] = i;
+            }
+        }
+        
+        public int Find(int x)
+        {
+            int parent = _parents[x];
+            if (parent != x)
+            {
+                _parents[x] = Find(parent);
+            }
+            return _parents[x];
+        }
+
+        public void Union(int a, int b)
+        {
+            int aSetRepresentative = Find(a);
+            int bSetRepresentative = Find(b);
+
+            if (aSetRepresentative < bSetRepresentative)
+            {
+                _parents[aSetRepresentative] = bSetRepresentative;
+            }
+            else if (aSetRepresentative > bSetRepresentative)
+            {
+                _parents[bSetRepresentative] = aSetRepresentative;
+            }
+        }
+    }
+
     private static void Main(string[] args)
     {
+        const int Blank = 0;
+
         int[] tokens = null!;
 
         // length = 2
@@ -19,6 +60,8 @@ internal class Program
         {
             set.Add(i);
         }
+
+        DisjointSet ds = new(n + 1);
         
         for (int i = 0; i < q; ++i) // max tc = 10^5
         {
@@ -27,20 +70,22 @@ internal class Program
             int b = tokens[1]; // [1, n] = [1, 10^5]
             int x = tokens[2]; // [1, 10^9]
 
-            SortedSet<int> subset = set.GetViewBetween(a, b);
-            while (true)
+            for (int j = a; j <= b; ++j) // average tc = α(10^5)
             {
-                if (subset.Count < 1)
-                    break;
-
-                int index = subset.Min;
-                canvas[index] = x;
-                subset.Remove(index);
+                if (canvas[j] != Blank)
+                {
+                    j = ds.Find(j);
+                }
+                else
+                {
+                    canvas[j] = x;
+                }
+                ds.Union(j, b);
             }
         }
-        
+
         StringBuilder sb = new();
-        for (int i = 1; i < canvas.Length; ++i) // max tc = 10^5
+        for (int i = 1; i < canvas.Length; ++i)
         {
             sb.Append($"{canvas[i]} ");
         }
