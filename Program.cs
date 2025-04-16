c46c5842b8c7de60769aee27bf77da912bded164
mafuyusame <mafuyu@tutamail.com>
2025-04-16 14:14:52 +0900

    [two pointer] boj 2018

--- Program.cs
+++ Program.cs
@@ -2,24 +2,29 @@
 {
     private static void Main(string[] args)
     {
-        string c = Console.ReadLine()!;
-        string s = Console.ReadLine()!;
+        int n = int.Parse(Console.ReadLine()!); // [1, 10'000'000]
+
+        int i = 0;
+        int j = 0;
 
         int cnt = 0;
         {
-            int streaks = 0;
-            for (int i = 0; i < s.Length; ++i)
+            int sum = 0;
+            while (i <= n && j <= n)
             {
-                char token = s[i];
-
-                if (token == c[streaks])
+                if (sum > n)
                 {
-                    ++streaks;
+                    sum -= i;
+                    ++i;
+                }
+                else
+                {
+                    ++j;
+                    sum += j;
                 }
 
-                if (streaks == c.Length)
+                if (sum == n)
                 {
-                    streaks = 0;
                     ++cnt;
                 }
             }
