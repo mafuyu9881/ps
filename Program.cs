0c1bb69eb075f42e5af996971ddf6354f17ea543
mafuyusame <mafuyu@tutamail.com>
2025-04-19 17:42:30 +0900

    [graph, simulation] BOJ 18231

--- Program.cs
+++ Program.cs
@@ -4,66 +4,97 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        SortedDictionary<char, int> Indices = new()
+        int[] tokens = null!;
+
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // [1, 2'000]
+        int m = tokens[1]; // [1, 2'100'000]
+
+        LinkedList<int>[] adjList = new LinkedList<int>[n + 1];
         {
-            { 'q', 0 },
-            { 'u', 1 },
-            { 'a', 2 },
-            { 'c', 3 },
-            { 'k', 4 },
-        };
-
-        // length = [5, 2'500]
-        // element = q, u, a, c, k
-        string s = Console.ReadLine()!;
-
-        int[] count = new int[5];
-
-        int ducksPeak = 0;
-        {
-            int ducks = 0;
-            for (int i = 0; i < s.Length; ++i)
+            for (int i = 1; i < adjList.Length; ++i)
             {
-                char c = s[i];
-
-                int currIndex = Indices[c];
-
-                if (currIndex <= 0)
-                {
-                    ++count[currIndex];
-                    ++ducks;
-                }
-                else
-                {
-                    int prevIndex = currIndex - 1;
-
-                    if (count[prevIndex] > 0)
-                    {
-                        --count[prevIndex];
-                    }
-                    else
-                    {
-                        ducksPeak = -1;
-                        break;
-                    }
-
-                    if (currIndex < 4)
-                    {
-                        ++count[currIndex];
-                    }
-                    else
-                    {
-                        --ducks;
-                    }
-                }
-                ducksPeak = Math.Max(ducksPeak, ducks);
+                adjList[i] = new();
             }
-
-            if (ducks > 0)
+            for (int i = 0; i < m; ++i)
             {
-                ducksPeak = -1;
+                tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+                int u = tokens[0]; // [1, n] = [1, 2'000]
+                int v = tokens[1]; // [1, n] = [1, 2'000]
+                adjList[u].AddLast(v);
+                adjList[v].AddLast(u);
             }
         }
-        Console.Write(ducksPeak);
+
+        bool[] receivedRuins = new bool[n + 1];
+        {
+            Console.ReadLine();
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            for (int i = 0; i < tokens.Length; ++i)
+            {
+                receivedRuins[tokens[i]] = true;
+            }
+        }
+
+        LinkedList<int> impactPoints = new();
+        {
+            bool[] simulatedRuins = new bool[n + 1];
+            for (int v = 1; v <= n; ++v)
+            {
+                if (receivedRuins[v] == false)
+                    continue;
+
+                bool impactable = true;
+                {
+                    var adjs = adjList[v];
+                    for (var lln = adjs.First; lln != null; lln = lln.Next)
+                    {
+                        int adjV = lln.Value;
+                        if (receivedRuins[adjV] == false)
+                        {
+                            impactable = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (impactable)
+                {
+                    var adjs = adjList[v];
+                    for (var lln = adjs.First; lln != null; lln = lln.Next)
+                    {
+                        int adjV = lln.Value;
+                        simulatedRuins[adjV] = true;
+                    }
+                    simulatedRuins[v] = true;
+
+                    impactPoints.AddLast(v);
+                }
+            }
+
+            for (int i = 1; i <= n; ++i)
+            {
+                if (receivedRuins[i] != simulatedRuins[i])
+                {
+                    impactPoints = null!;
+                    break;
+                }
+            }
+        }
+
+        StringBuilder sb = new();
+        if (impactPoints == null)
+        {
+            sb.Append(-1);
+        }
+        else
+        {
+            sb.AppendLine($"{impactPoints.Count}");
+            for (var lln = impactPoints.First; lln != null; lln = lln.Next)
+            {
+                sb.Append($"{lln.Value} ");
+            }
+        }
+        Console.Write(sb);
     }
 }
