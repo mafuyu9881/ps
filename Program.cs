9ed4cb8c72db47a6a8b7245b6705fb9f5027e1fd
mafuyusame <mafuyu@tutamail.com>
2025-04-23 18:27:33 +0900

    boj 25916 96% 오답 코드

--- Program.cs
+++ Program.cs
@@ -5,71 +5,46 @@
         int[] tokens = null!;
 
         tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // [1, 200'000]
-        int k = tokens[1]; // [1, 100]
+        int n = tokens[0]; // [1, 500'000]
+        int m = tokens[1]; // [1, 1'000'000'000]
 
-        // length = [1, n] = [1, 200'000]
-        // element = [1, 100'000]
         tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int[] sequence = new int[tokens.Length];
-        for (int i = 0; i < sequence.Length; ++i)
+        int[] holes = new int[tokens.Length];
+        for (int i = 0; i < holes.Length; ++i)
         {
-            sequence[i] = tokens[i];
+            holes[i] = tokens[i];
         }
 
-        int maxLength = 1;
+        long maxUsedVolume = 0;
         {
             int i = 0;
             int j = 0;
 
-            int duplicated = 0;
+            long usedVolume = holes[0];
 
-            int[] counts = new int[100000 + 1];
-            Action<int> AddCount = (number) =>
-            {
-                ++counts[number];
-
-                if (counts[number] == k + 1)
-                {
-                    ++duplicated;
-                }
-            };
-            Action<int> RemoveCount = (number) =>
-            {
-                if (counts[number] == k + 1)
-                {
-                    --duplicated;
-                }
-
-                --counts[number];
-            };
-
-            AddCount(sequence[j]);
-            
             while (true)
             {
-                if (duplicated > 0)
+                if (usedVolume < m)
                 {
-                    RemoveCount(sequence[i]);
-                    ++i;
-                }
-                else
-                {
-                    maxLength = Math.Max(maxLength, j - i + 1);
+                    maxUsedVolume = Math.Max(maxUsedVolume, usedVolume);
 
                     ++j;
-
-                    if (j < sequence.Length)
+                    if (j < holes.Length)
                     {
-                        AddCount(sequence[j]);
+                        usedVolume += holes[j];
                     }
                     else
                     {
                         break;
                     }
                 }
+                else
+                {
+                    usedVolume -= holes[i];
+                    ++i;
+                }
             }
         }
-        Console.Write(maxLength);
+        Console.Write(maxUsedVolume);
     }
 }
