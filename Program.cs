75e1e2181c56ffd742120b6e5363f58b6679a9ef
mafuyusame <mafuyu@tutamail.com>
2025-04-24 19:22:59 +0900

    [implementation] BOJ 4396

--- Program.cs
+++ Program.cs
@@ -1,35 +1,94 @@
-﻿using System.Collections;
+﻿using System.Text;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        const char Mine = '*';
+        
+        const int Offsets = 8;
+        int[] RowOffsets = new int[Offsets] { -1, 1, 0, 0, -1, -1, 1, 1 };
+        int[] ColOffsets = new int[Offsets] { 0, 0, -1, 1, -1, 1, -1, 1 };
 
-        BitArray bits = new(10000000 + 1);
+        int n = int.Parse(Console.ReadLine()!); // [1, 10]
 
-        int number = 0;
-
-        while (true)
+        char[,] map = new char[n, n];
+        for (int row = 0; row < n; ++row)
         {
-            int c = Console.Read();
-            if (c == ' ' || c == -1)
+            string s = Console.ReadLine()!;
+            for (int col = 0; col < n; ++col)
             {
-                if (bits[number])
+                map[row, col] = s[col];
+            }
+        }
+
+        bool exploded = false;
+        char[,] simulated = new char[n, n];
+        for (int row = 0; row < n; ++row) // max tc = 10
+        {
+            string s = Console.ReadLine()!;
+            for (int col = 0; col < n; ++col) // max tc = 10
+            {
+                char attr;
+                if (s[col] == 'x')
                 {
-                    Console.Write(number);
-                    break;
+                    if (map[row, col] == Mine)
+                    {
+                        attr = Mine;
+                        exploded = true;
+                    }
+                    else
+                    {
+                        int count = 0;
+                        for (int i = 0; i < Offsets; ++i) // max tc = 8
+                        {
+                            int adjRow = row + RowOffsets[i];
+                            if (adjRow < 0 || adjRow > n - 1)
+                                continue;
+
+                            int adjCol = col + ColOffsets[i];
+                            if (adjCol < 0 || adjCol > n - 1)
+                                continue;
+
+                            if (map[adjRow, adjCol] != Mine)
+                                continue;
+
+                            ++count;
+                        }
+                        attr = (char)(count + '0');
+                    }
                 }
                 else
                 {
-                    bits[number] = true;
+                    attr = '.';
                 }
-                number = 0;
-            }
-            else
-            {
-                number = number * 10 + c - '0';
+                simulated[row, col] = attr;
             }
         }
+
+        if (exploded)
+        {
+            for (int row = 0; row < n; ++row)
+            {
+                for (int col = 0; col < n; ++col)
+                {
+                    if (map[row, col] == Mine)
+                    {
+                        simulated[row, col] = Mine;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new();
+        for (int row = 0; row < n; ++row)
+        {
+            for (int col = 0; col < n; ++col)
+            {
+                sb.Append(simulated[row, col]);
+            }
+            sb.AppendLine();
+        }
+        Console.Write(sb);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+418339f9479d684cc1d956e1a8f1a487b9109cc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9630b5146a364887c85b28acae0510d608ddc1af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
