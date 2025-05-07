f91136ca232ab7220367663a8bcabe83f8a06060
mafuyusame <mafuyu@tutamail.com>
2025-05-08 06:25:55 +0900

    [two pointer] boj 6503

--- Program.cs
+++ Program.cs
@@ -1,90 +1,66 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // [1, 5'000]
-        int c = tokens[1]; // [1, 100'000'000]
+        int[] typed = new int[128];
+        int typedCharacters = 0;
 
-        // length = [1, 5'000]
-        // element = [1, 100'000'000]
-        int[] weights = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        Array.Sort(weights);
-
-        int satisfied = 0;
+        StringBuilder sb = new();
+        while (true)
         {
-            for (int i = 0; i < n; ++i) // max tc = 5'000
+            int m = int.Parse(Console.ReadLine()!); // [1, 128]
+            if (m == 0)
+                break;
+
+            string s = Console.ReadLine()!;
+
+            int maxLength = 0;
             {
-                if (weights[i] == c)
+                for (int i = 0; i < typed.Length; ++i)
                 {
-                    satisfied = 1;
-                    goto End;
+                    typed[i] = 0;
+                }
+                typedCharacters = 0;
+
+                int lo = 0;
+                int hi = 0;
+                int length = 0;
+                while (hi < s.Length)
+                {
+                    char newCharacter = s[hi];
+
+                    if (typed[newCharacter] < 1)
+                    {
+                        ++typedCharacters;
+                    }
+
+                    ++typed[newCharacter];
+
+                    while (typedCharacters > m)
+                    {
+                        char oldCharacter = s[lo];
+
+                        --typed[oldCharacter];
+
+                        if (typed[oldCharacter] < 1)
+                        {
+                            --typedCharacters;
+                        }
+
+                        --length;
+                        ++lo;
+                    }
+
+                    ++length;
+                    ++hi;
+
+                    maxLength = Math.Max(maxLength, length);
                 }
             }
-
-            int s = 0;
-            int e = n - 1;
-            while (s < e) // max tc = 5'000
-            {
-                int sum = weights[s] + weights[e];
-                if (sum > c)
-                {
-                    --e;
-                }
-                else if (sum < c)
-                {
-                    ++s;
-                }
-                else
-                {
-                    satisfied = 1;
-                    goto End;
-                }
-            }
-            
-            Func<int, int, int, bool> BinarySearch = (pair, lo, hi) =>
-            {
-                while (lo < hi - 1) // max tc = log2(5'000) = 12.xxxx
-                {
-                    int mid = (lo + hi) / 2;
-                        
-                    int tuple = pair + weights[mid];
-
-                    if (tuple < c)
-                    {
-                        lo = mid;
-                    }
-                    else if (tuple > c)
-                    {
-                        hi = mid;
-                    }
-                    else
-                    {
-                        satisfied = 1;
-                        return true;
-                    }
-                }
-
-                return false;
-            };
-
-            for (int i = 0; i < n; ++i) // max tc = 5'000
-            {
-                for (int j = i + 1; j < n; ++j) // max tc = 5'000
-                {
-                    int pair = weights[i] + weights[j];
-                    if (pair > c)
-                        break;
-
-                    if (BinarySearch(pair, (i + 1) - 1, (j - 1) + 1))
-                        goto End;
-
-                    if (BinarySearch(pair, (j + 1) + 1, (n - 1) + 1))
-                        goto End;
-                }
-            }
+            sb.AppendLine($"{maxLength}");
         }
-End:
-        Console.Write(satisfied);
+        Console.Write(sb);
     }
 }
