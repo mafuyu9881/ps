8ec522829056cf85190d4feb5d87634d73a55083
mafuyusame <mafuyu@tutamail.com>
2025-05-14 23:44:08 +0900

    boj 15964

--- Program.cs
+++ Program.cs
@@ -2,93 +2,11 @@
 {
     private static void Main(string[] args)
     {
-        const int InvalidIndex = -1;
-        const int Infinity = 200000000 + 1;
-
-        int n = int.Parse(Console.ReadLine()!); // [1, 200'000]
-
-        long traveled = 0;
-        {
-            int collectorX = 0;
-
-            List<int> coords = new();
-            int nearestIndex = InvalidIndex;
-
-            for (int i = 0; i < n; ++i)
-            {
-                int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-                int command = tokens[0]; // [1, 2]
-
-                if (command == 1)
-                {
-                    int x = tokens[1]; // [-100'000'000, 100'000'000]
-
-                    coords.Add(x);
-
-                    int index = coords.Count - 1;
-
-                    bool update = false;
-                    if (nearestIndex == InvalidIndex)
-                    {
-                        update = true;
-                    }
-                    else
-                    {
-                        int collectorXToX = Math.Abs(x - collectorX);
-                        int collectorXToNearestX = Math.Abs(coords[nearestIndex] - collectorX);
-                        update = (collectorXToX < collectorXToNearestX) ||
-                                 (collectorXToX == collectorXToNearestX && index < nearestIndex);
-                    }
-
-                    if (update)
-                    {
-                        nearestIndex = index;
-                    }
-                }
-                else // if (command == 2)
-                {
-                    if (nearestIndex != InvalidIndex)
-                    {
-                        traveled += Math.Abs(coords[nearestIndex] - collectorX);
-                        collectorX = coords[nearestIndex];
-
-                        int l = nearestIndex - 1;
-                        int r = nearestIndex + 1;
-
-                        while ((l > -1 && l < coords.Count) || (r > -1 && r < coords.Count))
-                        {
-                            int lDistance = Infinity;
-                            if (l > -1 && l < coords.Count)
-                            {
-                                lDistance = Math.Abs(coords[l] - collectorX);
-                            }
-
-                            int rDistance = Infinity;
-                            if (r > -1 && r < coords.Count)
-                            {
-                                rDistance = Math.Abs(coords[r] - collectorX);
-                            }
-
-                            if (lDistance <= rDistance)
-                            {
-                                collectorX = coords[l];
-                                traveled += lDistance;
-                                --l;
-                            }
-                            else
-                            {
-                                collectorX = coords[r];
-                                traveled += rDistance;
-                                ++r;
-                            }
-                        }
-
-                        coords.Clear();
-                        nearestIndex = InvalidIndex;
-                    }
-                }
-            }
-        }
-        Console.Write(traveled);
+        // length = 2
+        // element = [1, 100'000]
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int a = tokens[0];
+        int b = tokens[1];
+        Console.Write($"{(a + b) * (long)(a - b)}");
     }
 }
