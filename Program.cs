3629aa27223111bf76a46aa49215056133bd5ea5
mafuyusame <mafuyu@tutamail.com>
2025-05-24 10:57:11 +0900

    [math] boj 5717

--- Program.cs
+++ Program.cs
@@ -4,104 +4,18 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] tokens = null!;
-
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int width = tokens[0]; // [3, 100'000]
-        int walls = tokens[1]; // [0, N) = [0, 100'000)
-        int students = tokens[2]; // [1, 100'000]
-
-        int[] map = new int[width + 1];
-        for (int i = 0; i < walls; ++i)
-        {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int pos = tokens[0]; // [1, N] = [1, 100'000]
-            int durability = tokens[1]; // [1, 100'000]
-            map[pos] = durability;
-        }
-
-        long[] mapPrefixSumsFromLeft = new long[width + 2];
-        for (int i = 1; i <= width; ++i)
-        {
-            mapPrefixSumsFromLeft[i] = mapPrefixSumsFromLeft[i - 1] + map[i];
-        }
-
-        long[] mapPrefixSumsFromRight = new long[width + 2];
-        for (int i = width; i > 0; --i)
-        {
-            mapPrefixSumsFromRight[i] = mapPrefixSumsFromRight[i + 1] + map[i];
-        }
-
-        const int InvalidBrokenPos = -1;
-        int brokenPosFromLeft = InvalidBrokenPos;
-        int brokenPosFromRight = InvalidBrokenPos;
-
         StringBuilder sb = new();
-        for (int i = 0; i < students; ++i)
+        while (true)
         {
-            int pos = int.Parse(Console.ReadLine()!);
+            // length = 2
+            // element = [0, 5]
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int m = tokens[0];
+            int f = tokens[1];
+            if (m == 0 && f == 0)
+                break;
 
-            long leftDurabilities = mapPrefixSumsFromLeft[pos - 1];
-            if (brokenPosFromLeft != InvalidBrokenPos)
-            {
-                leftDurabilities = Math.Max(0, leftDurabilities - mapPrefixSumsFromLeft[brokenPosFromLeft]);
-            }
-
-            long rightDurabilities = mapPrefixSumsFromRight[pos + 1];
-            if (brokenPosFromRight != InvalidBrokenPos)
-            {
-                rightDurabilities = Math.Max(0, rightDurabilities - mapPrefixSumsFromRight[brokenPosFromRight]);
-            }
-
-            bool escapeToLeftward;
-            if (leftDurabilities < rightDurabilities)
-            {
-                escapeToLeftward = true;
-            }
-            else if (leftDurabilities > rightDurabilities)
-            {
-                escapeToLeftward = false;
-            }
-            else
-            {
-                int leftDistance = pos - 1;
-                int rightDistance = width - pos;
-                if (leftDistance <= rightDistance)
-                {
-                    escapeToLeftward = true;
-                }
-                else
-                {
-                    escapeToLeftward = false;
-                }
-            }
-
-            if (escapeToLeftward)
-            {
-                sb.AppendLine($"{leftDurabilities}");
-                if (brokenPosFromLeft == InvalidBrokenPos ||
-                    pos > brokenPosFromLeft)
-                {
-                    brokenPosFromLeft = pos;
-                }
-            }
-            else
-            {
-                sb.AppendLine($"{rightDurabilities}");
-                if (brokenPosFromRight == InvalidBrokenPos ||
-                    pos < brokenPosFromRight)
-                {
-                    brokenPosFromRight = pos;
-                }
-            }
-
-            if (brokenPosFromLeft != InvalidBrokenPos &&
-                brokenPosFromRight != InvalidBrokenPos &&
-                brokenPosFromLeft >= brokenPosFromRight)
-            {
-                brokenPosFromLeft = width;
-                brokenPosFromRight = 1;
-            }
+            sb.AppendLine($"{m + f}");
         }
         Console.Write(sb);
     }
