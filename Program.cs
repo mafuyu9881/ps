d028bd664ac01a4e146c53fe8916476d590fd18f
mafuyusame <mafuyu@tutamail.com>
2025-05-30 20:53:19 +0900

    boj 33678 2% 오답 코드

--- Program.cs
+++ Program.cs
@@ -1,17 +1,42 @@
-﻿using System.Text;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        int t = int.Parse(Console.ReadLine()!);
+        int[] tokens = null!;
 
-        StringBuilder sb = new();
-        for (int i = 0; i < t; ++i)
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0]; // [2, 200'000]
+        int k = tokens[1]; // [1, 1'000'000'000]
+        int x = tokens[2]; // [1, 10]
+
+        // length = [2, 200'000]
+        // element = [1, 1'000]
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+        int earned = 0;
+        int lo = 0 - 1;
+        int hi = (n - 1) + 1;
+        while (lo < hi - 1 && earned < k)
         {
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            sb.AppendLine($"Case #{i + 1}: {tokens[0] + tokens[1]}");
+            int loEarned = tokens[lo + 1] * x;
+            int hiEarned = tokens[hi - 1] * x;
+            if (loEarned < hiEarned)
+            {
+                earned += hiEarned;
+                --hi;
+            }
+            else
+            {
+                earned += loEarned;
+                ++lo;
+            }
         }
-        Console.Write(sb);
+
+        int streak = hi - lo - 1;
+        if (streak < 1)
+        {
+            streak = -1;
+        }
+        Console.Write(streak);
     }
 }
