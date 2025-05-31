84b4c45cf01f43a334ac2966e0805a6822bf531b
mafuyusame <mafuyu@tutamail.com>
2025-05-31 20:34:02 +0900

    boj 1253 15% 오답 코드

--- Program.cs
+++ Program.cs
@@ -2,41 +2,33 @@
 {
     static void Main(string[] args)
     {
-        int[] tokens = null!;
+        int n = int.Parse(Console.ReadLine()!); // [1, 2'000]
 
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // [2, 200'000]
-        int k = tokens[1]; // [1, 1'000'000'000]
-        int x = tokens[2]; // [1, 10]
+        // length = n = [1, 2'000]
+        // element = [1, 1'000'000'000]
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
 
-        // length = [2, 200'000]
-        // element = [1, 1'000]
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-        int[] prefixSums = new int[n + 1];
-        for (int i = 0; i < n; ++i)
+        int goods = 0;
         {
-            prefixSums[i + 1] = prefixSums[i] + tokens[i];
-        }
-
-        int entireSum = prefixSums[n];
-
-        int hi = 0;
-        int streak = 0;
-        for (int lo = 0; lo <= n; ++lo)
-        {
-            if (hi < lo)
+            SortedSet<int> ss = new();
+            for (int i = 0; i < n; ++i)
             {
-                hi = lo;
+                ss.Add(tokens[i]);
             }
 
-            while (hi <= n && k <= (x * prefixSums[lo]) + (entireSum - prefixSums[hi]))
+            for (int i = 0; i < n; ++i)
             {
-                ++hi;
+                for (int j = i + 1; j < n; ++j)
+                {
+                    int sum = tokens[i] + tokens[j];
+                    if (ss.Contains(sum))
+                    {
+                        ss.Remove(sum);
+                        ++goods;
+                    }
+                }
             }
-
-            streak = Math.Max(streak, (hi - 1) - lo);
         }
-        Console.Write(streak != 0 ? streak : -1);
+        Console.Write(goods);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ee360e96f359fc88938cc958ee676ffa9f259a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f544e0931b57b820b713398ba2663b57df5975d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
