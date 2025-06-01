f8265813722b2f8bf1f1b0056d6f69bf69a30e29
mafuyusame <mafuyu@tutamail.com>
2025-06-01 23:56:23 +0900

    goods 내부 구현 코드 제거

--- Program.cs
+++ Program.cs
@@ -10,24 +10,7 @@
 
         int goods = 0;
         {
-            SortedSet<int> ss = new();
-            for (int i = 0; i < n; ++i)
-            {
-                ss.Add(tokens[i]);
-            }
-
-            for (int i = 0; i < n; ++i)
-            {
-                for (int j = i + 1; j < n; ++j)
-                {
-                    int sum = tokens[i] + tokens[j];
-                    if (ss.Contains(sum))
-                    {
-                        ss.Remove(sum);
-                        ++goods;
-                    }
-                }
-            }
+            
         }
         Console.Write(goods);
     }
