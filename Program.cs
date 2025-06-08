bc7f7acac9e218e63317fd2fc204c9259c34d4ea
mafuyusame <mafuyu@tutamail.com>
2025-06-08 22:02:02 +0900

    [sort] boj 6840

--- Program.cs
+++ Program.cs
@@ -2,46 +2,11 @@
 {
     static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // [10, 100'000]
-        int s = tokens[1]; // [0, 100'000'000]
-
-        int[] sequence = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        long[] prefixSums = new long[n];
-        prefixSums[0] = sequence[0];
-        for (int i = 1; i < n; ++i)
-        {
-            prefixSums[i] = prefixSums[i - 1] + sequence[i];
-        }
-
-        int minLength = 100000 + 1;
-        {
-            long entireSum = prefixSums[n - 1];
-            if (entireSum < s)
-            {
-                minLength = 0;
-            }
-            else
-            {
-                long sum = entireSum;
-                int lo = 0;
-                int hi = n - 1;
-                while (lo < hi && sum >= s)
-                {
-                    if (sequence[lo] < sequence[hi])
-                    {
-                        sum -= sequence[lo];
-                        ++lo;
-                    }
-                    else
-                    {
-                        sum -= sequence[hi];
-                        --hi;
-                    }
-                }
-                minLength = hi - lo + 2;
-            }
-        }
-        Console.Write(minLength);
+        int[] tokens = new int[3];
+        tokens[0] = int.Parse(Console.ReadLine()!);
+        tokens[1] = int.Parse(Console.ReadLine()!);
+        tokens[2] = int.Parse(Console.ReadLine()!);
+        Array.Sort(tokens);
+        Console.Write(tokens[1]);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c490beeb3915519cf28f8b48a43e29ef775d3e3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46161d0e1477a16add9e584a2464a1d9f9a3bc81")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
