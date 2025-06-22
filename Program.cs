d91a26c0caaf26948fc5f3c009c4fbc540c6bf33
mafuyusame <mafuyu@tutamail.com>
2025-06-22 20:51:23 +0900

    [two pointer] boj 16472

--- Program.cs
+++ Program.cs
@@ -2,63 +2,42 @@
 {
     static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0]; // [2, 3'000'000]
-        int d = tokens[1]; // [2, 3'000]
-        int k = tokens[2]; // [2, 3'000]
-        int c = tokens[3]; // [1, d] = [1, 3'000]
+        int n = int.Parse(Console.ReadLine()!); // [1, 26]
 
-        int[] dishes = new int[n * 2];
-        for (int i = 0; i < n; ++i)
-        {
-            dishes[i] = int.Parse(Console.ReadLine()!);
-        }
-        for (int i = 0; i < n; ++i)
-        {
-            dishes[n + i] = dishes[i];
-        }
+        string s = Console.ReadLine()!;
 
-        int maxKinds = 0;
+        int[] count = new int['z' - 'a' + 1];
+
+        int maxLength = 0;
         {
-            int[] occupied = new int[d + 1];
+            int kinds = 0;
             int lo = 0;
             int hi = 0;
-            int kinds = 0;
-            while (hi < dishes.Length)
+            while (hi < s.Length)
             {
-                if (hi < k)
+                int newCharIndex = s[hi] - 'a';
+                if (count[newCharIndex] == 0)
                 {
-                    while (hi < k)
-                    {
-                        int newDish = dishes[hi];
-                        if (occupied[newDish] == 0)
-                        {
-                            ++kinds;
-                        }
-                        ++occupied[newDish];
-                        ++hi;
-                    }
+                    ++kinds;
                 }
-                else
+                ++count[newCharIndex];
+
+                while (kinds > n)
                 {
-                    int oldDish = dishes[lo];
-                    --occupied[oldDish];
-                    if (occupied[oldDish] == 0)
+                    int oldCharIndex = s[lo] - 'a';
+                    --count[oldCharIndex];
+                    if (count[oldCharIndex] == 0)
                     {
                         --kinds;
                     }
-                    int newDish = dishes[hi];
-                    if (occupied[newDish] == 0)
-                    {
-                        ++kinds;
-                    }
-                    ++occupied[newDish];
                     ++lo;
-                    ++hi;
                 }
-                maxKinds = Math.Max(maxKinds, (occupied[c] > 0) ? kinds : kinds + 1);
+
+                ++hi;
+
+                maxLength = Math.Max(maxLength, (hi - 1) - lo + 1);
             }
         }
-        Console.Write(maxKinds);
+        Console.Write(maxLength);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f64abac09e1abd54a616d7d2a58fcac319396af5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8018ade66bd793a615cea8c5ca120689bf9dd4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
