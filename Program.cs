4b801d9ebe3f9dcdf8a6d797bcbee574c3f030b8
mafuyusame <mafuyu@tutamail.com>
2025-06-23 15:43:44 +0900

    [bruteforcing, sorting, two pointer] boj 24432

--- Program.cs
+++ Program.cs
@@ -1,43 +1,81 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [1, 26]
+        int t = int.Parse(Console.ReadLine()!); // [1, 5]
 
-        string s = Console.ReadLine()!;
-
-        int[] count = new int['z' - 'a' + 1];
-
-        int maxLength = 0;
+        StringBuilder sb = new();
+        for (int i = 0; i < t; ++i)
         {
-            int kinds = 0;
-            int lo = 0;
-            int hi = 0;
-            while (hi < s.Length)
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int n = tokens[0]; // [2, 20]
+            int m = tokens[1]; // [2, 20]
+            int k = tokens[2]; // [2, min(n, m, 10)]
+
+            // length = n
+            // element = [1, 10^8]
+            int[] bSet = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            // length = m
+            // element = [1, 10^8]
+            int[] aSet = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+            List<int> bSetSums = new(1048576);
+            List<int> aSetSums = new(1048576);
+            ComputeSums(0, 0, 0, k, bSet, bSetSums);
+            ComputeSums(0, 0, 0, k, aSet, aSetSums);
+            bSetSums.Sort();
+            aSetSums.Sort();
+
+            int min = int.MaxValue;
             {
-                int newCharIndex = s[hi] - 'a';
-                if (count[newCharIndex] == 0)
+                int j = 0;
+                int l = 0;
+                while (j < bSetSums.Count && l < aSetSums.Count)
                 {
-                    ++kinds;
-                }
-                ++count[newCharIndex];
+                    int bSetSum = bSetSums[j];
+                    int aSetSum = aSetSums[l];
 
-                while (kinds > n)
-                {
-                    int oldCharIndex = s[lo] - 'a';
-                    --count[oldCharIndex];
-                    if (count[oldCharIndex] == 0)
+                    min = Math.Min(min, Math.Abs(bSetSum - aSetSum));
+
+                    if (bSetSum < aSetSum)
                     {
-                        --kinds;
+                        ++j;
                     }
-                    ++lo;
+                    else if (bSetSum > aSetSum)
+                    {
+                        ++l;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
+            }
 
-                ++hi;
+            int max = Math.Max(Math.Abs(bSetSums[0] - aSetSums[aSetSums.Count - 1]),
+                               Math.Abs(bSetSums[bSetSums.Count - 1] - aSetSums[0]));
 
-                maxLength = Math.Max(maxLength, (hi - 1) - lo + 1);
+            sb.AppendLine($"{min} {max}");
+        }
+        Console.Write(sb);
+    }
+
+    // tc <= 2^20
+    static void ComputeSums(int index, int taken, int setSum, int k, int[] set, List<int> setSums)
+    {
+        if (taken == k)
+        {
+            setSums.Add(setSum);
+        }
+        else
+        {
+            if (index < set.Length)
+            {
+                ComputeSums(index + 1, taken + 1, setSum + set[index], k, set, setSums);
+                ComputeSums(index + 1, taken, setSum, k, set, setSums);
             }
         }
-        Console.Write(maxLength);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8018ade66bd793a615cea8c5ca120689bf9dd4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d91a26c0caaf26948fc5f3c009c4fbc540c6bf33")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
