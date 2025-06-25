872202c2c0d9e3c77147f8f04387dc13f126b1fa
mafuyu <mafuyu@tutamail.com>
2025-06-25 16:42:02 +0900

    [prefix sum] boj 5341

--- Program.cs
+++ Program.cs
@@ -4,73 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        long[] counts = new long[100000 + 1];
-        for (int i = 2; i < counts.Length; ++i)
-        {
-            counts[i] = counts[i - 1] + (i - 2 + 1);
-        }
-
-        int t = int.Parse(Console.ReadLine()!); // [1, 10]
+        List<int> prefixSum = new(100000);
+        prefixSum.Add(0);
+        prefixSum.Add(1);
 
         StringBuilder sb = new();
-        for (int i = 0; i < t; ++i)
+        while (true)
         {
-            int n = int.Parse(Console.ReadLine()!); // [1, 100'000]
+            int n = int.Parse(Console.ReadLine()!);
+            if (n == 0)
+                break;
 
-            // length = n
-            // element = [-1'000'000'000, 1'000'000'000]
-            int[] sequence = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-            // lo = [0, sequence.Length - 1]
-            // hi = [lo + 1, sequence.Length - 1]
-            Func<int, int, bool> Zigzag = (lo, hi) =>
+            while (prefixSum.Count - 1 < n)
             {
-                if (hi - lo + 1 < 3)
-                {
-                    return sequence[hi] != sequence[hi - 1];
-                }
-                else
-                {
-                    if (sequence[hi] > sequence[hi - 1] && sequence[hi - 2] > sequence[hi - 1])
-                        return true;
-                    
-                    if (sequence[hi] < sequence[hi - 1] && sequence[hi - 2] < sequence[hi - 1])
-                        return true;
-
-                    return false;
-                }
-            };
-
-            long count = 0;
-            int lo = 0;
-            int hi = 0;
-            while (true)
-            {
-                if (hi - lo + 1 < 2)
-                    hi = lo + 1;
-
-                if (hi > sequence.Length - 1)
-                    break;
-
-                long increment = 0;
-                while (hi < sequence.Length)
-                {
-                    if (Zigzag(lo, hi))
-                    {
-                        increment = counts[hi - lo + 1];
-                        ++hi;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                
-                count += increment;
-
-                lo = (increment > 0) ? hi - 1 : hi;
+                prefixSum.Add(prefixSum.Count + prefixSum[prefixSum.Count - 1]);
             }
-            sb.AppendLine($"{count}");
+
+            sb.AppendLine($"{prefixSum[n]}");
         }
         Console.Write(sb);
     }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b801d9ebe3f9dcdf8a6d797bcbee574c3f030b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43ebd83ed0685dfad65dfc9ec939c84e5bad434")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
