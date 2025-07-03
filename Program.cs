ae66a8ead7d62704019975ddeb1646fd0d06f0e5
mafuyusame <mafuyu@tutamail.com>
2025-07-03 18:35:23 +0900

    [implementation] boj 30087

--- Program.cs
+++ Program.cs
@@ -1,16 +1,44 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int count = 0;
-        while (true)
-        {
-            string line = Console.ReadLine()!;
-            if (string.IsNullOrEmpty(line))
-                break;
+        int n = int.Parse(Console.ReadLine()!);
 
-            ++count;
+        StringBuilder sb = new();
+        for (int i = 0; i < n; ++i)
+        {
+            string semina = Console.ReadLine()!;
+            if (semina == "Algorithm")
+            {
+                sb.AppendLine("204");
+            }
+            else if (semina == "DataAnalysis")
+            {
+                sb.AppendLine("207");
+            }
+            else if (semina == "ArtificialIntelligence")
+            {
+                sb.AppendLine("302");
+            }
+            else if (semina == "CyberSecurity")
+            {
+                sb.AppendLine("B101");
+            }
+            else if (semina == "Network")
+            {
+                sb.AppendLine("303");
+            }
+            else if (semina == "Startup")
+            {
+                sb.AppendLine("501");
+            }
+            else if (semina == "TestStrategy")
+            {
+                sb.AppendLine("105");
+            }
         }
-        Console.Write(count);
+        Console.Write(sb);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee418a7027dcc159703bd71869282c01d0e6e73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e363f495b306926afa27743acab61733f1ca4df3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
