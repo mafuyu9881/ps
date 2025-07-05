b263010858fd31a67b7deeaa4323c8666d02d007
mafuyusame <mafuyu@tutamail.com>
2025-07-05 13:09:52 +0900

    [implementation] boj 28691

--- Program.cs
+++ Program.cs
@@ -2,59 +2,29 @@
 {
     static void Main(string[] args)
     {
-        const int Max = 4000000;
-        const int MaxSqrt = 2000;
+        string s = Console.ReadLine()!;
 
-        int n = int.Parse(Console.ReadLine()!);
-
-        List<int> primes = new(Max);
+        string output;
+        if (s == "M")
         {
-            bool[] prime = new bool[Max + 1];
-            for (int i = 0; i < Max + 1; ++i)
-            {
-                prime[i] = true;
-            }
-
-            for (int i = 2; i < MaxSqrt + 1; ++i)
-            {
-                if (prime[i] == false)
-                    continue;
-
-                for (int j = i * i; j < Max + 1; j += i)
-                {
-                    prime[j] = false;
-                }
-            }
-
-            for (int i = 2; i < Max + 1; ++i)
-            {
-                if (prime[i] == false)
-                    continue;
-
-                primes.Add(i);
-            }
+            output = "MatKor";
         }
-
-        int cases = 0;
+        else if (s == "W")
         {
-            int sum = 0;
-            int lo = 0;
-            for (int hi = 0; hi < primes.Count; ++hi)
-            {
-                sum += primes[hi];
-
-                while (lo < hi && sum > n)
-                {
-                    sum -= primes[lo];
-                    ++lo;
-                }
-
-                if (sum == n)
-                {
-                    ++cases;
-                }
-            }
+            output = "WiCys";
         }
-        Console.Write(cases);
+        else if (s == "C")
+        {
+            output = "CyKor";
+        }
+        else if (s == "A")
+        {
+            output = "AlKor";
+        }
+        else
+        {
+            output = "$clear";
+        }
+        Console.Write(output);
     }
 }
