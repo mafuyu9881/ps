69d467f0acfa6e45145302b589caf09bc5ac5e30
mafuyusame <mafuyu@tutamail.com>
2025-07-07 14:51:00 +0900

    [sort, two pointer] boj 2473

--- Program.cs
+++ Program.cs
@@ -2,37 +2,65 @@
 {
     static void Main(string[] args)
     {
-        int wavelength = int.Parse(Console.ReadLine()!);
+        int n = int.Parse(Console.ReadLine()!); // [3, 5'000]
 
-        string output;
-        if (wavelength >= 620 && wavelength <= 780)
+        // length = n
+        // element = [-1'000'000'000, 1'000'000'000]
+        int[] sols = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        Array.Sort(sols);
+
+        long minDiff = long.MaxValue;
+        int[] minDiffSols = new int[3];
+        for (int i = 0; i < n; ++i) // max tc = 5'000
         {
-            output = "Red";
+            int sol0 = sols[i];
+
+            int lo = 0;
+            int hi = n - 1;
+            while (true)
+            {
+                if (lo == i)
+                {
+                    ++lo;
+                }
+
+                if (hi == i)
+                {
+                    --hi;
+                }
+
+                if (lo >= hi)
+                    break;
+
+                int sol1 = sols[lo];
+                int sol2 = sols[hi];
+
+                long sum = (long)sol0 + sol1 + sol2;
+                long diff = Math.Abs(sum);
+
+                if (diff < minDiff)
+                {
+                    minDiff = diff;
+                    minDiffSols[0] = sol0;
+                    minDiffSols[1] = sol1;
+                    minDiffSols[2] = sol2;
+                }
+
+                if (sum < 0)
+                {
+                    ++lo;
+                }
+                else if (sum > 0)
+                {
+                    --hi;
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
-        else if (wavelength >= 590 && wavelength < 620)
-        {
-            output = "Orange";
-        }
-        else if (wavelength >= 570 && wavelength < 590)
-        {
-            output = "Yellow";
-        }
-        else if (wavelength >= 495 && wavelength < 570)
-        {
-            output = "Green";
-        }
-        else if (wavelength >= 450 && wavelength < 495)
-        {
-            output = "Blue";
-        }
-        else if (wavelength >= 425 && wavelength < 450)
-        {
-            output = "Indigo";
-        }
-        else
-        {
-            output = "Violet";
-        }
-        Console.Write(output);
+        Array.Sort(minDiffSols);
+        Console.Write($"{minDiffSols[0]} {minDiffSols[1]} {minDiffSols[2]}");
     }
 }
