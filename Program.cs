fb5bc7f1f130effeac85c0c643a7a472016c025f
mafuyusame <mafuyu@tutamail.com>
2025-07-18 12:13:28 +0900

    [bruteforcing] boj 3863

--- Program.cs
+++ Program.cs
@@ -1,36 +1,51 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [1, 100'000]
+        int[] tokens = null!;
 
-        int[] sequence = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-        int maxLength = 1;
-        int increasingLength = 1;
-        int decreasingLength = 1;
-        for (int i = 1; i < n; ++i) // max tc = 100'000 - 1
+        StringBuilder sb = new();
+        while (true)
         {
-            if (sequence[i] >= sequence[i - 1])
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int n = tokens[0]; // [1, 10'000]
+            int m = tokens[1]; // [1, 100]
+            if (n == 0 && m == 0)
+                break;
+
+            (int s, int e)[] calls = new (int, int)[n];
+            for (int i = 0; i < n; ++i) // max tc = 10'000
             {
-                ++increasingLength;
-            }
-            else
-            {
-                increasingLength = 1;
+                tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+                int source = tokens[0]; // [0, 10'000'000]
+                int destination = tokens[1]; // [0, 10'000'000]
+                int start = tokens[2]; // start + duration = [0, 2^31]
+                int duration = tokens[3]; // [1, 10'000]
+
+                calls[i] = (start, start + duration - 1);
             }
 
-            if (sequence[i] <= sequence[i - 1])
+            for (int i = 0; i < m; ++i) // max tc = 100
             {
-                ++decreasingLength;
-            }
-            else
-            {
-                decreasingLength = 1;
-            }
+                tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+                int start = tokens[0];
+                int duration = tokens[1];
+                int s = start;
+                int e = start + duration - 1;
 
-            maxLength = Math.Max(maxLength, Math.Max(increasingLength, decreasingLength));
+                int detected = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    if ((e < calls[j].s || s > calls[j].e) == false)
+                    {
+                        ++detected;
+                    }
+                }
+                sb.AppendLine($"{detected}");
+            }
         }
-        Console.Write(maxLength);
+        Console.Write(sb);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c397ce9aba27255ac2d9918775d514a2b459ace7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97956355b5c92e3ea716e7e7a3c5a14fa62c252")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
