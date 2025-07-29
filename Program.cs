8acdb5762bb3a078a66c5a03bdc2ec30a506f483
mafuyusame <mafuyu@tutamail.com>
2025-07-30 08:11:25 +0900

    [arbitary precision] boj 14928

--- Program.cs
+++ Program.cs
@@ -1,21 +1,10 @@
-﻿class Program
+﻿using System.Numerics;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!); // [1, 200'000]
-
-        // length = n
-        // element = [1, 1'000'000'000]
-        int[] stockPrices = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-        int maxProfit = 0;
-        int minStockPrice = 1000000000;
-        for (int i = 0; i < n; ++i)
-        {
-            int stockPrice = stockPrices[i];
-            minStockPrice = Math.Min(minStockPrice, stockPrice);
-            maxProfit = Math.Max(maxProfit, Math.Max(0, stockPrice - minStockPrice));
-        }
-        Console.Write(maxProfit);
+        BigInteger n = BigInteger.Parse(Console.ReadLine()!);
+        Console.Write(n % 20000303);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded0deb29677631baf7f87569b597b3fd9a58034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43723fff07d3cdcfb6ff2dd14caeac66fcd69528")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
