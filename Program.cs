1631cb8816d81edd65bf1ae162c5ada4b21cb6cc
mafuyusame <mafuyu@tutamail.com>
2025-08-01 07:37:59 +0900

    [arbitary precision] boj 22193

--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.ReadLine();
         BigInteger a = BigInteger.Parse(Console.ReadLine()!);
         BigInteger b = BigInteger.Parse(Console.ReadLine()!);
-        Console.Write(a + b);
+        Console.Write(a * b);
     }
 }
