6887eba0a586416e72514e37a861bc9608b001c5
mafuyusame <mafuyu@tutamail.com>
2025-08-04 06:09:32 +0900

    [math, string] boj 29699

--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@
         string s = "WelcomeToSMUPC";
 
         int n = int.Parse(Console.ReadLine()!);
-        n %= 14;
+        if (n % 14 == 0)
+        {
+            n = 14;
+        }
+        else
+        {
+            n %= 14;
+        }
         n -= 1;
 
         Console.Write(s[n]);
