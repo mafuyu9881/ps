582812dc52df0d23056ea561b39494391cb97cd1
mafuyusame <mafuyu@tutamail.com>
2025-08-06 08:15:44 +0900

    [binary search tree, string] boj 7785

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int h = int.Parse(Console.ReadLine()!); // [0, 23]
-        int m = int.Parse(Console.ReadLine()!); // [0, 59]
-        Console.Write(60 * h + m);
+        int n = int.Parse(Console.ReadLine()!);
+
+        var reverseComparer = Comparer<string>.Create((a, b) => string.Compare(b, a));
+        SortedSet<string> employees = new(reverseComparer);
+        for (int i = 0; i < n; ++i)
+        {
+            string[] tokens = Console.ReadLine()!.Split();
+            string name = tokens[0];
+            string behaviour = tokens[1];
+
+            if (behaviour == "enter")
+            {
+                employees.Add(name);
+            }
+            else
+            {
+                employees.Remove(name);
+            }
+        }
+
+        StringBuilder sb = new();
+        foreach (string name in employees)
+        {
+            sb.AppendLine(name);
+        }
+        Console.Write(sb);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce8a944c04b7c15ac9cbe0e5e83131fe4b53672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ec0eb0593d382168cf348e2f2867c9301b1c9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
