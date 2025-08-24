5260ff264d62e151dd15ac9fe026ef0e71acc949
mafuyusame <mafuyu@tutamail.com>
2025-08-25 06:37:12 +0900

    [math] boj 18096

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
-﻿using System.Numerics;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        BigInteger a = BigInteger.Parse(Console.ReadLine()!);
-        BigInteger b = BigInteger.Parse(Console.ReadLine()!);
-        BigInteger c = BigInteger.Parse(Console.ReadLine()!);
-        Console.Write((b - c) / a);
+        Console.Write(1);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b205f76fae58fdc4156f9e6cdeda8a1ba63734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ab5e92cba32f9a1f8a711ce635d9fe657482b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
