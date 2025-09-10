61b06c4214a26e19b92c83c97191404b92d9c707
mafuyusame <mafuyu@tutamail.com>
2025-09-10 23:43:17 +0900

    [math] boj 2530

--- Program.cs
+++ Program.cs
@@ -2,38 +2,18 @@
 {
     static void Main(string[] args)
     {
-        int[] sequence = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int a = tokens[0];
+        int b = tokens[1];
+        int c = tokens[2];
+        int d = int.Parse(Console.ReadLine()!);
 
-        int a = sequence[0];
-        int b = sequence[1];
-        int c = sequence[2];
+        int minuteInSeconds = 60;
+        int hourInSeconds = minuteInSeconds * minuteInSeconds;
+        int dayInSeconds = hourInSeconds * 24;
 
-        int output;
-        if (a == b && b == c)
-        {
-            output = 10000 + a * 1000;
-        }
-        else if (a != b && b != c && c != a)
-        {
-            output = Math.Max(a, Math.Max(b, c)) * 100;
-        }
-        else
-        {
-            int face;
-            if (a == b)
-            {
-                face = a;
-            }
-            else if (b == c)
-            {
-                face = b;
-            }
-            else //if (c == a)
-            {
-                face = c;
-            }
-            output = 1000 + face * 100;
-        }
-        Console.Write(output);
+        int oldTotalSeconds = a * hourInSeconds + b * minuteInSeconds + c;
+        int newTotalSeconds = (oldTotalSeconds + d) % dayInSeconds;
+        Console.Write($"{newTotalSeconds / hourInSeconds} {(newTotalSeconds % hourInSeconds) / minuteInSeconds} {newTotalSeconds % minuteInSeconds}");
     }
 }
