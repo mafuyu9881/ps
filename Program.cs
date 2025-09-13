1238f13d5fdf32967d5c685b67300f53ce3c5bd9
mafuyusame <mafuyu@tutamail.com>
2025-09-13 18:54:10 +0900

    [implementation] boj 4589

--- Program.cs
+++ Program.cs
@@ -1,8 +1,50 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        Console.Write(tokens[1] * 2 - tokens[0]);
+        StringBuilder sb = new();
+        sb.AppendLine("Gnomes:");
+        int n = int.Parse(Console.ReadLine()!);
+        for (int i = 0; i < n; ++i)
+        {
+            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+            int state = 0;
+
+            for (int j = 1; j < tokens.Length; ++j)
+            {
+                int prevToken = tokens[j - 1];
+                int currToken = tokens[j];
+                if (prevToken < currToken)
+                {
+                    if (state == 2)
+                    {
+                        state = -1;
+                        break;
+                    }
+
+                    state = 1;
+                }
+                else if (prevToken > currToken)
+                {
+                    if (state == 1)
+                    {
+                        state = -1;
+                        break;
+                    }
+
+                    state = 2;
+                }
+                //else
+                //{
+                //
+                //}
+            }
+
+            sb.AppendLine((state == -1) ? "Unordered" : "Ordered");
+        }
+        Console.Write(sb);
     }
 }
