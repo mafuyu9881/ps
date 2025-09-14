9ee02a0d5483b9e1fe6e3b4bab2fe31e7fcfbb0d
mafuyusame <mafuyu@tutamail.com>
2025-09-14 21:15:06 +0900

    [math] boj 5532

--- Program.cs
+++ Program.cs
@@ -1,50 +1,14 @@
-﻿using System.Text;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        StringBuilder sb = new();
-        sb.AppendLine("Gnomes:");
-        int n = int.Parse(Console.ReadLine()!);
-        for (int i = 0; i < n; ++i)
-        {
-            int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-
-            int state = 0;
-
-            for (int j = 1; j < tokens.Length; ++j)
-            {
-                int prevToken = tokens[j - 1];
-                int currToken = tokens[j];
-                if (prevToken < currToken)
-                {
-                    if (state == 2)
-                    {
-                        state = -1;
-                        break;
-                    }
-
-                    state = 1;
-                }
-                else if (prevToken > currToken)
-                {
-                    if (state == 1)
-                    {
-                        state = -1;
-                        break;
-                    }
-
-                    state = 2;
-                }
-                //else
-                //{
-                //
-                //}
-            }
-
-            sb.AppendLine((state == -1) ? "Unordered" : "Ordered");
-        }
-        Console.Write(sb);
+        int l = int.Parse(Console.ReadLine()!);
+        int a = int.Parse(Console.ReadLine()!);
+        int b = int.Parse(Console.ReadLine()!);
+        int c = int.Parse(Console.ReadLine()!);
+        int d = int.Parse(Console.ReadLine()!);
+        int p = a / c + ((a % c > 0) ? 1 : 0);
+        int q = b / d + ((b % d > 0) ? 1 : 0);
+        Console.Write(l - ((p > q) ? p : q));
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f12da4874ade511c27b9f2c929aa2fc91692dd4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1238f13d5fdf32967d5c685b67300f53ce3c5bd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
