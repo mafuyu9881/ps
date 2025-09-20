04960d74222b949638edd6adc88d85cd2e8b6428
mafuyusame <mafuyu@tutamail.com>
2025-09-20 22:32:56 +0900

    [math] boj 5565

--- Program.cs
+++ Program.cs
@@ -2,14 +2,11 @@
 {
     static void Main(string[] args)
     {
-        const int MinuteSeconds = 60;
-
-        int seconds = 0;
-        seconds += int.Parse(Console.ReadLine()!);
-        seconds += int.Parse(Console.ReadLine()!);
-        seconds += int.Parse(Console.ReadLine()!);
-        seconds += int.Parse(Console.ReadLine()!);
-
-        Console.Write($"{seconds / MinuteSeconds}\n{seconds % MinuteSeconds}");
+        int spent = int.Parse(Console.ReadLine()!);
+        for (int i = 0; i < 9; ++i)
+        {
+            spent -= int.Parse(Console.ReadLine()!);
+        }
+        Console.Write(spent);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7919233ce538a1b14076614bb28fd5fb9ff3838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196eb8187b838e8ac0eff5e7da87aea60afd4b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
