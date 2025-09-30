18afac99f77d75c576ff6823800b840e304c8e3d
mafuyusame <mafuyu@tutamail.com>
2025-10-01 07:22:14 +0900

    [implementation] boj 11282

--- Program.cs
+++ Program.cs
@@ -1,23 +1,8 @@
-﻿using System.Text;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        string s = Console.ReadLine()!;
-
-        int[] counts = new int['z' - 'a' + 1];
-
-        for (int i = 0; i < s.Length; ++i)
-        {
-            ++counts[s[i] - 'a'];
-        }
-
-        StringBuilder sb = new();
-        for (int i = 0; i < counts.Length; ++i)
-        {
-            sb.Append($"{counts[i]} ");
-        }
-        Console.Write(sb);
+        int n = int.Parse(Console.ReadLine()!);
+        Console.Write((char)(0xAC00 + (n - 1)));
     }
 }
