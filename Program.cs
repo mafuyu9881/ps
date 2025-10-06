2c8427c947cd9d70cbadc77f3c0f329bafba3c65
mafuyusame <mafuyu@tutamail.com>
2025-10-06 22:31:40 +0900

    [string] boj 11945

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int[] tokens0 = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int[] tokens1 = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        Console.Write(Math.Min(tokens0[0] + tokens1[1], tokens0[1] + tokens1[0]));
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0];
+        int m = tokens[1];
+
+        StringBuilder sb = new();
+        for (int i = 0; i < n; ++i)
+        {
+            string s = Console.ReadLine()!;
+            for (int j = 0; j < m; ++j)
+            {
+                sb.Append(s[m - 1 - j]);
+            }
+            sb.AppendLine();
+        }
+        Console.Write(sb);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa129a114fe332fab0a72e981006cda4f764559")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0374a6153c7678ff92acb8db6442611e64cbb3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
