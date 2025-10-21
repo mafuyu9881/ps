77727052f7b1e8c805fe312b180694defc845559
mafuyusame <mafuyu@tutamail.com>
2025-10-22 06:25:21 +0900

    [implementation] boj 6763

--- Program.cs
+++ Program.cs
@@ -2,8 +2,33 @@
 {
     static void Main(string[] args)
     {
-        int youngest = int.Parse(Console.ReadLine()!);
-        int middle = int.Parse(Console.ReadLine()!);
-        Console.Write(middle + (middle - youngest));
+        int limit = int.Parse(Console.ReadLine()!);
+        int recorded = int.Parse(Console.ReadLine()!);
+        int over = recorded - limit;
+
+        int fine = 0;
+        if (over > 30)
+        {
+            fine = 500;
+        }
+        else if (over > 20)
+        {
+            fine = 270;
+        }
+        else if (over > 1)
+        {
+            fine = 100;
+        }
+
+        string output;
+        if (fine > 0)
+        {
+            output = $"You are speeding and your fine is ${fine}.";
+        }
+        else
+        {
+            output = "Congratulations, you are within the speed limit!";
+        }
+        Console.Write(output);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f4ca49429af97c8c0479944bd1b155a7d3afd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa955bddebfe35f58d61de1d6989d376d9765d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
