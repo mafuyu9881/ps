0965cb8ba7023b3bb58d5a6fdf37e499859558c6
mafuyusame <mafuyu@tutamail.com>
2025-10-27 07:22:15 +0900

    [math] boj 6825

--- Program.cs
+++ Program.cs
@@ -2,25 +2,22 @@
 {
     static void Main(string[] args)
     {
-        string a = Console.ReadLine()!;
-        string b = Console.ReadLine()!;
-        string c = Console.ReadLine()!;
-
-        string isbn = "9780921418" + a + b + c;
-
-        int sum = 0;
-        for (int i = 0; i < isbn.Length; ++i)
+        double weight = double.Parse(Console.ReadLine()!);
+        double height = double.Parse(Console.ReadLine()!);
+        double bmi = weight / (height * height);
+        string message;
+        if (bmi > 25)
         {
-            int digit = isbn[i] - '0';
-            if (i % 2 == 0)
-            {
-                sum += digit * 1;
-            }
-            else
-            {
-                sum += digit * 3;
-            }
+            message = "Overweight";
         }
-        Console.Write($"The 1-3-sum is {sum}");
+        else if (bmi > 18.5)
+        {
+            message = "Normal weight";
+        }
+        else
+        {
+            message = "Underweight";
+        }
+        Console.Write(message);
     }
 }
