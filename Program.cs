e5cd1aa6fe8e738ea1771d13eea1e41368642b90
mafuyusame <mafuyu@tutamail.com>
2025-10-28 07:20:48 +0900

    [implementation] boj 6841

--- Program.cs
+++ Program.cs
@@ -1,23 +1,68 @@
-﻿class Program
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        double weight = double.Parse(Console.ReadLine()!);
-        double height = double.Parse(Console.ReadLine()!);
-        double bmi = weight / (height * height);
-        string message;
-        if (bmi > 25)
+        StringBuilder sb = new();
+        while (true)
         {
-            message = "Overweight";
+            string token = Console.ReadLine()!;
+            if (token == "CU")
+            {
+                sb.AppendLine("see you");
+            }
+            else if (token == ":-)")
+            {
+                sb.AppendLine("I’m happy");
+            }
+            else if (token == ":-(")
+            {
+                sb.AppendLine("I’m unhappy");
+            }
+            else if (token == ";-)")
+            {
+                sb.AppendLine("wink");
+            }
+            else if (token == ":-P")
+            {
+                sb.AppendLine("stick out my tongue");
+            }
+            else if (token == "(~.~)")
+            {
+                sb.AppendLine("sleepy");
+            }
+            else if (token == "TA")
+            {
+                sb.AppendLine("totally awesome");
+            }
+            else if (token == "CCC")
+            {
+                sb.AppendLine("Canadian Computing Competition");
+            }
+            else if (token == "CUZ")
+            {
+                sb.AppendLine("because");
+            }
+            else if (token == "TY")
+            {
+                sb.AppendLine("thank-you");
+            }
+            else if (token == "YW")
+            {
+                sb.AppendLine("you’re welcome");
+            }
+            else if (token == "TTYL")
+            {
+                sb.AppendLine("talk to you later");
+                break;
+            }
+            else
+            {
+                sb.AppendLine(token);
+            }
         }
-        else if (bmi > 18.5)
-        {
-            message = "Normal weight";
-        }
-        else
-        {
-            message = "Underweight";
-        }
-        Console.Write(message);
+        Console.Write(sb);
     }
 }
