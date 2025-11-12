b458c78b77911a9450e9a2a9c2f199d3a9ea014b
mafuyusame <mafuyu@tutamail.com>
2025-11-13 07:31:20 +0900

    [string] boj 13985

--- Program.cs
+++ Program.cs
@@ -2,22 +2,12 @@
 {
     static void Main(string[] args)
     {
-        int a = int.Parse(Console.ReadLine()!);
-        int b = int.Parse(Console.ReadLine()!);
-        int c = int.Parse(Console.ReadLine()!);
-        int d = int.Parse(Console.ReadLine()!);
+        string input = Console.ReadLine()!;
 
-        string output;
-        if ((a == 8 || a == 9) &&
-            (d == 8 || d == 9) &&
-            (b == c))
-        {
-            output = "ignore";
-        }
-        else
-        {
-            output = "answer";
-        }
-        Console.Write(output);
+        int a = input[0] - '0';
+        int b = input[4] - '0';
+        int c = input[8] - '0';
+
+        Console.Write((a + b == c) ? "YES" : "NO");
     }
 }
