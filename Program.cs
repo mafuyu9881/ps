ddc7f6a5e4bc0a3c473f7eb03adfc28cae119276
mafuyusame <mafuyu@tutamail.com>
2025-11-16 03:53:25 +0900

    [case work] boj 16199

--- Program.cs
+++ Program.cs
@@ -1,13 +1,39 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int[] tokens = null!;
 
-        int a = tokens[0];
-        int b = tokens[1];
-        int c = tokens[2];
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int birthYear = tokens[0];
+        int birthMonth = tokens[1];
+        int birthDay = tokens[2];
 
-        Console.Write(Math.Max(Math.Max(a + b, b + c), c + a));
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int currYear = tokens[0];
+        int currMonth = tokens[1];
+        int currDay = tokens[2];
+
+        int standard = currYear - birthYear;
+        if (currYear > birthYear)
+        {
+            if ((currMonth < birthMonth) ||
+                (currMonth == birthMonth && currDay < birthDay))
+            {
+                --standard;
+            }
+        }
+        
+        int count = currYear - birthYear + 1;
+
+        int year = currYear - birthYear;
+
+        StringBuilder output = new();
+        output.AppendLine($"{standard}");
+        output.AppendLine($"{count}");
+        output.AppendLine($"{year}");
+        Console.Write(output);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b458c78b77911a9450e9a2a9c2f199d3a9ea014b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+347ffc344b3319dc764a06fe916226323de67e29")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
