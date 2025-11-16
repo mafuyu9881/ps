e6acafe8efb9bcc98dea272ad023951310ad83cf
mafuyusame <mafuyu@tutamail.com>
2025-11-17 07:13:25 +0900

    [geometry] boj 16486

--- Program.cs
+++ Program.cs
@@ -2,20 +2,8 @@
 {
     static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0];
-        int m = tokens[1];
-        int k = tokens[2];
-
-        int oCardCount = m;
-        int xCardCount = n - m;
-
-        int oCount = k;
-        int xCount = n - k;
-
-        int output = 0;
-        output += Math.Min(oCardCount, oCount);
-        output += Math.Min(xCardCount, xCount);
-        Console.Write(output);
+        int d1 = int.Parse(Console.ReadLine()!);
+        int d2 = int.Parse(Console.ReadLine()!);
+        Console.Write((2 * (d1 + 3.141592 * d2)).ToString("F6"));
     }
 }
