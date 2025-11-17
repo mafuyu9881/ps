ba5a162b7d6402fa02f439b0651450eaccba7194
mafuyusame <mafuyu@tutamail.com>
2025-11-18 07:20:10 +0900

    [math] boj 16693

--- Program.cs
+++ Program.cs
@@ -2,8 +2,29 @@
 {
     static void Main(string[] args)
     {
-        int d1 = int.Parse(Console.ReadLine()!);
-        int d2 = int.Parse(Console.ReadLine()!);
-        Console.Write((2 * (d1 + 3.141592 * d2)).ToString("F6"));
+        int[] tokens = null!;
+
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int a1 = tokens[0];
+        int p1 = tokens[1];
+        double ratio1 = (double)a1 / p1;
+
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int r1 = tokens[0];
+        int p2 = tokens[1];
+        double a2 = r1 * r1 * 3.141592653589793;
+        
+        double ratio2 = a2 / p2;
+
+        string output;
+        if (ratio1 > ratio2)
+        {
+            output = "Slice of pizza";
+        }
+        else
+        {
+            output = "Whole pizza";
+        }
+        Console.Write(output);
     }
 }
