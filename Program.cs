68b7fc130ec9e79ad29067a2da127c2d9ff6af70
mafuyusame <mafuyu@tutamail.com>
2025-11-28 07:41:17 +0900

    [math] boj 19698

--- Program.cs
+++ Program.cs
@@ -3,30 +3,10 @@
     static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int s = tokens[0];
-        int k = tokens[1];
+        int n = tokens[0];
+        int w = tokens[1];
         int h = tokens[2];
-        int sum = s + k + h;
-        int min = Math.Min(s, Math.Min(k, h));
-
-        string output;
-        if (sum >= 100)
-        {
-            output = "OK";
-        }
-        else if (s == min)
-        {
-            output = "Soongsil";
-        }
-        else if (k == min)
-        {
-            output = "Korea";
-        }
-        else// if (h == min)
-        {
-            output = "Hanyang";
-        }
-
-        Console.Write(output);
+        int l = tokens[3];
+        Console.Write(Math.Min(n, (w / l) * (h / l)));
     }
 }
