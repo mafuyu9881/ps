345b0cbf63c2e921c561e3c59ba01871bed40f2e
mafuyusame <mafuyu@tutamail.com>
2025-12-23 08:11:44 +0900

    [bruteforcing] boj 25494

--- Program.cs
+++ Program.cs
@@ -1,28 +1,35 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        const int InvalidMinute = -1;
+        int t = int.Parse(Console.ReadLine()!);
 
-        int n = int.Parse(Console.ReadLine()!);
-
-        int minimumMinute = InvalidMinute;
-        for (int i = 0; i < n; ++i)
+        StringBuilder output = new();
+        for (int i = 0; i < t; ++i)
         {
             int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
             int a = tokens[0];
             int b = tokens[1];
+            int c = tokens[2];
 
-            if (a > b)
-                continue;
-
-            if (minimumMinute != InvalidMinute &&
-                minimumMinute <= b)
-                continue;
-
-            minimumMinute = b;
+            int tuples = 0;
+            for (int j = 1; j <= a; ++j)
+            {
+                for (int k = 1; k <= b; ++k)
+                {
+                    for (int l = 1; l <= c; ++l)
+                    {
+                        if ((j % k == k % l) && (k % l == l % j))
+                        {
+                            ++tuples;
+                        }
+                    }
+                }
+            }
+            output.AppendLine($"{tuples}");
         }
-
-        Console.Write(minimumMinute);
+        Console.Write(output);
     }
 }
