ba835d6616ed39d9cf7dc27b5a4c21f06bc4b91a
mafuyusame <mafuyu@tutamail.com>
2025-12-25 08:45:57 +0900

    [math] boj 25625

--- Program.cs
+++ Program.cs
@@ -1,25 +1,10 @@
-﻿using System.Text;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n0 = tokens[0];
-        int n1 = tokens[1];
-
-        StringBuilder output = new();
-
-        int a = 100 - n0;
-        int b = 100 - n1;
-        int c = 100 - (a + b);
-        int d = a * b;
-        int q = d / 100;
-        int r = d % 100;
-        output.AppendLine($"{a} {b} {c} {d} {q} {r}");
-        
-        output.AppendLine($"{c + q} {r}");
-
-        Console.Write(output);
+        int x = tokens[0];
+        int y = tokens[1];
+        Console.Write((x > y) ? (x + y) : (y - x));
     }
 }
