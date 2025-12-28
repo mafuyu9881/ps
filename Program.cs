ed1b82fb79213015d41f1bef22fc5a7ad18c3c2d
mafuyusame <mafuyu@tutamail.com>
2025-12-28 09:37:54 +0900

    [math] boj 14489

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
         int a = tokens[0];
         int b = tokens[1];
-        Console.Write(Math.Min(a / 2, b));
+        
+        int c = int.Parse(Console.ReadLine()!);
+
+        int sum = a + b;
+
+        Console.Write((sum >= c * 2) ? sum - c * 2 : sum);
     }
 }
