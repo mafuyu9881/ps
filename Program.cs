7c6620d6b327ce961f79bb2b3c874b482ed9560b
mafuyusame <mafuyu@tutamail.com>
2026-01-04 10:48:42 +0900

    [implementation] boj 30868

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int a = tokens[0];
-        int b = tokens[1];
-        Console.Write(2 * Math.Min(a - 1, b) + 1);
+        int t = int.Parse(Console.ReadLine()!);
+
+        StringBuilder output = new();
+        for (int i = 0; i < t; ++i)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            StringBuilder votes = new();
+            while (n > 0)
+            {
+                if (n >= 5)
+                {
+                    votes.Append("++++ ");
+                    n -= 5;
+                }
+                else
+                {
+                    votes.Append("|");
+                    n -= 1;
+                }
+            }
+            output.AppendLine(votes.ToString());
+        }
+        Console.Write(output);
     }
 }
