2308a1268f13ff52de97c24a031e089aca439a2f
mafuyusame <mafuyu@tutamail.com>
2026-01-10 18:38:30 +0900

    [string] boj 10203

--- Program.cs
+++ Program.cs
@@ -1,8 +1,33 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
-        Console.Write(n * (n - 1));
+        int t = int.Parse(Console.ReadLine()!);
+
+        StringBuilder output = new();
+        for (int i = 0; i < t; ++i)
+        {
+            string word = Console.ReadLine()!;
+
+            int vowels = 0;
+            for (int j = 0; j < word.Length; ++j)
+            {
+                char c = word[j];
+                if (c == 'a' ||
+                    c == 'e' ||
+                    c == 'i' ||
+                    c == 'o' ||
+                    c == 'u')
+                {
+                    ++vowels;
+                }
+            }
+            
+            output.AppendLine($"The number of vowels in {word} is {vowels}.");
+        }
+        
+        Console.Write(output);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a7bd311c811c45671943a4f77b1572deed8dd85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4740c6a28b469876651b5b901721a7e18092958")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
