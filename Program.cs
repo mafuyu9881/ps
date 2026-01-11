04f7668e8c2e5db5a710945d790804e57c59c59c
mafuyusame <mafuyu@tutamail.com>
2026-01-12 08:43:14 +0900

    [arbitrary precision] boj 1247

--- Program.cs
+++ Program.cs
@@ -1,21 +1,32 @@
-﻿class Program
+﻿using System.Text;
+using System.Numerics;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int x = tokens[0];
-        int y = tokens[1];
-        int w = tokens[2];
-        int h = tokens[3];
-        
-        int[] distances = new int[]
+        StringBuilder output = new();
+        for (int t = 0; t < 3; ++t)
         {
-            x,
-            y,
-            w - x,
-            h - y,
-        };
+            int n = int.Parse(Console.ReadLine()!);
 
-        Console.Write(distances.Min());
+            BigInteger sum = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                sum += long.Parse(Console.ReadLine()!);
+            }
+            
+            string sign = "0";
+            if (sum > 0)
+            {
+                sign = "+";
+            }
+            else if (sum < 0)
+            {
+                sign = "-";
+            }
+            output.AppendLine(sign);
+        }
+        Console.Write(output);
     }
 }
