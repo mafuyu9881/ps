dacd38b8040e2c074104d78001dd515b7945c10a
mafuyusame <mafuyu@tutamail.com>
2026-01-11 22:50:35 +0900

    [geometry] boj 1085

--- Program.cs
+++ Program.cs
@@ -1,33 +1,21 @@
-﻿using System.Text;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        int t = int.Parse(Console.ReadLine()!);
-
-        StringBuilder output = new();
-        for (int i = 0; i < t; ++i)
-        {
-            string word = Console.ReadLine()!;
-
-            int vowels = 0;
-            for (int j = 0; j < word.Length; ++j)
-            {
-                char c = word[j];
-                if (c == 'a' ||
-                    c == 'e' ||
-                    c == 'i' ||
-                    c == 'o' ||
-                    c == 'u')
-                {
-                    ++vowels;
-                }
-            }
-            
-            output.AppendLine($"The number of vowels in {word} is {vowels}.");
-        }
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int x = tokens[0];
+        int y = tokens[1];
+        int w = tokens[2];
+        int h = tokens[3];
         
-        Console.Write(output);
+        int[] distances = new int[]
+        {
+            x,
+            y,
+            w - x,
+            h - y,
+        };
+
+        Console.Write(distances.Min());
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4740c6a28b469876651b5b901721a7e18092958")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2308a1268f13ff52de97c24a031e089aca439a2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
