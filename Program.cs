e286c69281bffb3da9dffdb69a620af4cd7e478c
mafuyusame <mafuyu@tutamail.com>
2026-01-14 09:26:50 +0900

    [array] boj 1284

--- Program.cs
+++ Program.cs
@@ -1,32 +1,26 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
-        
-        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int[] digitWidths = { 4, 2, 3, 3, 3, 3, 3, 3, 3, 3 };
 
-        int ySum = 0;
-        int mSum = 0;
-        for (int i = 0; i < n; ++i)
+        StringBuilder output = new();
+        while (true)
         {
-            int token = tokens[i];
-            ySum += (token / 30 + 1) * 10;
-            mSum += (token / 60 + 1) * 15;
-        }
-        
-        string output;
-        if (ySum < mSum)
-        {
-            output = $"Y {ySum}";
-        }
-        else if (ySum > mSum)
-        {
-            output = $"M {mSum}";
-        }
-        else
-        {
-            output = $"Y M {ySum}";
+            string s = Console.ReadLine()!;
+            if (s == "0")
+                break;
+
+            int width = s.Length + 1;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                int digit = s[i] - '0';
+                width += digitWidths[digit];
+            }
+            
+            output.AppendLine($"{width}");
         }
         Console.Write(output);
     }
