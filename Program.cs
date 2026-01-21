e7eaf15e0bf8091ae319355f3466d757e3aae075
mafuyusame <mafuyu@tutamail.com>
2026-01-22 07:30:16 +0900

    [implementation] boj 2444

--- Program.cs
+++ Program.cs
@@ -9,9 +9,9 @@ class Program
         int width = 2 * (n - 1) + 1;
         
         StringBuilder output = new();
-        for (int i = n; i > 0; --i)
+        for (int i = width; i > 0; --i)
         {
-            int stars = 2 * i - 1;
+            int stars = 2 * ((i > n) ? width - i : i - 1) + 1;
 
             int blanks = (width - stars) / 2;
 
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3487a4672aa3f32a7a4d2fe8ece0f4fbabed83e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95525a38237715e554b0f80694543ce7eb74cfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
