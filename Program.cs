dcf7b9c117a0354d5789c0db6e122d0ff8b94925
mafuyusame <mafuyu@tutamail.com>
2026-01-24 12:10:29 +0900

    [implementation] boj 2446

--- Program.cs
+++ Program.cs
@@ -7,23 +7,20 @@ class Program
         int n = int.Parse(Console.ReadLine()!);
 
         int height = 2 * n - 1;
-        
+        int width = 2 * n - 1;
+
         StringBuilder output = new();
         for (int i = 0; i < height; ++i)
         {
             if (i < height / 2)
             {
-                output.Append('*', i + 1);
-                output.Append(' ', n - (i + 1));
-                output.Append(' ', n - (i + 1));
-                output.Append('*', i + 1);
+                output.Append(' ', i);
+                output.Append('*', width - i * 2);
             }
             else
             {
-                output.Append('*', n - ((i + 1) - n));
-                output.Append(' ', (i + 1) - n);
-                output.Append(' ', (i + 1) - n);
-                output.Append('*', n - ((i + 1) - n));
+                output.Append(' ', width / 2 - (i - width / 2));
+                output.Append('*', 2 * (i - width / 2) + 1);
             }
             output.AppendLine();
         }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7eaf15e0bf8091ae319355f3466d757e3aae075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca64f0da27c18ad319aad870e36a3225c4eb914")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
