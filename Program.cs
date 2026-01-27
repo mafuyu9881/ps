c64ce6ff073263d361ce215152e3026ffb428056
mafuyusame <mafuyu@tutamail.com>
2026-01-28 07:13:59 +0900

    [implementation] boj 2490

--- Program.cs
+++ Program.cs
@@ -1,42 +1,26 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        char[] lookup = new char[] { 'D', 'C', 'B', 'A', 'E' };
 
-        int maxPrizeMoney = 0;
-        for (int i = 0; i < n; ++i)
+        StringBuilder output = new();
+
+        for (int i = 0; i < 3; ++i)
         {
             int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int a = tokens[0];
-            int b = tokens[1];
-            int c = tokens[2];
 
-            int prizeMoney = 0;
-            if (a == b && b == c)
-            {
-                prizeMoney = 10000 + a * 1000;
-            }
-            else if (a == b)
-            {
-                prizeMoney = 1000 + a * 100;
-            }
-            else if (b == c)
-            {
-                prizeMoney = 1000 + b * 100;
-            }
-            else if (c == a)
-            {
-                prizeMoney = 1000 + c * 100;
-            }
-            else
-            {
-                prizeMoney = Math.Max(a, Math.Max(b, c)) * 100;
-            }
-            
-            maxPrizeMoney = Math.Max(prizeMoney, maxPrizeMoney);
+            int count = 0;
+            count += tokens[0];
+            count += tokens[1];
+            count += tokens[2];
+            count += tokens[3];
+
+            output.AppendLine($"{lookup[count]}");
         }
 
-        Console.Write(maxPrizeMoney);
+        Console.Write(output);
     }
 }
