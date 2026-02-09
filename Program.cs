2b39c233f30d0b115d796fb8e400550a49e59bd1
mafuyusame <mafuyu@tutamail.com>
2026-02-10 07:31:52 +0900

    [implementation] boj 1100

--- Program.cs
+++ Program.cs
@@ -4,39 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        SortedDictionary<string, int> value = new()
+        const int Height = 8;
+        const int Width = 8;
+
+        int count = 0;
+        for (int row = 0; row < Height; ++row)
         {
-            { "black", 0 },
-            { "brown", 1 },
-            { "red", 2 },
-            { "orange", 3 },
-            { "yellow", 4 },
-            { "green", 5 },
-            { "blue", 6 },
-            { "violet", 7 },
-            { "grey", 8 },
-            { "white", 9 },
-        };
+            string line = Console.ReadLine()!;
+            for (int col = 0; col < Width; ++col)
+            {
+                if (line[col] == '.')
+                    continue;
+                
+                if (row % 2 != col % 2)
+                    continue;
+                
+                ++count;
+            }
+        }
 
-        SortedDictionary<string, int> multiply = new()
-        {
-            { "black", 1 },
-            { "brown", 10 },
-            { "red", 100 },
-            { "orange", 1000 },
-            { "yellow", 10000 },
-            { "green", 100000 },
-            { "blue", 1000000 },
-            { "violet", 10000000 },
-            { "grey", 100000000 },
-            { "white", 1000000000 },
-        };
-
-        long resist = 0;
-        resist += value[Console.ReadLine()!] * 10;
-        resist += value[Console.ReadLine()!];
-        resist *= multiply[Console.ReadLine()!];
-
-        Console.Write(resist);
+        Console.Write(count);
     }
 }
