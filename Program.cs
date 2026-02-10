6f472d184434a476655a916751ecbeb61ffc83e1
mafuyusame <mafuyu@tutamail.com>
2026-02-11 07:20:19 +0900

    [string] boj 1159

--- Program.cs
+++ Program.cs
@@ -4,25 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        const int Height = 8;
-        const int Width = 8;
+        int n = int.Parse(Console.ReadLine()!);
 
-        int count = 0;
-        for (int row = 0; row < Height; ++row)
+        int[] count = new int['z' - 'a' + 1];
+
+        for (int i = 0; i < n; ++i)
         {
-            string line = Console.ReadLine()!;
-            for (int col = 0; col < Width; ++col)
-            {
-                if (line[col] == '.')
-                    continue;
-                
-                if (row % 2 != col % 2)
-                    continue;
-                
-                ++count;
-            }
+            string name = Console.ReadLine()!;
+
+            char firstChar = name[0];
+
+            int firstCharHash = LowerCaseToIndex(firstChar);
+
+            ++count[firstCharHash];
         }
 
-        Console.Write(count);
+        StringBuilder output = new();
+        for (int i = 0; i < count.Length; ++i)
+        {
+            if (count[i] < 5)
+                continue;
+
+            output.Append(IndexToLowerCase(i));
+        }
+
+        if (output.Length < 1)
+        {
+            output.Append("PREDAJA");
+        }
+
+        Console.Write(output);
+    }
+
+    static int LowerCaseToIndex(char c)
+    {
+        return c - 'a';
+    }
+    static char IndexToLowerCase(int index)
+    {
+        return (char)('a' + index);
     }
 }
