bcafa6e41487a5126137c2469d139ed50ef11e70
mafuyusame <mafuyu@tutamail.com>
2026-02-22 21:28:29 +0900

    [dp] boj 15841

--- Program.cs
+++ Program.cs
@@ -1,28 +1,32 @@
-﻿using System.Text;
+﻿using System.Numerics;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        const int MaxPassedMonths = 45;
+        const int Max = 490;
 
-        long[] fibonacci = new long[MaxPassedMonths + 1];
+        BigInteger[] fibonacci = new BigInteger[Max + 1];
         {
-            fibonacci[0] = 1;
+            fibonacci[0] = 0;
             fibonacci[1] = 1;
-            for (int i = 2; i <= MaxPassedMonths; ++i)
+            fibonacci[2] = 1;
+            for (int i = 3; i <= Max; ++i)
             {
                 fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
             }
         }
 
-        int t = int.Parse(Console.ReadLine()!);
-
         StringBuilder output = new();
         {
-            for (int i = 0; i < t; ++i)
+            while (true)
             {
-                output.AppendLine($"{fibonacci[int.Parse(Console.ReadLine()!)]}");
+                int x = int.Parse(Console.ReadLine()!);
+                if (x == -1)
+                    break;
+
+                output.AppendLine($"Hour {x}: {fibonacci[x]} cow(s) affected");
             }
         }
 
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c78a1155ef4a05039d09b739cdb0770afdd6d45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a59ccdd8d7264bf074818e9c1df3f38ce52d69")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
