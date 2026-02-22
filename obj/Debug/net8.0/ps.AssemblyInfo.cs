6c78a1155ef4a05039d09b739cdb0770afdd6d45
mafuyusame <mafuyu@tutamail.com>
2026-02-22 21:17:21 +0900

    [dp] boj 2748

--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01ae7b167cb048789e81bbce853aa668e32132b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfaffe561e5504a86f38b2b8e2760bbe988901f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
