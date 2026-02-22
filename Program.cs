abffca73feb8c104f46697ade9a8f747aac7aede
mafuyusame <mafuyu@tutamail.com>
2026-02-22 18:20:14 +0900

    [string] boj 34665

--- Program.cs
+++ Program.cs
@@ -1,21 +1,9 @@
-﻿using System.Numerics;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
-        
-        BigInteger a = 1;
-        BigInteger b = 1;
-
-        for (int i = 3; i <= n; ++i)
-        {
-            BigInteger temp = a + b;
-            a = b;
-            b = temp;
-        }
-
-        Console.Write(b);
+        string a = Console.ReadLine()!;
+        string b = Console.ReadLine()!;
+        Console.Write((a == b) ? "0" : "1550");
     }
 }
