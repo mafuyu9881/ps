5d37387e666db8f6e78e27f0d8a80aa536ee33a5
mafuyusame <mafuyu@tutamail.com>
2026-02-25 09:04:33 +0900

    [math] boj 1193

--- Program.cs
+++ Program.cs
@@ -2,16 +2,30 @@
 {
     static void Main(string[] args)
     {
+        // 1-based
+
         int x = int.Parse(Console.ReadLine()!);
 
-        int count = 0;
+        int diagonalRow = 1;
+        int lastIndexUntilPrevDiagonalRow = 0;
 
-        while (x > 0)
+        while (diagonalRow + lastIndexUntilPrevDiagonalRow < x)
         {
-            count += x & 1;
-            x >>= 1;
+            lastIndexUntilPrevDiagonalRow += diagonalRow;
+            diagonalRow += 1;
         }
 
-        Console.Write(count);
+        int diagonalCol = x - lastIndexUntilPrevDiagonalRow;
+
+        int numerator = diagonalRow - diagonalCol + 1;
+        int denominator = diagonalCol;
+        if (diagonalRow % 2 == 0)
+        {
+            int temp = numerator;
+            numerator = denominator;
+            denominator = temp;
+        }
+
+        Console.Write($"{numerator}/{denominator}");
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a7f29b957a847c92ef753fd4f9f7e984f244902")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ae9d193ef00e2b92f4536932e2cd58d6c52827")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
