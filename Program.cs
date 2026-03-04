7add20beb59446914e229831669ed40eaa945ee7
mafuyusame <mafuyu@tutamail.com>
2026-03-05 08:46:53 +0900

    [implementation] boj 34217

--- Program.cs
+++ Program.cs
@@ -1,79 +1,34 @@
-﻿using System.Net.Http.Headers;
-
-public class Program
+﻿public class Program
 {
     public static void Main(string[] args)
     {
         int[] tokens = null!;
 
         tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int r = tokens[0];
-        int c = tokens[1];
+        int a = tokens[0];
+        int b = tokens[1];
 
         tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int n = tokens[0];
+        int c = tokens[0];
+        int d = tokens[1];
 
-        int arrayLength = n + 1;
+        int hanyangRequiredTime = a + c;
+        int yongdapRequiredTime = b + d;
 
-        const int Invalid = -1;
-        int[] minRow = new int[arrayLength];
-        int[] maxRow = new int[arrayLength];
-        int[] minCol = new int[arrayLength];
-        int[] maxCol = new int[arrayLength];
-
-        for (int i = 0; i < arrayLength; ++i)
+        string output;
+        if (hanyangRequiredTime < yongdapRequiredTime)
         {
-            minRow[i] = Invalid;
-            maxRow[i] = Invalid;
-            minCol[i] = Invalid;
-            maxCol[i] = Invalid;
+            output = "Hanyang Univ.";
         }
-
-        for (int i = 0; i < n; ++i)
+        else if (hanyangRequiredTime > yongdapRequiredTime)
         {
-            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-            int a = tokens[0];
-            int v = tokens[1];
-            int h = tokens[2];
-
-            int oldMinRow = minRow[a];
-            if (oldMinRow == Invalid || v < oldMinRow)
-                minRow[a] = v;
-
-            int oldMaxRow = maxRow[a];
-            if (oldMaxRow == Invalid || v > oldMaxRow)
-                maxRow[a] = v;
-
-            int oldMinCol = minCol[a];
-            if (oldMinCol == Invalid || h < oldMinCol)
-                minCol[a] = h;
-
-            int oldMaxCol = maxCol[a];
-            if (oldMaxCol == Invalid || h > oldMaxCol)
-                maxCol[a] = h;
+            output = "Yongdap";
         }
-
-        int bestId = 0;
-        long bestArea = Invalid;
+        else
+        {
+            output = "Either";
+        }
         
-        for (int a = 1; a <= n; ++a)
-        {
-            if (minRow[a] == Invalid)
-                continue;
-
-            long height = (long)maxRow[a] - minRow[a] + 1;
-            long width = (long)maxCol[a] - minCol[a] + 1;
-            long area = height * width;
-
-            if ((bestArea == Invalid) ||
-                (area > bestArea) ||
-                (area == bestArea && a < bestId))
-            {
-                bestArea = area;
-                bestId = a;
-            }
-        }
-
-        Console.Write($"{bestId} {bestArea}");
+        Console.Write(output);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc41460b72e71f7aa5037764ee9f79e67766279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6a99941d001ce4792a1a782523b4f006bc7372")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
