0d6a99941d001ce4792a1a782523b4f006bc7372
mafuyusame <mafuyu@tutamail.com>
2026-03-04 09:36:13 +0900

    [implementation] boj 32344

--- Program.cs
+++ Program.cs
@@ -1,27 +1,79 @@
-﻿class Program
+﻿using System.Net.Http.Headers;
+
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        long[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
-        long a = tokens[0];
-        long b = tokens[1];
-        long c = tokens[2];
+        int[] tokens = null!;
 
-        long rhs = (a * a - b * b) * (a * a - c * c);
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int r = tokens[0];
+        int c = tokens[1];
 
-        long output = -1;
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int n = tokens[0];
+
+        int arrayLength = n + 1;
+
+        const int Invalid = -1;
+        int[] minRow = new int[arrayLength];
+        int[] maxRow = new int[arrayLength];
+        int[] minCol = new int[arrayLength];
+        int[] maxCol = new int[arrayLength];
+
+        for (int i = 0; i < arrayLength; ++i)
         {
-            for (long x = 1; x < a; ++x)
+            minRow[i] = Invalid;
+            maxRow[i] = Invalid;
+            minCol[i] = Invalid;
+            maxCol[i] = Invalid;
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int a = tokens[0];
+            int v = tokens[1];
+            int h = tokens[2];
+
+            int oldMinRow = minRow[a];
+            if (oldMinRow == Invalid || v < oldMinRow)
+                minRow[a] = v;
+
+            int oldMaxRow = maxRow[a];
+            if (oldMaxRow == Invalid || v > oldMaxRow)
+                maxRow[a] = v;
+
+            int oldMinCol = minCol[a];
+            if (oldMinCol == Invalid || h < oldMinCol)
+                minCol[a] = h;
+
+            int oldMaxCol = maxCol[a];
+            if (oldMaxCol == Invalid || h > oldMaxCol)
+                maxCol[a] = h;
+        }
+
+        int bestId = 0;
+        long bestArea = Invalid;
+        
+        for (int a = 1; a <= n; ++a)
+        {
+            if (minRow[a] == Invalid)
+                continue;
+
+            long height = (long)maxRow[a] - minRow[a] + 1;
+            long width = (long)maxCol[a] - minCol[a] + 1;
+            long area = height * width;
+
+            if ((bestArea == Invalid) ||
+                (area > bestArea) ||
+                (area == bestArea && a < bestId))
             {
-                long lhsSqrt = a * x + b * c;
-                if (lhsSqrt * lhsSqrt == rhs)
-                {
-                    output = x;
-                    break;
-                }
+                bestArea = area;
+                bestId = a;
             }
         }
 
-        Console.Write(output);
+        Console.Write($"{bestId} {bestArea}");
     }
 }
