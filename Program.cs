2baad9d3591b9180f0b7c83d0b9791d1855a828c
mafuyusame <mafuyu@tutamail.com>
2026-03-12 08:19:01 +0900

    [implementation] boj 2167

--- Program.cs
+++ Program.cs
@@ -1,18 +1,49 @@
-﻿public class Program
+﻿using System.Text;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        int[] tokens = null!;
 
-        long prev = 1;
-        long curr = 1;
-        for (int i = 3; i <= n; ++i)
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int height = tokens[0];
+        int width = tokens[1];
+
+        int[,] map = new int[height, width];
+        for (int row = 0; row < height; ++row)
         {
-            long next = (prev + curr) % 1000000007;
-            prev = curr;
-            curr = next;
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            for (int col = 0; col < width; ++col)
+            {
+                map[row, col] = tokens[col];
+            }
+        }
+        
+        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int k = tokens[0];
+
+        StringBuilder output = new();
+        for (int l = 0; l < k; ++l)
+        {
+            tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int i = tokens[0] - 1;
+            int j = tokens[1] - 1;
+            int x = tokens[2] - 1;
+            int y = tokens[3] - 1;
+
+            int sum = 0;
+            for (int row = i; row <= x; ++row)
+            {
+                for (int col = j; col <= y; ++col)
+                {
+                    sum += map[row, col];
+                }
+            }
+            
+            output.AppendLine($"{sum}");
         }
 
-        Console.Write($"{curr} {n - 2}");
+        Console.Write(output);
     }
 }
