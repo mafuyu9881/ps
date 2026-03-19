916e035113af20b2128c7193a08a38a18d431fa0
mafuyusame <mafuyu@tutamail.com>
2026-03-20 07:56:46 +0900

    [string] boj 26560

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
-﻿public class Program
+﻿using System.Text;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        int m = int.Parse(Console.ReadLine()!);
+        int n = int.Parse(Console.ReadLine()!);
 
-        int k = int.Parse(Console.ReadLine()!);
+        StringBuilder sb = new();
+        for (int i = 0; i < n; ++i)
+        {
+            string s = Console.ReadLine()!;
 
-        int c = int.Parse(Console.ReadLine()!);
+            if (s[s.Length - 1] != '.')
+            {
+                sb.AppendLine($"{s}.");
+            }
+            else
+            {
+                sb.AppendLine(s);
+            }
+        }
 
-        Console.Write(m * c);
+        Console.Write(sb);
     }
 }
