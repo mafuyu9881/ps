93f459988e4fd6752e2878fbf4b255c72404450f
mafuyusame <mafuyu@tutamail.com>
2026-03-27 22:10:57 +0900

    [math] boj 27389

--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
-﻿using System.Text;
-
-public class Program
+﻿public class Program
 {
     public static void Main(string[] args)
     {
-        StringBuilder output = new();
-        {
-            int n = int.Parse(Console.ReadLine()!);
+        int n = int.Parse(Console.ReadLine()!);
 
-            for (int i = 0; i < (n / 5); ++i)
-            {
-                output.Append("V");
-            }
-
-            for (int i = 0; i < (n % 5); ++i)
-            {
-                output.Append("I");
-            }
-        }
-        Console.Write(output);
+        Console.Write(n / 4.0);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9a78c21b82f8b46806c99a294127e4f9bdf32f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0963516ace4ffdb55feb633f4f24b19e573f6622")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
