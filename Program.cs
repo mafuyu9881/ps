50782f54f5afec70d158c88fbc500a3cbca8faec
mafuyusame <mafuyu@tutamail.com>
2026-03-31 08:39:49 +0900

    [implementation] boj 34945

--- Program.cs
+++ Program.cs
@@ -2,26 +2,8 @@
 {
     public static void Main(string[] args)
     {
-        string s = Console.ReadLine()!;
-        int n = int.Parse(s);
+        int x = int.Parse(Console.ReadLine()!);
 
-        string output;
-        if (s.Contains("7") == false && n % 7 != 0)
-        {
-            output = "0";
-        }
-        else if (s.Contains("7") == false && n % 7 == 0)
-        {
-            output = "1";
-        }
-        else if (n % 7 != 0)
-        {
-            output = "2";
-        }
-        else
-        {
-            output = "3";
-        }
-        Console.Write(output);
+        Console.Write(x < 6 ? "Oh My God!" : "Success!");
     }
 }
