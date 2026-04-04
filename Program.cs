ea830ddd631664d88210ada0db96e35322b5a59c
mafuyusame <mafuyu@tutamail.com>
2026-04-04 18:44:52 +0900

    [math] boj 34362

--- Program.cs
+++ Program.cs
@@ -2,10 +2,6 @@
 {
     public static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
-
-        int x = int.Parse(Console.ReadLine()!);
-
-        Console.Write((n - 1) * x);
+        Console.Write(double.Parse(Console.ReadLine()!) - 0.3);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762ca063fd8cb2556661c239f6672da42fde07fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3cf7cff36b9b83dca2016cb0ee8513174d82f29")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
