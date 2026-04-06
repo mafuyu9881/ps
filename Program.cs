4c728e36b6c1cb788cf0c3863d34eee21db0aa42
mafuyusame <mafuyu@tutamail.com>
2026-04-07 07:45:27 +0900

    [implementation] boj 32154

--- Program.cs
+++ Program.cs
@@ -1,16 +1,62 @@
-﻿public class Program
+﻿using System.Text;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        int[] tokens = null!;
+        int n = int.Parse(Console.ReadLine()!);
 
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int a = tokens[0];
-
-        tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        int w = tokens[0];
-        int v = tokens[1];
-
-        Console.Write((w / v) >= a ? 1 : 0);
+        StringBuilder output = new();
+        if (n == 1)
+        {
+            output.AppendLine("11");
+            output.AppendLine("A B C D E F G H J L M");
+        }
+        else if (n == 2)
+        {
+            output.AppendLine("9");
+            output.AppendLine("A C E F G H I L M");
+        }
+        else if (n == 3)
+        {
+            output.AppendLine("9");
+            output.AppendLine("A C E F G H I L M");
+        }
+        else if (n == 4)
+        {
+            output.AppendLine("9");
+            output.AppendLine("A B C E F G H L M");
+        }
+        else if (n == 5)
+        {
+            output.AppendLine("8");
+            output.AppendLine("A C E F G H L M");
+        }
+        else if (n == 6)
+        {
+            output.AppendLine("8");
+            output.AppendLine("A C E F G H L M");
+        }
+        else if (n == 7)
+        {
+            output.AppendLine("8");
+            output.AppendLine("A C E F G H L M");
+        }
+        else if (n == 8)
+        {
+            output.AppendLine("8");
+            output.AppendLine("A C E F G H L M");
+        }
+        else if (n == 9)
+        {
+            output.AppendLine("8");
+            output.AppendLine("A C E F G H L M");
+        }
+        else if (n == 10)
+        {
+            output.AppendLine("8");
+            output.AppendLine("A B C F G H L M");
+        }
+        Console.Write(output);
     }
 }
