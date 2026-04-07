c4bb4a8c963014e1dec9e3469c846b12fedf4fa3
mafuyusame <mafuyu@tutamail.com>
2026-04-08 07:27:46 +0900

    [math] boj 32498

--- Program.cs
+++ Program.cs
@@ -1,62 +1,19 @@
-﻿using System.Text;
-
-public class Program
+﻿public class Program
 {
     public static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine()!);
+        
+        int refused = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            int d = int.Parse(Console.ReadLine()!);
+            if (d % 2 != 0)
+            {
+                ++refused;
+            }
+        }
 
-        StringBuilder output = new();
-        if (n == 1)
-        {
-            output.AppendLine("11");
-            output.AppendLine("A B C D E F G H J L M");
-        }
-        else if (n == 2)
-        {
-            output.AppendLine("9");
-            output.AppendLine("A C E F G H I L M");
-        }
-        else if (n == 3)
-        {
-            output.AppendLine("9");
-            output.AppendLine("A C E F G H I L M");
-        }
-        else if (n == 4)
-        {
-            output.AppendLine("9");
-            output.AppendLine("A B C E F G H L M");
-        }
-        else if (n == 5)
-        {
-            output.AppendLine("8");
-            output.AppendLine("A C E F G H L M");
-        }
-        else if (n == 6)
-        {
-            output.AppendLine("8");
-            output.AppendLine("A C E F G H L M");
-        }
-        else if (n == 7)
-        {
-            output.AppendLine("8");
-            output.AppendLine("A C E F G H L M");
-        }
-        else if (n == 8)
-        {
-            output.AppendLine("8");
-            output.AppendLine("A C E F G H L M");
-        }
-        else if (n == 9)
-        {
-            output.AppendLine("8");
-            output.AppendLine("A C E F G H L M");
-        }
-        else if (n == 10)
-        {
-            output.AppendLine("8");
-            output.AppendLine("A B C F G H L M");
-        }
-        Console.Write(output);
+        Console.Write(refused);
     }
 }
