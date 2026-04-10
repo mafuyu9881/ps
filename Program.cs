c6190a756583733d40aa43132d9b5f4db8300860
mafuyusame <mafuyu@tutamail.com>
2026-04-11 07:29:30 +0900

    [math] boj 31429

--- Program.cs
+++ Program.cs
@@ -3,8 +3,53 @@
     public static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine()!);
-        int c = int.Parse(Console.ReadLine()!);
-        int p = int.Parse(Console.ReadLine()!);
-        Console.Write(n * p);
+
+        string output;
+        if (n == 1)
+        {
+            output = "12 1600";
+        }
+        else if (n == 2)
+        {
+            output = "11 894";
+        }
+        else if (n == 3)
+        {
+            output = "11 1327";
+        }
+        else if (n == 4)
+        {
+            output = "10 1311";
+        }
+        else if (n == 5)
+        {
+            output = "9 1004";
+        }
+        else if (n == 6)
+        {
+            output = "9 1178";
+        }
+        else if (n == 7)
+        {
+            output = "9 1357";
+        }
+        else if (n == 8)
+        {
+            output = "8 837";
+        }
+        else if (n == 9)
+        {
+            output = "7 1055";
+        }
+        else if (n == 10)
+        {
+            output = "6 556";
+        }
+        else //  if (n == 11)
+        {
+            output = "6 773";
+        }
+
+        Console.Write(output);
     }
 }
