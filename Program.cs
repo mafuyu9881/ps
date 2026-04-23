af86387659b6901aaef2123e6a599942a7fc56ec
mafuyusame <mafuyu@tutamail.com>
2026-04-24 07:10:51 +0900

    [math] boj 27890

--- Program.cs
+++ Program.cs
@@ -2,15 +2,24 @@
 {
     public static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()!);
+        int[] tokens = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        int x0 = tokens[0];
+        int n = tokens[1];
 
-        int[] preferences = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
-        
-        Console.Write
-        (
-            Math.Min(preferences[0], n) +
-            Math.Min(preferences[1], n) +
-            Math.Min(preferences[2], n)
-        );
+        int xn = x0;
+        for (int t = 0; t < n; ++t)
+        {
+            if (xn % 2 == 0)
+            {
+                xn /= 2;
+            }
+            else
+            {
+                xn *= 2;
+            }
+            xn ^= 6;
+        }
+
+        Console.Write(xn);
     }
 }
--- obj/Debug/net8.0/ps.AssemblyInfo.cs
+++ obj/Debug/net8.0/ps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bccd34fda580514dba992aade091d9e43c26b8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24e0443fdc8b03f1bacee4c2e7175b38b12c7848")]
 [assembly: System.Reflection.AssemblyProductAttribute("ps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
